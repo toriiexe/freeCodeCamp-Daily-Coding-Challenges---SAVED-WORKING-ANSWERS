77eb564c3d2af6a548d5236d14b1f7ff11d0a29b
Quoriee <snowii.torii@gmail.com>
2026-07-03 01:46:51 +0200

    message decoder python solution

---
+++ 2025/08-august/2025-08-22.py
@@ -0,0 +1,15 @@
+#Message Decoder
+def decode(message, shift):
+    alphaU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alphaL = 'abcdefghijklmnopqrstuvwxyz'
+    s = ''
+
+    for c in message:
+        if c.isalpha() and c.isupper():
+            s += alphaU[(alphaU.index(c) - shift) % 26]
+        elif c.isalpha() and c.islower():
+            s += alphaL[(alphaL.index(c) - shift) % 26]
+        else:
+            s += c
+
+    return s
