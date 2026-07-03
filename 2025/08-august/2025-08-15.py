355e3ac1618925fe3b173d37518fd0a5835e3f5d
Quoriee <snowii.torii@gmail.com>
2026-07-03 02:08:18 +0200

    jbelmud text python solution

---
+++ 2025/08-august/2025-08-15.py
@@ -0,0 +1,11 @@
+#jbelmud text
+def jbelmu(text):
+    words = text.split()
+    res = []
+    for word in words:
+        if len(word) <= 2:
+            res.append(word)
+        else:
+            mid = ''.join(sorted(word[1:-1]))
+            res.append(word[0] + mid + word[-1])
+    return ' '.join(res)
