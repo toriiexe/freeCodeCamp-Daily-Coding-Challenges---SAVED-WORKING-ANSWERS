24e9ef996a2bba5eba850de0486839499dd19a3c
Quoriee <snowii.torii@gmail.com>
2026-07-03 07:11:49 +0200

    wider aspect ratio python solution

--- solutions/2026/05-May/2026-05-29/py_2026-05-29.py
+++ solutions/2026/05-May/2026-05-29/py_2026-05-29.py
@@ -1,1 +1,14 @@
 # Python solution for 2026-05-29
+# Wider Aspect Ratio
+import math
+
+def get_wider_aspect_ratio(a, b):
+    wA, hA = map(int, a.split('x'))
+    wB, hB = map(int, b.split('x'))
+
+    if wA * hB >= wB * hA:
+        g = math.gcd(wA, hA)
+        return f"{wA // g}:{hA // g}"
+    else:
+        g = math.gcd(wB, hB)
+        return f"{wB // g}:{hB // g}"
