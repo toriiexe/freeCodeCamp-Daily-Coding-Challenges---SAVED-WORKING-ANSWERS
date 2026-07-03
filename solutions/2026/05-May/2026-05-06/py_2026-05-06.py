d7f871c63dcae26d7a1ac77943cee79b06e0297f
Quoriee <snowii.torii@gmail.com>
2026-07-03 02:35:52 +0200

    allergen friendly meals python solution

--- solutions/2026/05-May/2026-05-06/py_2026-05-06.py
+++ solutions/2026/05-May/2026-05-06/py_2026-05-06.py
@@ -1,1 +1,17 @@
 # Python solution for 2026-05-06
+#Allergen Friendly Meals
+def get_allergen_friendly_meals(meals, allergens):
+    safeMeals = []
+
+    for meal, mealAllergens in meals:
+        notSafe = False
+
+        for mealAllergen in mealAllergens:
+            if mealAllergen in allergens:
+                notSafe = True
+                break
+
+        if not notSafe:
+            safeMeals.append(meal)
+
+    return safeMeals
