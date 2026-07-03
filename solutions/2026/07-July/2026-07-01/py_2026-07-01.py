4f13f1496b101cbbbe14998578497e2e6235f7ee
Quoriee <snowii.torii@gmail.com>
2026-07-03 02:40:30 +0200

    lucky number python solution

--- solutions/2026/07-July/2026-07-01/py_2026-07-01.py
+++ solutions/2026/07-July/2026-07-01/py_2026-07-01.py
@@ -1,1 +1,16 @@
 # Python solution for 2026-07-01
+#Lucky Number
+def get_lucky_number(name):
+    firstName, lastName = name.lower().split()
+
+    fnV = sum(1 for ch in firstName if ch in 'aeiou')
+    fnC = sum(1 for ch in firstName if ch.isalpha() and ch not in 'aeiou')
+
+    lnV = sum(1 for ch in lastName if ch in 'aeiou')
+    lnC = sum(1 for ch in lastName if ch.isalpha() and ch not in 'aeiou')
+
+    smallerV = min(fnV, lnV) * min(fnC, lnC) * min(len(firstName), len(lastName))
+    largerV = max(fnV, lnV) * max(fnC, lnC) * max(len(firstName), len(lastName))
+
+    result = largerV - smallerV
+    return result if result != 0 else 13
