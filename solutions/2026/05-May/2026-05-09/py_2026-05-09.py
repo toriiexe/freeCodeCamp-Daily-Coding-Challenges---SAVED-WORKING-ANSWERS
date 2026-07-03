2f102ee6959cf2fba70e1d18894a53fbb551cf9c
Quoriee <snowii.torii@gmail.com>
2026-07-03 02:33:16 +0200

    transposed matrix python solution

--- solutions/2026/05-May/2026-05-09/py_2026-05-09.py
+++ solutions/2026/05-May/2026-05-09/py_2026-05-09.py
@@ -1,1 +1,10 @@
 # Python solution for 2026-05-09
+#Transposed Matrix
+def transpose(matrix):
+    m = [[None for col in range(len(matrix))] for row in range(len(matrix[0]))]
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            m[j][i] = matrix[i][j]
+
+    return m
