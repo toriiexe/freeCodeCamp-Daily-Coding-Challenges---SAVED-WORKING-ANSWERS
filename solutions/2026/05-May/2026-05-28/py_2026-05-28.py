24e8fed57b8280c41e9d45fe7e786c4f6abadc55
Quoriee <snowii.torii@gmail.com>
2026-07-03 03:16:52 +0200

    fizzbuzz count python solution

--- solutions/2026/05-May/2026-05-28/py_2026-05-28.py
+++ solutions/2026/05-May/2026-05-28/py_2026-05-28.py
@@ -1,1 +1,15 @@
 # Python solution for 2026-05-28
+#FizzBuzz Count
+def fizz_buzz_count(start, end):
+    d = {'fizz': 0, 'buzz': 0}
+
+    for n in range(start, end+1):
+        if n % 3 == 0 and n % 5 == 0:
+            d['fizz'] += 1
+            d['buzz'] += 1
+        elif n % 3 == 0:
+            d['fizz'] += 1
+        elif n % 5 == 0:
+            d['buzz'] += 1
+            
+    return d
