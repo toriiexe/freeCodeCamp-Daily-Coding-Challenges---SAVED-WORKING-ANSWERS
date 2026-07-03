5f09884651f608a9dcd81822dafe28bb3335f15c
Quoriee <snowii.torii@gmail.com>
2026-07-03 07:34:40 +0200

    sum of differences python solution

--- solutions/2026/05-May/2026-05-26/py_2026-05-26.py
+++ solutions/2026/05-May/2026-05-26/py_2026-05-26.py
@@ -1,1 +1,10 @@
 # Python solution for 2026-05-26
+# Sum of Differences
+def sum_of_differences(arr):
+    s = 0
+    length = len(arr)
+
+    for i in range(length-1):
+        s += arr[i+1] - arr[i]
+
+    return s
