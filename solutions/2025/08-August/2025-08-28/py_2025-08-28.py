7ac5c53d9bb6c94820bf9bf87fb958c0325640b2
Quoriee <snowii.torii@gmail.com>
2026-07-03 02:34:55 +0200

    second best python solution

--- solutions/2025/08-August/2025-08-28/py_2025-08-28.py
+++ solutions/2025/08-August/2025-08-28/py_2025-08-28.py
@@ -1,1 +1,13 @@
 # Python solution for 2025-08-28
+#Second Best
+def get_laptop_cost(laptops, budget):
+    laptopSorted = sorted(set(laptops), reverse=True)
+
+    if len(laptopSorted) > 1 and budget >= laptopSorted[1]:
+        return laptopSorted[1]
+
+    for laptop in laptopSorted:
+        if budget >= laptop:
+            return laptop
+
+    return 0
