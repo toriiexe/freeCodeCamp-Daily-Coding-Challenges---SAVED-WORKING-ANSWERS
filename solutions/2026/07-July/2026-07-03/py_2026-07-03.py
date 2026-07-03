9723d000e8ca3509405d7e367af732c1134f32b2
Quoriee <snowii.torii@gmail.com>
2026-07-03 23:03:34 +0200

    database migration python solution

--- solutions/2026/07-July/2026-07-03/py_2026-07-03.py
+++ solutions/2026/07-July/2026-07-03/py_2026-07-03.py
@@ -1,1 +1,8 @@
 # Python solution for 2026-07-03
+# Database Migration
+def migrate_record(schema, record):
+    res = record.copy()
+    for key, value in schema.items():
+        if key not in res:
+            res[key] = value
+    return res
