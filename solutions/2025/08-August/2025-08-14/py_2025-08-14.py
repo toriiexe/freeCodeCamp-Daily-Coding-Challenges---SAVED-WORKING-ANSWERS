a30686c1da480d58cfd4ba09502f892ad8e5520c
Quoriee <snowii.torii@gmail.com>
2026-07-03 03:00:11 +0200

    s p a c e j a m python solution

--- solutions/2025/08-August/2025-08-14/py_2025-08-14.py
+++ solutions/2025/08-August/2025-08-14/py_2025-08-14.py
@@ -1,1 +1,10 @@
 # Python solution for 2025-08-14
+#S P A C E J A M
+def space_jam(s):
+    s = s.replace(' ', '')
+    res = ''
+    
+    for c in s:
+        res += c.upper() + '  '
+    
+    return res.rstrip()
