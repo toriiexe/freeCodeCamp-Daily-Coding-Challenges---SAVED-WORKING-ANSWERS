7a9bc6e6e42236f128d734375ba09955c9d702f1
Quoriee <snowii.torii@gmail.com>
2026-07-03 02:36:58 +0200

    mile pace python solution

--- solutions/2025/08-August/2025-08-21/py_2025-08-21.py
+++ solutions/2025/08-August/2025-08-21/py_2025-08-21.py
@@ -1,1 +1,12 @@
 # Python solution for 2025-08-21
+#Mile Pace
+def mile_pace(miles, duration):
+    m, s = duration.split(':')
+    t = int(m) * 60 + int(s)
+
+    av = round(t / miles)
+
+    mm = av // 60
+    ss = av % 60
+
+    return f"{mm:02}:{ss:02}"
