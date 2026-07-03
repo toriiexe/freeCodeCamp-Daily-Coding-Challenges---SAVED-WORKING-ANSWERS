f640d207db976e12744eaae084b101fba68f8525
Quoriee <snowii.torii@gmail.com>
2026-07-03 02:55:08 +0200

    vowel balance python solution

--- solutions/2025/08-August/2025-08-11/py_2025-08-11.py
+++ solutions/2025/08-August/2025-08-11/py_2025-08-11.py
@@ -1,1 +1,13 @@
 # Python solution for 2025-08-11
+#Vowel Balance
+def is_balanced(s):
+    fHalf, sHalf = 0, 0
+    vowels = 'aeiouAEIOU'
+
+    for i in range(len(s)//2):
+        if s[i] in vowels:
+            fHalf += 1
+        if s[-(i+1)] in vowels:
+            sHalf += 1
+
+    return fHalf == sHalf
