147a382573b6749e7958a0455884c392c4547ebe
Quoriee <snowii.torii@gmail.com>
2026-07-03 02:59:34 +0200

    fibonacci sequence python solution

--- solutions/2025/08-August/2025-08-13/py_2025-08-13.py
+++ solutions/2025/08-August/2025-08-13/py_2025-08-13.py
@@ -1,1 +1,14 @@
 # Python solution for 2025-08-13
+#Fibonacci Sequence
+def fibonacci_sequence(start_sequence, length):
+    if length == 0:
+        return []
+
+    x, y = start_sequence[0], start_sequence[1]
+    arr = start_sequence[:length]
+
+    for i in range(length - 2):
+        x, y = y, y + x
+        arr.append(y)
+
+    return arr
