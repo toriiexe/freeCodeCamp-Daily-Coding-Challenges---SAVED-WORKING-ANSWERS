2d4e9a34fb8c006fc9785fff4393e2e879ce685c
Quoriee <snowii.torii@gmail.com>
2026-07-03 02:58:22 +0200

    base check python solution

--- solutions/2025/08-August/2025-08-12/py_2025-08-12.py
+++ solutions/2025/08-August/2025-08-12/py_2025-08-12.py
@@ -1,1 +1,8 @@
 # Python solution for 2025-08-12
+#Base Check
+def is_valid_number(n, base):
+    valid = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'[:base]
+    for c in n.upper():
+        if not c in valid:
+            return False
+    return True
