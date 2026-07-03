88265016f2dd98a391188413c86879209c9cf1ec
Quoriee <snowii.torii@gmail.com>
2026-07-03 07:30:27 +0200

    secret number js solution

--- solutions/2026/05-May/2026-05-25/js_2026-05-25.js
+++ solutions/2026/05-May/2026-05-25/js_2026-05-25.js
@@ -1,1 +1,13 @@
 // JavaScript solution for 2026-05-25
+// Secret Number
+function guessNumber(secret, guess) {
+    if (secret > guess){
+        return 'higher';
+    }
+    else if (secret < guess){
+        return 'lower';
+    }
+    else{
+        return 'you got it!';
+    }
+}
