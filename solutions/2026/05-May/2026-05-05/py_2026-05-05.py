91499ff2e55115103662eff2b38107b63e8d3cd7
Quoriee <snowii.torii@gmail.com>
2026-07-03 02:52:19 +0200

    narcissistic number python solution

--- solutions/2026/05-May/2026-05-05/py_2026-05-05.py
+++ solutions/2026/05-May/2026-05-05/py_2026-05-05.py
@@ -1,1 +1,10 @@
 # Python solution for 2026-05-05
+#Narcissistic Number
+def is_narcissistic(n):
+    nStr = str(n)
+    length = len(nStr)
+    s = 0
+    for i in range(length):
+        num = int(nStr[i])
+        s += num ** length
+    return s == n
