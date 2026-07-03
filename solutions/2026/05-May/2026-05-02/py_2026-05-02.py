70ec90b9ea92a25eb06b1659da21ac233c231c21
Quoriee <snowii.torii@gmail.com>
2026-07-03 02:32:38 +0200

    deepest brackets python solution

--- solutions/2026/05-May/2026-05-02/py_2026-05-02.py
+++ solutions/2026/05-May/2026-05-02/py_2026-05-02.py
@@ -1,1 +1,35 @@
 # Python solution for 2026-05-02
+#Deepest Brackets
+def get_deepest_brackets(s):
+
+    maxLevel = 0
+    level = 0
+
+    for c in s:
+        if c in '({[':
+            level += 1
+            if level > maxLevel:
+                maxLevel = level
+        elif c in ')]}':
+            level -= 1
+
+    level = 0
+    temp = ''
+    res = ''
+
+    for c in s:
+        if c in '({[':
+            level += 1
+            temp = ''
+
+        elif c in ')]}':
+            if level == maxLevel:
+                res = temp
+                return res
+            level -= 1
+
+        else:
+            if level == maxLevel:
+                temp += c
+
+    return res
