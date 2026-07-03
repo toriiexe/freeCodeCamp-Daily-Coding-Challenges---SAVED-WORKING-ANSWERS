9266896630e644421d5e8ef41b4c438a30039842
Quoriee <snowii.torii@gmail.com>
2026-07-03 02:47:34 +0200

    max profit python solution

--- solutions/2026/07-July/2026-07-02/py_2026-07-02.py
+++ solutions/2026/07-July/2026-07-02/py_2026-07-02.py
@@ -1,1 +1,16 @@
 # Python solution for 2026-07-02
+#Max Profit
+def get_max_profit(prices, budget):
+    profit = 0
+    
+    for i, buy in enumerate(prices):
+        shares = int(budget // buy)
+        
+        for j in range(i + 1, len(prices)):
+            sell = prices[j]
+            possible = shares * (sell - buy)
+            
+            if possible > profit:
+                profit = possible
+
+    return f"{profit:.2f}"
