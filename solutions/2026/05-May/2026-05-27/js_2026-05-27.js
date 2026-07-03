c579727068b20ad4dd0b3e774e816ecd2bfffa1e
Quoriee <snowii.torii@gmail.com>
2026-07-04 00:07:32 +0200

    pizza party js solution

--- solutions/2026/05-May/2026-05-27/js_2026-05-27.js
+++ solutions/2026/05-May/2026-05-27/js_2026-05-27.js
@@ -1,1 +1,12 @@
 // JavaScript solution for 2026-05-27
+// Pizza Party
+function getPizzasToOrder(hoursWorked) {
+    let slices = 0;
+
+    for (const hours of hoursWorked) {
+        const willGet = Math.ceil(hours / 3);
+        slices += Math.max(willGet, 2);
+    }
+
+    return Math.ceil(slices / 8);
+}
