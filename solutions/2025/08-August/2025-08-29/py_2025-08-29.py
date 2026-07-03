751a7d132ecff72b8f8545d04223f1042df8fae8
Quoriee <snowii.torii@gmail.com>
2026-07-03 03:07:08 +0200

    candlelight python solution

--- solutions/2025/08-August/2025-08-29/py_2025-08-29.py
+++ solutions/2025/08-August/2025-08-29/py_2025-08-29.py
@@ -1,1 +1,15 @@
 # Python solution for 2025-08-29
+#Candlelight
+def burn_candles(candles, leftovers_needed):
+    burned = 0
+    leftovers = 0
+    current = candles
+
+    while current > 0:
+        burned += current
+        leftovers += current
+
+        current = leftovers // leftovers_needed
+        leftovers = leftovers % leftovers_needed
+
+    return burned
