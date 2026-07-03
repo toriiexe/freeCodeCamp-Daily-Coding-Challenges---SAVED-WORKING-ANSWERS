f22e0d42f85e92adca9d933486b44426618f3ada
Quoriee <snowii.torii@gmail.com>
2026-07-04 00:04:35 +0200

    pizza party python solution

--- solutions/2026/05-May/2026-05-27/py_2026-05-27.py
+++ solutions/2026/05-May/2026-05-27/py_2026-05-27.py
@@ -1,1 +1,12 @@
 # Python solution for 2026-05-27
+# Pizza party
+from math import ceil
+
+def get_pizzas_to_order(hours_worked):
+    slices = 0
+
+    for hours in hours_worked:
+        willGet = ceil(hours / 3)
+        slices += max(willGet, 2)
+
+    return ceil(slices / 8)
