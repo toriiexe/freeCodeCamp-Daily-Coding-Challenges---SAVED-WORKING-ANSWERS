e00186d9d1b403cb7dcf0ce914a179d615eae30f
Quoriee <snowii.torii@gmail.com>
2026-07-03 03:22:03 +0200

    fizzbuzz count javascript solution

--- solutions/2026/05-May/2026-05-28/js_2026-05-28.js
+++ solutions/2026/05-May/2026-05-28/js_2026-05-28.js
@@ -1,1 +1,18 @@
 // JavaScript solution for 2026-05-28
+// FizzBuzz Count
+function fizzBuzzCount(start, end) {
+    let d = {'fizz': 0, 'buzz': 0};
+    for (let i = start; i <= end; i++){
+        if (i % 15 === 0){
+            d['fizz'] += 1;
+            d['buzz'] += 1;
+        }
+        else if (i % 3 === 0){
+            d['fizz'] += 1;
+        }
+        else if (i % 5 === 0){
+            d['buzz'] += 1;
+        }
+    }
+    return d;
+}
