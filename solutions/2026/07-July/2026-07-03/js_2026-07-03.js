1a24b1cd1fee87030ed8b386a4a4b9d2debc535e
Quoriee <snowii.torii@gmail.com>
2026-07-03 23:56:29 +0200

    database migration js solution

--- solutions/2026/07-July/2026-07-03/js_2026-07-03.js
+++ solutions/2026/07-July/2026-07-03/js_2026-07-03.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2026-07-03
+// Database Migration
+function migrateRecord(schema, record) {
+    let res = { ...record };
+    for (const [key, value] of Object.entries(schema)){
+        if (!(key in res)){
+            res[key] = value;
+            }
+    }
+    return res;
+}
