2c0edd9b5ff045a573ed4d2fa20b044eb5475a9d
Quoriee <snowii.torii@gmail.com>
2026-07-03 07:35:52 +0200

    sum of differences js solution

--- solutions/2026/05-May/2026-05-26/js_2026-05-26.js
+++ solutions/2026/05-May/2026-05-26/js_2026-05-26.js
@@ -1,1 +1,10 @@
 // JavaScript solution for 2026-05-26
+// Sum of Differences
+function sumOfDifferences(arr) {
+    let s = 0;
+    
+    for (let i = 0; i < arr.length - 1; i++){
+        s += arr[i+1] - arr[i];
+    }
+    return s;
+}
