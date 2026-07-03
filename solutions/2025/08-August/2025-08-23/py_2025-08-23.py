dc20e427f86ef6f1c5eaf147c11c45e7dd1807f2
Quoriee <snowii.torii@gmail.com>
2026-07-03 02:34:09 +0200

    unnatural prime python solution

--- solutions/2025/08-August/2025-08-23/py_2025-08-23.py
+++ solutions/2025/08-August/2025-08-23/py_2025-08-23.py
@@ -1,1 +1,13 @@
 # Python solution for 2025-08-23
+#Unnatural Prime
+def is_unnatural_prime(n):
+    num = abs(n)
+
+    if num <= 1:
+        return False
+
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+
+    return True
