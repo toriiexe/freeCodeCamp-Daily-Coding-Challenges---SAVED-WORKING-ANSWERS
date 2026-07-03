5fac2e9e485032ec08f3c3d7dc6117524f9047a0
Quoriee <snowii.torii@gmail.com>
2026-07-03 02:37:58 +0200

    targeted sum python solution

--- solutions/2025/08-August/2025-08-17/py_2025-08-17.py
+++ solutions/2025/08-August/2025-08-17/py_2025-08-17.py
@@ -1,1 +1,14 @@
 # Python solution for 2025-08-17
+#Targeted sum
+def find_target(arr, target):
+    d = {}
+
+    for i, num in enumerate(arr):
+        needed = target - num
+
+        if needed in d:
+            return [d[needed], i]
+
+        d[num] = i
+
+    return "Target not found"
