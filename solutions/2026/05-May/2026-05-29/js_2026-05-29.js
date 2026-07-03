c005cf467ba0f186caefffa995bc7f72dbf50896
Quoriee <snowii.torii@gmail.com>
2026-07-03 07:19:19 +0200

    wider aspect radio js solution

--- solutions/2026/05-May/2026-05-29/js_2026-05-29.js
+++ solutions/2026/05-May/2026-05-29/js_2026-05-29.js
@@ -1,1 +1,20 @@
 // JavaScript solution for 2026-05-29
+//Wider Aspect Ratio
+
+function gcd(a, b) {
+    return b === 0 ? Math.abs(a) : gcd(b, a % b);
+}
+
+function getWiderAspectRatio(a, b) {
+    let [wA, hA] = a.split('x').map(Number);
+    let [wB, hB] = b.split('x').map(Number);
+
+    if (wA * hB >= wB * hA) {
+        const g = gcd(wA, hA);
+        return `${wA / g}:${hA / g}`;
+    } 
+    else {
+        const g = gcd(wB, hB);
+        return `${wB / g}:${hB / g}`;
+    }
+}
