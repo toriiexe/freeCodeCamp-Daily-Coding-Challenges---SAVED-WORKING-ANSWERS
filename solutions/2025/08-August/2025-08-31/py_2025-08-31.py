4246040a41efd4e90e99595d01d77164b2c826e6
Quoriee <snowii.torii@gmail.com>
2026-07-03 03:03:45 +0200

    hex generator python solution

--- solutions/2025/08-August/2025-08-31/py_2025-08-31.py
+++ solutions/2025/08-August/2025-08-31/py_2025-08-31.py
@@ -1,1 +1,28 @@
 # Python solution for 2025-08-31
+#Hex Generator
+import random
+
+def generate_hex(color):
+
+    if color not in ('red', 'green', 'blue'):
+        return 'Invalid color'
+
+    dominant = random.randint(128, 255)
+
+    if color == 'red':
+        r = dominant
+        g = random.randint(0, dominant - 1)
+        b = random.randint(0, dominant - 1)
+
+    elif color == 'green':
+        g = dominant
+        r = random.randint(0, dominant - 1)
+        b = random.randint(0, dominant - 1)
+
+    #blue
+    else:
+        b = dominant
+        r = random.randint(0, dominant - 1)
+        g = random.randint(0, dominant - 1)
+
+    return f"{r:02X}{g:02X}{b:02X}"
