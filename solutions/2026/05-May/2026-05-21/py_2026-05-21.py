562401373d5d66c8790272a6947bc1e8b273a42a
Quoriee <snowii.torii@gmail.com>
2026-07-04 13:31:53 +0200

    i before e python solution

--- solutions/2026/05-May/2026-05-21/py_2026-05-21.py
+++ solutions/2026/05-May/2026-05-21/py_2026-05-21.py
@@ -1,1 +1,12 @@
 # Python solution for 2026-05-21
+# I Before E
+import re
+"""
+Python RegEx - Lookbehinds
+(?<=pattern) - Matches text only if it is immediately preceded by the given pattern, without including that pattern in the matched result.
+(?<!pattern) - Matches text only if it is not immediately preceded by the given pattern.
+"""
+def i_before_e(sentence):
+    sentence = re.sub(r'(?<!c)ei', 'ie', sentence) #not preceded by 'c'
+    sentence = re.sub(r'(?<=c)ie', 'ei', sentence) #preceded by 'c'
+    return sentence
