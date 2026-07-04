621481f5526f2df03bf03d0e8aed9008fec19018
Quoriee <snowii.torii@gmail.com>
2026-07-04 12:00:49 +0200

    kaprekar's routine python solution

--- solutions/2026/07-July/2026-07-04/py_2026-07-04.py
+++ solutions/2026/07-July/2026-07-04/py_2026-07-04.py
@@ -1,1 +1,13 @@
 # Python solution for 2026-07-04
+# Kaprekar's Routine
+def kaprekar(n):
+    counter = 0
+
+    while n != 6174:
+        s = str(n).zfill(4)
+        dsc = int("".join(sorted(s)))
+        asc = int("".join(sorted(s, reverse=True)))
+        n = asc - dsc
+        counter += 1
+
+    return counter
