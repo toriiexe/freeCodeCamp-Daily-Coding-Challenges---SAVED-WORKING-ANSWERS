b3e5fa5016b5abbd4178231cb4ad6442d1b12374
Quoriee <snowii.torii@gmail.com>
2026-07-04 15:32:12 +0200

    jbelmud text js solution

--- solutions/2025/08-August/2025-08-15/js_2025-08-15.js
+++ solutions/2025/08-August/2025-08-15/js_2025-08-15.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2025-08-15
+// Jbelmud Text
+function jbelmu(text) {
+    const words = text.split(' ');
+    let res = [];
+
+    for (const word of words){
+        if (word.length <= 2){
+        res.push(word);
+        }
+        else{
+        const mid = word.slice(1,-1).split('').sort().join('');
+        res.push(word[0] + mid + word[word.length - 1]);
+        }
+    }
+    return res.join(' ');
+}
