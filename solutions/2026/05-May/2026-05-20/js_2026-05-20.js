56e0b5372483d20a64e78c3c8d4e0bd198ef782b
Quoriee <snowii.torii@gmail.com>
2026-07-04 13:18:20 +0200

    string zipper js solution

--- solutions/2026/05-May/2026-05-20/js_2026-05-20.js
+++ solutions/2026/05-May/2026-05-20/js_2026-05-20.js
@@ -1,1 +1,12 @@
 // JavaScript solution for 2026-05-20
+// String Zipper
+function zipStrings(a, b) {
+    let result = "";
+    const minLength = Math.min(a.length, b.length);
+
+    for (let i = 0; i < minLength; i++){
+        result += a[i] + b[i];
+    }
+
+    return result + a.substring(minLength) + b.substring(minLength);
+}
