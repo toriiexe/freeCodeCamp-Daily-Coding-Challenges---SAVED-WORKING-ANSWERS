5c4a0358ea6a5fd5cb9d72ca40ff87fc3f102517
Quoriee <snowii.torii@gmail.com>
2026-07-04 12:19:55 +0200

    max profit js solution

--- solutions/2026/07-July/2026-07-02/js_2026-07-02.js
+++ solutions/2026/07-July/2026-07-02/js_2026-07-02.js
@@ -1,1 +1,20 @@
 // JavaScript solution for 2026-07-02
+// Max Profit
+function getMaxProfit(prices, budget) {
+    let profit = 0;
+
+    for (const [i, buy] of prices.entries()) {
+        const shares = Math.floor(budget / buy);
+
+        for (let j = i + 1; j < prices.length; j++) {
+        const sell = prices[j];
+        const possible = shares * (sell - buy);
+
+        if (possible > profit) {
+            profit = possible;
+        }
+        }
+    }
+
+    return profit.toFixed(2);
+}
