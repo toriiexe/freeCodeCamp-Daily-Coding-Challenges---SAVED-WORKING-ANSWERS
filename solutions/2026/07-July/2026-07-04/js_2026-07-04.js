4faf64da977e912b00b3e8e19d4f5c94e33c3b3c
Quoriee <snowii.torii@gmail.com>
2026-07-04 12:13:40 +0200

    kaprekar's routine js solution

--- solutions/2026/07-July/2026-07-04/js_2026-07-04.js
+++ solutions/2026/07-July/2026-07-04/js_2026-07-04.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2026-07-04
+// Kaprekar's Routine
+function kaprekar(n) {
+    let counter = 0;
+
+    while (n != 6174){
+        let s = String(n).padStart(4, "0").split("");
+        let dsc = Number(s.slice().sort().join(""));
+        let asc = Number(s.slice().sort().reverse().join(""));
+        n = asc - dsc;
+        counter ++;
+    }
+    return counter;
+}
