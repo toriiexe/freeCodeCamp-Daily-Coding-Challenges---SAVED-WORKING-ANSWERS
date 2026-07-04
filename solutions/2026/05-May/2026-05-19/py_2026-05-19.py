c99f3456f50fe1dc0e8195c3061e0afaef1c2911
Quoriee <snowii.torii@gmail.com>
2026-07-04 12:56:07 +0200

    sleep debt python solution

--- solutions/2026/05-May/2026-05-19/py_2026-05-19.py
+++ solutions/2026/05-May/2026-05-19/py_2026-05-19.py
@@ -1,1 +1,16 @@
 # Python solution for 2026-05-19
+# Sleep Debt
+def sleep_debt(hours_slept, target_hours):
+    howManyNightsSlept = len(hours_slept)
+    shouldHaveSlept = target_hours * howManyNightsSlept
+    totalSlept = sum(hours_slept)
+    debt = shouldHaveSlept - totalSlept + target_hours
+    return max(debt, 0)
+
+"""
+# Python solution for 2026-05-19
+# Sleep Debt - SHORTER SOLUTION
+def sleep_debt(hours_slept, target_hours):
+    debt = (len(hours_slept) + 1) * target_hours - sum(hours_slept)
+    return max(debt, 0)
+"""
