dc50b3dc82e5728737b44c61314dee791af06c62
Quoriee <snowii.torii@gmail.com>
2026-07-04 14:03:55 +0200

    i before e js solution

--- solutions/2026/05-May/2026-05-21/js_2026-05-21.js
+++ solutions/2026/05-May/2026-05-21/js_2026-05-21.js
@@ -1,1 +1,16 @@
 // JavaScript solution for 2026-05-21
+// I Before E
+
+/*
+JavaScript RegExp - Lookbehinds
+
+(?<=pattern) - Matches text only if it is immediately preceded by the given pattern,
+without including that pattern in the matched result.
+
+(?<!pattern) - Matches text only if it is not immediately preceded by the given pattern.
+*/
+function iBeforeE(sentence) {
+    sentence = sentence.replace(/(?<=c)ie/g, 'ei');
+    sentence = sentence.replace(/(?<!c)ei/g, 'ie');
+    return sentence;
+}
