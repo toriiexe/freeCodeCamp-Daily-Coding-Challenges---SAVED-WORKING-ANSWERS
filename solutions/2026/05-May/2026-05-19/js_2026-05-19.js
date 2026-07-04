256f491ec00c22a9ad358237a5a658ed18771c8c
Quoriee <snowii.torii@gmail.com>
2026-07-04 13:00:36 +0200

    sleep debt js solution

--- solutions/2026/05-May/2026-05-19/js_2026-05-19.js
+++ solutions/2026/05-May/2026-05-19/js_2026-05-19.js
@@ -1,1 +1,10 @@
 // JavaScript solution for 2026-05-19
+// Sleep Debt
+function getSum(total, num) {
+    return total + num;
+}
+
+    function sleepDebt(hoursSlept, targetHours) {
+    const debt = (hoursSlept.length + 1) * targetHours - hoursSlept.reduce(getSum, 0);
+    return Math.max(debt, 0);
+}
