10a50a6f93a129caacf7e0b02b8090ea9aff3955
Quoriee <snowii.torii@gmail.com>
2026-07-05 12:46:23 +0200

    offending element python solution

--- solutions/2026/05-May/2026-05-13/py_2026-05-13.py
+++ solutions/2026/05-May/2026-05-13/py_2026-05-13.py
@@ -1,1 +1,8 @@
 # Python solution for 2026-05-13
+# Offending element
+def find_offender(arr):
+    for i in range(len(arr) - 1):
+        if arr[i] > arr[i + 1]:
+            if i > 0 and arr[i + 1] < arr[i - 1]:
+                return i + 1
+            return i
