58cd069af07ffa0f92ec92a590e7c8439aa7cc2a
Quoriee <snowii.torii@gmail.com>
2026-07-05 13:12:01 +0200

    mirror image python solution

--- solutions/2026/05-May/2026-05-14/py_2026-05-14.py
+++ solutions/2026/05-May/2026-05-14/py_2026-05-14.py
@@ -1,1 +1,30 @@
 # Python solution for 2026-05-14
+# Mirror Image
+def is_mirror_image(s1, s2):
+    symetric_chars = 'WTYUIOHAXVMwoxv08=+:|-_*^!. '
+    swaps = {
+        '[':']', ']':'[',
+        '{':'}', '}':'{',
+        '<':'>', '>':'<',
+        'b':'d', 'd':'b',
+        'p':'q', 'q':'p',
+        '(':')', ')':'('
+    }
+
+    if len(s1) != len(s2):
+        return False
+
+    s2_reversed = s2[::-1]
+
+    for i in range(len(s1)):
+        if s1[i] not in symetric_chars and s1[i] not in swaps:
+            return False
+
+        if s1[i] in swaps:
+            if swaps[s1[i]] != s2_reversed[i]:
+                return False
+        else:
+            if s1[i] != s2_reversed[i]:
+                return False
+
+    return True
