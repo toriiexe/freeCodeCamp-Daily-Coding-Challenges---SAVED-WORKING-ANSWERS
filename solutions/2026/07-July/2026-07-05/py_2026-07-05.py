916cc1cbc2d16c7832287a16c38963cf83b02587
Quoriee <snowii.torii@gmail.com>
2026-07-05 09:23:54 +0200

    bucket fill python solution

--- solutions/2026/07-July/2026-07-05/py_2026-07-05.py
+++ solutions/2026/07-July/2026-07-05/py_2026-07-05.py
@@ -1,1 +1,22 @@
 # Python solution for 2026-07-05
+# Bucket Fill
+def fill(i, j, grid, value_to_change, new_value):
+    if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
+        return
+    if grid[i][j] != value_to_change:
+        return
+
+    grid[i][j] = new_value
+
+    fill(i-1, j, grid, value_to_change, new_value)
+    fill(i+1, j, grid, value_to_change, new_value)
+    fill(i, j-1, grid, value_to_change, new_value)
+    fill(i, j+1, grid, value_to_change, new_value)
+
+    return grid
+
+
+def bucket_fill(grid, pos, new_value):
+    value_to_change = grid[pos[0]][pos[1]]
+    fill(pos[0], pos[1], grid, value_to_change, new_value)
+    return grid
