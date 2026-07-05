1f098c77efc55710f2325e1f747783161034bd07
Quoriee <snowii.torii@gmail.com>
2026-07-06 00:38:50 +0200

    mongo id date js solution

--- solutions/2026/05-May/2026-05-17/js_2026-05-17.js
+++ solutions/2026/05-May/2026-05-17/js_2026-05-17.js
@@ -1,1 +1,6 @@
 // JavaScript solution for 2026-05-17
+// Mongo ID Date
+function mongoIdToDate(id) {
+    const timestamp = parseInt(id.slice(0, 8), 16);
+    return new Date(timestamp * 1000).toISOString();
+}
