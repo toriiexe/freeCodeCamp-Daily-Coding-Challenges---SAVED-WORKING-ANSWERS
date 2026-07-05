b36174a76648f9180f74a7ef3eaa92c6f06232aa
Quoriee <snowii.torii@gmail.com>
2026-07-06 01:36:18 +0200

    parentheses combinations js solution

--- solutions/2026/05-May/2026-05-31/js_2026-05-31.js
+++ solutions/2026/05-May/2026-05-31/js_2026-05-31.js
@@ -1,1 +1,15 @@
 // JavaScript solution for 2026-05-31
+// Parentheses Combinations
+function factorialBigInt(n) {
+    if (n < 0) return undefined;
+    let result = 1n;
+    for (let i = 2n; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+function getCombinations(n) {
+    n = BigInt(n);
+    return factorialBigInt(2n * n) / (factorialBigInt(n + 1n) * factorialBigInt(n));
+}
