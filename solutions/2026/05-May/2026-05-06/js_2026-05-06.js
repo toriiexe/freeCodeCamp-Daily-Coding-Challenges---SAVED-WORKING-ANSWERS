b4d62fe4c418aae5fe987fd67c92da9083a135d7
Quoriee <snowii.torii@gmail.com>
2026-07-05 10:28:12 +0200

    allergen friendly meals js solution

--- solutions/2026/05-May/2026-05-06/js_2026-05-06.js
+++ solutions/2026/05-May/2026-05-06/js_2026-05-06.js
@@ -1,1 +1,20 @@
 // JavaScript solution for 2026-05-06
+// Allergen Friendly Meals
+function getAllergenFriendlyMeals(meals, allergens) {
+    let safeMeals = [];
+
+    for (const [meal, mealAllergens] of meals){
+        let notSafe = false;
+
+        for (const mealAllergen of mealAllergens){
+        if (allergens.includes(mealAllergen)){
+            notSafe = true;
+            break;
+        }
+        }
+        if (!(notSafe)){
+        safeMeals.push(meal);
+        }
+    }
+    return safeMeals;
+}
