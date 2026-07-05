ea7c905f919532716753efc6b9210eef4193585e
Quoriee <snowii.torii@gmail.com>
2026-07-05 11:10:10 +0200

    transposed matrix js solution

--- solutions/2026/05-May/2026-05-09/js_2026-05-09.js
+++ solutions/2026/05-May/2026-05-09/js_2026-05-09.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2026-05-09
+// Transposed Matrix
+function transpose(matrix) {
+    let m = [];
+
+    for (let i = 0; i < matrix[0].length; i++) {
+        m[i] = [];
+        for (let j = 0; j < matrix.length; j++) {
+            m[i][j] = matrix[j][i];
+        }
+    }
+
+    return m;
+}
