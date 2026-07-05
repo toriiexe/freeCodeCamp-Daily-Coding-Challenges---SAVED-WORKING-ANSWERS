0a9cb3768bc4884bc31d0abaab208901123fa939
Quoriee <snowii.torii@gmail.com>
2026-07-05 12:50:31 +0200

    offending element readme

--- solutions/2026/05-May/2026-05-13/js_2026-05-13.js
+++ solutions/2026/05-May/2026-05-13/js_2026-05-13.js
@@ -1,1 +1,12 @@
 // JavaScript solution for 2026-05-13
+// Offending Element
+function findOffender(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            if (i > 0 && arr[i + 1] < arr[i - 1]) {
+                return i + 1;
+            }
+            return i;
+        }
+    }
+}
