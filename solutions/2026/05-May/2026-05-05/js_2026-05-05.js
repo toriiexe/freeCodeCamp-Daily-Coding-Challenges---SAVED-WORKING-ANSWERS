33e715e43c19b330e290615682fa3f48068b0b12
Quoriee <snowii.torii@gmail.com>
2026-07-05 09:49:40 +0200

    narcissistic number js solution

--- solutions/2026/05-May/2026-05-05/js_2026-05-05.js
+++ solutions/2026/05-May/2026-05-05/js_2026-05-05.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2026-05-05
+// Narcissistic Number
+function isNarcissistic(n) {
+    const nStr = String(n);
+    const length = nStr.length;
+    let s = 0;
+
+    for (let i = 0; i < length; i++) {
+        const num = Number(nStr[i]);
+        s += Math.pow(num, length);
+    }
+
+    return s === n;
+}
