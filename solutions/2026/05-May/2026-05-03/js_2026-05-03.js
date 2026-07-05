962a56bed1eb65f39dc29a631a579d0ec0e8d53d
Quoriee <snowii.torii@gmail.com>
2026-07-05 09:58:53 +0200

    good day js solution

--- solutions/2026/05-May/2026-05-03/js_2026-05-03.js
+++ solutions/2026/05-May/2026-05-03/js_2026-05-03.js
@@ -1,1 +1,19 @@
 // JavaScript solution for 2026-05-03
+// Good Day
+function getGreeting(s) {
+    const [h, m] = s.split(':');
+    const time = 60 * Number(h) + Number(m);
+
+    if (5 * 60 <= time && time <= 11 * 60 + 59) {
+        return 'Good morning';
+    } 
+    else if (12 * 60 <= time && time <= 17 * 60 + 59) {
+        return 'Good afternoon';
+    } 
+    else if (18 * 60 <= time && time <= 21 * 60 + 59) {
+        return 'Good evening';
+    } 
+    else if ((22 * 60 <= time && time <= 23 * 60 + 59) || (0 <= time && time <= 4 * 60 + 59)) {
+        return 'Good night';
+    }
+}
