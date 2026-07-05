d677656c9a3220cb9648383da5d992e271c99f5e
Quoriee <snowii.torii@gmail.com>
2026-07-05 23:57:37 +0200

    bingo range js solution

--- solutions/2026/05-May/2026-05-18/js_2026-05-18.js
+++ solutions/2026/05-May/2026-05-18/js_2026-05-18.js
@@ -1,1 +1,20 @@
 // JavaScript solution for 2026-05-18
+// Bingo Range
+function getBingoRange(letter) {
+    const d = {
+        B: [1, 15],
+        I: [16, 30],
+        N: [31, 45],
+        G: [46, 60],
+        O: [61, 75]
+    };
+
+    const [start, end] = d[letter];
+    const result = [];
+
+    for (let i = start; i <= end; i++) {
+        result.push(i);
+    }
+
+    return result;
+}
