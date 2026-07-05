cf66c548cef910224abd5e0d4ea46ab37cdace35
Quoriee <snowii.torii@gmail.com>
2026-07-05 11:50:23 +0200

    oldest person python solution

--- solutions/2026/05-May/2026-05-11/py_2026-05-11.py
+++ solutions/2026/05-May/2026-05-11/py_2026-05-11.py
@@ -1,1 +1,9 @@
 # Python solution for 2026-05-11
+# Oldest Person
+def get_oldest(people):
+    if not people:
+        return []
+
+    max_age = max(person['age'] for person in people)
+
+    return [person['name'] for person in people if person['age'] == max_age]
