37e6f60e2ee8db18f5fe6e59126591805d382c5c
Quoriee <snowii.torii@gmail.com>
2026-07-05 09:33:28 +0200

    bucket fill js solution

--- solutions/2026/07-July/2026-07-05/js_2026-07-05.js
+++ solutions/2026/07-July/2026-07-05/js_2026-07-05.js
@@ -1,1 +1,31 @@
 // JavaScript solution for 2026-07-05
+// Bucket Fill
+function fill(i, j, grid, valueToChange, newValue){
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length){
+        return;
+    }
+    if (grid[i][j] != valueToChange){
+        return;
+    }
+
+    grid[i][j] = newValue;
+
+    fill(i-1, j, grid, valueToChange, newValue)
+    fill(i+1, j, grid, valueToChange, newValue)
+    fill(i, j-1, grid, valueToChange, newValue)
+    fill(i, j+1, grid, valueToChange, newValue)
+
+    return grid;
+}
+
+
+function bucketFill(grid, [row, col], newValue) {
+    let valueToChange = grid[row][col]
+    if (valueToChange === newValue){
+        return grid;
+    }
+
+    fill(row, col, grid, valueToChange, newValue)
+    
+    return grid;
+}
