9057320317d71a9569da86e83f5c8e89fc19f975
Quoriee <snowii.torii@gmail.com>
2026-07-05 10:11:19 +0200

    good day py solution

--- solutions/2026/05-May/2026-05-03/py_2026-05-03.py
+++ solutions/2026/05-May/2026-05-03/py_2026-05-03.py
@@ -13,3 +13,14 @@ def get_greeting(s):
     elif 22 * 60 <= time <= 23 * 60 + 59 or 0 <= time <= 4 * 60 + 59:
         return 'Good night'
     
+
+"""
+# Good Day - SHORTER VERSION
+def get_greeting(s):
+    h, m = map(int, s.split(':'))
+    t = h * 60 + m
+
+    # 5:00-11:59 morning, 12:00-17:59 afternoon,
+    # 18:00-21:59 evening, 22:00-4:59 night
+    return 'Good morning' if 5 * 60 <= t < 12 * 60 else 'Good afternoon' if 12 * 60 <= t < 18 * 60 else 'Good evening' if 18 * 60 <= t < 22 * 60 else 'Good night'
+"""    
