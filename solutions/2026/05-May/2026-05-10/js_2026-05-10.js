e234f8c63356806229ecc56ed512246375452d78
Quoriee <snowii.torii@gmail.com>
2026-07-05 11:40:09 +0200

    isbn-13 validator js solution

--- solutions/2026/05-May/2026-05-10/js_2026-05-10.js
+++ solutions/2026/05-May/2026-05-10/js_2026-05-10.js
@@ -1,1 +1,25 @@
 // JavaScript solution for 2026-05-10
+// ISBN-13 Validator
+function isValidIsbn13(str) {
+    const s = str.replace(/-/g, '');
+
+    if (s.length !== 13) {
+        return false;
+    }
+
+    for (let c of s) {
+        if (c < '0' || c > '9') {
+        return false;
+        }
+    }
+
+    let total = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const digit = Number(s[i]);
+
+        total += (i % 2 === 0) ? digit : digit * 3;
+    }
+
+    return total % 10 === 0;
+}
