29a35ebd803bf3d491fa3923f6b1937ef8bec2fe
Quoriee <snowii.torii@gmail.com>
2026-07-05 23:58:22 +0200

    bingo range python solution

--- solutions/2026/05-May/2026-05-18/py_2026-05-18.py
+++ solutions/2026/05-May/2026-05-18/py_2026-05-18.py
@@ -1,6 +1,11 @@
 # Python solution for 2026-05-18
 # Bingo Range
 def get_bingo_range(letter):
-    d = {'B': [1, 15], 'I': [16, 30], 
-    'N': [31,45], 'G': [46,60], 'O': [61,75]}
+    d = {
+        'B': [1, 15], 
+        'I': [16, 30], 
+        'N': [31,45], 
+        'G': [46,60], 
+        'O': [61,75]
+        }
     return [i for i in range(d[letter][0], d[letter][1]+1)]
