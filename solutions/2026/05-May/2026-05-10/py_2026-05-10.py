dee6d53ddb3b70c0b70977bec852479730068850
Quoriee <snowii.torii@gmail.com>
2026-07-05 11:16:53 +0200

    isbn-13 validator python solution

--- solutions/2026/05-May/2026-05-10/py_2026-05-10.py
+++ solutions/2026/05-May/2026-05-10/py_2026-05-10.py
@@ -1,1 +1,15 @@
 # Python solution for 2026-05-10
+# ISBN-13 Validator
+def is_valid_isbn_13(s):
+    s = s.replace('-', '')
+
+    if len(s) != 13 or not s.isdigit():
+        return False
+
+    total = 0
+
+    for i, ch in enumerate(s):
+        digit = int(ch)
+        total += digit if i % 2 == 0 else digit * 3
+
+    return total % 10 == 0
