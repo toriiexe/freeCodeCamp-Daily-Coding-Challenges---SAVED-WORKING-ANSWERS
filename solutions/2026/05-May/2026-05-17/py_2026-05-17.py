a51164d138db7262662fed36ee0291206e506aa9
Quoriee <snowii.torii@gmail.com>
2026-07-06 00:36:54 +0200

    mongo id date python solution

--- solutions/2026/05-May/2026-05-17/py_2026-05-17.py
+++ solutions/2026/05-May/2026-05-17/py_2026-05-17.py
@@ -1,1 +1,8 @@
 # Python solution for 2026-05-17
+# Mongo ID Date
+from datetime import datetime, timezone
+
+def mongo_id_to_date(s):
+    timestamp = int(s[:8], 16)
+
+    return datetime.fromtimestamp(timestamp, tz=timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.000Z")
