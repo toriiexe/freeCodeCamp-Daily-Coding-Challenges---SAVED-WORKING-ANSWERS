0f493733a3c70fe63b8717a94dc394884fc7f89c
Quoriee <snowii.torii@gmail.com>
2026-07-05 12:11:53 +0200

    character frequency python solution

--- solutions/2026/05-May/2026-05-12/py_2026-05-12.py
+++ solutions/2026/05-May/2026-05-12/py_2026-05-12.py
@@ -1,1 +1,10 @@
 # Python solution for 2026-05-12
+# Character Frequency
+def get_frequency(s):
+    freq = {}
+    for c in s:
+        if c in freq:
+            freq[c] += 1
+        else:
+            freq[c] = 1
+    return freq
