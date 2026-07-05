f7474dc6292924e1143dc80ffbdb20334b8f2f89
Quoriee <snowii.torii@gmail.com>
2026-07-05 12:17:16 +0200

    character frequency js solution

--- solutions/2026/05-May/2026-05-12/js_2026-05-12.js
+++ solutions/2026/05-May/2026-05-12/js_2026-05-12.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2026-05-12
+// Character Frequency
+function getFrequency(str) {
+    const freq = {};
+
+    for (const c of str) {
+        freq[c] = (freq[c] || 0) + 1;
+    }
+
+    return freq;
+}
