d37a4b055da519a7af4e16f234191c1b95e36840
Quoriee <snowii.torii@gmail.com>
2026-07-05 23:56:17 +0200

    bingo range python solution

--- solutions/2026/05-May/2026-05-18/py_2026-05-18.py
+++ solutions/2026/05-May/2026-05-18/py_2026-05-18.py
@@ -1,26 +1,6 @@
 # Python solution for 2026-05-18
-# Coffee Order Parser
-def format_coffee_order(order):
-    menu = [
-        ("cold brew", 4.50),
-        ("oat latte", 5.00),
-        ("cappuccino", 4.75),
-        ("espresso", 3.00),
-        ("vanilla syrup", 0.75),
-        ("caramel drizzle", 0.60),
-        ("extra shot", 0.50),
-        ("oat milk", 0.75),
-        ("cream", 0.75),
-    ]
-
-    order = order.lower()
-
-    items = []
-    total = 0
-
-    for name, price in menu:
-        if name in order:
-            items.append(name)
-            total += price
-
-    return f"{' + '.join(items)}: ${total:.2f}"
+# Bingo Range
+def get_bingo_range(letter):
+    d = {'B': [1, 15], 'I': [16, 30], 
+    'N': [31,45], 'G': [46,60], 'O': [61,75]}
+    return [i for i in range(d[letter][0], d[letter][1]+1)]
