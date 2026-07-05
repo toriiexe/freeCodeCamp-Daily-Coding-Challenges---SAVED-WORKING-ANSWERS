e0b4a671d91fe3e79d04878d9e53f332c4854175
Quoriee <snowii.torii@gmail.com>
2026-07-05 12:06:33 +0200

    oldest person js solution

--- solutions/2026/05-May/2026-05-11/js_2026-05-11.js
+++ solutions/2026/05-May/2026-05-11/js_2026-05-11.js
@@ -1,1 +1,22 @@
 // JavaScript solution for 2026-05-11
+// Oldest Person
+function getOldest(people) {
+    if (!people || people.length === 0) {
+        return [];
+    }
+
+    let maxAge = -1;
+    let oldest = [];
+
+    for (const person of people) {
+        if (person.age > maxAge) {
+        maxAge = person.age;
+        oldest = [person.name];
+        } 
+        else if (person.age === maxAge) {
+        oldest.push(person.name);
+        }
+    }
+
+    return oldest;
+}
