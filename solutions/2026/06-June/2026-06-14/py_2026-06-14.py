2b6938497a9afb1ec9e2a562d182deb9b2a3d108
Quoriee <snowii.torii@gmail.com>
2026-07-07 00:45:46 +0200

    credit card validator python solution

--- solutions/2026/06-June/2026-06-14/py_2026-06-14.py
+++ solutions/2026/06-June/2026-06-14/py_2026-06-14.py
@@ -1,1 +1,17 @@
 # Python solution for 2026-06-14
+# Credit Card Validator
+def is_valid_card(number):
+    num = number[::-1]
+    s = 0
+
+    for i in range(len(num)):
+        digit = int(num[i])
+
+        if i % 2 == 1:
+            digit *= 2
+            if digit > 9:
+                digit -= 9
+
+        s += digit
+
+    return s % 10 == 0
