ceb503e952c3621ea26f7db6ca1f3e468b0e8ec8
Quoriee <snowii.torii@gmail.com>
2026-07-06 23:44:29 +0200

    1337 speak js solution

--- solutions/2026/06-June/2026-06-22/js_2026-06-22.js
+++ solutions/2026/06-June/2026-06-22/js_2026-06-22.js
@@ -1,1 +1,26 @@
 // JavaScript solution for 2026-06-22
+// 1337 Speak
+function makeLeet(str) {
+    const subs = {
+        'a': '4',
+        'e': '3',
+        'g': '9',
+        'i': '1',
+        'l': '1',
+        'o': '0',
+        's': '5',
+        't': '7'
+    }
+
+    let res = '';
+
+    for (const c of str) {
+        if (c in subs) {
+        res += subs[c];
+        } else {
+        res += c;
+        }
+    }
+
+    return res;
+}
