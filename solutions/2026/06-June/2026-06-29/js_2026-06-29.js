8cc249fd6db4bbb2405a769e0193ad3db90238da
Quoriee <snowii.torii@gmail.com>
2026-07-06 23:22:55 +0200

    song mood finder js solution

--- solutions/2026/06-June/2026-06-29/js_2026-06-29.js
+++ solutions/2026/06-June/2026-06-29/js_2026-06-29.js
@@ -1,1 +1,36 @@
 // JavaScript solution for 2026-06-29
+// Song Mood Finder
+function getMood(genre, bpm) {
+    let mood = '';
+
+    if (genre === 'classical') {
+        if (bpm >= 60 && bpm <= 109) {
+        mood = 'focus';
+        } else if (bpm >= 110 && bpm <= 180) {
+        mood = 'happy';
+        }
+
+    } else if (genre === 'electronic') {
+        if (bpm >= 60 && bpm <= 89) {
+        mood = 'focus';
+        } else if (bpm >= 90 && bpm <= 134) {
+        mood = 'happy';
+        } else if (bpm >= 135 && bpm <= 180) {
+        mood = 'hype';
+        }
+
+    } else if (genre === 'pop') {
+        if (bpm >= 60 && bpm <= 180) {
+        mood = 'happy';
+        }
+
+    } else if (genre === 'rock') {
+        if (bpm >= 60 && bpm <= 129) {
+        mood = 'happy';
+        } else if (bpm >= 130 && bpm <= 180) {
+        mood = 'hype';
+        }
+    }
+
+    return mood;
+}
