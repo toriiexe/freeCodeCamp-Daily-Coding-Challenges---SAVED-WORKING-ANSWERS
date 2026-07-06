d5d28847c5d00c1e3e622bd373382b49c18a2493
Quoriee <snowii.torii@gmail.com>
2026-07-07 01:47:14 +0200

    schema validator part 4 python solution

--- solutions/2026/06-June/2026-06-04/py_2026-06-04.py
+++ solutions/2026/06-June/2026-06-04/py_2026-06-04.py
@@ -1,1 +1,6 @@
 # Python solution for 2026-06-04
+# Schema Validator Part 4
+def is_valid_schema(obj):
+    Roles = ['user', 'creator', 'moderator', 'staff', 'admin']
+    supporter = "supporter" in obj
+    return ('username' in obj and isinstance(obj['username'], str)) and ('posts' in obj and isinstance(obj['posts'], int) and not isinstance(obj['posts'], bool)) and ('verified' in obj and isinstance(obj['verified'], bool)) and ('role' in obj and obj['role'] in Roles) and (not supporter or isinstance(obj['supporter'], bool))
