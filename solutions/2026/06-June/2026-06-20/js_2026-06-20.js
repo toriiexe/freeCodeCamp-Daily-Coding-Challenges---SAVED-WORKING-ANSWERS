587fcff3d4c84802af9343838e4a0365daf14aa9
Quoriee <snowii.torii@gmail.com>
2026-07-06 23:50:54 +0200

    prime factorization js solution

--- solutions/2026/06-June/2026-06-20/js_2026-06-20.js
+++ solutions/2026/06-June/2026-06-20/js_2026-06-20.js
@@ -1,1 +1,19 @@
 // JavaScript solution for 2026-06-20
+// Prime Factorization
+function primeFactorization(n) {
+    let factors = [];
+    let i = 2;
+
+    while (i * i <= n){
+        while (n % i === 0){
+        factors.push(i);
+        n /= i;
+        }
+        i++;
+    }
+
+    if (n > 1){
+        factors.push(n);
+    }
+    return factors;
+}
