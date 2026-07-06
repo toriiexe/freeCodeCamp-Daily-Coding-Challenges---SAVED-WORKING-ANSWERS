84daaad722f24dfd764abc1ea74ff3bfc1dc7179
Quoriee <snowii.torii@gmail.com>
2026-07-07 00:26:26 +0200

    british to american python solution

--- solutions/2026/06-June/2026-06-16/py_2026-06-16.py
+++ solutions/2026/06-June/2026-06-16/py_2026-06-16.py
@@ -1,1 +1,24 @@
 # Python solution for 2026-06-16
+# British to American
+def british_to_american(sentence):
+    brToAm = {
+        "colour": "color",
+        "flavour": "flavor",
+        "honour": "honor",
+        "neighbour": "neighbor",
+        "labour": "labor",
+        "humour": "humor",
+        "centre": "center",
+        "fibre": "fiber",
+        "defence": "defense",
+        "offence": "offense",
+        "organise": "organize",
+        "recognise": "recognize",
+        "analyse": "analyze"
+    }
+
+    for br, am in brToAm.items():
+        sentence = sentence.replace(br, am)
+        sentence = sentence.replace(br.capitalize(), am.capitalize())
+
+    return sentence
