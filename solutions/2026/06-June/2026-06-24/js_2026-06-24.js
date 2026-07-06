d9888571eb84b1cfe3e7f3ec673e13a870fad947
Quoriee <snowii.torii@gmail.com>
2026-07-06 23:34:24 +0200

    dna mutations js solution

--- solutions/2026/06-June/2026-06-24/js_2026-06-24.js
+++ solutions/2026/06-June/2026-06-24/js_2026-06-24.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2026-06-24
+// DNA Mutations
+function detectMutations(strand1, strand2) {
+    let res = [];
+    for (let i = 0; i < strand1.length; i++){
+        if (strand1[i] !== strand2[i]){
+        res.push(i);
+        }
+    }
+    return res;
+}
