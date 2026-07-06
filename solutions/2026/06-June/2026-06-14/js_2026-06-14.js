372a87a97d54b74a38e74a1e508cc3e0cdd6a609
Quoriee <snowii.torii@gmail.com>
2026-07-07 00:47:57 +0200

    credit card validator

--- solutions/2026/06-June/2026-06-14/js_2026-06-14.js
+++ solutions/2026/06-June/2026-06-14/js_2026-06-14.js
@@ -1,1 +1,21 @@
 // JavaScript solution for 2026-06-14
+// Credit Card Validator
+function isValidCard(number) {
+    const num = number.split('').reverse().join('');
+    let s = 0;
+
+    for (let i = 0; i < num.length; i++){
+        let digit = Number(num[i]);
+
+        if (i % 2 == 1){
+        digit *= 2;
+        if (digit > 9){
+            digit -= 9;
+        }
+        }
+
+        s += digit;
+    }
+
+    return s % 10 === 0;
+}
