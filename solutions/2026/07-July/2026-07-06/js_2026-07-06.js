e81c2870e173527a5845a0ecaf3c447f6e5cecf8
Quoriee <snowii.torii@gmail.com>
2026-07-06 10:07:48 +0200

    lowercase words js solution

--- solutions/2026/07-July/2026-07-06/js_2026-07-06.js
+++ solutions/2026/07-July/2026-07-06/js_2026-07-06.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2026-07-06
+// lowercase words
+function getLowercaseWords(str) {
+    let res = []
+    for (const word of str.split(' ')){
+        if (word === word.toLowerCase){
+            res.push(word);
+        }
+    }
+    return res.join(' ');
+}
