e4060c57025ca0f379b1c9662c478a23d7573883
Quoriee <snowii.torii@gmail.com>
2026-07-06 23:42:35 +0200

    1337 speak python solution

--- solutions/2026/06-June/2026-06-22/py_2026-06-22.py
+++ solutions/2026/06-June/2026-06-22/py_2026-06-22.py
@@ -1,1 +1,15 @@
 # Python solution for 2026-06-22
+# 1337 Speak
+def make_leet(s):
+    subs = {
+        'a': '4',
+        'e': '3',
+        'g': '9',
+        'i': '1',
+        'l': '1',
+        'o': '0',
+        's': '5',
+        't': '7'
+    }
+
+    return ''.join(subs[ch] if ch in subs else ch for ch in s)
