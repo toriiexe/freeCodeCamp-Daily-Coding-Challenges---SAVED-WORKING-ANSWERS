61b48f2f3f5d6b4843ef5e718b98a1435df2a8b2
Quoriee <snowii.torii@gmail.com>
2026-07-06 23:48:53 +0200

    prime factorization python solution

--- solutions/2026/06-June/2026-06-20/py_2026-06-20.py
+++ solutions/2026/06-June/2026-06-20/py_2026-06-20.py
@@ -1,1 +1,16 @@
 # Python solution for 2026-06-20
+# Prime Factorization
+def prime_factorization(n):
+    factors = []
+    i = 2
+
+    while i * i <= n:
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+        i += 1
+
+    if n > 1:
+        factors.append(n)
+
+    return factors
