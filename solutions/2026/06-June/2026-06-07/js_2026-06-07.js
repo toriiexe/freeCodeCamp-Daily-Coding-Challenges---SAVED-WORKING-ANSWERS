25f230b8b25c4a35b22ddcfb4352a53eb3a9343a
Quoriee <snowii.torii@gmail.com>
2026-07-07 01:15:59 +0200

    last load js solution

--- solutions/2026/06-June/2026-06-07/js_2026-06-07.js
+++ solutions/2026/06-June/2026-06-07/js_2026-06-07.js
@@ -1,1 +1,9 @@
 // JavaScript solution for 2026-06-07
+// Last Load
+function lastLoadDate(scoops, usage) {
+    let sUsage = 0;
+    for (const n of usage){
+        sUsage += n;
+    }
+    return Math.floor(scoops / (sUsage / usage.length));
+}
