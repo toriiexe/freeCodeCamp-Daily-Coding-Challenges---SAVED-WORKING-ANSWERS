4711a78b3bdd0bdda3affbb6e2b18508e7361984
Quoriee <snowii.torii@gmail.com>
2026-07-06 23:25:57 +0200

    duplicate character count js solution

--- solutions/2026/06-June/2026-06-30/js_2026-06-30.js
+++ solutions/2026/06-June/2026-06-30/js_2026-06-30.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2026-06-30
+// Duplicate Character Count
+function duplicateCharacterCount(str1, str2) {
+    const unique = new Set(str1);
+    let count = 0;
+
+    for (const char of str2) {
+        if (unique.has(char)) {
+        count++;
+        }
+    }
+
+    return count;
+}
