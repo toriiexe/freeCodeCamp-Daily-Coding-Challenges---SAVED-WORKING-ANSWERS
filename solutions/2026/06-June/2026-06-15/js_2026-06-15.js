4764f71c31a56d3a37b24ece3079c923c794e81a
Quoriee <snowii.torii@gmail.com>
2026-07-07 00:33:11 +0200

    number sort js solution

--- solutions/2026/06-June/2026-06-15/js_2026-06-15.js
+++ solutions/2026/06-June/2026-06-15/js_2026-06-15.js
@@ -1,1 +1,9 @@
 // JavaScript solution for 2026-06-15
+// Number Sort
+function sortNumbers(str) {
+    let res = [];
+    for (const s of str.split(',')){
+        res.push(Number(s));
+    }
+    return res.sort((a, b) => a - b);
+}
