931e20ea53fdf26874d37bc784c020c3de657089
Quoriee <snowii.torii@gmail.com>
2026-07-07 00:55:02 +0200

    schema validator part 1 js solution

--- solutions/2026/06-June/2026-06-01/js_2026-06-01.js
+++ solutions/2026/06-June/2026-06-01/js_2026-06-01.js
@@ -1,1 +1,10 @@
 // JavaScript solution for 2026-06-01
+// Schema Validator Part 1
+function isValidSchema(obj) {
+    for (const [key, value] of Object.entries(obj)) {
+        if (key === "username") {
+        return typeof value === "string";
+        }
+    }
+    return false;
+}
