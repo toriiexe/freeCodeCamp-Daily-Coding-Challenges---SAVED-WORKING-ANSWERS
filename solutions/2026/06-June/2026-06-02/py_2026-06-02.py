daa755d7cb3acbbf90351d099b2363f0f986933a
Quoriee <snowii.torii@gmail.com>
2026-07-07 01:48:51 +0200

    schema validator part 2 python solution

--- solutions/2026/06-June/2026-06-02/py_2026-06-02.py
+++ solutions/2026/06-June/2026-06-02/py_2026-06-02.py
@@ -1,4 +1,4 @@
 # Python solution for 2026-06-02
 # Schema Validator Part 2
 def is_valid_schema(obj):
-    return ('username' in obj and isinstance(obj['username'], str)) and ('posts' in obj and isinstance(obj['posts'], int)) and ('verified' in obj and isinstance(obj['verified'], bool))
+    return ('username' in obj and isinstance(obj['username'], str)) and ('posts' in obj and isinstance(obj['posts'], int) and not isinstance(obj['posts'], bool)) and ('verified' in obj and isinstance(obj['verified'], bool))
