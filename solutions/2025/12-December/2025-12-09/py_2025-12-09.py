255d99b04bfde01559cd231eb8749056eb1ec3d9
Quoriee <snowii.torii@gmail.com>
2026-07-07 17:00:50 +0200

    most frequent python solution

--- solutions/2025/12-December/2025-12-09/py_2025-12-09.py
+++ solutions/2025/12-December/2025-12-09/py_2025-12-09.py
@@ -1,1 +1,15 @@
 # Python solution for 2025-12-09
+# Most Frequent
+def most_frequent(arr):
+    d = {}
+    for el in arr:
+        d[el] = d.get(el, 0) + 1
+    return max(d, key=d.get)
+
+"""
+# Most Frequent - WITH LIBRARY VERSION
+from collections import Counter
+
+def most_frequent(arr):
+    return Counter(arr).most_common(1)[0][0]
+"""
