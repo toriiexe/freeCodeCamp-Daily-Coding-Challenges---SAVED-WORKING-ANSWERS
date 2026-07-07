c08a4fbddc836767bca6847741ba4a1bac2f44a4
Quoriee <snowii.torii@gmail.com>
2026-07-07 17:51:48 +0200

    speed check python solution

--- solutions/2025/12-December/2025-12-15/py_2025-12-15.py
+++ solutions/2025/12-December/2025-12-15/py_2025-12-15.py
@@ -1,1 +1,5 @@
 # Python solution for 2025-12-15
+# Speed Check
+def speed_check(speed_mph, speed_limit_kph):
+    toKph = 1.60934 * speed_mph
+    return 'Not Speeding' if toKph <= speed_limit_kph else 'Warning' if toKph <= speed_limit_kph + 5 else 'Ticket'
