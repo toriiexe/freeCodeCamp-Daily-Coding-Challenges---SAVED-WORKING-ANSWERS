8c0e06ad42dd487ddbc1fa28bdbd2cc44f5a5ea8
Quoriee <snowii.torii@gmail.com>
2026-07-07 02:19:34 +0200

    jet lagged js solution

--- solutions/2026/06-June/2026-06-08/js_2026-06-08.js
+++ solutions/2026/06-June/2026-06-08/js_2026-06-08.js
@@ -1,1 +1,18 @@
 // JavaScript solution for 2026-06-08
+// Jet Lagged
+function getJetLagHours(departureCity, arrivalCity, flightDuration, direction) {
+
+    const timezone_diff = {
+        'Los Angeles': -8,
+        'New York': -5,
+        'London': 0,
+        'Istanbul': 3,
+        'Dubai': 4,
+        'Hong Kong': 8,
+        'Tokyo': 9
+    }
+
+    const directionMultiplier = direction === 'east' ? 1.5 : 1.0
+
+    return Math.round((Math.abs(timezone_diff[arrivalCity] - timezone_diff[departureCity]) + (flightDuration * 0.1) * directionMultiplier) * 10) / 10;
+}
