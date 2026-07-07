3a5e9a816417c6c50366ea38c0f48095ced1cf61
Quoriee <snowii.torii@gmail.com>
2026-07-07 17:20:06 +0200

    capitalize it js solution

--- solutions/2025/12-December/2025-12-14/js_2025-12-14.js
+++ solutions/2025/12-December/2025-12-14/js_2025-12-14.js
@@ -1,1 +1,13 @@
 // JavaScript solution for 2025-12-14
+// Capitalize It
+function titleCase(title) {
+    let res = [];
+    for (const word of title.split(' ')){
+        if (!word) {
+        res.push('');
+        continue;
+        }
+        res.push(word[0].toUpperCase() + word.slice(1).toLowerCase());
+    }
+    return res.join(' ');
+}
