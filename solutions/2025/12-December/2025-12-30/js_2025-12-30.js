e4edd96f0a7b242f9a14cdbb95ab4a94c2e0b225
Quoriee <snowii.torii@gmail.com>
2026-07-07 17:40:58 +0200

    sum the string js solution

--- solutions/2025/12-December/2025-12-30/js_2025-12-30.js
+++ solutions/2025/12-December/2025-12-30/js_2025-12-30.js
@@ -1,1 +1,21 @@
 // JavaScript solution for 2025-12-30
+// Sum the String
+function stringSum(str) {
+    let res = 0;
+    let temp = "";
+
+    for (const c of str) {
+        if (c >= "0" && c <= "9") {
+        temp += c;
+        } else if (temp) {
+        res += Number(temp);
+        temp = "";
+        }
+    }
+
+    if (temp) {
+        res += Number(temp);
+    }
+
+    return res;
+}
