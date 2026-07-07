25445177e9f6f26cd1a5c1203905c9161193cb08
Quoriee <snowii.torii@gmail.com>
2026-07-07 06:25:19 +0200

    blood bank js solution

--- solutions/2026/06-June/2026-06-26/js_2026-06-26.js
+++ solutions/2026/06-June/2026-06-26/js_2026-06-26.js
@@ -1,1 +1,26 @@
 // JavaScript solution for 2026-06-26
+// Blood Bank
+function triageBlood(bank, patients) {
+    const exc = {
+        AB: ["AB", "A", "B", "O"],
+        A: ["A", "O"],
+        B: ["B", "O"],
+        O: ["O"]
+    };
+
+    let served = 0;
+
+    for (const bloodType of patients) {
+        for (const donor of exc[bloodType]) {
+        const index = bank.indexOf(donor);
+
+        if (index !== -1) {
+            bank.splice(index, 1);
+            served++;
+            break;
+        }
+        }
+    }
+
+    return `${served} of ${patients.length} patients served`;
+}
