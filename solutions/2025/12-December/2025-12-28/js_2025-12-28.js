8781ab3889cbc3b5f6732b0649bfc0b208c04c8c
Quoriee <snowii.torii@gmail.com>
2026-07-07 18:29:52 +0200

    SCREAMING_SNAKE_CASE js solution

--- solutions/2025/12-December/2025-12-28/js_2025-12-28.js
+++ solutions/2025/12-December/2025-12-28/js_2025-12-28.js
@@ -1,1 +1,7 @@
 // JavaScript solution for 2025-12-28
+// SCREAMING_SNAKE_CASE
+function toScreamingSnakeCase(variableName) {
+    variableName = variableName.replace(/-/g, "_");
+    variableName = variableName.replace(/(?!^)([A-Z])/g, "_$1");
+    return variableName.toUpperCase();
+}
