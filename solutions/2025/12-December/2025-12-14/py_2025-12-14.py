bbbc8a8b44b61dd35a74fd4c03c5b7c0eb3263f4
Quoriee <snowii.torii@gmail.com>
2026-07-07 17:17:11 +0200

    capitalize it python solution

--- solutions/2025/12-December/2025-12-14/py_2025-12-14.py
+++ solutions/2025/12-December/2025-12-14/py_2025-12-14.py
@@ -1,1 +1,10 @@
 # Python solution for 2025-12-14
+# Capitalize It
+def title_case(title):
+    return ' '.join([word[0].upper() + word[1:].lower() for word in title.split()])
+
+
+""" Capitalize It - WITH ONE STRING METHOD
+def title_case(t):
+    return t.title()
+"""
