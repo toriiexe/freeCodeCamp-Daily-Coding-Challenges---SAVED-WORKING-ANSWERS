071bfa58bbd5330d8d73682906cc4f48f56fc23f
Quoriee <snowii.torii@gmail.com>
2026-07-07 23:43:16 +0200

    odd or even day python solution

--- solutions/2026/01-January/2026-01-27/py_2026-01-27.py
+++ solutions/2026/01-January/2026-01-27/py_2026-01-27.py
@@ -1,1 +1,5 @@
 # Python solution for 2026-01-27
+# Odd or Even Day
+from datetime import datetime, timezone
+def odd_or_even_day(timestamp):
+    return 'even' if datetime.fromtimestamp(timestamp / 1000, tz=timezone.utc).day % 2 == 0 else 'odd'
