538fdd3473c8fcdfb5487237bbc20c0325e53781
Quoriee <snowii.torii@gmail.com>
2026-07-07 17:52:58 +0200

    speed check js solution

--- solutions/2025/12-December/2025-12-15/js_2025-12-15.js
+++ solutions/2025/12-December/2025-12-15/js_2025-12-15.js
@@ -1,1 +1,8 @@
 // JavaScript solution for 2025-12-15
+// Speed Check
+function speedCheck(speedMph, speedLimitKph) {
+    const toKph = 1.60934 * speedMph;
+    if (toKph <= speedLimitKph) return 'Not Speeding';
+    if (toKph <= speedLimitKph + 5) return 'Warning';
+    return 'Ticket';
+}
