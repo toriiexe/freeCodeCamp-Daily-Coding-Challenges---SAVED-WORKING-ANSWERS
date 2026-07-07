f7bfdeeac56d1c515674c9dca281b26d20dc6aa6
Quoriee <snowii.torii@gmail.com>
2026-07-07 06:22:16 +0200

    blood bank python solution

--- solutions/2026/06-June/2026-06-26/py_2026-06-26.py
+++ solutions/2026/06-June/2026-06-26/py_2026-06-26.py
@@ -1,1 +1,20 @@
 # Python solution for 2026-06-26
+# Blood Bank
+def triage_blood(bank, patients):
+    exc = {
+        'AB': ['AB', 'A', 'B', 'O'],
+        'A': ['A', 'O'],
+        'B': ['B', 'O'],
+        'O': ['O']
+    }
+
+    served = 0
+
+    for bloodtype in patients:
+        for donor in exc[bloodtype]:
+            if donor in bank:
+                bank.remove(donor)
+                served += 1
+                break
+
+    return f'{served} of {len(patients)} patients served'
