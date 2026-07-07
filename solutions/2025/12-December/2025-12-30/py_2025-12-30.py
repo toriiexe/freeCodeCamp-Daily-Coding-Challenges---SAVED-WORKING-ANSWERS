92aed61a3d6cd0678460389cba8529abc7c6c780
Quoriee <snowii.torii@gmail.com>
2026-07-07 17:37:47 +0200

    sum the string python solution

--- solutions/2025/12-December/2025-12-30/py_2025-12-30.py
+++ solutions/2025/12-December/2025-12-30/py_2025-12-30.py
@@ -1,1 +1,17 @@
 # Python solution for 2025-12-30
+# Sum the String
+def string_sum(s):
+    res = 0
+    temp = ""
+
+    for c in s:
+        if c.isdigit():
+            temp += c
+        elif temp:
+            res += int(temp)
+            temp = ""
+
+    if temp:
+        res += int(temp)
+
+    return res
