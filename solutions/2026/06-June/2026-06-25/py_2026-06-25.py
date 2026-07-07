cde4715ad089e73a7612cbfe3f3ac8530804f6a7
Quoriee <snowii.torii@gmail.com>
2026-07-07 06:38:14 +0200

    frontmatter parser python solution

--- solutions/2026/06-June/2026-06-25/py_2026-06-25.py
+++ solutions/2026/06-June/2026-06-25/py_2026-06-25.py
@@ -1,1 +1,21 @@
 # Python solution for 2026-06-25
+# Frontmatter Parser
+def parse_frontmatter(s):
+    d = {}
+    for line in s.strip().split("\n")[1:-1]:
+        key, value = line.split(": ", 1)
+        if value == "true":
+            value = True
+        elif value == "false":
+            value = False
+        else:
+            try:
+                value = int(value)
+            except ValueError:
+                try:
+                    value = float(value)
+                except ValueError:
+                    pass
+
+        d[key] = value
+    return d
