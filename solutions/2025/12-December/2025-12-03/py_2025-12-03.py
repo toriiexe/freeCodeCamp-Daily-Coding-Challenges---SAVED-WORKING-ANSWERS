ccbec7a23563860a31fbf668f555c67067f48696
Quoriee <snowii.torii@gmail.com>
2026-07-07 18:43:30 +0200

    markdown ordered list item coverter python solution

--- solutions/2025/12-December/2025-12-03/py_2025-12-03.py
+++ solutions/2025/12-December/2025-12-03/py_2025-12-03.py
@@ -1,1 +1,8 @@
 # Python solution for 2025-12-03
+# Markdown Ordered List Item Converter
+import re
+def convert_list_item(markdown):
+    match = re.match(r'^\s*([1-9]\d*)\.\s+(.+)$', markdown)
+    if match:
+        return f'<li>{match.group(2)}</li>'
+    return 'Invalid format'
