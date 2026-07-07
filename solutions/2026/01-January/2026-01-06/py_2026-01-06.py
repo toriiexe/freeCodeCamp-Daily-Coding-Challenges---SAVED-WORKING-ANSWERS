0183c7f821ab47f6d0ef743ada0f4480797b343b
Quoriee <snowii.torii@gmail.com>
2026-07-07 19:18:11 +0200

    vOwElcAsE python solution

--- solutions/2026/01-January/2026-01-06/py_2026-01-06.py
+++ solutions/2026/01-January/2026-01-06/py_2026-01-06.py
@@ -1,1 +1,12 @@
 # Python solution for 2026-01-06
+# vOwElcAsE
+def vowel_case(s):
+    res = []
+    for letter in s:
+        if letter.lower() in 'aeiou':
+            res.append(letter.upper())
+        elif letter.isalpha():
+            res.append(letter.lower())
+        else:
+            res.append(letter)
+    return ''.join(res)
