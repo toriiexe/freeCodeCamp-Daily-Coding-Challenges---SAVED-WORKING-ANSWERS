25ae365248685c23e28ea7ee3c16fc29864b6eb9
Quoriee <snowii.torii@gmail.com>
2026-07-07 17:07:32 +0200

    most frequent js solution

--- solutions/2025/12-December/2025-12-09/js_2025-12-09.js
+++ solutions/2025/12-December/2025-12-09/js_2025-12-09.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2025-12-09
+// Most Frequent
+function mostFrequent(arr) {
+    const d = new Map();
+
+    for (const el of arr) {
+        d.set(el, (d.get(el) || 0) + 1);
+    }
+
+    return [...d.keys()].reduce((a, b) => d.get(a) > d.get(b) ? a : b);
+}
