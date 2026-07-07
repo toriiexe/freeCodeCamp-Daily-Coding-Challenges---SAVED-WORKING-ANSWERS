2cdffb892f8e309898ed64a5fe157087a26c1680
Quoriee <snowii.torii@gmail.com>
2026-07-07 19:04:46 +0200

    markdown bold parser js solution

--- solutions/2025/12-December/2025-12-10/js_2025-12-10.js
+++ solutions/2025/12-December/2025-12-10/js_2025-12-10.js
@@ -1,1 +1,5 @@
 // JavaScript solution for 2025-12-10
+// Markdown Bold Parser
+function parseBold(markdown) {
+    return markdown.replace(/\*\*(\S(?:.*?\S)?)\*\*|__(\S(?:.*?\S)?)__/g, (match, group1, group2) => `<b>${group1 || group2}</b>`);
+}
