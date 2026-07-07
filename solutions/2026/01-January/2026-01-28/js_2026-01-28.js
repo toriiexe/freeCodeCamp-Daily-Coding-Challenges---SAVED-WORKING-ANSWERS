304394ec3fc00cc793ba6fe8c01b2959c4bf8269
Quoriee <snowii.torii@gmail.com>
2026-07-08 00:13:15 +0200

    flatten the array js solution

--- solutions/2026/01-January/2026-01-28/js_2026-01-28.js
+++ solutions/2026/01-January/2026-01-28/js_2026-01-28.js
@@ -1,1 +1,15 @@
 // JavaScript solution for 2026-01-28
+// Flatten the Array
+function flatten(arr) {
+    let res = [];
+
+    for (const x of arr) {
+        if (Array.isArray(x)) {
+        res.push(...flatten(x));
+        } else {
+        res.push(x);
+        }
+    }
+
+    return res;
+}
