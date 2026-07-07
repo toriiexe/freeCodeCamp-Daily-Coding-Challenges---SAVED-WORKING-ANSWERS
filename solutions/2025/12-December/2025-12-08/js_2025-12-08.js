20de638b91463ff5680a7a628cf53847e132de7f
Quoriee <snowii.torii@gmail.com>
2026-07-07 16:31:51 +0200

    pounds to kilograms

--- solutions/2025/12-December/2025-12-08/js_2025-12-08.js
+++ solutions/2025/12-December/2025-12-08/js_2025-12-08.js
@@ -1,1 +1,10 @@
 // JavaScript solution for 2025-12-08
+// Pounds to Kilograms
+function convertToKgs(lbs) {
+    const kgs = lbs * 0.453592;
+
+    const lbsStr = lbs === 1 ? 'pound' : 'pounds';
+    const kgsStr = Math.round(kgs * 100) / 100 === 1 ? 'kilogram' : 'kilograms';
+
+    return `${lbs} ${lbsStr} equals ${kgs.toFixed(2)} ${kgsStr}.`;
+}
