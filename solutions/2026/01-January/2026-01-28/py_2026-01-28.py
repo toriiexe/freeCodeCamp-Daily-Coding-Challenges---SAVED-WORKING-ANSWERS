2502945cbab3c42bfb668f48998bfdf4c34651db
Quoriee <snowii.torii@gmail.com>
2026-07-08 00:10:23 +0200

    flatten the array python solution

--- solutions/2026/01-January/2026-01-28/py_2026-01-28.py
+++ solutions/2026/01-January/2026-01-28/py_2026-01-28.py
@@ -1,1 +1,10 @@
 # Python solution for 2026-01-28
+# Flatten the Array
+def flatten(arr):
+    res = []
+    for x in arr:
+        if isinstance(x, list):
+            res.extend(flatten(x))
+        else:
+            res.append(x)
+    return res
