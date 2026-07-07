244298fdddb4072b7e0958a3f1a399fc3d4daed8
Quoriee <snowii.torii@gmail.com>
2026-07-07 19:31:33 +0200

    rock, paper, scissors python solution

--- solutions/2025/12-December/2025-12-27/py_2025-12-27.py
+++ solutions/2025/12-December/2025-12-27/py_2025-12-27.py
@@ -1,1 +1,13 @@
 # Python solution for 2025-12-27
+# Rock, Paper, Scissors
+def rock_paper_scissors(player1, player2):
+    beats = {
+        'Rock': 'Scissors',
+        'Paper': 'Rock',
+        'Scissors': 'Paper'
+    }
+    if (beats[player1] == player2):
+        return 'Player 1 wins'
+    if (beats[player2] == player1):
+        return 'Player 2 wins'
+    return 'Tie'
