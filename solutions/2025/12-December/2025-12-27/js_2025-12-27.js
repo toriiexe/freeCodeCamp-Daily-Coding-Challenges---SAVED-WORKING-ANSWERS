f8d62eb5e2359c5366cf3e74561f1d617f5ee6a9
Quoriee <snowii.torii@gmail.com>
2026-07-07 19:33:05 +0200

    rock, paper, scissors js solution

--- solutions/2025/12-December/2025-12-27/js_2025-12-27.js
+++ solutions/2025/12-December/2025-12-27/js_2025-12-27.js
@@ -1,1 +1,15 @@
 // JavaScript solution for 2025-12-27
+// Rock, Paper, Scissors
+function rockPaperScissors(player1, player2) {
+    const beats = {
+            'Rock': 'Scissors',
+            'Paper': 'Rock',
+            'Scissors': 'Paper'
+            }
+
+    if (beats[player1] == player2) 
+        return 'Player 1 wins';
+    if (beats[player2] == player1)
+        return 'Player 2 wins';
+    return 'Tie';
+}
