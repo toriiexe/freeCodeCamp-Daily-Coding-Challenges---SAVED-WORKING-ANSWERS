af4d44c33106305e1e59a81e43adc3f385755d2b
Quoriee <snowii.torii@gmail.com>
2026-07-07 17:11:33 +0200

    roman numeral builder python solution

--- solutions/2025/12-December/2025-12-11/py_2025-12-11.py
+++ solutions/2025/12-December/2025-12-11/py_2025-12-11.py
@@ -1,1 +1,14 @@
 # Python solution for 2025-12-11
+# Roman Numeral Builder
+def to_roman(num):
+    roman = [
+        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+        (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+        (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+    ]
+    s = ''    
+    for v, l in roman:
+        while num >= v:
+            s += l
+            num -= v    
+    return s
