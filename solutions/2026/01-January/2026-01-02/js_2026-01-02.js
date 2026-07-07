5f2fb1e0474403f11d7976fc3729896be49a20a9
Quoriee <snowii.torii@gmail.com>
2026-07-07 19:13:02 +0200

    nth fibonacci number js solution

--- solutions/2026/01-January/2026-01-02/js_2026-01-02.js
+++ solutions/2026/01-January/2026-01-02/js_2026-01-02.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2026-01-02
+// Nth Fibonacci Number
+function nthFibonacci(n) {
+    let a = 0;
+    let b = 1;
+
+    for (let i = 0; i < n-1; i++) {
+        let temp = a;
+        a = b;
+        b = temp + b;
+    }
+
+    return a;
+}
