bac29acd5cf914be5717909762d7cb39cc2d4072
Quoriee <snowii.torii@gmail.com>
2026-07-07 19:21:47 +0200

    vOwElcAsE js solution

--- solutions/2026/01-January/2026-01-06/js_2026-01-06.js
+++ solutions/2026/01-January/2026-01-06/js_2026-01-06.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2026-01-06
+// vOwElcAsE
+function vowelCase(str) {
+    let res = [];
+
+    for (const letter of str) {
+        if ('aeiou'.includes(letter.toLowerCase())) {
+        res.push(letter.toUpperCase());
+        } else if (letter.match(/[a-zA-Z]/)) {
+        res.push(letter.toLowerCase());
+        } else {
+        res.push(letter);
+        }
+    }
+
+    return res.join('');
+}
