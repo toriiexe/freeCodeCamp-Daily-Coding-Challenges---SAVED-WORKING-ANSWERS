7c76bf5071845350b6225efa2efe7b87960f64ff
Quoriee <snowii.torii@gmail.com>
2026-07-08 00:06:22 +0200

    consonant case js solution

--- solutions/2026/01-January/2026-01-20/js_2026-01-20.js
+++ solutions/2026/01-January/2026-01-20/js_2026-01-20.js
@@ -1,1 +1,15 @@
 // JavaScript solution for 2026-01-20
+// Consonant Case
+function toConsonantCase(str) {
+    let res = '';
+    for (const c of str){
+        if ('aeiouAEIOU'.includes(c)){
+        res += c.toLowerCase();
+        } else if (c === '-'){
+        res += '_';
+        } else{
+        res += c.toUpperCase();
+        }
+    }
+    return res;
+}
