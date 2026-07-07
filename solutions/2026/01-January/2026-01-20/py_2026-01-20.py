361f067357cecbaa991d018d9baf03660b6d618e
Quoriee <snowii.torii@gmail.com>
2026-07-08 00:04:20 +0200

    consonant case python solution

--- solutions/2026/01-January/2026-01-20/py_2026-01-20.py
+++ solutions/2026/01-January/2026-01-20/py_2026-01-20.py
@@ -1,1 +1,13 @@
 # Python solution for 2026-01-20
+# Consonant Case
+def to_consonant_case(s):
+    res = ''
+    for c in s:
+        if c.lower() in 'aeiou':
+            res += c.lower()
+        elif c == '-':
+            res += '_'
+        else:
+            res += c.upper()
+
+    return res
