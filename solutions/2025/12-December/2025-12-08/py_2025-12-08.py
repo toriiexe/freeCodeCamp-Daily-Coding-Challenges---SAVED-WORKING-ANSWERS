9d6c619ad7eab3e03bdd8d599f97c83e0ef9ee23
Quoriee <snowii.torii@gmail.com>
2026-07-07 16:22:13 +0200

    pounds to kilograms python solution

--- solutions/2025/12-December/2025-12-08/py_2025-12-08.py
+++ solutions/2025/12-December/2025-12-08/py_2025-12-08.py
@@ -1,1 +1,5 @@
 # Python solution for 2025-12-08
+# Pounds to Kilograms
+def convert_to_kgs(lbs):
+    kgs = lbs * 0.453592
+    return f"{lbs} {'pound' if lbs == 1 else 'pounds'} equals {kgs:.2f} {'kilogram' if round(kgs, 2) == 1 else 'kilograms'}."
