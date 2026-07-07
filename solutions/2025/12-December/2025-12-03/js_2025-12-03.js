4c9eb14753e5ee4663b9ceea978354fcd9eacab9
Quoriee <snowii.torii@gmail.com>
2026-07-07 18:51:17 +0200

    markdown ordered list item converter js solution

--- solutions/2025/12-December/2025-12-03/js_2025-12-03.js
+++ solutions/2025/12-December/2025-12-03/js_2025-12-03.js
@@ -1,1 +1,8 @@
 // JavaScript solution for 2025-12-03
+// Markdown Ordered List Item Converter
+function convertListItem(markdown) {
+    const match = markdown.match(/^\s*([1-9]\d*)\.\s+(.+)$/);
+
+    if (match) return `<li>${match[2]}</li>`;
+    return "Invalid format";
+}
