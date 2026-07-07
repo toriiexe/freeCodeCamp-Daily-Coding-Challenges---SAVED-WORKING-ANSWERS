a2449b0975b2e502013b9e7abe6790eb762c2227
Quoriee <snowii.torii@gmail.com>
2026-07-07 02:23:43 +0200

    itinerary arrangements python solution

--- solutions/2026/06-June/2026-06-10/py_2026-06-10.py
+++ solutions/2026/06-June/2026-06-10/py_2026-06-10.py
@@ -1,1 +1,6 @@
 # Python solution for 2026-06-10
+# Itinerary Arrangements
+from math import factorial as fact
+def get_itinerary_count(stops):
+    len_of_stops = len(stops)
+    return fact(len_of_stops) * (2 * len_of_stops - 3)
