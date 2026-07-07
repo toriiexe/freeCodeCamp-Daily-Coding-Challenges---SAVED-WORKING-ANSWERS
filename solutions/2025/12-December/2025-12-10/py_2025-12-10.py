1ab015e9b4ab6b2cdeb74cdc19e44e7989a0b573
Quoriee <snowii.torii@gmail.com>
2026-07-07 19:03:54 +0200

    markdown bold parser python solution

--- solutions/2025/12-December/2025-12-10/py_2025-12-10.py
+++ solutions/2025/12-December/2025-12-10/py_2025-12-10.py
@@ -1,1 +1,5 @@
 # Python solution for 2025-12-10
+# Markdown Bold Parser
+import re
+def parse_bold(markdown):
+    return re.sub(r'\*\*(\S(?:.*?\S)?)\*\*|__(\S(?:.*?\S)?)__', lambda m: f'<b>{m.group(1) or m.group(2)}</b>', markdown)
