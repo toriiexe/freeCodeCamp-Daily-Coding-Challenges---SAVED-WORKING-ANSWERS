6db9f64a17297d29cb0b4d533406d9ffe701ab5f
Quoriee <snowii.torii@gmail.com>
2026-07-07 02:27:21 +0200

    itinerary arrangements js solution

--- solutions/2026/06-June/2026-06-10/js_2026-06-10.js
+++ solutions/2026/06-June/2026-06-10/js_2026-06-10.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2026-06-10
+// Itinerary Arrangements
+function factorial(n) {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+    }
+
+function getItineraryCount(stops) {
+    const lenOfStops = stops.length;
+    return factorial(lenOfStops) * (2 * lenOfStops - 3);
+}
