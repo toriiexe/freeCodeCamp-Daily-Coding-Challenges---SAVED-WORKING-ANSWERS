6fdafc6831fbdd784f9760b014bea88ff6336d1f
Quoriee <snowii.torii@gmail.com>
2026-07-07 10:19:32 +0200

    frontmatter parser js solution

--- solutions/2026/06-June/2026-06-25/js_2026-06-25.js
+++ solutions/2026/06-June/2026-06-25/js_2026-06-25.js
@@ -1,1 +1,23 @@
 // JavaScript solution for 2026-06-25
+// Frontmatter Parser
+
+function parseFrontmatter(str) {
+    const result = {};
+
+    for (const line of str.split("\n").slice(1, -1)) {
+        const [key, ...rest] = line.split(": ");
+        let value = rest.join(": ");
+
+        if (value === "true") {
+        value = true;
+        } else if (value === "false") {
+        value = false;
+        } else if (!isNaN(value)) {
+        value = value.includes(".") ? parseFloat(value) : parseInt(value, 10);
+        }
+
+        result[key] = value;
+    }
+
+    return result;
+}
