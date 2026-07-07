f12c0df9b5e0eca57f902e03fe559e0d445003bf
Quoriee <snowii.torii@gmail.com>
2026-07-07 18:26:08 +0200

    SCREAMING_SNAKE_CASE python solution

--- solutions/2025/12-December/2025-12-28/py_2025-12-28.py
+++ solutions/2025/12-December/2025-12-28/py_2025-12-28.py
@@ -1,1 +1,7 @@
 # Python solution for 2025-12-28
+# SCREAMING_SNAKE_CASE
+import re
+def to_screaming_snake_case(variable_name):
+    variable_name = variable_name.replace("-", "_")
+    variable_name = re.sub(r'(?<!^)([A-Z])', r'_\1', variable_name)
+    return variable_name.upper()
