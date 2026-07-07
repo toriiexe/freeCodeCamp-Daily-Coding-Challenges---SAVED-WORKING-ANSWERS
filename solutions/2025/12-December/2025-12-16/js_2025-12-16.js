275da04a16c0f51327f33b80b6480edf9123d6fc
Quoriee <snowii.torii@gmail.com>
2026-07-07 19:28:26 +0200

    consonant count js solution

--- solutions/2025/12-December/2025-12-16/js_2025-12-16.js
+++ solutions/2025/12-December/2025-12-16/js_2025-12-16.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2025-12-16
+// Consonant Count
+function hasConsonantCount(text, target) {
+    let s = 0;
+    for (const c of text){
+        if (c.match(/[a-zA-Z]/) && !('aeiouAEIOU'.includes(c))){
+        s++;
+        }
+    }
+    return s === target;
+}
