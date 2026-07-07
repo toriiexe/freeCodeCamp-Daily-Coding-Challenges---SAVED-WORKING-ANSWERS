74884d66d8f8d88c3e61116332bdbe1c423b5d5a
Quoriee <snowii.torii@gmail.com>
2026-07-07 02:11:20 +0200

    schema validator part 6 js solution

--- solutions/2026/06-June/2026-06-06/js_2026-06-06.js
+++ solutions/2026/06-June/2026-06-06/js_2026-06-06.js
@@ -1,1 +1,12 @@
 // JavaScript solution for 2026-06-06
+// Schema Validator Part 6
+function isValidSchema(obj) {
+    const roles = ['user', 'creator', 'moderator', 'staff', 'admin'];
+
+    function isValidUser(user) {
+        const supporter = "supporter" in user;
+        return (typeof user.username === 'string' && typeof user.posts === 'number' && typeof user.verified === 'boolean' && roles.includes(user.role) && (!(supporter) || typeof user.supporter === 'boolean') && Array.isArray(user.badges) && user.badges.every(badge => typeof badge === "string"));
+    }
+
+    return (Array.isArray(obj.users) && obj.users.every(user => isValidUser(user)))
+}
