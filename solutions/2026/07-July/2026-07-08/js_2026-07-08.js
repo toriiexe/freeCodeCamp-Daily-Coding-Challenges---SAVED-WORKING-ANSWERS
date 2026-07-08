ca98b71169affd29075c0bfd886b9ed67e3e1472
Quoriee <snowii.torii@gmail.com>
2026-07-08 14:56:06 +0200

    issue triage js solution

--- solutions/2026/07-July/2026-07-08/js_2026-07-08.js
+++ solutions/2026/07-July/2026-07-08/js_2026-07-08.js
@@ -1,1 +1,9 @@
 // JavaScript solution for 2026-07-08
+// Issue Triage
+function triageIssue(ms, message) {
+    const days = ms / 86400000;
+    const hasBump = message.toLowerCase().includes('bump');
+    if (days < 7) return 'leave it';
+    if (hasBump) return 'close it';
+    return 'bump it';
+}
