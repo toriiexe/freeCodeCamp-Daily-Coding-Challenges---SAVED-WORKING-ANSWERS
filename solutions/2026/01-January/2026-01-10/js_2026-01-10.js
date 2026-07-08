938b88538ee702b53283134daa7ba87c1a3b3f8a
Quoriee <snowii.torii@gmail.com>
2026-07-08 16:20:02 +0200

    tic-tac-toe js solution

--- solutions/2026/01-January/2026-01-10/js_2026-01-10.js
+++ solutions/2026/01-January/2026-01-10/js_2026-01-10.js
@@ -1,1 +1,18 @@
 // JavaScript solution for 2026-01-10
+// Tic-Tac-Toe
+function ticTacToe(board) {
+	const lines = [
+		...board,
+		...[0, 1, 2].map(c => [board[0][c], board[1][c], board[2][c]]),
+		[0, 1, 2].map(i => board[i][i]),
+		[0, 1, 2].map(i => board[i][2 - i])
+	];
+
+	for (const line of lines) {
+		if (line[0] === line[1] && line[1] === line[2]) {
+			return `${line[0]} wins`;
+		}
+	}
+
+	return 'Draw';
+}
