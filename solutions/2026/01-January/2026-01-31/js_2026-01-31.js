f18e842d305c73ad745282b5c97e52cd20f97f8e
Quoriee <snowii.torii@gmail.com>
2026-07-08 15:23:24 +0200

    zodiac finder js solution

--- solutions/2026/01-January/2026-01-31/js_2026-01-31.js
+++ solutions/2026/01-January/2026-01-31/js_2026-01-31.js
@@ -1,1 +1,28 @@
 // JavaScript solution for 2026-01-31
+// Zodiac Finder
+function getSign(dateStr) {
+    const dt = new Date(dateStr);
+
+    if (isNaN(dt.getTime())) throw new Error("Invalid date");
+
+    const month = dt.getMonth() + 1;
+    const day = dt.getDate();
+
+    const zodiac = {
+      1: ["Capricorn", "Aquarius", 20],
+      2: ["Aquarius", "Pisces", 19],
+      3: ["Pisces", "Aries", 21],
+      4: ["Aries", "Taurus", 20],
+      5: ["Taurus", "Gemini", 21],
+      6: ["Gemini", "Cancer", 21],
+      7: ["Cancer", "Leo", 23],
+      8: ["Leo", "Virgo", 23],
+      9: ["Virgo", "Libra", 23],
+      10: ["Libra", "Scorpio", 23],
+      11: ["Scorpio", "Sagittarius", 22],
+      12: ["Sagittarius", "Capricorn", 22]
+    };
+
+    const res = zodiac[month];
+    return res[Number(day >= res[2])];
+}
