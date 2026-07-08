a8627e042f515eab4e11f299ea6d0f015af194c2
Quoriee <snowii.torii@gmail.com>
2026-07-08 15:31:38 +0200

    valid pawn moves python solution

--- solutions/2026/01-January/2026-01-30/py_2026-01-30.py
+++ solutions/2026/01-January/2026-01-30/py_2026-01-30.py
@@ -1,1 +1,14 @@
 # Python solution for 2026-01-30
+# Valid Pawn Moves
+def find_pawn_moves(position):
+    c = position[0]
+    r = int(position[1])
+
+    res = []
+
+    if r < 8:
+        res.append(f"{c}{r + 1}")
+        if r == 2:
+            res.append(f"{c}{r + 2}")
+
+    return res
