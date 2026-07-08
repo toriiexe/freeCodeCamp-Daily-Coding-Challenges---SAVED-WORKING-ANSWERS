2d4dc81f033b2cdddb92edbcc434951056df1d08
Quoriee <snowii.torii@gmail.com>
2026-07-08 14:53:27 +0200

    issue triage python solution

--- solutions/2026/07-July/2026-07-08/py_2026-07-08.py
+++ solutions/2026/07-July/2026-07-08/py_2026-07-08.py
@@ -1,1 +1,6 @@
 # Python solution for 2026-07-08
+# Issue Triage
+def triage_issue(ms, message):
+    days = ms//86_400_000
+    has_bump = 'bump' in message.lower()
+    return 'leave it' if days < 7 else 'close it' if has_bump else 'bump it'
