bc89005400e04571a0b8725791e80b9d0f6461c6
Quoriee <snowii.torii@gmail.com>
2026-07-08 15:33:29 +0200

    valid pawn moves js solution

--- solutions/2026/01-January/2026-01-30/js_2026-01-30.js
+++ solutions/2026/01-January/2026-01-30/js_2026-01-30.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2026-01-30
+// Valid Pawn Moves
+function findPawnMoves(position) {
+    const c = position[0];
+    const r = Number(position[1]);
+
+    let res = [];
+
+    if (r < 8){
+        res.push(`${c}${r + 1}`);
+        if (r === 2){
+            res.push(`${c}${r + 2}`);
+        }
+    }
+
+    return res;
+}
