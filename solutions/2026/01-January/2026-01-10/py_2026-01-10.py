1ccadc6c451603fc78fac4971f4957e13cb6bca3
Quoriee <snowii.torii@gmail.com>
2026-07-08 16:18:25 +0200

    tic-tac-toe python solution

--- solutions/2026/01-January/2026-01-10/py_2026-01-10.py
+++ solutions/2026/01-January/2026-01-10/py_2026-01-10.py
@@ -1,1 +1,17 @@
 # Python solution for 2026-01-10
+# Tic-Tac-Toe
+def tic_tac_toe(board):
+    lines = (
+        board +
+        [[board[r][c] for r in range(3)] for c in range(3)] +
+        [
+            [board[i][i] for i in range(3)],
+            [board[i][2 - i] for i in range(3)]
+        ]
+    )
+
+    for line in lines:
+        if line[0] == line[1] == line[2]:
+            return f'{line[0]} wins'
+
+    return 'Draw'
