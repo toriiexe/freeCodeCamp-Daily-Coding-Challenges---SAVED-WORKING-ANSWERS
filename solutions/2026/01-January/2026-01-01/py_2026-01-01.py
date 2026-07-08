ac47ee36f20e7342fda9586fe06e54d313e59f86
Quoriee <snowii.torii@gmail.com>
2026-07-08 15:54:24 +0200

    resolution streak python streak

--- solutions/2026/01-January/2026-01-01/py_2026-01-01.py
+++ solutions/2026/01-January/2026-01-01/py_2026-01-01.py
@@ -1,1 +1,10 @@
 # Python solution for 2026-01-01
+# Resolution Streak
+def resolution_streak(days):
+    n = 0
+    for day in days:
+        if day[0] >= 10_000 and day[1] <= 120 and day[2] >= 5:
+            n += 1
+        else:
+            return f'Resolution failed on day {n+1}: {n} day streak.'
+    return f'Resolution on track: {n} day streak.'
