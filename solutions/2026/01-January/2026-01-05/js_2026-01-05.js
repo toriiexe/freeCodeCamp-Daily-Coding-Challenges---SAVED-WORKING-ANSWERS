e91d6cbc4da7e9855552cf9029b81700eb231438
Quoriee <snowii.torii@gmail.com>
2026-07-08 15:46:54 +0200

    tire pressure js solution

--- solutions/2026/01-January/2026-01-05/js_2026-01-05.js
+++ solutions/2026/01-January/2026-01-05/js_2026-01-05.js
@@ -1,1 +1,21 @@
 // JavaScript solution for 2026-01-05
+// Tire Pressure
+function tireStatus(pressuresPSI, rangeBar) {
+    const barValue = 14.5038;
+    const minBar = rangeBar[0] * barValue;
+    const maxBar = rangeBar[1] * barValue;
+
+    const res = [];
+
+    for (const pressure of pressuresPSI) {
+        if (pressure < minBar) {
+            res.push("Low");
+        } else if (pressure <= maxBar) {
+            res.push("Good");
+        } else {
+            res.push("High");
+        }
+    }
+
+    return res;
+}
