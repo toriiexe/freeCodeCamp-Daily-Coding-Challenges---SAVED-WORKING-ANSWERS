989f73094c4bc01b40b75e66a56635c2801f988d
Quoriee <snowii.torii@gmail.com>
2026-07-08 15:56:29 +0200

    resolution streak js solution

--- solutions/2026/01-January/2026-01-01/js_2026-01-01.js
+++ solutions/2026/01-January/2026-01-01/js_2026-01-01.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2026-01-01
+// Resolution Streak
+function resolutionStreak(days) {
+    let n = 0;
+    for (const day of days){
+        if (day[0] >= 10_000 && day[1] <= 120 && day[2] >= 5){
+        n += 1
+        } else{
+        return `Resolution failed on day ${n+1}: ${n} day streak.`
+        }
+    }
+
+    return `Resolution on track: ${n} day streak.`
+}
