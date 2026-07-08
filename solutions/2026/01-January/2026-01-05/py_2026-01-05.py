46bd197f37c5a2c1bd5d5c734dea6d7bceefeebc
Quoriee <snowii.torii@gmail.com>
2026-07-08 15:46:12 +0200

    tire pressure python solution

--- solutions/2026/01-January/2026-01-05/py_2026-01-05.py
+++ solutions/2026/01-January/2026-01-05/py_2026-01-05.py
@@ -1,1 +1,17 @@
 # Python solution for 2026-01-05
+# Tire Pressure
+def tire_status(pressures_psi, range_bar): 
+    barValue = 14.5038 
+    minBar, maxBar = range_bar[0] * barValue, range_bar[1] * barValue 
+
+    res = [] 
+
+    for pressure in pressures_psi: 
+        if pressure < minBar: 
+            res.append('Low') 
+        elif pressure <= maxBar: 
+            res.append('Good') 
+        else: 
+            res.append('High') 
+            
+    return res
