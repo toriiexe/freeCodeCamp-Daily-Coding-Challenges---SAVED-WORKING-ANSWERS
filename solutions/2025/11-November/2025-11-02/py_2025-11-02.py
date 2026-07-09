4210d9da2506f4359df6c70adefe74910f0ede19
Quoriee <snowii.torii@gmail.com>
2026-07-09 16:25:48 +0200

    infected python solution

--- solutions/2025/11-November/2025-11-02/py_2025-11-02.py
+++ solutions/2025/11-November/2025-11-02/py_2025-11-02.py
@@ -1,1 +1,12 @@
 # Python solution for 2025-11-02
+# Infected
+import math
+def infected(days):
+    s = 1
+
+    for day in range(1, days + 1):
+        s *= 2
+        if day % 3 == 0:
+            s -= math.ceil(s * 0.2)
+
+    return s
