3c8a08efbf552802f8e377a56844494e5e21c74a
Quoriee <snowii.torii@gmail.com>
2026-07-10 01:13:08 +0200

    stellar classification python solution

--- solutions/2025/10-October/2025-10-04/py_2025-10-04.py
+++ solutions/2025/10-October/2025-10-04/py_2025-10-04.py
@@ -1,1 +1,17 @@
 # Python solution for 2025-10-04
+# Stellar Classification
+def classification(temp):
+    if temp >= 30_000:
+        return 'O'
+    elif temp >= 10_000:
+        return 'B'
+    elif temp >= 7_500:
+        return 'A'
+    elif temp >= 6_000:
+        return 'F'
+    elif temp >= 5_200:
+        return 'G'
+    elif temp >= 3_700:
+        return 'K'
+    elif temp >= 0:
+        return 'M'
