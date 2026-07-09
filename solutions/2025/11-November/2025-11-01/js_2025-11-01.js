ee3bbcb089d7cbe81f859c4fcf141ccb542b272c
Quoriee <snowii.torii@gmail.com>
2026-07-09 16:21:15 +0200

    signature validation js solution

--- solutions/2025/11-November/2025-11-01/js_2025-11-01.js
+++ solutions/2025/11-November/2025-11-01/js_2025-11-01.js
@@ -1,1 +1,15 @@
 // JavaScript solution for 2025-11-01
+// Signature Validation
+function verify(message, key, signature) {
+	let s = 0;
+
+	for (const ch of message + key) {
+		if ('A' <= ch && ch <= 'Z') {
+			s += ch.charCodeAt(0) - 65 + 27;
+		} else if ('a' <= ch && ch <= 'z') {
+			s += ch.charCodeAt(0) - 97 + 1;
+		}
+	}
+
+	return s === signature;
+}
