4840686e9e75a0b85a671ed3d36843a0060d56ae
Quoriee <snowii.torii@gmail.com>
2026-07-10 00:15:01 +0200

    nth prime python solution

--- solutions/2025/10-October/2025-10-30/py_2025-10-30.py
+++ solutions/2025/10-October/2025-10-30/py_2025-10-30.py
@@ -1,1 +1,17 @@
 # Python solution for 2025-10-30
+# Nth Prime
+def nth_prime(n):
+    primes = []
+    num = 2
+
+    while len(primes) < n:
+        is_prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+        num += 1
+
+    return primes[-1]
