2d01719084a1190013545591154a780aed2efd5c
Quoriee <snowii.torii@gmail.com>
2026-07-09 23:36:15 +0200

    spooky~case js solution

--- solutions/2025/10-October/2025-10-31/js_2025-10-31.js
+++ solutions/2025/10-October/2025-10-31/js_2025-10-31.js
@@ -1,1 +1,23 @@
 // JavaScript solution for 2025-10-31
+// SpOoKy~CaSe
+function spookify(boo) {
+    boo = boo.replaceAll('_', '~').replaceAll('-', '~');
+
+    let result = []
+    let count = 0;
+
+    for (const ch of boo){
+        if (ch === '~'){
+        result.push('~');
+        } else{
+        if (count % 2 === 0){
+            result.push(ch.toUpperCase());
+        } else{
+            result.push(ch.toLowerCase());
+        }
+        count++;
+        }
+    }
+
+    return result.join('');
+}
