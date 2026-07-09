5521a3e4c3123a6978f1ff5f495363a5a87d8ac3
Quoriee <snowii.torii@gmail.com>
2026-07-09 23:47:22 +0200

    Update py_2025-10-30.py

--- solutions/2025/10-October/2025-10-30/py_2025-10-30.py
+++ solutions/2025/10-October/2025-10-30/py_2025-10-30.py
@@ -1,11 +1,1 @@
 # Python solution for 2025-10-30
-# Complementary DNA
-def complementary_dna(strand):
-    mapping = {
-        'A': 'T',
-        'T': 'A',
-        'C': 'G',
-        'G': 'C'
-    }
-
-    return ''.join(mapping[key] for key in strand)
