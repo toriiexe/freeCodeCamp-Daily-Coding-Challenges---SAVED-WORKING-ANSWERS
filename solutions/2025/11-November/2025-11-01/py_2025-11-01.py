edce6ffb84f88bdb85f130fb3df1995973559be6
Quoriee <snowii.torii@gmail.com>
2026-07-09 16:19:13 +0200

    signature validation python solution

--- solutions/2025/11-November/2025-11-01/py_2025-11-01.py
+++ solutions/2025/11-November/2025-11-01/py_2025-11-01.py
@@ -1,1 +1,13 @@
 # Python solution for 2025-11-01
+# Signature Validation
+def verify(message, key, signature):
+    s = 0
+
+    for ch in message + key:
+        if ch.isalpha():
+            if ch.islower():
+                s += ord(ch) - ord('a') + 1
+            else:
+                s += ord(ch) - ord('A') + 27
+
+    return s == signature
