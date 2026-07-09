094b7944f7dbabd97929561b27b09c235d9e4ef3
Quoriee <snowii.torii@gmail.com>
2026-07-10 01:14:43 +0200

    stellar classification js solution

--- solutions/2025/10-October/2025-10-04/js_2025-10-04.js
+++ solutions/2025/10-October/2025-10-04/js_2025-10-04.js
@@ -1,1 +1,19 @@
 // JavaScript solution for 2025-10-04
+// Stellar Classification
+function classification(temp) {
+    if (temp >= 30_000){
+        return 'O';
+    } else if (temp >= 10_000){
+        return 'B';
+    } else if (temp >= 7_500){
+        return 'A';
+    } else if (temp >= 6_000){
+        return 'F';
+    } else if (temp >= 5_200){
+        return 'G';
+    } else if (temp >= 3_700){
+        return 'K';
+    } else if (temp >= 0){
+        return 'M';
+    }
+}
