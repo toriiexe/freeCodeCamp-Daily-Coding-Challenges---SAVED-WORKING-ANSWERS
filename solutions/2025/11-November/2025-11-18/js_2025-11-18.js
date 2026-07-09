e12fa6d60ebf8f466a739437b72de1df73872710
Quoriee <snowii.torii@gmail.com>
2026-07-09 17:28:16 +0200

    100 characters js solution

--- solutions/2025/11-November/2025-11-18/js_2025-11-18.js
+++ solutions/2025/11-November/2025-11-18/js_2025-11-18.js
@@ -1,1 +1,9 @@
 // JavaScript solution for 2025-11-18
+// 100 Characters
+function oneHundred(chars) {
+    let res = '';
+    while (res.length < 100){
+        res += chars[res.length % chars.length]
+    }
+    return res;
+}
