ed267d4e85f4ab573d78482f2f706a62fca861a7
Quoriee <snowii.torii@gmail.com>
2026-07-09 23:33:47 +0200

    spooky~case python solution

--- solutions/2025/10-October/2025-10-31/py_2025-10-31.py
+++ solutions/2025/10-October/2025-10-31/py_2025-10-31.py
@@ -1,1 +1,19 @@
 # Python solution for 2025-10-31
+# SpOoKy~CaSe
+def spookify(boo):
+    boo = boo.replace('_', '~').replace('-', '~')
+
+    result = []
+    count = 0
+
+    for ch in boo:
+        if ch == '~':
+            result.append('~')
+        else:
+            if count % 2 == 0:
+                result.append(ch.upper())
+            else:
+                result.append(ch.lower())
+            count += 1
+
+    return ''.join(result)
