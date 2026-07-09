b4a29343e1dce6f8b6093a077b44ec49e885f072
Quoriee <snowii.torii@gmail.com>
2026-07-09 17:22:38 +0200

    word search python solution

--- solutions/2025/11-November/2025-11-09/py_2025-11-09.py
+++ solutions/2025/11-November/2025-11-09/py_2025-11-09.py
@@ -1,1 +1,24 @@
 # Python solution for 2025-11-09
+# Word Search
+def find_word(matrix, word):
+    for r, row in enumerate(matrix):
+        row = ''.join(row)
+
+        if word in row:
+            s = row.index(word)
+            return [[r, s], [r, s + len(word) - 1]]
+
+        if word[::-1] in row:
+            s = row.index(word[::-1])
+            return [[r, s + len(word) - 1], [r, s]]
+
+    for c in range(len(matrix[0])):
+        col = ''.join(matrix[r][c] for r in range(len(matrix)))
+
+        if word in col:
+            s = col.index(word)
+            return [[s, c], [s + len(word) - 1, c]]
+
+        if word[::-1] in col:
+            s = col.index(word[::-1])
+            return [[s + len(word) - 1, c], [s, c]]
