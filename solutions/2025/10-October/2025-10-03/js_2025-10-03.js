1d8359dbb197cf5215d6c18e203b983cbdf62371
Quoriee <snowii.torii@gmail.com>
2026-07-10 01:06:08 +0200

    p@ssw0rd str3ngth! js solution

--- solutions/2025/10-October/2025-10-03/js_2025-10-03.js
+++ solutions/2025/10-October/2025-10-03/js_2025-10-03.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2025-10-03
+// P@ssw0rd Str3ngth!
+function checkStrength(password) {
+	let passed = 0;
+
+	if (password.length >= 8) passed++;
+
+	if (/[a-z]/.test(password) && /[A-Z]/.test(password)) passed++;
+
+	if (/[0-9]/.test(password)) passed++;
+
+	if (/[!@#$%^&*]/.test(password)) passed++;
+
+	if (passed < 2) return 'weak';
+	if (passed < 4) return 'medium';
+	return 'strong';
+}
