215d5bae430709956392c86cc5684dfc3e25b291
Quoriee <snowii.torii@gmail.com>
2026-07-09 16:27:26 +0200

    infected js solution

--- solutions/2025/11-November/2025-11-02/js_2025-11-02.js
+++ solutions/2025/11-November/2025-11-02/js_2025-11-02.js
@@ -1,1 +1,12 @@
 // JavaScript solution for 2025-11-02
+// Infected
+	let s = 1;
+
+	for (let day = 1; day <= days; day++) {
+		s *= 2;
+		if (day % 3 === 0) {
+			s -= Math.ceil(s * 0.2);
+		}
+	}
+
+	return s;
