9abc48b82e9d61f0dc64d3c406216d0ccf358263
Quoriee <snowii.torii@gmail.com>
2026-07-09 17:52:45 +0200

    character count python solution

--- solutions/2025/11-November/2025-11-23/py_2025-11-23.py
+++ solutions/2025/11-November/2025-11-23/py_2025-11-23.py
@@ -1,1 +1,11 @@
 # Python solution for 2025-11-23
+# Character Count
+def count_characters(sentence):
+    res = []
+    chars = set(sentence.lower())
+
+    for ch in chars:
+        if ch.isalpha():
+            res.append(f'{ch} {sentence.lower().count(ch)}')
+
+    return sorted(res, key=lambda c: c.split()[0])
