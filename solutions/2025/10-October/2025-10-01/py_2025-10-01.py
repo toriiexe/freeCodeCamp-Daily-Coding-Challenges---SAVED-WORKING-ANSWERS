2a4a777112bd4578680729ab1232bcc209f56811
Quoriee <snowii.torii@gmail.com>
2026-07-10 00:51:29 +0200

    binary to decimal python solution

--- solutions/2025/10-October/2025-10-01/py_2025-10-01.py
+++ solutions/2025/10-October/2025-10-01/py_2025-10-01.py
@@ -1,1 +1,14 @@
 # Python solution for 2025-10-01
+# Binary to Decimal
+def to_decimal(binary):
+    s = 0
+    for i, b in enumerate(binary[::-1]):
+        s += int(b) * 2**i
+    return s
+
+
+"""
+# Binary to Decimal - SHORTER VERSION
+def to_decimal(binary):
+    return int(binary, 2)
+"""
