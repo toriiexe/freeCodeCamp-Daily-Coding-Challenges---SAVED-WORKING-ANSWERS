91cdb0d7a279cda1dfc0d4fe7608d8e0850b9802
Quoriee <snowii.torii@gmail.com>
2026-07-10 00:15:41 +0200

    nth prime js solution

--- solutions/2025/10-October/2025-10-30/js_2025-10-30.js
+++ solutions/2025/10-October/2025-10-30/js_2025-10-30.js
@@ -1,1 +1,25 @@
 // JavaScript solution for 2025-10-30
+// Nth Prime
+function nthPrime(n) {
+	let primes = [];
+	let num = 2;
+
+	while (primes.length < n) {
+		let isPrime = true;
+
+		for (let i = 2; i < num; i++) {
+			if (num % i === 0) {
+				isPrime = false;
+				break;
+			}
+		}
+
+		if (isPrime) {
+			primes.push(num);
+		}
+
+		num++;
+	}
+
+	return primes[primes.length - 1];
+}
