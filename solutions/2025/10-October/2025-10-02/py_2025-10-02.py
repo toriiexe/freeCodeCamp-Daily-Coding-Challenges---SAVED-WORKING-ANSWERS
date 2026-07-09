825f28209c111ef9ae629934365924f936062d6a
Quoriee <snowii.torii@gmail.com>
2026-07-10 00:19:51 +0200

    decimal to binary python version

--- solutions/2025/10-October/2025-10-02/py_2025-10-02.py
+++ solutions/2025/10-October/2025-10-02/py_2025-10-02.py
@@ -1,1 +1,14 @@
 # Python solution for 2025-10-02
+# Decimal to Binary
+def to_binary(decimal):
+    res = ''
+    while decimal >= 1:
+        res += str(decimal % 2)
+        decimal //= 2
+    return res[::-1]
+
+"""
+# Decimal to Binary - SHORTER VERSION
+def to_binary(decimal):
+    return bin(decimal)[2:]
+"""
