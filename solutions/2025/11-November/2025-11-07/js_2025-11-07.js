58b5dd5567fcb83351732e2a098d4016c59b443b
Quoriee <snowii.torii@gmail.com>
2026-07-09 16:55:01 +0200

    counting cards js solution

--- solutions/2025/11-November/2025-11-07/js_2025-11-07.js
+++ solutions/2025/11-November/2025-11-07/js_2025-11-07.js
@@ -1,1 +1,12 @@
 // JavaScript solution for 2025-11-07
+// Counting Cards
+function factorial(n) {
+	if (n === 0 || n === 1) {
+		return 1;
+	}
+	return n * factorial(n - 1);
+}
+
+function combinations(cards) {
+	return factorial(52) / (factorial(cards) * factorial(52 - cards));
+}
