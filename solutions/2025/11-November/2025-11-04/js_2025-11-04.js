f55f6fbcf5123b5048dc8e5809df1e8f1efa90fb
Quoriee <snowii.torii@gmail.com>
2026-07-09 16:31:39 +0200

    image search js solution

--- solutions/2025/11-November/2025-11-04/js_2025-11-04.js
+++ solutions/2025/11-November/2025-11-04/js_2025-11-04.js
@@ -1,1 +1,6 @@
 // JavaScript solution for 2025-11-04
+// Image Search
+function imageSearch(images, term) {
+    term = term.toLowerCase();
+	return images.filter(image => image.toLowerCase().includes(term));
+}
