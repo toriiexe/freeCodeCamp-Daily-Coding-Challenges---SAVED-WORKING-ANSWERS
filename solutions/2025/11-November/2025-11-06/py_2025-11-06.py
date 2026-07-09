101e83518e3f0a8a968fd435e3cd1ba17655ab5f
Quoriee <snowii.torii@gmail.com>
2026-07-09 16:46:09 +0200

    weekday finder python solution

--- solutions/2025/11-November/2025-11-06/py_2025-11-06.py
+++ solutions/2025/11-November/2025-11-06/py_2025-11-06.py
@@ -1,1 +1,7 @@
 # Python solution for 2025-11-06
+# Weekday Finder
+import datetime
+def get_weekday(date_string):
+    year, month, day = map(int, date_string.split('-'))
+    date_ymd = datetime.datetime(year, month, day)
+    return date_ymd.strftime('%A')
