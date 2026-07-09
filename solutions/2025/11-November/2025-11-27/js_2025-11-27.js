37f391ffe96a68ee099d30dc4aa508d5b08a122d
Quoriee <snowii.torii@gmail.com>
2026-07-09 18:47:28 +0200

    what's my age? js solution

--- solutions/2025/11-November/2025-11-27/js_2025-11-27.js
+++ solutions/2025/11-November/2025-11-27/js_2025-11-27.js
@@ -1,1 +1,15 @@
 // JavaScript solution for 2025-11-27
+// What's my age again?
+function calculateAge(birthday) {
+    const yt = 2025, mt = 11, dt = 27;
+
+    const [y, m, d] = birthday.split('-').map(Number);
+
+    let age = yt - y;
+
+    if (m > mt || (m === mt && d > dt)) {
+        age -= 1;
+    }
+
+    return age;
+}
