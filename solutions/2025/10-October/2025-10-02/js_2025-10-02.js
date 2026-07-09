8c71f7e8724b70130d7a8fb52e6230f5bc02afd9
Quoriee <snowii.torii@gmail.com>
2026-07-10 00:34:02 +0200

    decimal to binary js solution

--- solutions/2025/10-October/2025-10-02/js_2025-10-02.js
+++ solutions/2025/10-October/2025-10-02/js_2025-10-02.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2025-10-02
+// Decimal to Binary
+function toBinary(decimal) {
+	if (decimal === 0) return '0';
+
+	let res = '';
+
+	while (decimal) {
+		res += (decimal % 2).toString();
+		decimal = Math.floor(decimal / 2);
+	}
+
+	return res.split('').reverse().join('');
+}
