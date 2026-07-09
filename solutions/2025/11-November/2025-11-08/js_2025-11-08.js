b5b1b699c748870e93022112b342ba48e6eb4994
Quoriee <snowii.torii@gmail.com>
2026-07-09 17:00:23 +0200

    character limit js solution

--- solutions/2025/11-November/2025-11-08/js_2025-11-08.js
+++ solutions/2025/11-November/2025-11-08/js_2025-11-08.js
@@ -1,1 +1,8 @@
 // JavaScript solution for 2025-11-08
+// Character Limit
+function canPost(message) {
+    const messageLength = message.length;
+    if (messageLength <= 40) return 'short post';
+    if (messageLength <= 80) return 'long post';
+    return 'invalid post';
+}
