aa0813b2d91a81dca4ae8bacaced82b2dfa50737
Quoriee <snowii.torii@gmail.com>
2026-07-09 16:12:00 +0200

    issue triage 2 js solution

--- solutions/2026/07-July/2026-07-09/js_2026-07-09.js
+++ solutions/2026/07-July/2026-07-09/js_2026-07-09.js
@@ -1,1 +1,33 @@
 // JavaScript solution for 2026-07-09
+// Issue Triage 2
+function triageIssue(title, labels) {
+	if (labels.length === 0) {
+		if (title.includes('error') || title.includes('bug')) {
+			labels.push('bug', 'needs triage');
+		} else if (title.includes('feature') || title.includes('add')) {
+			labels.push('enhancement', 'discussing');
+		}
+	} else {
+		if (labels.includes('needs triage') && (title.includes('simple') || title.includes('easy'))) {
+			labels.splice(labels.indexOf('needs triage'), 1);
+			labels.push('good first issue');
+		} else if (labels.includes('discussing') && (title.includes('planned') || title.includes('next'))) {
+			labels.splice(labels.indexOf('discussing'), 1);
+			labels.push('on the roadmap');
+		} else if (labels.includes('needs triage') || labels.includes('discussing')) {
+			if (labels.includes('needs triage')) {
+				labels.splice(labels.indexOf('needs triage'), 1);
+			}
+			if (labels.includes('discussing')) {
+				labels.splice(labels.indexOf('discussing'), 1);
+			}
+			labels.push('help wanted');
+		}
+	}
+
+	if (title.includes('security')) {
+		labels.push('critical');
+	}
+
+	return labels;
+}
