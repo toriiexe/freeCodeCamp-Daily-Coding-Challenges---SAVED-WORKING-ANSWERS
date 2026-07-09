1b7da8987e6ef847a0aacd505d3df446ac19098d
Quoriee <snowii.torii@gmail.com>
2026-07-09 17:37:45 +0200

    longest word readme

--- solutions/2025/11-November/2025-11-20/py_2025-11-20.py
+++ solutions/2025/11-November/2025-11-20/py_2025-11-20.py
@@ -1,1 +1,20 @@
 # Python solution for 2025-11-20
+# Longest Word
+def longest_word(sentence):
+    res = ''
+    length = 0
+
+    for word in sentence.split():
+        clean = ''
+        lengthTemp = 0
+
+        for w in word:
+            if w.isalpha():
+                clean += w
+                lengthTemp += 1
+
+        if lengthTemp > length:
+            res = clean
+            length = lengthTemp
+
+    return res
