01c9a16cb37926a5e529bfa581ab93072e6a9e67
Quoriee <snowii.torii@gmail.com>
2026-07-09 17:23:49 +0200

    word search js solution

--- solutions/2025/11-November/2025-11-09/js_2025-11-09.js
+++ solutions/2025/11-November/2025-11-09/js_2025-11-09.js
@@ -1,1 +1,34 @@
 // JavaScript solution for 2025-11-09
+// Word Search
+function findWord(matrix, word) {
+    for (let r = 0; r < matrix.length; r++) {
+        let row = matrix[r].join("");
+
+        if (row.includes(word)) {
+            let s = row.indexOf(word);
+            return [[r, s], [r, s + word.length - 1]];
+        }
+
+        if (row.includes([...word].reverse().join(""))) {
+            let s = row.indexOf([...word].reverse().join(""));
+            return [[r, s + word.length - 1], [r, s]];
+        }
+    }
+
+    for (let c = 0; c < matrix[0].length; c++) {
+        let col = "";
+
+        for (let r = 0; r < matrix.length; r++)
+            col += matrix[r][c];
+
+        if (col.includes(word)) {
+            let s = col.indexOf(word);
+            return [[s, c], [s + word.length - 1, c]];
+        }
+
+        if (col.includes([...word].reverse().join(""))) {
+            let s = col.indexOf([...word].reverse().join(""));
+            return [[s + word.length - 1, c], [s, c]];
+        }
+    }
+}
