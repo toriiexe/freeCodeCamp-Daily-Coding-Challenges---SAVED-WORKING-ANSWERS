f6d48da81b1e23bc946eb5cd21c393d92326332b
Quoriee <snowii.torii@gmail.com>
2026-07-09 16:37:53 +0200

    matrix builder js solution

--- solutions/2025/11-November/2025-11-05/js_2025-11-05.js
+++ solutions/2025/11-November/2025-11-05/js_2025-11-05.js
@@ -1,1 +1,13 @@
 // JavaScript solution for 2025-11-05
+// Matrix Builder
+function buildMatrix(rows, cols) {
+    let matrix = [];
+    for (let row = 0; row < rows; row++){
+        let temp = [];
+        for (let col = 0; col < cols; col++){
+        temp.push(0);
+        }
+        matrix.push(temp);
+    }
+    return matrix;
+}
