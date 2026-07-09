d4a31b1ee54cc66a34704d7ccebb87a319129705
Quoriee <snowii.torii@gmail.com>
2026-07-09 16:43:28 +0200

    weekday finder js solution

--- solutions/2025/11-November/2025-11-06/js_2025-11-06.js
+++ solutions/2025/11-November/2025-11-06/js_2025-11-06.js
@@ -1,1 +1,9 @@
 // JavaScript solution for 2025-11-06
+// Weekday Finder
+function getWeekday(dateString) {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+	const [year, month, day] = dateString.split("-").map(Number);
+
+    return days[new Date(year, month - 1, day).getDay()];
+}
