352080a4b61478dc844e32a6a737ebdbe2aa6e77
Quoriee <snowii.torii@gmail.com>
2026-07-09 18:47:17 +0200

    what's my age again? python solution

--- solutions/2025/11-November/2025-11-27/py_2025-11-27.py
+++ solutions/2025/11-November/2025-11-27/py_2025-11-27.py
@@ -1,1 +1,12 @@
 # Python solution for 2025-11-27
+# What's my age again?
+def calculate_age(birthday):
+    yt, mt, dt = 2025, 11, 27
+    y, m, d = map(int, birthday.split('-'))
+
+    age = yt - y
+
+    if (m, d) > (mt, dt):
+        age -= 1
+
+    return age
