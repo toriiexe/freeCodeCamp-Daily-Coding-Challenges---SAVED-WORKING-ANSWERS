0819c003ac9ea4a27e49c28871a9820e4ef2131a
Quoriee <snowii.torii@gmail.com>
2026-07-09 17:31:31 +0200

    vowels and consonants js solution

--- solutions/2025/11-November/2025-11-11/js_2025-11-11.js
+++ solutions/2025/11-November/2025-11-11/js_2025-11-11.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2025-11-11
+// Vowels and Consonants
+function count(str) {
+    let vowels = 0,
+        consonants = 0;
+
+        for (const ch of str.toLowerCase()){
+        if ('a' <= ch && ch <= 'z'){
+            if ('aeiou'.includes(ch)){
+            vowels++;
+            } else{
+            consonants++;
+            }
+        }
+        }
+    return [vowels, consonants];
+}
