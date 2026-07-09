f1f028a45d0a422a159410a9eaae48852cc1329f
Quoriee <snowii.torii@gmail.com>
2026-07-09 18:20:58 +0200

    gcd js solution

--- solutions/2025/11-November/2025-11-15/js_2025-11-15.js
+++ solutions/2025/11-November/2025-11-15/js_2025-11-15.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2025-11-15
+// GCD
+function gcd(x, y) {
+    const lowest = Math.min(x,y);
+    let hcf = 1;
+
+    for (let i = 1; i <= lowest; i++){
+        if (x % i == 0 && y % i == 0){
+        hcf = i;
+        }
+    }
+    
+    return hcf;
+}
