6f3ba03f4ebef6b31915dd9a878dffc419cd8b8d
Quoriee <snowii.torii@gmail.com>
2026-07-09 16:59:16 +0200

    character limit python solution

--- solutions/2025/11-November/2025-11-08/py_2025-11-08.py
+++ solutions/2025/11-November/2025-11-08/py_2025-11-08.py
@@ -1,1 +1,10 @@
 # Python solution for 2025-11-08
+# Character Limit
+def can_post(message):
+    length = len(message)
+    if length <= 40:
+        return 'short post'
+    elif length <= 80:
+        return 'long post'
+    else:
+        return 'invalid post'
