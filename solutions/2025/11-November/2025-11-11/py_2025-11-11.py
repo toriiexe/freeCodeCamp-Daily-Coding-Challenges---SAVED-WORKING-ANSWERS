1324ec91c57af6ded19b8755aa75e536afbb4128
Quoriee <snowii.torii@gmail.com>
2026-07-09 17:30:19 +0200

    vowels and consonants python solution

--- solutions/2025/11-November/2025-11-11/py_2025-11-11.py
+++ solutions/2025/11-November/2025-11-11/py_2025-11-11.py
@@ -1,1 +1,11 @@
 # Python solution for 2025-11-11
+# Vowels and Consonants
+def count(s):
+    vowels, consonants = 0, 0
+    for ch in s.lower():
+        if ch.isalpha():
+            if ch in 'aeiou':
+                vowels += 1
+            else:
+                consonants += 1
+    return [vowels, consonants]
