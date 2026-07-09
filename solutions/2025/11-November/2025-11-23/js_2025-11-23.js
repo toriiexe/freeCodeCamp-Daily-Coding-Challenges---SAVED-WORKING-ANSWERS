279aafa0d9b28befdb9cb8c65b9e811a1eafd2b9
Quoriee <snowii.torii@gmail.com>
2026-07-09 18:04:39 +0200

    character count js solution

--- solutions/2025/11-November/2025-11-23/js_2025-11-23.js
+++ solutions/2025/11-November/2025-11-23/js_2025-11-23.js
@@ -1,1 +1,22 @@
 // JavaScript solution for 2025-11-23
+// Character Count
+function countCharacters(sentence) {
+    let res = [];
+    const chars = new Set(sentence.toLowerCase());
+
+    for (const ch of chars) {
+        if ('a' <= ch && ch <= 'z') {
+        let count = 0;
+
+        for (const c of sentence.toLowerCase()) {
+            if (c === ch) {
+            count++;
+            }
+        }
+
+        res.push(`${ch} ${count}`);
+        }
+    }
+
+    return res.sort();
+}
