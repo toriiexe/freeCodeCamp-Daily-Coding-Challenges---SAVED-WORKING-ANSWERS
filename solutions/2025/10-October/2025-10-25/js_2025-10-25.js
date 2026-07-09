d7e0a9d23f034167f4371013dc952a3c9889ed7e
Quoriee <snowii.torii@gmail.com>
2026-07-09 23:49:39 +0200

    complementary dna js solution

--- solutions/2025/10-October/2025-10-25/js_2025-10-25.js
+++ solutions/2025/10-October/2025-10-25/js_2025-10-25.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2025-10-25
+// Complementary DNA
+function complementaryDNA(strand) {
+    const mapping = {
+        'A': 'T',
+        'T': 'A',
+        'C': 'G',
+        'G': 'C'
+    }
+
+    let res = [];
+    for (const key of strand){
+        res.push(mapping[key]);
+    }
+
+    return res.join('');
+}
