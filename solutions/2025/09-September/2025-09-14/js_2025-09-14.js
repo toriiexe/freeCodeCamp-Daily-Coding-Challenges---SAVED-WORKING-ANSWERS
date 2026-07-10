9a01dce6bf264fba5e4aceacb07ce2591514cccc
Quoriee <snowii.torii@gmail.com>
2026-07-10 23:37:04 +0200

    word frequency js solution

--- solutions/2025/09-September/2025-09-14/js_2025-09-14.js
+++ solutions/2025/09-September/2025-09-14/js_2025-09-14.js
@@ -1,1 +1,19 @@
 // JavaScript solution for 2025-09-14
+// Word Frequency
+function getWords(paragraph) {
+    const p = paragraph.toLowerCase().replaceAll(',', '').replaceAll('.', '').replaceAll('!', '');
+
+    const d = {};
+
+    for (const word of p.split(' ')) {
+        if (word in d) {
+            d[word] += 1;
+        } else {
+            d[word] = 1;
+        }
+    }
+
+    const desc = Object.entries(d).sort((a, b) => b[1] - a[1]);
+
+    return desc.slice(0, 3).map(([word, count]) => word);
+}
