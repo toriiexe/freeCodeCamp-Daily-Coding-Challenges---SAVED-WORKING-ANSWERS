ec5176b9789a9f163c27edb6bf1919f2bdaa1985
Quoriee <snowii.torii@gmail.com>
2026-07-10 23:39:42 +0200

    thermostat adjuster js solution

--- solutions/2025/09-September/2025-09-15/js_2025-09-15.js
+++ solutions/2025/09-September/2025-09-15/js_2025-09-15.js
@@ -1,1 +1,7 @@
 // JavaScript solution for 2025-09-15
+// Thermostat Adjuster
+function adjustThermostat(temp, target) {
+    if (temp < target) return 'heat';
+    if (temp > target) return 'cool';
+    return 'hold';
+}
