a2de37cdf9640b5114638b79d4d8bc1641f5e325
Quoriee <snowii.torii@gmail.com>
2026-07-10 15:52:20 +0200

    rgb to hex js solution

--- solutions/2025/09-September/2025-09-02/js_2025-09-02.js
+++ solutions/2025/09-September/2025-09-02/js_2025-09-02.js
@@ -1,1 +1,8 @@
 // JavaScript solution for 2025-09-02
+// RGB to Hex
+function rgbToHex(rgb) {
+    rgb = rgb.replace('rgb(', '').replace(')', '');
+    let [r, g, b] = rgb.split(', ');
+
+    return `#${parseInt(r).toString(16).padStart(2, '0')}${parseInt(g).toString(16).padStart(2, '0')}${parseInt(b).toString(16).padStart(2, '0')}`;
+}
