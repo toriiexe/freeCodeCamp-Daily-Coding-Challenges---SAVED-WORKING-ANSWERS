75883567a6ce48a5094e87335742733779674608
Quoriee <snowii.torii@gmail.com>
2026-07-10 14:48:24 +0200

    24 to 12 js solution

--- solutions/2025/10-October/2025-10-13/js_2025-10-13.js
+++ solutions/2025/10-October/2025-10-13/js_2025-10-13.js
@@ -1,1 +1,21 @@
 // JavaScript solution for 2025-10-13
+// 24 to 12
+function to12(time) {
+    let h = Number(time.slice(0, 2));
+    let m = Number(time.slice(2, 4));
+    let period = '';
+
+    if (h === 0) {
+        h = 12;
+        period = 'AM';
+    } else if (h < 12) {
+        period = 'AM';
+    } else if (h === 12) {
+        period = 'PM';
+    } else {
+        h -= 12;
+        period = 'PM';
+    }
+
+    return `${h}:${String(m).padStart(2, '0')} ${period}`;
+}
