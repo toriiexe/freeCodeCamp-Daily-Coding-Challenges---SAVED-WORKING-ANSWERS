6ace81a9c35e6641d9d20449a2d55ab0f475fefd
Quoriee <snowii.torii@gmail.com>
2026-07-10 23:44:08 +0200

    sentence capitalizer js solution

--- solutions/2025/09-September/2025-09-16/js_2025-09-16.js
+++ solutions/2025/09-September/2025-09-16/js_2025-09-16.js
@@ -1,1 +1,21 @@
 // JavaScript solution for 2025-09-16
+// Sentence Capitalizer
+function capitalize(paragraph) {
+    let res = '';
+    let bigLetter = true;
+
+    for (const c of paragraph) {
+        if (bigLetter && /[a-z]/i.test(c)) {
+            res += c.toUpperCase();
+            bigLetter = false;
+        } else {
+            res += c;
+        }
+
+        if ('.?!'.includes(c)) {
+            bigLetter = true;
+        }
+    }
+
+    return res;
+}
