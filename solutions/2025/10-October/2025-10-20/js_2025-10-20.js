cb6294e10ebdc27ce16fddaab053a0bdd4f09d59
Quoriee <snowii.torii@gmail.com>
2026-07-10 15:16:32 +0200

    tip calculator js solution

--- solutions/2025/10-October/2025-10-20/js_2025-10-20.js
+++ solutions/2025/10-October/2025-10-20/js_2025-10-20.js
@@ -1,1 +1,8 @@
 // JavaScript solution for 2025-10-20
+// Tip Calculator
+function calculateTips(mealPrice, customTip) {
+    const price = Number(mealPrice.slice(1));
+    const custTip = Number(customTip.slice(0, -1)) / 100;
+
+    return [`\$${(price * 0.15).toFixed(2)}`, `\$${(price * 0.2).toFixed(2)}`, `\$${(price * custTip).toFixed(2)}`];
+}
