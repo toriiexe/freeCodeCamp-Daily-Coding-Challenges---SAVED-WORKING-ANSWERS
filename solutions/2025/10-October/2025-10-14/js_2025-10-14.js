793ef174508a1bdc048a83f244ba1c30662f5d54
Quoriee <snowii.torii@gmail.com>
2026-07-10 14:53:54 +0200

    string count js solution

--- solutions/2025/10-October/2025-10-14/js_2025-10-14.js
+++ solutions/2025/10-October/2025-10-14/js_2025-10-14.js
@@ -1,1 +1,18 @@
 // JavaScript solution for 2025-10-14
+// String Count
+function count(text, pattern) {
+    let count = 0;
+
+    const textLen = text.length;
+    const patternLen = pattern.length;
+
+    if (textLen >= patternLen) {
+        for (let i = 0; i <= textLen - patternLen; i++) {
+        if (text.slice(i, i + patternLen) === pattern) {
+            count++;
+        }
+        }
+    }
+
+    return count;
+}
