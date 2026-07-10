845bdd25ddb25e3adf37675ec9bc40008a2b261b
Quoriee <snowii.torii@gmail.com>
2026-07-10 15:38:30 +0200

    tribonacci sequence python solution

--- solutions/2025/09-September/2025-09-01/py_2025-09-01.py
+++ solutions/2025/09-September/2025-09-01/py_2025-09-01.py
@@ -1,1 +1,7 @@
 # Python solution for 2025-09-01
+# Tribonacci Sequence
+def tribonacci_sequence(start_sequence, length):
+    res = start_sequence[:length]
+    for i in range(length - 3): 
+        res.append(sum(res[-3:]))
+    return res
