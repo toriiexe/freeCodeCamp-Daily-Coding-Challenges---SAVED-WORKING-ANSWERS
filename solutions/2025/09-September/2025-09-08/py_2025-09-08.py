3264bdcc13041c6be1744642a41f2d191b5b3beb
Quoriee <snowii.torii@gmail.com>
2026-07-10 22:39:55 +0200

    acronym builder python solution

--- solutions/2025/09-September/2025-09-08/py_2025-09-08.py
+++ solutions/2025/09-September/2025-09-08/py_2025-09-08.py
@@ -1,1 +1,11 @@
 # Python solution for 2025-09-08
+# Acronym Builder
+def build_acronym(s):
+    forbidden = {"a", "for", "an", "and", "by", "of"}
+
+    acronym = ""
+    for i, word in enumerate(s.split()):
+        if i == 0 or word.lower() not in forbidden:
+            acronym += word[0].upper()
+
+    return acronym
