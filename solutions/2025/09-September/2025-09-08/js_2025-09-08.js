14e61c5c6703def6e2d05aa224c2111b312fc1a4
Quoriee <snowii.torii@gmail.com>
2026-07-10 22:44:48 +0200

    acronym builder js solution

--- solutions/2025/09-September/2025-09-08/js_2025-09-08.js
+++ solutions/2025/09-September/2025-09-08/js_2025-09-08.js
@@ -1,1 +1,15 @@
 // JavaScript solution for 2025-09-08
+// Acronym Builder
+function buildAcronym(str) {
+    const forbidden = new Set(["a", "for", "an", "and", "by", "of"]);
+
+    let acronym = "";
+
+    for (const [i, word] of str.split(' ').entries()) {
+        if (i === 0 || !forbidden.has(word.toLowerCase())) {
+            acronym += word[0].toUpperCase();
+        }
+    }
+
+    return acronym;
+}
