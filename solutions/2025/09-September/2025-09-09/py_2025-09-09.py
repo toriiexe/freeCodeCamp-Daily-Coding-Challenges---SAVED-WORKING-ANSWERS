507c34d3e833ce5bb4ed4300c75f27b9ace99a7e
Quoriee <snowii.torii@gmail.com>
2026-07-10 22:48:27 +0200

    unique characters python solution

--- solutions/2025/09-September/2025-09-09/py_2025-09-09.py
+++ solutions/2025/09-September/2025-09-09/py_2025-09-09.py
@@ -1,1 +1,10 @@
 # Python solution for 2025-09-09
+# Unique Characters
+def all_unique(s):
+    chars = []
+    for c in s:
+        if c in chars:
+            return False
+        else:
+            chars.append(c)
+    return True
