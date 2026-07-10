bd9eeb4154311d787cd09adec6f3b14df76c21cb
Quoriee <snowii.torii@gmail.com>
2026-07-10 16:19:34 +0200

    vowel repeater python solution

--- solutions/2025/09-September/2025-09-04/py_2025-09-04.py
+++ solutions/2025/09-September/2025-09-04/py_2025-09-04.py
@@ -1,1 +1,12 @@
 # Python solution for 2025-09-04
+# Vowel Repeater
+def repeat_vowels(s):
+    res = ''
+    vowels_to_repeat = 0
+    for c in s:
+        if c in 'AEIOUaeiou':
+            res += c + c.lower() * vowels_to_repeat
+            vowels_to_repeat += 1
+        else:
+            res += c
+    return res
