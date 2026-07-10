32fbe1ec08be5fcbeffd9cb3732df06facd02680
Quoriee <snowii.torii@gmail.com>
2026-07-11 01:40:36 +0200

    phone number formatter js solution

--- solutions/2025/09-September/2025-09-30/js_2025-09-30.js
+++ solutions/2025/09-September/2025-09-30/js_2025-09-30.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2025-09-30
+// Phone Number Formatter
+function formatNumber(number) {
+    /*
+    if (!/^\d{11}$/.test(number)) {
+            return "Invalid number";
+        }
+    */
+   
+    return `+${number[0]} (${number.slice(1,4)}) ${number.slice(4,7)}-${number.slice(7)}`;
+}
