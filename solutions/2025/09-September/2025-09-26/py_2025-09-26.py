ba115a530c7d87b2a0b4ba7ffa797710f12262de
Quoriee <snowii.torii@gmail.com>
2026-07-11 00:07:31 +0200

    caught speeding python solution

--- solutions/2025/09-September/2025-09-26/py_2025-09-26.py
+++ solutions/2025/09-September/2025-09-26/py_2025-09-26.py
@@ -1,1 +1,15 @@
 # Python solution for 2025-09-26
+# Caught Speeding
+def speeding(speeds, limit):
+    speeding_amount = 0
+    beyond_speed_limit_total = 0
+
+    for speed in speeds:
+        if speed > limit:
+            speeding_amount += 1
+            beyond_speed_limit_total += speed - limit
+
+    if speeding_amount == 0:
+        return [0, 0]
+
+    return [speeding_amount, beyond_speed_limit_total / speeding_amount]
