d0ac7b57abc8953c678220a735166f4aa9036e90
Quoriee <snowii.torii@gmail.com>
2026-07-10 15:06:59 +0200

    duration formatter python solution

--- solutions/2025/10-October/2025-10-26/py_2025-10-26.py
+++ solutions/2025/10-October/2025-10-26/py_2025-10-26.py
@@ -1,1 +1,13 @@
 # Python solution for 2025-10-26
+# Duration Formatter
+def format(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    secs = seconds % 60
+
+    if hours > 0:
+        return f'{hours}:{minutes:02d}:{secs:02d}'
+    elif minutes > 0:
+        return f'{minutes}:{secs:02d}'
+    else:
+        return f'0:{secs:02d}'
