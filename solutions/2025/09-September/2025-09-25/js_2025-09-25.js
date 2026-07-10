8bc4015eb98fd925a8f6db8349ec2e16bf6626d3
Quoriee <snowii.torii@gmail.com>
2026-07-11 00:16:16 +0200

    2nd largest js solution

--- solutions/2025/09-September/2025-09-25/js_2025-09-25.js
+++ solutions/2025/09-September/2025-09-25/js_2025-09-25.js
@@ -1,5 +1,24 @@
 // JavaScript solution for 2025-09-25
 // 2nd Largest
 function secondLargest(arr) {
+    let largest = -Infinity;
+    let second = -Infinity;
+
+    for (const num of new Set(arr)) {
+        if (num > largest) {
+            second = largest;
+            largest = num;
+        } else if (num > second) {
+            second = num;
+        }
+    }
+
+    return second;
+}
+
+/*
+// 2nd Largest
+function secondLargest(arr) {
     return [...new Set(arr)].sort((a, b) => b - a)[1];
 }
+*/
