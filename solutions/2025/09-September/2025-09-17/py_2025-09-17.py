d89b2da2d099599bf8726c4ad3ecedd5ba0d0ca7
Quoriee <snowii.torii@gmail.com>
2026-07-10 23:51:12 +0200

    slug generator python solution

--- solutions/2025/09-September/2025-09-17/py_2025-09-17.py
+++ solutions/2025/09-September/2025-09-17/py_2025-09-17.py
@@ -1,1 +1,13 @@
 # Python solution for 2025-09-17
+# Slug Generator
+def generate_slug(s):
+    s = ' '.join(s.split())
+    res = ''
+
+    for c in s:
+        if c.isalnum():
+            res += c.lower()
+        elif c == ' ':
+            res += '%20'
+
+    return res
