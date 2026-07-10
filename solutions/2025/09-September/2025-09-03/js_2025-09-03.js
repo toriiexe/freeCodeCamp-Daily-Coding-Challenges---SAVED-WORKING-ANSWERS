4961463eda42c38b0b9ea7992064e5368f3e1ccb
Quoriee <snowii.torii@gmail.com>
2026-07-10 16:07:35 +0200

    pangram js solution

--- solutions/2025/09-September/2025-09-03/js_2025-09-03.js
+++ solutions/2025/09-September/2025-09-03/js_2025-09-03.js
@@ -1,1 +1,19 @@
 // JavaScript solution for 2025-09-03
+// Pangram
+function isPangram(sentence, letters) {
+    let sentenceSet = new Set([...sentence.toLowerCase()].filter(c => c.match(/[a-z]/)));
+
+    let lettersSet = new Set(letters.toLowerCase());
+
+    if (sentenceSet.size !== lettersSet.size) {
+        return false;
+    }
+
+    for (let letter of lettersSet) {
+        if (!sentenceSet.has(letter)) {
+        return false;
+        }
+    }
+
+    return true;
+}
