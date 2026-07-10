404398175370b714d92fb7cafe8a06d9b745e3a8
Quoriee <snowii.torii@gmail.com>
2026-07-11 01:52:28 +0200

    spam detector python solution

--- solutions/2025/09-September/2025-09-27/py_2025-09-27.py
+++ solutions/2025/09-September/2025-09-27/py_2025-09-27.py
@@ -1,1 +1,23 @@
 # Python solution for 2025-09-27
+# Spam Detector
+def is_spam(number):
+    n = number.split(' ')
+    a, bbb, ccc, dddd = n[0][1:], n[1][1:4], n[2][:3], n[2][4:]
+
+    if len(a) > 2 or not a.startswith('0'):
+        return True
+
+    b = int(bbb)
+    if b < 200 or b > 900:
+        return True
+
+    c = sum(int(x) for x in ccc)
+    if str(c) in dddd:
+        return True
+
+    nums = a + bbb + ccc + dddd
+    for i in range(len(nums) - 3):
+        if len(set(nums[i:i+4])) == 1:
+            return True
+    
+    return False
