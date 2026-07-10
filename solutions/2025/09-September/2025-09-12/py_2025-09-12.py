b13ca316d87c31fca0cf6d7de594bfc5b44289e2
Quoriee <snowii.torii@gmail.com>
2026-07-10 23:13:50 +0200

    screen time python solution

--- solutions/2025/09-September/2025-09-12/py_2025-09-12.py
+++ solutions/2025/09-September/2025-09-12/py_2025-09-12.py
@@ -1,1 +1,15 @@
 # Python solution for 2025-09-12
+# Screen Time
+def too_much_screen_time(hours):
+    for hour in hours:
+        if hour >= 10:
+            return True
+
+    for i in range(len(hours) - 2):
+        if (hours[i] + hours[i+1] + hours[i+2]) / 3 >= 8:
+            return True
+
+    if sum(hours) / len(hours) >= 6:
+        return True
+
+    return False
