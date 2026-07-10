60a7d16e6cd119d90d8a7667ffc0714e908542ae
Quoriee <snowii.torii@gmail.com>
2026-07-10 22:32:58 +0200

    roman numeral parser js solution

--- solutions/2025/09-September/2025-09-07/js_2025-09-07.js
+++ solutions/2025/09-September/2025-09-07/js_2025-09-07.js
@@ -1,1 +1,30 @@
 // JavaScript solution for 2025-09-07
+// Roman Numeral Parser
+function parseRomanNumeral(numeral) {
+    const dict = new Map([
+        ['I', 1],
+        ['V', 5],
+        ['X', 10],
+        ['L', 50],
+        ['C', 100],
+        ['D', 500],
+        ['M', 1000]
+    ]);
+
+    let last = 0;
+    let total = 0;
+
+    for (const c of numeral.split('').reverse()) {
+        const value = dict.get(c);
+
+        if (value < last) {
+            total -= value;
+        } else {
+            total += value;
+        }
+
+        last = value;
+    }
+
+    return total;
+}
