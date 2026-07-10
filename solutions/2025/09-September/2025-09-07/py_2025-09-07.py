8a2ba1eb619985a497e3f19c4f60ad462fba5d90
Quoriee <snowii.torii@gmail.com>
2026-07-10 22:30:08 +0200

    roman numeral parser python solution

--- solutions/2025/09-September/2025-09-07/py_2025-09-07.py
+++ solutions/2025/09-September/2025-09-07/py_2025-09-07.py
@@ -1,1 +1,23 @@
 # Python solution for 2025-09-07
+# Roman Numeral Parser
+def parse_roman_numeral(numeral):
+    dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    last, total = 0, 0
+    for c in list(numeral)[::-1]:
+        if last == 0:
+            total += dict[c]
+        elif last > dict[c]:
+            total -= dict[c]
+        else:
+            total += dict[c]
+        last = dict[c]
+    return total
