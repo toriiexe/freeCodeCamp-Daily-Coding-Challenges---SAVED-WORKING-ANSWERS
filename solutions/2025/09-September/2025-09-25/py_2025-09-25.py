fedd8d2dc6148d3ff9cd41af56a3bb73b246dee4
Quoriee <snowii.torii@gmail.com>
2026-07-11 00:14:35 +0200

    2nd largest python solution

--- solutions/2025/09-September/2025-09-25/py_2025-09-25.py
+++ solutions/2025/09-September/2025-09-25/py_2025-09-25.py
@@ -1,4 +1,20 @@
 # Python solution for 2025-09-25
 # 2nd Largest
 def second_largest(arr):
+    first_largest_num = float('-inf')
+    second_largest_num = float('-inf')
+
+    for n in arr:
+        if n > first_largest_num:
+            second_largest_num = first_largest_num
+            first_largest_num = n
+        elif n > second_largest_num and n != first_largest_num:
+            second_largest_num = n
+
+    return second_largest_num
+
+"""
+# 2nd Largest - SHORTER VERSION
+def second_largest(arr):
     return sorted(list(set(arr)), reverse=True)[1]
+"""
