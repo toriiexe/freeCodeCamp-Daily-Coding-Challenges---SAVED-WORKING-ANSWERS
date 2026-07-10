843405d6b36cd210ddfac41ef5a01bc6bc1ba19c
Quoriee <snowii.torii@gmail.com>
2026-07-10 14:10:21 +0200

    favorite songs js solution

--- solutions/2025/10-October/2025-10-23/js_2025-10-23.js
+++ solutions/2025/10-October/2025-10-23/js_2025-10-23.js
@@ -1,1 +1,6 @@
 // JavaScript solution for 2025-10-23
+// Favorite Songs
+function favoriteSongs(playlist) {
+    const desc = playlist.sort((a, b) => b.plays - a.plays);
+    return [desc[0].title, desc[1].title];
+}
