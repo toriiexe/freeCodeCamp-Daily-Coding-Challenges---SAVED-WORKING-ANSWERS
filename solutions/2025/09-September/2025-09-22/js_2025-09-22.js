62e8eae126e29be6a39c9915c864a11ab09e9f0c
Quoriee <snowii.torii@gmail.com>
2026-07-11 00:28:32 +0200

    digits vs letters js solution

--- solutions/2025/09-September/2025-09-22/js_2025-09-22.js
+++ solutions/2025/09-September/2025-09-22/js_2025-09-22.js
@@ -1,1 +1,16 @@
 // JavaScript solution for 2025-09-22
+// Digits vs Letters
+function digitsOrLetters(str) {
+    let digitsCount = 0;
+    let lettersCount = 0;
+
+    for (const c of str) {
+        if (/\d/.test(c)) {
+            digitsCount++;
+        } else if (/[a-z]/i.test(c)) {
+            lettersCount++;
+        }
+    }
+
+    return digitsCount > lettersCount ? 'digits' : digitsCount < lettersCount ? 'letters' : 'tie';
+}
