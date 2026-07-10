3e3881a4d280ee8a09d75a2ff50fa868fcd3af4f
Quoriee <snowii.torii@gmail.com>
2026-07-11 01:02:48 +0200

    perfect square python solution

--- solutions/2025/09-September/2025-09-24/py_2025-09-24.py
+++ solutions/2025/09-September/2025-09-24/py_2025-09-24.py
@@ -1,1 +1,9 @@
 # Python solution for 2025-09-24
+# Perfect Square
+import math
+def is_perfect_square(n):
+    if n < 0:
+        return False
+        
+    root = math.isqrt(n)
+    return root * root == n
