aaefdb524030e2d911c8b67ae09c708998758fbb
Quoriee <snowii.torii@gmail.com>
2026-07-10 23:51:05 +0200

    slug generator js solution

--- solutions/2025/09-September/2025-09-17/js_2025-09-17.js
+++ solutions/2025/09-September/2025-09-17/js_2025-09-17.js
@@ -1,1 +1,16 @@
 // JavaScript solution for 2025-09-17
+// Slug Generator
+function generateSlug(str) {
+    const s = str.trim().replace(/\s+/g, ' ');
+    let res = '';
+
+    for (const c of s) {
+        if (('0' <= c && c <= '9') || /[a-z]/i.test(c)) {
+            res += c.toLowerCase();
+        } else if (c === ' ') {
+            res += '%20';
+        }
+    }
+
+    return res;
+}
