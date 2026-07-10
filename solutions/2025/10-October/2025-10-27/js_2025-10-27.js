5be0023b062552eca651670a37105d726003bf5f
Quoriee <snowii.torii@gmail.com>
2026-07-10 14:13:06 +0200

    integer sequence js solution

--- solutions/2025/10-October/2025-10-27/js_2025-10-27.js
+++ solutions/2025/10-October/2025-10-27/js_2025-10-27.js
@@ -1,1 +1,9 @@
 // JavaScript solution for 2025-10-27
+// Integer Sequence
+function sequence(n) {
+    let res = '';
+    for (let i = 1; i <= n; i++){
+        res += i.toString();
+    }
+    return res;
+}
