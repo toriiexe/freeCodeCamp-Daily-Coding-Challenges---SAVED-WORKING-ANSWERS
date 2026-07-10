df5c81c1bca5e096c1e980108cafffb55a2ea07b
Quoriee <snowii.torii@gmail.com>
2026-07-10 15:28:44 +0200

    hidden treasure python solution

--- solutions/2025/10-October/2025-10-24/py_2025-10-24.py
+++ solutions/2025/10-October/2025-10-24/py_2025-10-24.py
@@ -1,1 +1,21 @@
 # Python solution for 2025-10-24
+# Hidden Treasure
+def dive(map, coordinates):
+    row, col = coordinates
+    
+    if map[row][col] == '-':
+        return 'Empty'
+    
+    o_counter = 0
+    for r in range(len(map)):
+        for c in range(len(map[r])):
+            if map[r][c] == 'O':
+                o_counter += 1
+    
+    if map[row][col] == 'O':
+        if o_counter == 1:
+            return 'Recovered'
+        else:
+            return 'Found'
+    
+    return 'Found'
