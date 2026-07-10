b305c2d4ef41dd8f16672c29d8b74220a7d75ead
Quoriee <snowii.torii@gmail.com>
2026-07-10 23:21:33 +0200

    screen time js solution

--- solutions/2025/09-September/2025-09-12/js_2025-09-12.js
+++ solutions/2025/09-September/2025-09-12/js_2025-09-12.js
@@ -1,1 +1,21 @@
 // JavaScript solution for 2025-09-12
+// Screen Time
+function tooMuchScreenTime(hours) {
+    for (const hour of hours) {
+        if (hour >= 10) {
+            return true;
+        }
+    }
+
+    for (let i = 0; i < hours.length - 2; i++) {
+        if ((hours[i] + hours[i + 1] + hours[i + 2]) / 3 >= 8) {
+            return true;
+        }
+    }
+
+    if (hours.reduce((sum, hour) => sum + hour, 0) / hours.length >= 6) {
+        return true;
+    }
+
+    return false;
+}
