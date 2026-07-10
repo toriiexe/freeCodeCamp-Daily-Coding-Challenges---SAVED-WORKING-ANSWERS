c8840aa6035431c3e004d82cbb71f99b0fa4b20e
Quoriee <snowii.torii@gmail.com>
2026-07-10 16:27:14 +0200

    ipv4 validator python solution

--- solutions/2025/09-September/2025-09-05/py_2025-09-05.py
+++ solutions/2025/09-September/2025-09-05/py_2025-09-05.py
@@ -1,1 +1,16 @@
 # Python solution for 2025-09-05
+# IPv4 Validator
+def is_valid_ipv4(ipv4):
+    parts = ipv4.split('.')
+
+    if len(parts) != 4:
+        return False
+
+    for part in parts:
+        if not part.isdigit() or (part != '0' and part.startswith('0')):
+            return False
+
+        if not 0 <= int(part) <= 255:
+            return False
+
+    return True
