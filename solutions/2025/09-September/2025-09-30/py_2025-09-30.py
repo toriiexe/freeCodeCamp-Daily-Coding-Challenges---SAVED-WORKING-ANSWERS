0107c1e2d49bd500c5fd07ba16c41ba02e24a2f1
Quoriee <snowii.torii@gmail.com>
2026-07-11 01:42:00 +0200

    phone number formatter python solution

--- solutions/2025/09-September/2025-09-30/py_2025-09-30.py
+++ solutions/2025/09-September/2025-09-30/py_2025-09-30.py
@@ -1,4 +1,9 @@
 # Python solution for 2025-09-30
 # Phone Number Formatter
 def format_number(number):
+    """
+    if len(number) != 11 or not number.isdigit():
+        return 'Invalid number'
+    """
+    
     return f'+{number[0]} ({number[1:4]}) {number[4:7]}-{number[7:]}'
