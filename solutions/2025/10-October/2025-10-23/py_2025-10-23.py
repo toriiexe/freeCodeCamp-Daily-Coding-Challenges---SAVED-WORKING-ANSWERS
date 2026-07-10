f96039d3e87292a3c484829f3feae45517b85fb2
Quoriee <snowii.torii@gmail.com>
2026-07-10 14:05:02 +0200

    favorite songs python solution

--- solutions/2025/10-October/2025-10-23/py_2025-10-23.py
+++ solutions/2025/10-October/2025-10-23/py_2025-10-23.py
@@ -1,1 +1,5 @@
 # Python solution for 2025-10-23
+# Favorite Songs
+def favorite_songs(playlist):
+    desc = sorted(playlist, key=lambda item: item['plays'], reverse=True)
+    return [desc[0]['title'], desc[1]['title']]
