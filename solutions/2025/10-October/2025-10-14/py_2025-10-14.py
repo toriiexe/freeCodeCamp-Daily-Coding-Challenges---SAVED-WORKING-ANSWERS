35d02c4808b339cb1f5fad192e4ebe6b1ecafc39
Quoriee <snowii.torii@gmail.com>
2026-07-10 14:51:44 +0200

    string count python solution

--- solutions/2025/10-October/2025-10-14/py_2025-10-14.py
+++ solutions/2025/10-October/2025-10-14/py_2025-10-14.py
@@ -1,1 +1,14 @@
 # Python solution for 2025-10-14
+# String Count
+def count(text, parameter):
+    count = 0
+
+    text_len = len(text)
+    par_len = len(parameter)
+
+    if text_len >= par_len:
+        for i in range(text_len - par_len + 1):
+            if text[i:i + par_len] == parameter:
+                count += 1
+
+    return count
