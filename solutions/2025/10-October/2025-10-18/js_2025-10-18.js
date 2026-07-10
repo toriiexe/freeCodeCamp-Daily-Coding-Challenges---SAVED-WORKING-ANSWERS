9667d4d4703f621719e18540289f433ccc254e02
Quoriee <snowii.torii@gmail.com>
2026-07-10 13:56:29 +0200

    missing socks js solution

--- solutions/2025/10-October/2025-10-18/js_2025-10-18.js
+++ solutions/2025/10-October/2025-10-18/js_2025-10-18.js
@@ -1,1 +1,27 @@
 // JavaScript solution for 2025-10-18
+// Missing Socks
+function sockPairs(pairs, cycles) {
+    let single = pairs * 2;
+
+    for (let cycle = 1; cycle <= cycles; cycle++) {
+        if (cycle % 2 === 0) {
+            single--;
+        }
+
+        if (cycle % 3 === 0) {
+            single++;
+        }
+
+        if (cycle % 5 === 0) {
+            single--;
+        }
+
+        if (cycle % 10 === 0) {
+            single += 2;
+        }
+
+        single = Math.max(0, single);
+    }
+
+    return Math.floor(single / 2);
+}
