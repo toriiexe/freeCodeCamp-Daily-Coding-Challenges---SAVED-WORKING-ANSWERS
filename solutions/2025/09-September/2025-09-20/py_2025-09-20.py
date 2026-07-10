cd59097b2101ea2c7d2ba45c60bf8674a955dd2c
Quoriee <snowii.torii@gmail.com>
2026-07-11 00:01:26 +0200

    file storage python solution

--- solutions/2025/09-September/2025-09-20/py_2025-09-20.py
+++ solutions/2025/09-September/2025-09-20/py_2025-09-20.py
@@ -1,1 +1,11 @@
 # Python solution for 2025-09-20
+# File Storage
+def number_of_files(file_size, file_unit, drive_size_gb):
+    conversion = {
+        'B': 1,
+        'KB': 1_000,
+        'MB': 1_000_000,
+        'GB': 1_000_000_000
+    }
+
+    return (drive_size_gb * conversion['GB']) // (file_size * conversion[file_unit])
