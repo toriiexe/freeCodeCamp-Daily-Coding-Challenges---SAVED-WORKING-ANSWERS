91cd27a4d40a2988902e50d6edceb672610e12dc
Quoriee <snowii.torii@gmail.com>
2026-07-10 17:59:12 +0200

    matrix rotate python solution

--- solutions/2025/09-September/2025-09-06/py_2025-09-06.py
+++ solutions/2025/09-September/2025-09-06/py_2025-09-06.py
@@ -1,1 +1,13 @@
 # Python solution for 2025-09-06
+# Matrix Rotate
+def rotate(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    res = [[0 for _ in range(rows)] for _ in range(cols)]
+
+    for r in range(rows):
+        for c in range(cols):
+            res[c][rows - 1 - r] = matrix[r][c]
+
+    return res
