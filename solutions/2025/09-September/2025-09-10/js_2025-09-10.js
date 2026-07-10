7b8b13d139e17c0c355eabf8acc203b620a80536
Quoriee <snowii.torii@gmail.com>
2026-07-10 23:03:27 +0200

    array diff js solution

--- solutions/2025/09-September/2025-09-10/js_2025-09-10.js
+++ solutions/2025/09-September/2025-09-10/js_2025-09-10.js
@@ -1,1 +1,40 @@
 // JavaScript solution for 2025-09-10
+// Array Diff
+function arrayDiff(arr1, arr2) {
+    let res = [];
+
+    for (const x of arr1) {
+        if (!arr2.includes(x) && !res.includes(x)) {
+            res.push(x);
+        }
+    }
+
+    for (const y of arr2) {
+        if (!arr1.includes(y) && !res.includes(y)) {
+            res.push(y);
+        }
+    }
+
+    return res.sort();
+}
+
+
+/*
+// Array Diff - SETS
+function arrayDiff(arr1, arr2) {
+    const set1 = new Set(arr1);
+    const set2 = new Set(arr2);
+
+    const res = [];
+
+    for (const x of set1) {
+        if (!set2.has(x)) res.push(x);
+    }
+
+    for (const y of set2) {
+        if (!set1.has(y)) res.push(y);
+    }
+
+    return res.sort();
+}
+*/
