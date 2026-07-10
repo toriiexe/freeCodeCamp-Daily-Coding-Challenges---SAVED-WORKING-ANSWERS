f233f247b1dd11de6b3e1bb408024eb7d37bd365
Quoriee <snowii.torii@gmail.com>
2026-07-11 00:19:10 +0200

    longest word python solution

--- solutions/2025/09-September/2025-09-29/py_2025-09-29.py
+++ solutions/2025/09-September/2025-09-29/py_2025-09-29.py
@@ -1,1 +1,18 @@
 # Python solution for 2025-09-29
+# Longest Word
+def get_longest_word(sentence):
+    s = sentence.replace('.', '')
+    the_longest_word = ''
+
+    for word in s.split():
+        if len(word) > len(the_longest_word):
+            the_longest_word = word
+
+    return the_longest_word
+
+
+"""
+# Longest Word - SHORTER VERSION
+# def get_longest_word(sentence):
+    return max(sentence.replace('.', '').split(), key=len) 
+"""
