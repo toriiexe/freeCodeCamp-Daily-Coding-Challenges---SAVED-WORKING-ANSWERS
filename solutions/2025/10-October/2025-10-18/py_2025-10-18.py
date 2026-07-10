b3fe130d0726ad725dbb4538e78a3db2a7b45156
Quoriee <snowii.torii@gmail.com>
2026-07-10 13:54:27 +0200

    missing socks python solution

--- solutions/2025/10-October/2025-10-18/py_2025-10-18.py
+++ solutions/2025/10-October/2025-10-18/py_2025-10-18.py
@@ -1,1 +1,21 @@
 # Python solution for 2025-10-18
+# Missing Socks
+def sock_pairs(pairs, cycles):
+    single = pairs * 2
+
+    for cycle in range(1, cycles + 1):
+        if cycle % 2 == 0:
+            single -= 1
+
+        if cycle % 3 == 0:
+            single += 1
+
+        if cycle % 5 == 0:
+            single -= 1
+
+        if cycle % 10 == 0:
+            single += 2
+
+        single = max(0, single)
+
+    return single // 2
