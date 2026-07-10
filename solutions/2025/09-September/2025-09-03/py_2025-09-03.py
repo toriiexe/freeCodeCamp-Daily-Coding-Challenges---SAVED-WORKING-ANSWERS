eff295aac8bb800a1f50a5af2a4f74c736d38bb1
Quoriee <snowii.torii@gmail.com>
2026-07-10 16:00:45 +0200

    pangram python solution

--- solutions/2025/09-September/2025-09-03/py_2025-09-03.py
+++ solutions/2025/09-September/2025-09-03/py_2025-09-03.py
@@ -1,1 +1,7 @@
 # Python solution for 2025-09-03
+# Pangram
+def is_pangram(sentence, letters):
+    s = ''.join([c for c in sentence if c.isalpha()])
+    sentence_set = set(s.lower())
+    letters_set = set(letters.lower())
+    return sentence_set == letters_set
