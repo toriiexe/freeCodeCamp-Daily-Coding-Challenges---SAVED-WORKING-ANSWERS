47fe3b16a9cd1e18bbf09b3439bc65a92c0f1a0f
Quoriee <snowii.torii@gmail.com>
2026-07-10 14:43:28 +0200

    24 to 12 py solution

--- solutions/2025/10-October/2025-10-13/py_2025-10-13.py
+++ solutions/2025/10-October/2025-10-13/py_2025-10-13.py
@@ -1,1 +1,17 @@
 # Python solution for 2025-10-13
+# 24 to 12
+def to_12(time):
+    h, m = int(time[:2]), int(time[2:])
+
+    if h == 0:
+        h = 12
+        period = 'AM'
+    elif h < 12:
+        period = 'AM'
+    elif h == 12:
+        period = 'PM'
+    else:
+        h -= 12
+        period = 'PM'
+
+    return f'{h}:{m:02d} {period}'
