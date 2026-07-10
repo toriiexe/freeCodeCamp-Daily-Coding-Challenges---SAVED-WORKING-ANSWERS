30ce2dc966e853737bd2bc8e6812923f50a9848d
Quoriee <snowii.torii@gmail.com>
2026-07-10 15:42:30 +0200

    tribonacci sequence js solution

--- solutions/2025/09-September/2025-09-01/js_2025-09-01.js
+++ solutions/2025/09-September/2025-09-01/js_2025-09-01.js
@@ -1,1 +1,16 @@
 // JavaScript solution for 2025-09-01
+// Tribonacci Sequence
+function tribonacciSequence(startSequence, length) {
+    if (length === 0) {
+        return [];
+    }
+
+    let res = startSequence.slice(0, length);
+
+    while (res.length < length) {
+        const next = res[res.length - 1] + res[res.length - 2] + res[res.length - 3];
+        res.push(next);
+    }
+
+    return res;
+}
