3532b33b5ce8338a933d9c761f7e7e9e019dbcbe
Quoriee <snowii.torii@gmail.com>
2026-07-11 01:16:24 +0200

    string mirror js solution

--- solutions/2025/09-September/2025-09-23/js_2025-09-23.js
+++ solutions/2025/09-September/2025-09-23/js_2025-09-23.js
@@ -1,1 +1,20 @@
 // JavaScript solution for 2025-09-23
+// String Mirror
+function isMirror(str1, str2) {
+    let clean1 = '';
+    let clean2 = '';
+
+    for (const c of str1) {
+        if (/[a-zA-Z]/.test(c)) {
+            clean1 += c;
+        }
+    }
+
+    for (const c of str2) {
+        if (/[a-zA-Z]/.test(c)) {
+            clean2 += c;
+        }
+    }
+
+    return clean1 === [...clean2].reverse().join('');
+}
