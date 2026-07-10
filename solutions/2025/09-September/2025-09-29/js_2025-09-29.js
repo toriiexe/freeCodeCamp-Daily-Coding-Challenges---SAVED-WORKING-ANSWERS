82a1234ac64f69d861e7e34fe35d310f7b94070e
Quoriee <snowii.torii@gmail.com>
2026-07-11 00:21:58 +0200

    longest word js solution

--- solutions/2025/09-September/2025-09-29/js_2025-09-29.js
+++ solutions/2025/09-September/2025-09-29/js_2025-09-29.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2025-09-29
+// Longest Word
+function getLongestWord(sentence) {
+    const s = sentence.replaceAll('.', '');
+    let longestWord = '';
+
+    for (const word of s.split(/\s+/)) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+
+    return longestWord;
+}
