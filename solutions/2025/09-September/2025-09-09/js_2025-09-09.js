2316d433a155a05b144e3a61d4afd52921379bdc
Quoriee <snowii.torii@gmail.com>
2026-07-10 22:52:14 +0200

    unique characters js solution

--- solutions/2025/09-September/2025-09-09/js_2025-09-09.js
+++ solutions/2025/09-September/2025-09-09/js_2025-09-09.js
@@ -1,1 +1,13 @@
 // JavaScript solution for 2025-09-09
+// Unique Characters
+function allUnique(str) {
+    let chars = [];
+    for (const c of str){
+        if (chars.includes(c)){
+            return false;
+        } else{
+            chars.push(c);
+        }
+    }
+    return true;
+}
