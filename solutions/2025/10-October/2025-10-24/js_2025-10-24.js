483a3e82455b1f89d1087fd279b77150de4c14d5
Quoriee <snowii.torii@gmail.com>
2026-07-10 15:31:55 +0200

    hidden treasure js solution

--- solutions/2025/10-October/2025-10-24/js_2025-10-24.js
+++ solutions/2025/10-October/2025-10-24/js_2025-10-24.js
@@ -1,1 +1,28 @@
 // JavaScript solution for 2025-10-24
+// Hidden Treasure
+function dive(map, coordinates) {
+    const [row, col] = coordinates;
+
+    if (map[row][col] === '-'){
+        return 'Empty';
+    }
+
+    let oCounter = 0;
+    for (let r = 0; r < map.length; r++){
+        for (let c = 0; c < map[r].length; c++){
+        if (map[r][c] === 'O'){
+            oCounter++;
+        }
+        }
+    }
+
+    if (map[row][col] === 'O'){
+        if (oCounter === 1){
+        return 'Recovered';
+        } else{
+        return 'Found';
+        }
+    }
+    
+    return 'Found';
+}
