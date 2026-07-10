9fae54a0968c0760b9e2a01322d80c35a8d84278
Quoriee <snowii.torii@gmail.com>
2026-07-11 01:04:05 +0200

    perfect square js solution

--- solutions/2025/09-September/2025-09-24/js_2025-09-24.js
+++ solutions/2025/09-September/2025-09-24/js_2025-09-24.js
@@ -1,1 +1,8 @@
 // JavaScript solution for 2025-09-24
+// Perfect Square
+function isPerfectSquare(n) {
+    if (n < 0) return false;
+    
+    const root = Math.floor(Math.sqrt(n));
+    return root * root === n;
+}
