8b6bd31983a00e0daf6c9bb8319bbb28a072e841
Quoriee <snowii.torii@gmail.com>
2026-07-11 00:26:30 +0200

    digits vs letters python solution

--- solutions/2025/09-September/2025-09-22/py_2025-09-22.py
+++ solutions/2025/09-September/2025-09-22/py_2025-09-22.py
@@ -1,1 +1,13 @@
 # Python solution for 2025-09-22
+# Digits vs Letters
+def digits_or_letters(s):
+    digits_count = 0
+    letters_count = 0
+
+    for c in s:
+        if c.isdigit():
+            digits_count += 1
+        elif c.isalpha():
+            letters_count += 1
+
+    return 'digits' if digits_count > letters_count else 'letters' if digits_count < letters_count else 'tie'
