bfe7d3e5a5b52450c5880aef655bef76e83faf98
Quoriee <snowii.torii@gmail.com>
2026-07-10 15:13:50 +0200

    tip calculator python solution

--- solutions/2025/10-October/2025-10-20/py_2025-10-20.py
+++ solutions/2025/10-October/2025-10-20/py_2025-10-20.py
@@ -1,1 +1,7 @@
 # Python solution for 2025-10-20
+# Tip Calculator
+def calculate_tips(meal_price, custom_tip):
+    price = float(meal_price[1:])
+    cust_tip = float(custom_tip[:-1]) / 100
+
+    return [f'${price * 0.15:.2f}', f'${price * 0.20:.2f}', f'${price * cust_tip:.2f}']
