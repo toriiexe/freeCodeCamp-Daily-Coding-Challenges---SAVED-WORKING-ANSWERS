4751b4fe932e1f7222184cdaf09158d255f5f88c
Quoriee <snowii.torii@gmail.com>
2026-07-11 00:08:27 +0200

    caught speeding js solution

--- solutions/2025/09-September/2025-09-26/js_2025-09-26.js
+++ solutions/2025/09-September/2025-09-26/js_2025-09-26.js
@@ -1,1 +1,19 @@
 // JavaScript solution for 2025-09-26
+// Caught Speeding
+function speeding(speeds, limit) {
+    let speedingAmount = 0;
+    let beyondSpeedLimitTotal = 0;
+
+    for (const speed of speeds) {
+        if (speed > limit) {
+            speedingAmount += 1;
+            beyondSpeedLimitTotal += speed - limit;
+        }
+    }
+
+    if (speedingAmount === 0) {
+        return [0, 0];
+    }
+
+    return [speedingAmount, beyondSpeedLimitTotal / speedingAmount];
+}
