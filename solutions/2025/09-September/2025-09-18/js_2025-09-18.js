465c0036d977d44eab4c853e46762115cf098ba5
Quoriee <snowii.torii@gmail.com>
2026-07-10 23:54:28 +0200

    fill the tank js solution

--- solutions/2025/09-September/2025-09-18/js_2025-09-18.js
+++ solutions/2025/09-September/2025-09-18/js_2025-09-18.js
@@ -1,1 +1,5 @@
 // JavaScript solution for 2025-09-18
+// Fill The Tank
+function costToFill(tankSize, fuelLevel, pricePerGallon) {
+  return `\$${((tankSize - fuelLevel) * pricePerGallon).toFixed(2)}`;
+}
