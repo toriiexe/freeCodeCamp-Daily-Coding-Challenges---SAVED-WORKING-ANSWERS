d8bc4788cd39b298ea58bf12d5d49fed17ed33eb
Quoriee <snowii.torii@gmail.com>
2026-07-10 16:22:38 +0200

    vowel repeater js solution

--- solutions/2025/09-September/2025-09-04/js_2025-09-04.js
+++ solutions/2025/09-September/2025-09-04/js_2025-09-04.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2025-09-04
+// Vowel Repeater
+function repeatVowels(str) {
+    let res = '';
+    let vowelsToRepeat = 0;
+
+    for (const c of str) {
+        if ('AEIOUaeiou'.includes(c)) {
+        res += c + c.toLowerCase().repeat(vowelsToRepeat);
+        vowelsToRepeat++;
+        } else {
+        res += c;
+        }
+    }
+
+    return res;
+}
