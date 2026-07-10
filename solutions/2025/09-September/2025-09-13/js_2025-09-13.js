6393e9a023cf41fc392747a50f700afeb433f2b4
Quoriee <snowii.torii@gmail.com>
2026-07-10 23:28:26 +0200

    missing numbers js solution

--- solutions/2025/09-September/2025-09-13/js_2025-09-13.js
+++ solutions/2025/09-September/2025-09-13/js_2025-09-13.js
@@ -1,1 +1,13 @@
 // JavaScript solution for 2025-09-13
+// Missing Numbers
+function findMissingNumbers(arr) {
+    let res = [];
+
+    for (let i = 1; i <= Math.max(...arr); i++){
+        if (!(arr.includes(i))){
+        res.push(i);
+        }
+    }
+    
+    return res;
+}
