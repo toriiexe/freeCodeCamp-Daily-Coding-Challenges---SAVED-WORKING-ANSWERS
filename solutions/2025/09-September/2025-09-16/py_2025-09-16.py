d5ce106d301ccaf829006d549ef6da5711f9dde4
Quoriee <snowii.torii@gmail.com>
2026-07-10 23:42:10 +0200

    sentence capitalizer python solution

--- solutions/2025/09-September/2025-09-16/py_2025-09-16.py
+++ solutions/2025/09-September/2025-09-16/py_2025-09-16.py
@@ -1,1 +1,17 @@
 # Python solution for 2025-09-16
+# Sentence Capitalizer
+def capitalize(paragraph):
+    res = ''
+    bigLetter = True
+
+    for c in paragraph:
+        if bigLetter and c.isalpha():
+            res += c.upper()
+            bigLetter = False
+        else:
+            res += c
+
+        if c in '.?!':
+            bigLetter = True
+
+    return res
