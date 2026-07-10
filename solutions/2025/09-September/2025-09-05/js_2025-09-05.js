c58c724948050461f325ee43ae9ca879ef3e4227
Quoriee <snowii.torii@gmail.com>
2026-07-10 16:32:03 +0200

    ipv4 validator js solution

--- solutions/2025/09-September/2025-09-05/js_2025-09-05.js
+++ solutions/2025/09-September/2025-09-05/js_2025-09-05.js
@@ -1,1 +1,28 @@
 // JavaScript solution for 2025-09-05
+// IPv4 Validator
+function isValidIPv4(ipv4) {
+    const parts = ipv4.split('.');
+
+    if (parts.length != 4){
+        return false;
+    }
+
+    for (const part of parts){
+        if (!(/^\d+$/.test(part))){
+        return false;
+        }
+        
+        if (part !== '0' && part.startsWith('0')) {
+        return false;
+        }
+
+        const num = Number(part);
+        if (!(0 <= num && num <= 255)){
+        return false;
+        }
+
+
+    }
+
+    return true;
+}
