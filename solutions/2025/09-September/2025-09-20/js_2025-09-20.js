57853a08b8617d5927561bb0222074c48521579f
Quoriee <snowii.torii@gmail.com>
2026-07-11 00:03:45 +0200

    file storage js solution

--- solutions/2025/09-September/2025-09-20/js_2025-09-20.js
+++ solutions/2025/09-September/2025-09-20/js_2025-09-20.js
@@ -1,1 +1,12 @@
 // JavaScript solution for 2025-09-20
+// File Storage
+function numberOfFiles(fileSize, fileUnit, driveSizeGb) {
+    const conversion = {
+        B: 1,
+        KB: 1000,
+        MB: 1000000,
+        GB: 1000000000
+    };
+
+    return Math.floor((driveSizeGb * conversion.GB) / (fileSize * conversion[fileUnit]));
+}
