1748c21be78907cce0b1d37d832b72758082776c
Quoriee <snowii.torii@gmail.com>
2026-07-10 18:02:01 +0200

    matrix rotate js solution

--- solutions/2025/09-September/2025-09-06/js_2025-09-06.js
+++ solutions/2025/09-September/2025-09-06/js_2025-09-06.js
@@ -1,1 +1,23 @@
 // JavaScript solution for 2025-09-06
+// Matrix Rotate
+function rotate(matrix) {
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+
+    let res = []
+    for (let i = 0; i < cols; i++){
+        let temp = [];
+        for (let j = 0; j < rows; j++){
+        temp.push(0);
+        }
+        res.push(temp);
+    }
+
+    for (let r = 0; r < rows; r++){
+        for (let c = 0; c < cols; c++){
+        res[c][rows - 1 - r] = matrix[r][c]
+        }
+    }
+
+    return res;
+}
