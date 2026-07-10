c3847a274c48beb34c8bb4d5a7fb56e793a79647
Quoriee <snowii.torii@gmail.com>
2026-07-10 15:47:15 +0200

    rgb to hex python solution

--- solutions/2025/09-September/2025-09-02/py_2025-09-02.py
+++ solutions/2025/09-September/2025-09-02/py_2025-09-02.py
@@ -1,1 +1,6 @@
 # Python solution for 2025-09-02
+# RGB to Hex
+def rgb_to_hex(rgb):
+    rgb = rgb.replace('rgb(', '').replace(')', '')
+    r, g, b = map(int, rgb.split(', '))
+    return f'#{r:02x}{g:02x}{b:02x}'
