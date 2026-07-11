a5fa99227f72a2e3ad2cead8f93452f446418f29
Quoriee <snowii.torii@gmail.com>
2026-07-11 02:54:33 +0200

    flattened js solution

--- solutions/2026/03-March/2026-03-01/js_2026-03-01.js
+++ solutions/2026/03-March/2026-03-01/js_2026-03-01.js
@@ -1,1 +1,10 @@
 // JavaScript solution for 2026-03-01
+// Flattened
+function isFlat(arr) {
+    for (const el of arr) {
+        if (Array.isArray(el)) {
+            return false;
+        }
+    }
+    return true;
+}
