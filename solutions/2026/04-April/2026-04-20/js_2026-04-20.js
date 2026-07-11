532922b6428cb3ae7e2cd2689f14b643616c1d51
Quoriee <snowii.torii@gmail.com>
2026-07-11 14:54:56 +0200

    acronym finder js solution

--- solutions/2026/04-April/2026-04-20/js_2026-04-20.js
+++ solutions/2026/04-April/2026-04-20/js_2026-04-20.js
@@ -1,1 +1,13 @@
 // JavaScript solution for 2026-04-20
+// Acronym Finder
+function findOrg(acronym) {
+    const acronyms = {
+        'NASA': 'National Avocado Storage Authority',
+        'CIA': 'Cats Infiltration Agency',
+        'FBI': 'Fluffy Beanbag Inspectors',
+        'DOJ': 'Department Of Jelly',
+        'WHO': 'Wild Honey Organization',
+        'EPA': 'Eating Pancakes Administration'
+    }
+    return acronyms[acronym];
+}
