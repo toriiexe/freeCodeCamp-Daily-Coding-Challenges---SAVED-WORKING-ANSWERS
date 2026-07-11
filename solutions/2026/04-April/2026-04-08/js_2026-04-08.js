1060fbac15b8e7bdf486acdb7a693fecd08c8a09
Quoriee <snowii.torii@gmail.com>
2026-07-11 23:00:43 +0200

    fizzbuzz validator js solution

--- solutions/2026/04-April/2026-04-08/js_2026-04-08.js
+++ solutions/2026/04-April/2026-04-08/js_2026-04-08.js
@@ -1,1 +1,42 @@
 // JavaScript solution for 2026-04-08
+// FizzBuzz Validator
+function isFizzBuzz(arr) {
+    let start;
+
+    for (let i = 0; i < arr.length; i++) {
+        const el = arr[i];
+        if (Number.isInteger(el)) {
+            start = el - i;
+            break;
+        }
+    }
+
+    if (start === undefined) {
+        return false;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        const el = arr[i];
+        const n = start + i;
+
+        if (n % 15 === 0) {
+            if (el !== "FizzBuzz") {
+                return false;
+            }
+        } else if (n % 3 === 0) {
+            if (el !== "Fizz") {
+                return false;
+            }
+        } else if (n % 5 === 0) {
+            if (el !== "Buzz") {
+                return false;
+            }
+        } else {
+            if (el !== n) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
