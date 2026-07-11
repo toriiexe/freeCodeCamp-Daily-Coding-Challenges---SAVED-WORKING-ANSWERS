c8d9d4cfb85c4a8a0d163e98beafbcc5137a8a46
Quoriee <snowii.torii@gmail.com>
2026-07-11 14:53:53 +0200

    acronym finder python solution

--- solutions/2026/04-April/2026-04-20/py_2026-04-20.py
+++ solutions/2026/04-April/2026-04-20/py_2026-04-20.py
@@ -1,1 +1,12 @@
 # Python solution for 2026-04-20
+# Acronym Finder
+def find_org(acronym):
+    acronyms = {
+        'NASA': 'National Avocado Storage Authority',
+        'CIA': 'Cats Infiltration Agency',
+        'FBI': 'Fluffy Beanbag Inspectors',
+        'DOJ': 'Department Of Jelly',
+        'WHO': 'Wild Honey Organization',
+        'EPA': 'Eating Pancakes Administration'
+    }
+    return acronyms[acronym]
