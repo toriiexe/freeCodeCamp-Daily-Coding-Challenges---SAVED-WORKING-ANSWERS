37c178cd7c2580412c980522aa6d124a9fec4706
Quoriee <snowii.torii@gmail.com>
2026-07-12 05:02:17 +0200

    pocket change js solution

--- solutions/2026/02-February/2026-02-05/js_2026-02-05.js
+++ solutions/2026/02-February/2026-02-05/js_2026-02-05.js
@@ -1,1 +1,6 @@
 // JavaScript solution for 2026-02-05
+// Pocket Change
+function countChange(change) {
+    const total = change.reduce((sum, coin) => sum + coin, 0);
+    return `\$${Math.floor(total / 100)}.${String(total % 100).padStart(2, "0")}`;
+}
