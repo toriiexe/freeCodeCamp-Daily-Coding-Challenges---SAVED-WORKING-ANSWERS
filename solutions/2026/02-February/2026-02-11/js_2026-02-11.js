d825314e0653688d8a5f154187cad2ebaa9c4af1
Quoriee <snowii.torii@gmail.com>
2026-07-12 13:55:03 +0200

    2026 winter games day 6: figure skating js solution

--- solutions/2026/02-February/2026-02-11/js_2026-02-11.js
+++ solutions/2026/02-February/2026-02-11/js_2026-02-11.js
@@ -1,1 +1,5 @@
 // JavaScript solution for 2026-02-11
+// 2026 Winter Games Day 6: Figure Skating
+function computeScore(judgeScores, ...penalties) {
+    return judgeScores.reduce((a, b) => a + b, 0) - Math.min(...judgeScores) - Math.max(...judgeScores) - penalties.reduce((a, b) => a + b, 0);
+}
