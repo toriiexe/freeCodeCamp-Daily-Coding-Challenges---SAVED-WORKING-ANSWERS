cc8099ef72386ef2bcd7581f68eb0e5c63b069ff
Quoriee <snowii.torii@gmail.com>
2026-07-12 13:53:36 +0200

    2026 winter games day 6: figure skating python solution

--- solutions/2026/02-February/2026-02-11/py_2026-02-11.py
+++ solutions/2026/02-February/2026-02-11/py_2026-02-11.py
@@ -1,1 +1,4 @@
 # Python solution for 2026-02-11
+# 2026 Winter Games Day 6: Figure Skating
+def compute_score(judge_scores, *penalties):
+    return sum(judge_scores) - min(judge_scores) - max(judge_scores) - sum(penalties)
