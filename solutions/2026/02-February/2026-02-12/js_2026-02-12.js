cc37a8a7046e40b2239143f37c1000c5d3c0610c
Quoriee <snowii.torii@gmail.com>
2026-07-12 14:09:56 +0200

    2026 winter games day 7: speed skating js solution

--- solutions/2026/02-February/2026-02-12/js_2026-02-12.js
+++ solutions/2026/02-February/2026-02-12/js_2026-02-12.js
@@ -1,1 +1,9 @@
 // JavaScript solution for 2026-02-12
+// 2026 Winter Games Day 7: Speed Skating
+function largestDifference(skater1, skater2) {
+    let differences = skater1.map(function (e, i) {
+        return Math.abs(e - skater2[i]);
+    });
+
+    return differences.indexOf(Math.max(...differences)) + 1;
+}
