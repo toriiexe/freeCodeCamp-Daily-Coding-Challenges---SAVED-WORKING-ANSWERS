1296ace71a384e53a8ac7fb3d12c22c1a5005bec
Quoriee <snowii.torii@gmail.com>
2026-07-12 04:01:58 +0200

    word length converter js solution

--- solutions/2026/03-March/2026-03-11/js_2026-03-11.js
+++ solutions/2026/03-March/2026-03-11/js_2026-03-11.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2026-03-11
+// Word Length Converter
+function convertWords(str) {
+    let res = [];
+
+    for (const s of str.split(/\s+/)){
+        res.push(s.length);
+
+    }
+    return res.join(' ');
+}
