f8bb8b2920f8ca4a57025536916cdcb1cb348d4f
Quoriee <snowii.torii@gmail.com>
2026-07-12 13:47:21 +0200

    2026 winter games day 5: cross-country skiing python solution

--- solutions/2026/02-February/2026-02-10/py_2026-02-10.py
+++ solutions/2026/02-February/2026-02-10/py_2026-02-10.py
@@ -1,1 +1,16 @@
 # Python solution for 2026-02-10
+# 2026 Winter Games Day 5: Cross-Country Skiing
+def get_relative_results(results):
+    toSec = []
+    for r in results:
+        h, m, s = map(int, r.split(':'))
+        toSec.append(h * 3600 + m * 60 + s)
+
+    behind = ['0']
+    winner = toSec[0]
+
+    for i in range(1, len(toSec)):
+        diff = toSec[i] - winner
+        behind.append(f'+{diff//60}:{diff%60:02d}')
+
+    return behind
