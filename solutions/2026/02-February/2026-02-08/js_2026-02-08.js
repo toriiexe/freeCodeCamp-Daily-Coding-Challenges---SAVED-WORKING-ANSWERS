438ffd673ba949f1a3f52029bf50263154f0fe60
Quoriee <snowii.torii@gmail.com>
2026-07-12 05:23:22 +0200

    2026 winter games day 3: biathlon js solution

--- solutions/2026/02-February/2026-02-08/js_2026-02-08.js
+++ solutions/2026/02-February/2026-02-08/js_2026-02-08.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2026-02-08
+// 2026 Winter Games Day 3: Biathlon
+function calculatePenaltyDistance(rounds) {
+    let s = 0;
+
+    for (let i = 0; i < rounds.length; i++){
+        s += (5 - rounds[i]) * 150;
+    }
+
+    return s;
+}
