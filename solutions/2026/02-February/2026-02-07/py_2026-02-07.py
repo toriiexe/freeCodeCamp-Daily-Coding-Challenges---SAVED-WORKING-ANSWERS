a166693057f82a3150515764632570c53afa039e
Quoriee <snowii.torii@gmail.com>
2026-07-12 05:10:35 +0200

    2026 winter games day 2: snowboarding python solution

--- solutions/2026/02-February/2026-02-07/py_2026-02-07.py
+++ solutions/2026/02-February/2026-02-07/py_2026-02-07.py
@@ -1,1 +1,8 @@
 # Python solution for 2026-02-07
+# 2026 Winter Games Day 2: Snowboarding
+def get_landing_stance(start_stance, rotation):
+    reversed_start_stance = {
+        'Regular': 'Goofy',
+        'Goofy': 'Regular'
+    }
+    return reversed_start_stance[start_stance] if (abs(rotation) // 180) % 2 != 0 else start_stance
