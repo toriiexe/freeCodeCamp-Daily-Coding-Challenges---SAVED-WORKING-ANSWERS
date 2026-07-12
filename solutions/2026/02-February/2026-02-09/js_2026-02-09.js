fbd64cebc459156611f25f0c765da9fb7ab72516
Quoriee <snowii.torii@gmail.com>
2026-07-12 05:28:40 +0200

    2026 winter games day 4: ski jumping js solution

--- solutions/2026/02-February/2026-02-09/js_2026-02-09.js
+++ solutions/2026/02-February/2026-02-09/js_2026-02-09.js
@@ -1,1 +1,12 @@
 // JavaScript solution for 2026-02-09
+// 2026 Winter Games Day 4: Ski Jumping
+function skiJumpMedal(distancePoints, stylePoints, windComp, kPointBonus) {
+    const ranking = [165.5, 172.0, 158.0, 180.0, 169.5, 175.0, 162.0, 170.0].sort().reverse()
+
+    const total = distancePoints + stylePoints + windComp + kPointBonus
+
+    if (total > ranking[0]) return 'Gold';
+    if (total > ranking[1]) return 'Silver';
+    if (total > ranking[2]) return 'Bronze';
+    return 'No Medal';
+}
