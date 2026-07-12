b5edb9ceccaa82f313c94e5cc41792b094a15665
Quoriee <snowii.torii@gmail.com>
2026-07-12 05:12:57 +0200

    2026 winter games day 2: snowboarding js solution

--- solutions/2026/02-February/2026-02-07/js_2026-02-07.js
+++ solutions/2026/02-February/2026-02-07/js_2026-02-07.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2026-02-07
+// 2026 Winter Games Day 2: Snowboarding
+function getLandingStance(startStance, rotation) {
+  const reversedStartStance = {
+        'Regular': 'Goofy',
+        'Goofy': 'Regular'
+    }
+    if (Math.floor(Math.abs(rotation) / 180) % 2 !== 0) return reversedStartStance[startStance];
+    
+    return startStance;
+}
