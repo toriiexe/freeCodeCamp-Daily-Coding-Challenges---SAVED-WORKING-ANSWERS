4fec4d423c6559009e0d901803cd1d288486966b
Quoriee <snowii.torii@gmail.com>
2026-07-12 05:26:50 +0200

    2026 winter games day 4: ski jumping python solution

--- solutions/2026/02-February/2026-02-09/py_2026-02-09.py
+++ solutions/2026/02-February/2026-02-09/py_2026-02-09.py
@@ -1,1 +1,15 @@
 # Python solution for 2026-02-09
+# 2026 Winter Games Day 4: Ski Jumping
+def ski_jump_medal(distance_points, style_points, wind_comp, k_point_bonus):
+    ranking = sorted([165.5, 172.0, 158.0, 180.0, 169.5, 175.0, 162.0, 170.0], reverse=True)
+
+    total = distance_points + style_points + wind_comp + k_point_bonus
+    
+    if (total > ranking[0]):
+        return 'Gold'
+    elif (total > ranking[1]):
+        return 'Silver'
+    elif (total > ranking[2]):
+        return 'Bronze'
+    else:
+        return 'No Medal'
