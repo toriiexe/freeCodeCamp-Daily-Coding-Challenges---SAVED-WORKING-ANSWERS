960cea8671243dc77a8ef26345af31ec366e8b9b
Quoriee <snowii.torii@gmail.com>
2026-07-12 04:08:54 +0200

    element size js solution

--- solutions/2026/03-March/2026-03-07/js_2026-03-07.js
+++ solutions/2026/03-March/2026-03-07/js_2026-03-07.js
@@ -1,1 +1,10 @@
 // JavaScript solution for 2026-03-07
+// Element Size
+function getElementSize(windowSize, elementVw, elementVh) {
+    const [w, h] = windowSize.split(" x ").map(Number);
+
+    const vW = Number(elementVw.slice(0, -2));
+    const vH = Number(elementVh.slice(0, -2));
+
+    return `${Math.floor(w * vW / 100)} x ${Math.floor(h * vH / 100)}`;
+}
