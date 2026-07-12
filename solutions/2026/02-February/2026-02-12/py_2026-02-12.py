279ff6a23da73b00feedb2ad15ba5bb93f5d7aa5
Quoriee <snowii.torii@gmail.com>
2026-07-12 14:07:36 +0200

    2026 winter games day 7: speed skating python solution

--- solutions/2026/02-February/2026-02-12/py_2026-02-12.py
+++ solutions/2026/02-February/2026-02-12/py_2026-02-12.py
@@ -1,1 +1,5 @@
 # Python solution for 2026-02-12
+# 2026 Winter Games Day 7: Speed Skating
+def largest_difference(skater1, skater2):
+    differences = [abs(a - b) for a, b in zip(skater1, skater2)]
+    return differences.index(max(differences)) + 1
