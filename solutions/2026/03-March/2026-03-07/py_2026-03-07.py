be2de7503bc277ea6f81c7a31554d06d993e5ed6
Quoriee <snowii.torii@gmail.com>
2026-07-12 04:06:06 +0200

    element size python solution

--- solutions/2026/03-March/2026-03-07/py_2026-03-07.py
+++ solutions/2026/03-March/2026-03-07/py_2026-03-07.py
@@ -1,1 +1,8 @@
 # Python solution for 2026-03-07
+# Element Size
+def get_element_size(window_size, element_vw, element_vh):
+    w, h = map(int, window_size.split(' x '))
+    vw = int(element_vw[:-2])
+    vh = int(element_vh[:-2])
+
+    return f'{w * vw // 100} x {h * vh // 100}'
