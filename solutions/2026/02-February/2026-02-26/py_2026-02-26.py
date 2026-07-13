ae148bc7e9f23d976dcce7861ee9334645c621df
Quoriee <snowii.torii@gmail.com>
2026-07-13 16:05:43 +0200

    letter and number count python solution

--- solutions/2026/02-February/2026-02-26/py_2026-02-26.py
+++ solutions/2026/02-February/2026-02-26/py_2026-02-26.py
@@ -1,1 +1,11 @@
 # Python solution for 2026-02-26
+# Letter and Number Count
+import re
+def count_letters_and_numbers(s):
+    letters_quantity = len(re.findall('[a-zA-Z]', s))
+    letters_str = 'letters' if letters_quantity != 1 else 'letter'
+
+    digits_quantity = len(re.findall('[0-9]', s))
+    digits_str = 'numbers' if digits_quantity != 1 else 'number'
+
+    return f'The string has {letters_quantity} {letters_str} and {digits_quantity} {digits_str}.'
