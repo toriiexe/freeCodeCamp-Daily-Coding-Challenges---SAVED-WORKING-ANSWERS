f8dd476911d963820f0fc5ecd842ddfd59a4b776
Quoriee <snowii.torii@gmail.com>
2026-07-13 23:17:50 +0200

    2026 winter games day 15: freestyle skiing python solution

--- solutions/2026/02-February/2026-02-20/py_2026-02-20.py
+++ solutions/2026/02-February/2026-02-20/py_2026-02-20.py
@@ -1,1 +1,10 @@
 # Python solution for 2026-02-20
+# 2026 Winter Games Day 15: Freestyle Skiing
+def is_valid_trick(trick_name):
+    first_parts = ['Misty', 'Ghost', 'Thunder', 'Solar', 'Sky', 'Phantom', 'Frozen', 'Polar']
+
+    second_parts = ['Twister', 'Icequake', 'Avalanche', 'Vortex', 'Snowstorm', 'Frostbite', 'Blizzard', 'Shadow']
+
+    first_part, second_part = trick_name.split()
+    
+    return (first_part in first_parts) and (second_part in second_parts)
