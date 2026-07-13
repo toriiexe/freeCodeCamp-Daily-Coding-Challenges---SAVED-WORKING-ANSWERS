b75e1af31a412ac4917c17e5eb56993b191ec03e
Quoriee <snowii.torii@gmail.com>
2026-07-13 23:26:10 +0200

    2026 winter games day 14: ski mountaineering python solution

--- solutions/2026/02-February/2026-02-19/py_2026-02-19.py
+++ solutions/2026/02-February/2026-02-19/py_2026-02-19.py
@@ -1,1 +1,22 @@
 # Python solution for 2026-02-19
+# 2026 Winter Games Day 14: Ski Mountaineering
+def avalanche_risk(snow_depth, slope):
+    risks = {
+        'Gentle': {
+            'Shallow': 'Safe', 
+            'Moderate': 'Safe', 
+            'Deep': 'Safe'
+            },
+        'Steep': {
+            'Shallow': 'Safe', 
+            'Moderate': 'Risky', 
+            'Deep': 'Risky'
+            },
+        'Very Steep': {
+            'Shallow': 'Safe', 
+            'Moderate': 'Risky', 
+            'Deep': 'Risky'
+            }
+    }
+
+    return risks[slope][snow_depth]
