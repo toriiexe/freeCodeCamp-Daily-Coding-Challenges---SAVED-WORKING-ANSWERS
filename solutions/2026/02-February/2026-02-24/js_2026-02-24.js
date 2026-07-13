5a4f5ff53d44b2fa9d27e2bae00bccd5dbed9e16
Quoriee <snowii.torii@gmail.com>
2026-07-13 22:22:34 +0200

    business day count js solution

--- solutions/2026/02-February/2026-02-24/js_2026-02-24.js
+++ solutions/2026/02-February/2026-02-24/js_2026-02-24.js
@@ -1,1 +1,22 @@
 // JavaScript solution for 2026-02-24
+// Business Day Count
+function countBusinessDays(start, end) {
+    let counter = 0;
+
+    let d1 = new Date(start);
+    let d2 = new Date(end);
+
+    let current = new Date(d1);
+
+    while (current <= d2) {
+        let day = current.getDay();
+
+        if (day !== 0 && day !== 6) {
+            counter++;
+        }
+
+        current.setDate(current.getDate() + 1);
+    }
+
+    return counter;
+}
