50b6c1f2af28e2b92cd530534d8fcb460e4d7f41
Quoriee <snowii.torii@gmail.com>
2026-07-13 14:07:03 +0200

    horoscope match js solution

--- solutions/2026/07-July/2026-07-12/js_2026-07-12.js
+++ solutions/2026/07-July/2026-07-12/js_2026-07-12.js
@@ -1,1 +1,26 @@
 // JavaScript solution for 2026-07-12
+// Horoscope Match
+function horoscopeMatch(sign1, sign2) {
+    const signs = [
+        'Aries', 'Taurus', 'Gemini',
+        'Cancer', 'Leo', 'Virgo',
+        'Libra', 'Scorpio', 'Sagittarius',
+        'Capricorn', 'Aquarius', 'Pisces'
+    ];
+
+    const compatibility = new Map([
+        [0, 100],
+        [1, 40],
+        [2, 80],
+        [3, 30],
+        [4, 90],
+        [5, 20],
+        [6, 50]
+    ]);
+
+    const indx1 = signs.indexOf(sign1);
+    const indx2 = signs.indexOf(sign2);
+
+    const distance = Math.abs(indx1 - indx2);
+    return `${compatibility.get(Math.min(distance, 12 - distance))}%`;
+}
