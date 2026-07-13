9d0ca721a5a76e80f45a883f5b1ae205c601a94d
Quoriee <snowii.torii@gmail.com>
2026-07-13 15:43:47 +0200

    exact change js solution

--- solutions/2026/07-July/2026-07-10/js_2026-07-10.js
+++ solutions/2026/07-July/2026-07-10/js_2026-07-10.js
@@ -1,1 +1,24 @@
 // JavaScript solution for 2026-07-10
+// Exact Change
+function exactChange(amount) {
+    const coins = [1, 5, 10, 25];
+
+    const dp = [];
+    
+    for (let i = 0; i <= amount; i++){
+        dp.push(0);
+    }
+    //^^ or
+    // const dp = new Array(amount + 1).fill(0);
+
+    dp[0] = 1;
+
+    for (const coin of coins){
+        for (let i = coin; i <= amount; i++){
+        dp[i] += dp[i-coin];
+        }
+    }
+
+
+    return dp[amount];
+}
