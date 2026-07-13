79adfd4f18faca3209c5a96317c78900a6d6eb74
Quoriee <snowii.torii@gmail.com>
2026-07-13 23:11:40 +0200

    2026 winter games day 10: alpine skiing python solution

--- solutions/2026/02-February/2026-02-15/py_2026-02-15.py
+++ solutions/2026/02-February/2026-02-15/py_2026-02-15.py
@@ -1,1 +1,15 @@
 # Python solution for 2026-02-15
+# 2026 Winter Games Day 10: Alpine Skiing
+def get_hill_rating(drop, distance, hill_type):
+    steepness = drop / distance
+
+    multiplier = 1.2 if hill_type == 'Downhill' else 0.9 if hill_type == 'Slalom' else 1.0
+
+    adjusted_steepness = steepness * multiplier
+
+    if adjusted_steepness <= 0.1:
+        return 'Green'
+    elif adjusted_steepness <= 0.25:
+        return 'Blue'
+    else:
+        return 'Black'
