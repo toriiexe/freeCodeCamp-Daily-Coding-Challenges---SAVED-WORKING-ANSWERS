06985bd699001e672804971c8ae820c7d2c43e58
Quoriee <snowii.torii@gmail.com>
2026-07-13 15:00:51 +0200

    nearest multiple python solution

--- solutions/2026/07-July/2026-07-07/py_2026-07-07.py
+++ solutions/2026/07-July/2026-07-07/py_2026-07-07.py
@@ -1,1 +1,10 @@
 # Python solution for 2026-07-07
+# Nearest Multiple
+def round_to_nearest_multiple(num, multiple):
+    first_endpoint = num - num % multiple
+    second_endpoint = first_endpoint + multiple
+    
+    if num - first_endpoint < second_endpoint - num:
+        return first_endpoint
+    else:
+        return second_endpoint
