d2d84526174f4d10db088464bb57658d169a6392
Quoriee <snowii.torii@gmail.com>
2026-07-13 15:02:36 +0200

    nearest multiple js solution

--- solutions/2026/07-July/2026-07-07/js_2026-07-07.js
+++ solutions/2026/07-July/2026-07-07/js_2026-07-07.js
@@ -1,1 +1,12 @@
 // JavaScript solution for 2026-07-07
+// Nearest Multiple
+function roundToNearestMultiple(num, multiple) {
+    const first_endpoint = num - num % multiple;
+    const second_endpoint = first_endpoint + multiple;
+    
+    if (num - first_endpoint < second_endpoint - num){
+        return first_endpoint;
+    } else{
+        return second_endpoint;
+    }
+}
