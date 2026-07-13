73bee10401a7a71978f6faee36c2ada5999e226c
Quoriee <snowii.torii@gmail.com>
2026-07-13 04:06:45 +0200

    2026 winter games day 8: luge python solution

--- solutions/2026/02-February/2026-02-13/py_2026-02-13.py
+++ solutions/2026/02-February/2026-02-13/py_2026-02-13.py
@@ -1,1 +1,13 @@
 # Python solution for 2026-02-13
+# 2026 Winter Games Day 8: Luge
+def get_fastest_speed(times):
+    segments = [320, 280, 350, 300, 250]
+    speeds = []
+
+    for i in range(5):
+        speeds.append(segments[i] / times[i])
+
+    fastest = max(speeds)
+    segment = speeds.index(fastest) + 1
+
+    return f"The luger's fastest speed was {fastest:.2f} m/s on segment {segment}."
