79b2b34da4c6e187349e2209a9547e46fa50f1aa
Quoriee <snowii.torii@gmail.com>
2026-07-13 22:59:48 +0200

    2026 winter games day 9: skeleton python solution

--- solutions/2026/02-February/2026-02-14/py_2026-02-14.py
+++ solutions/2026/02-February/2026-02-14/py_2026-02-14.py
@@ -1,1 +1,19 @@
 # Python solution for 2026-02-14
+# 2026 Winter Games Day 9: Skeleton
+import re
+def get_difficulty(track):
+    
+    r_extra_points = re.findall('(R(?=L))', track)
+    l_extra_points = re.findall('(L(?=R))', track)
+
+    r_points = len(r_extra_points) * 15 + (track.count('R') - len(r_extra_points)) * 5
+    l_points = len(l_extra_points) * 15 + (track.count('L') - len(l_extra_points)) * 5
+
+    total = r_points + l_points
+
+    if total >= 0 and total <= 100:
+        return 'Easy'
+    elif total <= 200:
+        return 'Medium'
+    else:
+        return 'Hard'
