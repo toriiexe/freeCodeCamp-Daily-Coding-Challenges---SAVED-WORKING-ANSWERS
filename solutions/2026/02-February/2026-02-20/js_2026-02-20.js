d676beb9d464c928c3df1587c23d33068aa3c0f7
Quoriee <snowii.torii@gmail.com>
2026-07-13 23:19:34 +0200

    2026 winter games day 15: freestyle skiing js solution

--- solutions/2026/02-February/2026-02-20/js_2026-02-20.js
+++ solutions/2026/02-February/2026-02-20/js_2026-02-20.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2026-02-20
+// 2026 Winter Games Day 15: Freestyle Skiing
+function isValidTrick(trickName) {
+    const firstParts = ['Misty', 'Ghost', 'Thunder', 'Solar', 'Sky', 'Phantom', 'Frozen', 'Polar'];
+
+    const secondParts = ['Twister', 'Icequake', 'Avalanche', 'Vortex', 'Snowstorm', 'Frostbite', 'Blizzard', 'Shadow'];
+
+    const [firstPart, secondPart] = trickName.split(' ');
+
+    return (firstParts.includes(firstPart) && secondParts.includes(secondPart));
+}
