0507eec76522b38e415842283937f5b687ca6880
Quoriee <snowii.torii@gmail.com>
2026-07-13 23:27:57 +0200

    2026 winter games day 14: ski mountaineering js solution

--- solutions/2026/02-February/2026-02-19/js_2026-02-19.js
+++ solutions/2026/02-February/2026-02-19/js_2026-02-19.js
@@ -1,1 +1,23 @@
 // JavaScript solution for 2026-02-19
+// 2026 Winter Games Day 14: Ski Mountaineering
+function avalancheRisk(snowDepth, slope) {
+    const risks = {
+        'Gentle': {
+            'Shallow': 'Safe',
+            'Moderate': 'Safe',
+            'Deep': 'Safe'
+        },
+        'Steep': {
+            'Shallow': 'Safe',
+            'Moderate': 'Risky',
+            'Deep': 'Risky'
+        },
+        'Very Steep': {
+            'Shallow': 'Safe',
+            'Moderate': 'Risky',
+            'Deep': 'Risky'
+        }
+    };
+
+    return risks[slope][snowDepth];
+}
