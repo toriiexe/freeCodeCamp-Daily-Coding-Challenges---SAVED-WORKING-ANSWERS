b2c7695059b315ccf855b580d95ff0844923de0b
Quoriee <snowii.torii@gmail.com>
2026-07-13 23:44:42 +0200

    2026 winter games day 13: nordic combined python solution

--- solutions/2026/02-February/2026-02-18/py_2026-02-18.py
+++ solutions/2026/02-February/2026-02-18/py_2026-02-18.py
@@ -1,1 +1,11 @@
 # Python solution for 2026-02-18
+# 2026 Winter Games Day 13: Nordic Combined
+from math import ceil
+def calculate_start_delays(jump_scores):
+    the_best = max(jump_scores)
+    res = []
+
+    for jump_score in jump_scores:
+        res.append(ceil((the_best - jump_score) * 1.5))
+
+    return res
