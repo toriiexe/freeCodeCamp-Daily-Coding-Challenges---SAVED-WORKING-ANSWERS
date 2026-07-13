0986fe7aa101f412a00c8dc187bfb43413d3b10d
Quoriee <snowii.torii@gmail.com>
2026-07-13 16:05:51 +0200

    letter and number count js solution

--- solutions/2026/02-February/2026-02-26/js_2026-02-26.js
+++ solutions/2026/02-February/2026-02-26/js_2026-02-26.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2026-02-26
+// Letter and Number Count
+function countLettersAndNumbers(str) {
+    const lettersQuantity = (str.match(/[a-zA-Z]/g) || []).length;
+    const lettersStr = lettersQuantity !== 1 ? 'letters' : 'letter';
+
+    const numbersQuantity = (str.match(/[0-9]/g) || []).length;
+    const numbersStr = numbersQuantity !== 1 ? 'numbers' : 'number';
+
+    return `The string has ${lettersQuantity} ${lettersStr} and ${numbersQuantity} ${numbersStr}.`;
+}
