a7c10ee486438378428217fca73f2eb69201ee0f
Quoriee <snowii.torii@gmail.com>
2026-07-13 22:06:28 +0200

    sequential difference js solution

--- solutions/2026/02-February/2026-02-25/js_2026-02-25.js
+++ solutions/2026/02-February/2026-02-25/js_2026-02-25.js
@@ -1,1 +1,22 @@
 // JavaScript solution for 2026-02-25
+// Sequential Difference
+function findDifferences(arr) {
+    const res = [];
+
+    for (let i = 0; i < arr.length; i++){
+        if (i !== arr.length - 1){
+        res.push(arr[i+1] - arr[i]);
+        } else{
+        res.push(0);
+        }
+    }
+    
+    return res;
+}
+
+/*
+// Sequential Difference - SHORTER VERSION
+function findDifferences(arr) {
+    return arr.map((num, i) => arr[i + 1] - num || 0);
+}
+*/
