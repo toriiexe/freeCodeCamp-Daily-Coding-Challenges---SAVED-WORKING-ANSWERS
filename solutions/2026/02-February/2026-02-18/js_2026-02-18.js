45a727cc54da0f029e08eb2688cdb55c41a64d49
Quoriee <snowii.torii@gmail.com>
2026-07-13 23:45:25 +0200

    2026 winter games day 13: nordic combined js solution

--- solutions/2026/02-February/2026-02-18/js_2026-02-18.js
+++ solutions/2026/02-February/2026-02-18/js_2026-02-18.js
@@ -1,1 +1,12 @@
 // JavaScript solution for 2026-02-18
+// 2026 Winter Games Day 13: Nordic Combined
+function calculateStartDelays(jumpScores) {
+    const theBest = Math.max(...jumpScores);
+    const res = [];
+
+    for (const jumpScore of jumpScores) {
+        res.push(Math.ceil((theBest - jumpScore) * 1.5));
+    }
+
+    return res;
+}
