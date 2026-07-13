7013100775e9f72893ea81448dce93d541fc59e2
Quoriee <snowii.torii@gmail.com>
2026-07-13 04:07:44 +0200

    2026 winter games day 8: luge js solution

--- solutions/2026/02-February/2026-02-13/js_2026-02-13.js
+++ solutions/2026/02-February/2026-02-13/js_2026-02-13.js
@@ -1,1 +1,15 @@
 // JavaScript solution for 2026-02-13
+// 2026 Winter Games Day 8: Luge
+function getFastestSpeed(times) {
+    let segments = [320, 280, 350, 300, 250];
+    let speeds = [];
+
+    for (let i = 0; i < 5; i++) {
+        speeds.push(segments[i] / times[i]);
+    }
+
+    let fastest = Math.max(...speeds);
+    let segment = speeds.indexOf(fastest) + 1;
+
+    return `The luger's fastest speed was ${fastest.toFixed(2)} m/s on segment ${segment}.`;
+}
