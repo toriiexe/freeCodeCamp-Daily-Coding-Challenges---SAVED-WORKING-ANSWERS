d36d15cf678d726e95a222c08b92484be8b24547
Quoriee <snowii.torii@gmail.com>
2026-07-13 22:53:51 +0200

    blood type compatibility python solution

--- solutions/2026/02-February/2026-02-23/py_2026-02-23.py
+++ solutions/2026/02-February/2026-02-23/py_2026-02-23.py
@@ -1,1 +1,16 @@
 # Python solution for 2026-02-23
+# Blood Type Compatibility
+def can_donate(donor, recipient):
+
+    possible_donations = {
+        'O-':  ['O-', 'O+', 'A-', 'A+', 'B-', 'B+', 'AB-', 'AB+'],
+        'O+':  ['O+', 'A+', 'B+', 'AB+'],
+        'A-':  ['A-', 'A+', 'AB-', 'AB+'],
+        'A+':  ['A+', 'AB+'],
+        'B-':  ['B-', 'B+', 'AB-', 'AB+'],
+        'B+':  ['B+', 'AB+'],
+        'AB-': ['AB-', 'AB+'],
+        'AB+': ['AB+']
+    }
+
+    return recipient in possible_donations[donor]
