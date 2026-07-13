bd593d2ff5cf96608740e701ff596762d8d41ddd
Quoriee <snowii.torii@gmail.com>
2026-07-13 22:54:26 +0200

    blood type compatibility js solution

--- solutions/2026/02-February/2026-02-23/js_2026-02-23.js
+++ solutions/2026/02-February/2026-02-23/js_2026-02-23.js
@@ -1,1 +1,16 @@
 // JavaScript solution for 2026-02-23
+// Blood Type Compatibility
+function canDonate(donor, recipient) {
+    const possibleDonations = {
+        'O-': ['O-', 'O+', 'A-', 'A+', 'B-', 'B+', 'AB-', 'AB+'],
+        'O+': ['O+', 'A+', 'B+', 'AB+'],
+        'A-': ['A-', 'A+', 'AB-', 'AB+'],
+        'A+': ['A+', 'AB+'],
+        'B-': ['B-', 'B+', 'AB-', 'AB+'],
+        'B+': ['B+', 'AB+'],
+        'AB-': ['AB-', 'AB+'],
+        'AB+': ['AB+']
+    };
+
+    return possibleDonations[donor].includes(recipient);
+}
