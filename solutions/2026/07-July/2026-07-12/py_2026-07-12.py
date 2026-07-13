ddb3b28589fc495e939314f29d6579b0a3bddc40
Quoriee <snowii.torii@gmail.com>
2026-07-13 13:53:00 +0200

    horoscope match python solution

--- solutions/2026/07-July/2026-07-12/py_2026-07-12.py
+++ solutions/2026/07-July/2026-07-12/py_2026-07-12.py
@@ -1,1 +1,25 @@
 # Python solution for 2026-07-12
+# Horoscope Match
+def horoscope_match(sign1, sign2):
+    signs = [
+        'Aries', 'Taurus', 'Gemini',
+        'Cancer', 'Leo', 'Virgo',
+        'Libra', 'Scorpio','Sagittarius',
+        'Capricorn', 'Aquarius', 'Pisces'
+        ]
+
+    compatibility = {
+        0: 100,
+        1: 40,
+        2: 80,
+        3: 30,
+        4: 90,
+        5: 20,
+        6: 50
+    }
+
+    indx1 = signs.index(sign1)
+    indx2 = signs.index(sign2)
+
+    distance = abs(indx1 - indx2)
+    return f'{compatibility[min(distance, 12-distance)]}%'
