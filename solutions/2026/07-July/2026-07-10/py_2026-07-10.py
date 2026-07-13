a410df6c15a5390bda5537856b710c294fef38d2
Quoriee <snowii.torii@gmail.com>
2026-07-13 15:42:20 +0200

    exact change python solution

--- solutions/2026/07-July/2026-07-10/py_2026-07-10.py
+++ solutions/2026/07-July/2026-07-10/py_2026-07-10.py
@@ -1,1 +1,13 @@
 # Python solution for 2026-07-10
+# Exact Change
+def exact_change(amount):
+    coins = [1, 5, 10, 25]
+
+    dp = [0] * (amount + 1)
+    dp[0] = 1
+
+    for coin in coins:
+        for i in range(coin, amount+1):
+            dp[i] += dp[i-coin]
+
+    return dp[amount]
