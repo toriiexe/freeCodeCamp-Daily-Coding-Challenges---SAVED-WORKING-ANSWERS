188184456457a161d0c87a2373ca1b30cf34b644
Quoriee <snowii.torii@gmail.com>
2026-07-13 22:22:13 +0200

    business day count python solution

--- solutions/2026/02-February/2026-02-24/py_2026-02-24.py
+++ solutions/2026/02-February/2026-02-24/py_2026-02-24.py
@@ -1,1 +1,18 @@
 # Python solution for 2026-02-24
+# Business Day Count
+from datetime import datetime, timedelta
+
+def count_business_days(start, end):
+    counter = 0
+
+    d1 = datetime.strptime(start, "%Y-%m-%d").date()
+    d2 = datetime.strptime(end, "%Y-%m-%d").date()
+
+    diff = (d2 - d1).days
+
+    for i in range(diff + 1):
+        day = d1 + timedelta(days=i)
+        if day.weekday() < 5:
+            counter += 1
+
+    return counter
