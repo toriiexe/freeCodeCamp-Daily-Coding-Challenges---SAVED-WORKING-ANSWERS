722ae20adca1f2bb2db7ee6cd64130f4b06dad54
Quoriee <snowii.torii@gmail.com>
2026-07-15 01:44:24 +0200

    sorted array swap python solution

--- solutions/2026/04-April/2026-04-15/py_2026-04-15.py
+++ solutions/2026/04-April/2026-04-15/py_2026-04-15.py
@@ -1,1 +1,9 @@
 # Python solution for 2026-04-15
+# Sorted Array Swap
+def sort_and_swap(arr):
+    arr = sorted(arr)
+
+    for i in range(3, len(arr), 3):
+        arr[i], arr[i - 1] = arr[i - 1], arr[i]
+
+    return arr
