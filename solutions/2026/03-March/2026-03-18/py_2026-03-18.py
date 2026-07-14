3dbe05783c112e59fbebc1b894f4c3ee71f267cf
Quoriee <snowii.torii@gmail.com>
2026-07-14 12:10:11 +0200

    largest number python solution

--- solutions/2026/03-March/2026-03-18/py_2026-03-18.py
+++ solutions/2026/03-March/2026-03-18/py_2026-03-18.py
@@ -1,1 +1,7 @@
 # Python solution for 2026-03-18
+# Largest Number
+import re
+def largest_number(s):
+    with_spaces = re.sub('[,!?:;]', ' ', s)
+    nums = map(float, with_spaces.split())
+    return max(nums)
