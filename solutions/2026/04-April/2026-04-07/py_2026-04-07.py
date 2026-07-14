da4fc6ec21ca7a5d2c6b21aeb3398aa09a2cabd2
Quoriee <snowii.torii@gmail.com>
2026-07-15 01:54:09 +0200

    palindrome characters python solution

--- solutions/2026/04-April/2026-04-07/py_2026-04-07.py
+++ solutions/2026/04-April/2026-04-07/py_2026-04-07.py
@@ -1,9 +1,10 @@
 # Python solution for 2026-04-07
 # Palindrome Characters
 def palindrome_locator(s):
+    str_length = len(s)
     if s == s[::-1]:
-        if len(s) % 2 == 1:
-            return s[len(s)//2]
+        if str_length % 2 == 1:
+            return s[str_length//2]
         else:
-            return s[len(s)//2-1] + s[len(s)//2]
+            return s[str_length//2-1] + s[str_length//2]
     return 'none'
