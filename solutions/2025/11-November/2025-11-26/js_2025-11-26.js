ded58dd3953c8d5c7a9b52b3aebb6cc8d05f3d9a
Quoriee <snowii.torii@gmail.com>
2026-07-14 17:01:19 +0200

    buzzfizz js solution

--- solutions/2025/11-November/2025-11-26/js_2025-11-26.js
+++ solutions/2025/11-November/2025-11-26/js_2025-11-26.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2025-11-26
+// BuzzFizz
+function isFizzBuzz(sequence) {
+  for (let i = 1; i <= sequence.length; i++) {
+    if (i % 15 === 0) {
+      if (sequence[i - 1] !== 'FizzBuzz') return false;
+    } else if (i % 3 === 0) {
+      if (sequence[i - 1] !== 'Fizz') return false;
+    } else if (i % 5 === 0) {
+      if (sequence[i - 1] !== 'Buzz') return false;
+    } else if (sequence[i - 1] !== i) {
+      return false;
+    }
+  }
+
+  return true;
+}
