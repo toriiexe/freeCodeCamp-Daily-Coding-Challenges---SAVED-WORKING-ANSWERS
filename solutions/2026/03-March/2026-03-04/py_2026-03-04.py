20d3cd7fe52cef5a547fcb3c127b64962aee82df
Quoriee <snowii.torii@gmail.com>
2026-07-14 10:51:57 +0200

    playing card values python solution

--- solutions/2026/03-March/2026-03-04/py_2026-03-04.py
+++ solutions/2026/03-March/2026-03-04/py_2026-03-04.py
@@ -1,1 +1,20 @@
 # Python solution for 2026-03-04
+# Playing Card Values
+def card_values(cards):
+    letters_to_values = {
+        'A': 1,
+        'J': 10,
+        'Q': 10,
+        'K': 10
+    }
+
+    res = []
+
+    for card in cards:
+        value = card[:-1]
+        if value.isdigit():
+            res.append(int(value))
+        else:
+            res.append(letters_to_values[value])
+
+    return res
