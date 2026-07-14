273a7e64e9277318993b30268d23f4f465c68344
Quoriee <snowii.torii@gmail.com>
2026-07-14 10:42:01 +0200

    array insertion python solution

--- solutions/2026/03-March/2026-03-10/py_2026-03-10.py
+++ solutions/2026/03-March/2026-03-10/py_2026-03-10.py
@@ -1,1 +1,18 @@
 # Python solution for 2026-03-10
+# Array Insertion
+def insert_into_array(arr, value, index):
+    arr.append(None)
+
+    for i in range(len(arr) - 1, index, -1):
+        arr[i] = arr[i - 1]
+
+    arr[index] = value
+    return arr
+
+
+"""
+# Array Insertion - SHORTER VERSION
+def insert_into_array(arr, value, index):
+    arr.insert(index, value)
+    return arr
+"""
