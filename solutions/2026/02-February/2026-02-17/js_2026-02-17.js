b2c5e55e914129b27cb51be97b7524f51508ef97
Quoriee <snowii.torii@gmail.com>
2026-07-14 10:18:36 +0200

    2026 winter games day 12: bobsled js solution

--- solutions/2026/02-February/2026-02-17/js_2026-02-17.js
+++ solutions/2026/02-February/2026-02-17/js_2026-02-17.js
@@ -1,1 +1,20 @@
 // JavaScript solution for 2026-02-17
+// 2026 Winter Games Day 12: Bobsled
+function checkEligibility(athleteWeights, sledWeight) {
+    const minSled = {
+        1: 162,
+        2: 170,
+        4: 210
+    };
+
+    const maxTotal = {
+        1: 247,
+        2: 390,
+        4: 630
+    };
+
+    const teamSize = athleteWeights.length;
+    const totalWeight = athleteWeights.reduce((sum, weight) => sum + weight, 0) + sledWeight;
+
+    return sledWeight >= minSled[teamSize] && totalWeight <= maxTotal[teamSize] ? "Eligible" : "Not Eligible";
+}
