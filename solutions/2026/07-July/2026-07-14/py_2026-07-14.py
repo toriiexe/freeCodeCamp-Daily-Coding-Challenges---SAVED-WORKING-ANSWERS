5415161780fa80db8a0de4a7e528f294a28df510
Quoriee <snowii.torii@gmail.com>
2026-07-14 10:20:38 +0200

    pet age calculator python solution

--- solutions/2026/07-July/2026-07-14/py_2026-07-14.py
+++ solutions/2026/07-July/2026-07-14/py_2026-07-14.py
@@ -1,1 +1,14 @@
 # Python solution for 2026-07-14
+# Pet Age Calculator
+def pet_years(pet, age):
+    multiplier = {
+        'dog': 7,
+        'cat': 6,
+        'rabbit': 8,
+        'hamster': 30,
+        'guinea pig': 12,
+        'goldfish': 6,
+        'bird': 5
+    }
+
+    return age * multiplier[pet]
