71163ac6b174067d37b9488e114e397c958f9567
Quoriee <snowii.torii@gmail.com>
2026-07-14 14:47:05 +0200

    fizzbuzz python solution

--- solutions/2025/11-November/2025-11-25/py_2025-11-25.py
+++ solutions/2025/11-November/2025-11-25/py_2025-11-25.py
@@ -1,1 +1,4 @@
 # Python solution for 2025-11-25
+# FizzBuzz
+def fizz_buzz(n):
+    return ['FizzBuzz' if i % 15 == 0 else 'Fizz' if i % 3 == 0 else 'Buzz' if i % 5 == 0 else i for i in range(1, n + 1)]
