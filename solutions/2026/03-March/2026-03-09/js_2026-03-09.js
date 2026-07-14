b682137c68f4e284a6513bb3c81bb4d7ff3e8055
Quoriee <snowii.torii@gmail.com>
2026-07-14 10:35:24 +0200

    array sum js solution

--- solutions/2026/03-March/2026-03-09/js_2026-03-09.js
+++ solutions/2026/03-March/2026-03-09/js_2026-03-09.js
@@ -1,1 +1,18 @@
 // JavaScript solution for 2026-03-09
+// Array Sum
+function sumArray(numbers) {
+    let total = 0;
+
+    for (const num of numbers){
+        total += num;
+    }
+    
+    return total;
+}
+
+/*
+// Array Sum - SHORTER VERSION
+function sumArray(numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+}
+*/
