2cb430efc0e3b9fcbbf17b61e82102907532220c
Quoriee <snowii.torii@gmail.com>
2026-07-14 10:22:46 +0200

    pet age calculator js solution

--- solutions/2026/07-July/2026-07-14/js_2026-07-14.js
+++ solutions/2026/07-July/2026-07-14/js_2026-07-14.js
@@ -1,1 +1,15 @@
 // JavaScript solution for 2026-07-14
+// Pet Age Calculator
+function petYears(pet, age) {
+    const multiplier = {
+        'dog': 7,
+        'cat': 6,
+        'rabbit': 8,
+        'hamster': 30,
+        'guinea pig': 12,
+        'goldfish': 6,
+        'bird': 5
+    }
+
+    return age * multiplier[pet];
+}
