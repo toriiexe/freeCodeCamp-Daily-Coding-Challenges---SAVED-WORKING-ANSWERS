324c2c52c64bac6aafa9bac897a87dbc63da52ea
Quoriee <snowii.torii@gmail.com>
2026-07-14 10:17:30 +0200

    2026 winter games day 12: bobsled python solution

--- solutions/2026/02-February/2026-02-17/py_2026-02-17.py
+++ solutions/2026/02-February/2026-02-17/py_2026-02-17.py
@@ -1,1 +1,19 @@
 # Python solution for 2026-02-17
+# 2026 Winter Games Day 12: Bobsled
+def check_eligibility(athlete_weights, sled_weight):
+    sled = {
+        1: 162,
+        2: 170,
+        4: 210
+    }
+
+    sled_with_athlethes = {
+        1: 247,
+        2: 390,
+        4: 630
+    }
+
+    team_size = len(athlete_weights)
+    total_weight = sum(athlete_weights) + sled_weight
+
+    return 'Eligible' if (sled_weight >= sled[team_size] and (sled_with_athlethes[team_size] >= total_weight)) else 'Not Eligible'
