95128f14f95410b90ee3343230823305edb9ea7b
Quoriee <snowii.torii@gmail.com>
2026-07-14 10:55:49 +0200

    playing card values js solution

--- solutions/2026/03-March/2026-03-04/js_2026-03-04.js
+++ solutions/2026/03-March/2026-03-04/js_2026-03-04.js
@@ -1,1 +1,23 @@
 // JavaScript solution for 2026-03-04
+// Playing Card Values
+function cardValues(cards) {
+    const lettersToValues = {
+        'A': 1,
+        'J': 10,
+        'Q': 10,
+        'K': 10
+    }
+
+    const res = [];
+
+    for (const card of cards){
+        const value = card.slice(0, -1);
+        if (/^\d+$/.test(value)){
+            res.push(Number(value));
+        } else{
+            res.push(lettersToValues[value] ?? 0);
+        }
+    }
+
+    return res;
+}
