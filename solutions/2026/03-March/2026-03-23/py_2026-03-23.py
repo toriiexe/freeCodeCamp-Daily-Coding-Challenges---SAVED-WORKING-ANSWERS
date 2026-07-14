efa17b29ec4fe21aff588ebc9e453301f1f3dd2d
Quoriee <snowii.torii@gmail.com>
2026-07-14 11:56:19 +0200

    no consecutive repeats python solution

--- solutions/2026/03-March/2026-03-23/py_2026-03-23.py
+++ solutions/2026/03-March/2026-03-23/py_2026-03-23.py
@@ -1,1 +1,7 @@
 # Python solution for 2026-03-23
+# No Consecutive Repeats
+def has_no_repeats(s):
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            return False
+    return True
