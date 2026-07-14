7b113315c6519b90d32f493628f9bb701d2054e6
Quoriee <snowii.torii@gmail.com>
2026-07-15 01:53:23 +0200

    palindrome characters js solution

--- solutions/2026/04-April/2026-04-07/js_2026-04-07.js
+++ solutions/2026/04-April/2026-04-07/js_2026-04-07.js
@@ -1,1 +1,13 @@
 // JavaScript solution for 2026-04-07
+// Palindrome Characters
+function palindromeLocator(str) {
+    const strLength = str.length;
+    if (str === str.split('').reverse().join('')) {
+        if (strLength % 2 === 1) {
+            return str.slice(strLength / 2, strLength / 2 + 1);
+        } else {
+            return str.slice(strLength/ 2 - 1, strLength / 2) + str.slice(strLength / 2, strLength / 2 + 1);
+        }
+    }
+    return 'none';
+}
