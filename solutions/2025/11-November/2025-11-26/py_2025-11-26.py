eada8da55186889d94b8d8ccc001752b76129539
Quoriee <snowii.torii@gmail.com>
2026-07-14 17:00:36 +0200

    buzzfizz python solution

--- solutions/2025/11-November/2025-11-26/py_2025-11-26.py
+++ solutions/2025/11-November/2025-11-26/py_2025-11-26.py
@@ -1,1 +1,17 @@
 # Python solution for 2025-11-26
+# BuzzFizz
+def is_fizz_buzz(sequence):
+    for i in range(1, len(sequence) + 1):
+        if i % 15 == 0:
+            if sequence[i - 1] != 'FizzBuzz':
+                return False
+        elif i % 3 == 0:
+            if sequence[i - 1] != 'Fizz':
+                return False
+        elif i % 5 == 0:
+            if sequence[i - 1] != 'Buzz':
+                return False
+        elif sequence[i - 1] != i:
+            return False
+
+    return True
