e1cadb61989f2e7bfc8539779058cdfb13398e6e
Quoriee <snowii.torii@gmail.com>
2026-07-14 10:47:42 +0200

    array insertion js solution

--- solutions/2026/03-March/2026-03-10/js_2026-03-10.js
+++ solutions/2026/03-March/2026-03-10/js_2026-03-10.js
@@ -1,1 +1,22 @@
 // JavaScript solution for 2026-03-10
+// Array Insertion
+function insertIntoArray(arr, value, index) {
+    arr.push(null);
+
+    for (let i = arr.length - 1; i > index; i--){
+        arr[i] = arr[i - 1];
+    }
+
+    arr[index] = value;
+    
+    return arr;
+}
+
+
+/*
+// Array Insertion - SHORTER VERSION
+function insertIntoArray(arr, value, index) {
+    arr.splice(index, 0, value);
+    return arr;
+}
+*/
