5098e1198f48b8f0e1a35cba945b971e8d60cbb7
Quoriee <snowii.torii@gmail.com>
2026-07-15 01:46:06 +0200

    sorted array swap js solution

--- solutions/2026/04-April/2026-04-15/js_2026-04-15.js
+++ solutions/2026/04-April/2026-04-15/js_2026-04-15.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2026-04-15
+// Sorted Array Swap
+function sortAndSwap(arr) {
+    arr.sort((a, b) => a - b);
+    for (let i = 3; i < arr.length; i += 3){
+        let temp = arr[i];
+        arr[i] = arr[i-1];
+        arr[i-1] = temp;
+    }
+    return arr;
+}
