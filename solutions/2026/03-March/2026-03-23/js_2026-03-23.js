0e68f5d6c9ca9cc56fcbdca57d2fa7f266c4473f
Quoriee <snowii.torii@gmail.com>
2026-07-14 11:57:19 +0200

    no consecutive repeats js solution

--- solutions/2026/03-March/2026-03-23/js_2026-03-23.js
+++ solutions/2026/03-March/2026-03-23/js_2026-03-23.js
@@ -1,1 +1,10 @@
 // JavaScript solution for 2026-03-23
+// No Consecutive Repeats
+function hasNoRepeats(str) {
+    for (let i = 0; i < str.length-1; i++){
+        if (str[i] === str[i+1]){
+        return false;
+        }
+    }
+    return true;
+}
