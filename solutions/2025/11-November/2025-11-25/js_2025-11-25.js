046b4b6acf5b40b476ea28deb055efe411361fbd
Quoriee <snowii.torii@gmail.com>
2026-07-14 14:48:53 +0200

    fizzbuzz js solution

--- solutions/2025/11-November/2025-11-25/js_2025-11-25.js
+++ solutions/2025/11-November/2025-11-25/js_2025-11-25.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2025-11-25
+// FizzBuzz
+function fizzBuzz(n) {
+    const res = [];
+    for (let i = 1; i <= n; i++){
+        if (i % 15 === 0){
+        res.push('FizzBuzz');
+        } else if (i % 3 === 0){
+        res.push('Fizz');
+        } else if (i % 5 === 0){
+        res.push('Buzz');
+        } else{
+        res.push(i);
+        }
+    }
+    return res;
+}
