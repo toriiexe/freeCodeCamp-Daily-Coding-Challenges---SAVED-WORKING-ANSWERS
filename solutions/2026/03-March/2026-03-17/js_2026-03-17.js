fdb3ef9a8187c388332057943c35a0d7929cb012
Quoriee <snowii.torii@gmail.com>
2026-07-14 11:43:35 +0200

    anniversary milestones js solution

--- solutions/2026/03-March/2026-03-17/js_2026-03-17.js
+++ solutions/2026/03-March/2026-03-17/js_2026-03-17.js
@@ -1,1 +1,21 @@
 // JavaScript solution for 2026-03-17
+// Anniversary Milestones
+function getMilestone(years) {
+    const milestones = [
+        [70, 'Platinum'],
+        [60, 'Diamond'],
+        [50, 'Gold'],
+        [40, 'Ruby'],
+        [25, 'Silver'],
+        [10, 'Tin'],
+        [5, 'Wood'],
+        [1, 'Paper'],
+        [0, 'Newlyweds'],
+    ];
+
+    for (const [years_married, milestone] of milestones){
+        if (years >= years_married){
+        return milestone;
+        }
+    }
+}
