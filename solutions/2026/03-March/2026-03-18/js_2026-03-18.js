6e294c1352c5cc018403076413cae6391833f314
Quoriee <snowii.torii@gmail.com>
2026-07-14 12:13:01 +0200

    largest number js solution

--- solutions/2026/03-March/2026-03-18/js_2026-03-18.js
+++ solutions/2026/03-March/2026-03-18/js_2026-03-18.js
@@ -1,1 +1,7 @@
 // JavaScript solution for 2026-03-18
+// Largest Number
+function largestNumber(str) {
+    const withoutSeparators = str.replace(/[,!?:;]/g, ' ').trim().split(/\s+/);
+    const nums = withoutSeparators.map((n) => Number(n));
+    return Math.max(...nums);
+}
