2d052bb9f467b89e88b580df3bb54aed752f67e7
Quoriee <snowii.torii@gmail.com>
2026-07-14 11:35:11 +0200

    hsl validator python solution

--- solutions/2026/03-March/2026-03-08/py_2026-03-08.py
+++ solutions/2026/03-March/2026-03-08/py_2026-03-08.py
@@ -1,1 +1,15 @@
 # Python solution for 2026-03-08
+# HSL Validator
+import re
+def is_valid_hsl(hsl):
+    pattern = r"^hsl\(\s*\d+\s*,\s*\d+%\s*,\s*\d+%\s*\)\s*;?$"
+    
+    matched = re.fullmatch(pattern, hsl)
+    if not matched:
+        return False
+
+    values = re.fullmatch(r"hsl\(\s*(\d+)\s*,\s*(\d+)%\s*,\s*(\d+)%\s*\)\s*;?", hsl)
+
+    h, s, l = map(int, values.groups())
+
+    return 0 <= h <= 360 and 0 <= s <= 100 and 0 <= l <= 100
