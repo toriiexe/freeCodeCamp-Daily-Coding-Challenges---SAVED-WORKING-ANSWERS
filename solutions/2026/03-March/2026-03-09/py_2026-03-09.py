d2edcd71b5026160d5fbb21e377c2cbfb673edb0
Quoriee <snowii.torii@gmail.com>
2026-07-14 10:32:34 +0200

    array sum python solution

--- solutions/2026/03-March/2026-03-09/py_2026-03-09.py
+++ solutions/2026/03-March/2026-03-09/py_2026-03-09.py
@@ -1,1 +1,15 @@
 # Python solution for 2026-03-09
+# Array Sum
+def sum_array(numbers):
+    c = 0
+    for num in numbers:
+        c += num
+    return c
+
+
+
+"""
+# Array Sum - SHORTER VERSION
+def sum_array(numbers):
+    return sum(numbers)
+"""
