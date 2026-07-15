a6a53606bdbfcb54623b7256cd6ee3cd2cb7c457
Quoriee <snowii.torii@gmail.com>
2026-07-15 02:35:26 +0200

    wake-up alarm js solution

--- solutions/2026/03-March/2026-03-31/js_2026-03-31.js
+++ solutions/2026/03-March/2026-03-31/js_2026-03-31.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2026-03-31
+// Wake-Up Alarm
+function alarmCheck(alarmTime, wakeTime) {
+    const [alarmH, alarmM] = alarmTime.split(':').map(Number);
+    const [wakeH, wakeM] = wakeTime.split(':').map(Number);
+
+    const alarmTotal = alarmH * 60 + alarmM;
+    const wakeTotal = wakeH * 60 + wakeM;
+
+    if (wakeTotal < alarmTotal) {
+        return 'early';
+    } else if (wakeTotal <= alarmTotal + 10) {
+        return 'on time';
+    } else {
+        return 'late';
+    }
+}
