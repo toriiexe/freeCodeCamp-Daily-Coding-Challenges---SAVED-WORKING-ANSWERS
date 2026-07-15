23df552c02ff1de699f1ed4da727c1fa479cb090
Quoriee <snowii.torii@gmail.com>
2026-07-16 01:56:31 +0200

    fibonacci sequence js solution

--- solutions/2025/08-August/2025-08-13/js_2025-08-13.js
+++ solutions/2025/08-August/2025-08-13/js_2025-08-13.js
@@ -1,1 +1,18 @@
 // JavaScript solution for 2025-08-13
+// Fibonacci Sequence
+function fibonacciSequence(startSequence, length) {
+    if (length === 0) {
+        return [];
+    }
+
+    let x = startSequence[0];
+    let y = startSequence[1];
+    let arr = startSequence.slice(0, length);
+
+    for (let i = 0; i < length - 2; i++) {
+        [x, y] = [y, y + x];
+        arr.push(y);
+    }
+
+    return arr;
+}
