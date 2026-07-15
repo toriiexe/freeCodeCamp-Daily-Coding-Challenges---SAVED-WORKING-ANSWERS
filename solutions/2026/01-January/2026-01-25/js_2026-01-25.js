d1e485b8c38489bd6a8de20e6271dabe0c482b20
Quoriee <snowii.torii@gmail.com>
2026-07-15 04:30:38 +0200

    scaled image js solution

--- solutions/2026/01-January/2026-01-25/js_2026-01-25.js
+++ solutions/2026/01-January/2026-01-25/js_2026-01-25.js
@@ -1,1 +1,6 @@
 // JavaScript solution for 2026-01-25
+// Scaled Image
+function scaleImage(size, scale) {
+    const [w, h] = size.split('x').map(Number);
+    return `${Math.round(w * scale)}x${Math.round(h * scale)}`;
+}
