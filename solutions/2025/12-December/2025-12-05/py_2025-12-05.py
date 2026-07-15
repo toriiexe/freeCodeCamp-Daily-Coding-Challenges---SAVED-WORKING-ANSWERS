ceec42301ad8eb338b9b3efdd00ceb0a53e3e65e
Quoriee <snowii.torii@gmail.com>
2026-07-15 22:12:20 +0200

    symmetric difference python solution

--- solutions/2025/12-December/2025-12-05/py_2025-12-05.py
+++ solutions/2025/12-December/2025-12-05/py_2025-12-05.py
@@ -1,1 +1,14 @@
 # Python solution for 2025-12-05
+# Symmetric Difference
+def difference(arr1, arr2):
+    res = []
+
+    for x in arr1:
+        if x not in arr2 and x not in res:
+            res.append(x)
+
+    for y in arr2:
+        if y not in arr1 and y not in res:
+            res.append(y)
+
+    return res
