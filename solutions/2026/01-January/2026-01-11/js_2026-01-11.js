20b5f2c0b771d5ea0a1eb1478cc9a26dd50b2261
Quoriee <snowii.torii@gmail.com>
2026-07-15 03:41:05 +0200

    par for the hole js solution

--- solutions/2026/01-January/2026-01-11/js_2026-01-11.js
+++ solutions/2026/01-January/2026-01-11/js_2026-01-11.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2026-01-11
+// Par for the Hole
+function golfScore(par, strokes) {
+    if (strokes === 1) {
+        return 'Hole in one!';
+    } else if (strokes === par - 2) {
+        return 'Eagle';
+    } else if (strokes === par - 1) {
+        return 'Birdie';
+    } else if (par === strokes) {
+        return 'Par';
+    } else if (strokes === par + 1) {
+        return 'Bogey';
+    } else if (strokes === par + 2) {
+        return 'Double bogey';
+    }
+}
