36b3e179d8f46dae35a7ed82044621004ece5727
Quoriee <snowii.torii@gmail.com>
2026-07-16 01:59:25 +0200

    s p a c e j a m js solution

--- solutions/2025/08-August/2025-08-14/js_2025-08-14.js
+++ solutions/2025/08-August/2025-08-14/js_2025-08-14.js
@@ -1,1 +1,20 @@
 // JavaScript solution for 2025-08-14
+// S P A C E J A M
+function spaceJam(s) {
+    s = s.replaceAll(' ', '');
+    let res = '';
+
+    for (const c of s) {
+        res += c.toUpperCase() + '  ';
+    }
+
+    return res.trimEnd();
+}
+
+
+/*
+// S P A C E J A M - SHORTER VERSION
+function spaceJam(s) {
+    return s.replaceAll(' ', '').toUpperCase().split('').join('  ');
+}
+*/
