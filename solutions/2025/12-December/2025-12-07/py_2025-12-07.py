b7138dfed5fd66bc31d5aafdb537631420d22918
Quoriee <snowii.torii@gmail.com>
2026-07-15 22:44:48 +0200

    string compression python solution

--- solutions/2025/12-December/2025-12-07/py_2025-12-07.py
+++ solutions/2025/12-December/2025-12-07/py_2025-12-07.py
@@ -1,1 +1,26 @@
 # Python solution for 2025-12-07
+# String Compression
+def compress_string(sentence):
+    words = []
+    res = ''
+
+    for w in sentence.split():
+        if not words:
+            words.append(w)
+            continue
+        
+        if w == words[0]:
+            words.append(w)
+        else:
+            if len(words) > 1:
+                res += f'{words[0]}({len(words)}) '
+            else:
+                res += f'{words[0]} '
+            words = [w]
+
+    if len(words) > 1:
+        res += f'{words[0]}({len(words)})'
+    else:
+        res += words[0]
+
+    return res
