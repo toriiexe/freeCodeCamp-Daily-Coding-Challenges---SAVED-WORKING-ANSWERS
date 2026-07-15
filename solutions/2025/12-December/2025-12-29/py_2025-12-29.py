9e4a7c47d37a39ffb13932abed38688264f911d4
Quoriee <snowii.torii@gmail.com>
2026-07-15 04:43:06 +0200

    takeoff fuel python solution

--- solutions/2025/12-December/2025-12-29/py_2025-12-29.py
+++ solutions/2025/12-December/2025-12-29/py_2025-12-29.py
@@ -1,1 +1,11 @@
 # Python solution for 2025-12-29
+# Takeoff Fuel
+import math
+def fuel_to_add(current_gallons, required_liters):
+    gallon = 3.78541
+    current_liters = current_gallons * gallon
+    diff = current_liters - required_liters
+    if diff >= 0:
+        return 0
+    else:
+        return math.ceil(abs(diff / gallon))
