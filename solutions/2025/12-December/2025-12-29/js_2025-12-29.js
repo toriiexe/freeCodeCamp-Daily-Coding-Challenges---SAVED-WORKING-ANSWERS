77f4d9a19dbfa7376ffb265ececf7a463ca6071c
Quoriee <snowii.torii@gmail.com>
2026-07-15 04:43:47 +0200

    takeoff fuel js solution

--- solutions/2025/12-December/2025-12-29/js_2025-12-29.js
+++ solutions/2025/12-December/2025-12-29/js_2025-12-29.js
@@ -1,1 +1,13 @@
 // JavaScript solution for 2025-12-29
+// Takeoff Fuel
+function fuelToAdd(currentGallons, requiredLiters) {
+    const gallon = 3.78541;
+    const currentLiters = currentGallons * gallon;
+    const diff = currentLiters - requiredLiters;
+
+    if (diff >= 0) {
+        return 0;
+    } else {
+        return Math.ceil(Math.abs(diff / gallon));
+    }
+}
