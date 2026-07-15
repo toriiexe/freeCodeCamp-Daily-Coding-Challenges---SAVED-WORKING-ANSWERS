d4996df877a91bb2252d7ce179feb68fe4cd1dcf
Quoriee <snowii.torii@gmail.com>
2026-07-15 03:29:37 +0200

    class average python solution

--- solutions/2026/01-January/2026-01-22/py_2026-01-22.py
+++ solutions/2026/01-January/2026-01-22/py_2026-01-22.py
@@ -1,1 +1,23 @@
 # Python solution for 2026-01-22
+# Class Average
+def get_average_grade(scores):
+    avg_score = sum(scores) / len(scores)
+    grades = [
+        (97, "A+"),
+        (93, "A"),
+        (90, "A-"),
+        (87, "B+"),
+        (83, "B"),
+        (80, "B-"),
+        (77, "C+"),
+        (73, "C"),
+        (70, "C-"),
+        (67, "D+"),
+        (63, "D"),
+        (60, "D-"),
+        (0, "F")
+    ]
+
+    for minimum, grade in grades:
+        if avg_score >= minimum:
+            return grade
