07d4f04cb7fc92841d0c1487c0d602ae9d098c5b
Quoriee <snowii.torii@gmail.com>
2026-07-15 22:54:21 +0200

    purge most frequent js solution

--- solutions/2025/12-December/2025-12-20/js_2025-12-20.js
+++ solutions/2025/12-December/2025-12-20/js_2025-12-20.js
@@ -1,1 +1,13 @@
 // JavaScript solution for 2025-12-20
+// Purge Most Frequent
+function purgeMostFrequent(arr) {
+    const counts = {};
+
+    for (const el of arr) {
+        counts[el] = (counts[el] || 0) + 1;
+    }
+
+    const most = Math.max(...Object.values(counts));
+
+    return arr.filter(el => counts[el] !== most);
+}
