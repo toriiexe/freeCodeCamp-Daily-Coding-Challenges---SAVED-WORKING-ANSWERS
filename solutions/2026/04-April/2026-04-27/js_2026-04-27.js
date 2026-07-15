cfda2496f0f29607db5f0ef3472c9ed2a6b7bae6
Quoriee <snowii.torii@gmail.com>
2026-07-15 05:06:26 +0200

    word score js solution

--- solutions/2026/04-April/2026-04-27/js_2026-04-27.js
+++ solutions/2026/04-April/2026-04-27/js_2026-04-27.js
@@ -1,1 +1,5 @@
 // JavaScript solution for 2026-04-27
+// Word Score
+function getWordScore(word) {
+    return [...word].reduce((s, w) => s + w.toUpperCase().charCodeAt(0) - 'A'.charCodeAt(0) + 1, 0);
+}
