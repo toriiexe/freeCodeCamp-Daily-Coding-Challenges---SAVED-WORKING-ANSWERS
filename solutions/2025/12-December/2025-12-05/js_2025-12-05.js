bebe354a0ea2aeee393b23492c58e9a3c3f495d5
Quoriee <snowii.torii@gmail.com>
2026-07-15 22:14:05 +0200

    symmetric difference js solution

--- solutions/2025/12-December/2025-12-05/js_2025-12-05.js
+++ solutions/2025/12-December/2025-12-05/js_2025-12-05.js
@@ -1,1 +1,19 @@
 // JavaScript solution for 2025-12-05
+// Symmetric Difference
+function difference(arr1, arr2) {
+    const res = [];
+
+    for (const x of arr1){
+        if (!(arr2.includes(x)) && !(res.includes(x))){
+            res.push(x);
+        }
+    }
+
+    for (const y of arr2){
+        if (!(arr1.includes(y)) && !(res.includes(y))){
+            res.push(y);
+        }
+    }
+
+    return res;
+}
