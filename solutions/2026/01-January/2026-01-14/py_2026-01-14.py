7877ea51511bbdfd66ffceca0c820f3d84bd309c
Quoriee <snowii.torii@gmail.com>
2026-07-15 04:36:20 +0200

    markdown link parser python solution

--- solutions/2026/01-January/2026-01-14/py_2026-01-14.py
+++ solutions/2026/01-January/2026-01-14/py_2026-01-14.py
@@ -1,1 +1,7 @@
 # Python solution for 2026-01-14
+# Markdown Link Parser
+def parse_link(markdown):
+    text, href = markdown.split('](')
+    text = text[1:]
+    href = href[:-1]
+    return f'<a href="{href}">{text}</a>'
