b44f9758ad3bc3d4aaf57e9345ed7527ab355ff7
Quoriee <snowii.torii@gmail.com>
2026-07-15 03:15:12 +0200

    pascal's triangle row python solution

--- solutions/2026/03-March/2026-03-28/py_2026-03-28.py
+++ solutions/2026/03-March/2026-03-28/py_2026-03-28.py
@@ -1,1 +1,29 @@
 # Python solution for 2026-03-28
+# Pascal's Triangle Row
+def pascal_row(n):
+    row = [1]
+    
+    for i in range(n-1):
+        new_row = [1]
+        for j in range(len(row) - 1):
+            new_row.append(row[j] + row[j + 1])
+        new_row.append(1)
+        row = new_row
+    
+    return row
+
+"""
+def pascal_row(n):
+    pascal = [[1]]
+    
+    for i in range(1, n):
+        temp = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                temp.append(1)
+            else:
+                temp.append(pascal[i-1][j-1] + pascal[i-1][j])
+        pascal.append(temp)
+    
+    return pascal[-1]
+"""
