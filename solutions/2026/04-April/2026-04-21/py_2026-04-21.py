779738fa547b45181ebe364826e0d79f1e4a9aa2
Quoriee <snowii.torii@gmail.com>
2026-07-15 05:00:52 +0200

    odd words python solution

--- solutions/2026/04-April/2026-04-21/py_2026-04-21.py
+++ solutions/2026/04-April/2026-04-21/py_2026-04-21.py
@@ -1,1 +1,8 @@
 # Python solution for 2026-04-21
+# Odd Words
+def get_odd_words(s):
+    res = []
+    for word in s.split():
+        if len(word) % 2 == 1:
+            res.append(word)
+    return ' '.join(res)
