38e33e07709e05c562f07719162f6eae86e052ba
Quoriee <snowii.torii@gmail.com>
2026-07-15 03:00:43 +0200

    isbn-10 validator js solution

--- solutions/2026/03-March/2026-03-29/js_2026-03-29.js
+++ solutions/2026/03-March/2026-03-29/js_2026-03-29.js
@@ -1,1 +1,18 @@
 // JavaScript solution for 2026-03-29
+// ISBN-10 Validator
+function isValidIsbn10(str) {
+    str = str.replace(/-/g, '');
+
+    if (!/^[0-9]{9}[0-9X]$/.test(str)) {
+        return false;
+    }
+
+    let total = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        let value = str[i] === 'X' ? 10 : Number(str[i]);
+        total += value * (i + 1);
+    }
+
+    return total % 11 === 0;
+}
