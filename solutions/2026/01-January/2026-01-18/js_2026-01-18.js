50f6df624456c3867a9b58726a83dfa9d5a88016
Quoriee <snowii.torii@gmail.com>
2026-07-15 03:23:57 +0200

    free shipping js solution

--- solutions/2026/01-January/2026-01-18/js_2026-01-18.js
+++ solutions/2026/01-January/2026-01-18/js_2026-01-18.js
@@ -1,1 +1,16 @@
 // JavaScript solution for 2026-01-18
+// Free Shipping
+function getsFreeShipping(cart, minimum) {
+    const items = {
+        shirt: 34.25,
+        jeans: 48.50,
+        shoes: 75.00,
+        hat: 19.95,
+        socks: 15.00,
+        jacket: 109.95
+    };
+    
+    const total = cart.reduce((sum, item) => sum + items[item], 0);
+
+    return total >= minimum;
+}
