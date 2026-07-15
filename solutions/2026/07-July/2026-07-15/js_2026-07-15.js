b8d3540c2a0b41e257e009baf8938600a0fda942
Quoriee <snowii.torii@gmail.com>
2026-07-15 22:00:20 +0200

    array chunks js solution

--- solutions/2026/07-July/2026-07-15/js_2026-07-15.js
+++ solutions/2026/07-July/2026-07-15/js_2026-07-15.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2026-07-15
+// Array Chunks
+function chunkArray(arr, size) {
+    if (size <= 0){
+        throw new Error('Invalid size value');
+    }
+    const res = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        res.push(arr.slice(i, i + size));
+    }
+
+    return res;
+}
