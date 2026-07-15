ad56984d3d83dce7f956fa3ed1457ef6367ba672
Quoriee <snowii.torii@gmail.com>
2026-07-15 02:33:39 +0200

    wake-up alarm readme

--- solutions/2026/03-March/2026-03-31/py_2026-03-31.py
+++ solutions/2026/03-March/2026-03-31/py_2026-03-31.py
@@ -1,1 +1,15 @@
 # Python solution for 2026-03-31
+# Wake-Up Alarm
+def alarm_check(alarm_time, wake_time):
+    alarm_h, alarm_m = alarm_time.split(':')
+    alarm_total = int(alarm_h) * 60 + int(alarm_m)
+
+    wake_h, wake_m = wake_time.split(':')
+    wake_total = int(wake_h) * 60 + int(wake_m)
+
+    if wake_total < alarm_total:
+        return 'early'
+    elif wake_total <= alarm_total + 10:
+        return 'on time'
+    else:
+        return 'late'
