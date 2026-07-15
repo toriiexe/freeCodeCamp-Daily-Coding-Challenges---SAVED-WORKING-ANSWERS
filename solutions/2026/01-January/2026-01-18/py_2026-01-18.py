80264f4cf55135fc143d18cf174d3abf304e6a77
Quoriee <snowii.torii@gmail.com>
2026-07-15 03:22:06 +0200

    free shipping python solution

--- solutions/2026/01-January/2026-01-18/py_2026-01-18.py
+++ solutions/2026/01-January/2026-01-18/py_2026-01-18.py
@@ -1,1 +1,14 @@
 # Python solution for 2026-01-18
+# Free Shipping
+def gets_free_shipping(cart, minimum):
+    items = {
+        'shirt': 34.25,
+        'jeans': 48.50,
+        'shoes': 75.00,
+        'hat': 19.95,
+        'socks': 15.00,
+        'jacket': 109.95
+    }
+
+    total = sum([items[item] for item in cart])
+    return total >= minimum
