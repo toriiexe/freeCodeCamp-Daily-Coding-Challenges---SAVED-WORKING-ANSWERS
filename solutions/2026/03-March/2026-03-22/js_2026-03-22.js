b3b0e0cc95c3723d17c9d9f283bdf6dc272de68d
Quoriee <snowii.torii@gmail.com>
2026-07-15 02:40:31 +0200

    coffee roast detector js solution

--- solutions/2026/03-March/2026-03-22/js_2026-03-22.js
+++ solutions/2026/03-March/2026-03-22/js_2026-03-22.js
@@ -1,1 +1,25 @@
 // JavaScript solution for 2026-03-22
+//  Coffee Roast Detector
+function detectRoast(beans) {
+    let totalPoints = 0;
+
+    for (const bean of beans) {
+        if (bean === "'") {
+            totalPoints += 1;
+        } else if (bean === "-") {
+            totalPoints += 2;
+        } else if (bean === ".") {
+            totalPoints += 3;
+        }
+    }
+
+    const average = totalPoints / beans.length;
+
+    if (average < 1.75) {
+        return "Light";
+    } else if (average <= 2.5) {
+        return "Medium";
+    } else {
+        return "Dark";
+    }
+}
