f8c707380680c7ef4e108e9b72eb7d6811921511
Quoriee <snowii.torii@gmail.com>
2026-07-15 22:46:04 +0200

    string compression js solution

--- solutions/2025/12-December/2025-12-07/js_2025-12-07.js
+++ solutions/2025/12-December/2025-12-07/js_2025-12-07.js
@@ -1,1 +1,32 @@
 // JavaScript solution for 2025-12-07
+// String Compression
+function compressString(sentence) {
+    let words = [];
+    let res = '';
+
+    for (let w of sentence.split(' ')) {
+        if (words.length === 0) {
+            words.push(w);
+            continue;
+        }
+
+        if (w === words[0]) {
+            words.push(w);
+        } else {
+            if (words.length > 1) {
+                res += `${words[0]}(${words.length}) `;
+            } else {
+                res += `${words[0]} `;
+            }
+            words = [w];
+        }
+    }
+
+    if (words.length > 1) {
+        res += `${words[0]}(${words.length})`;
+    } else {
+        res += words[0];
+    }
+
+    return res;
+}
