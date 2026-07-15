50e49abaa70ef9a5cf7602badf350baa616eb72e
Quoriee <snowii.torii@gmail.com>
2026-07-15 02:59:35 +0200

    isbn-10 validator python solution

--- solutions/2026/03-March/2026-03-29/py_2026-03-29.py
+++ solutions/2026/03-March/2026-03-29/py_2026-03-29.py
@@ -1,1 +1,16 @@
 # Python solution for 2026-03-29
+# ISBN-10 Validator
+import re
+def is_valid_isbn10(s):
+    s = s.replace('-', '')
+
+    if not re.fullmatch(r'[0-9]{9}[0-9X]', s):
+        return False
+
+    total = 0
+
+    for i, char in enumerate(s, start=1):
+        value = 10 if char == 'X' else int(char)
+        total += value * i
+
+    return total % 11 == 0
