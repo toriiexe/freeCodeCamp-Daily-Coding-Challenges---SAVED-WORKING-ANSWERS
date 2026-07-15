55adb24003910d04c1c7d40be3b6a3e64b702f72
Quoriee <snowii.torii@gmail.com>
2026-07-15 03:51:04 +0200

    knight moves js solution

--- solutions/2026/01-January/2026-01-17/js_2026-01-17.js
+++ solutions/2026/01-January/2026-01-17/js_2026-01-17.js
@@ -1,1 +1,30 @@
 // JavaScript solution for 2026-01-17
+// Knight Moves
+function knightMoves(position) {
+    const directions = [
+        [1, 2],
+        [2, 1],
+        [2, -1],
+        [1, -2],
+        [-1, -2],
+        [-2, -1],
+        [-2, 1],
+        [-1, 2]
+    ];
+
+    const column = position.charCodeAt(0) - 'A'.charCodeAt(0);
+    const row = Number(position[1]) - 1;
+
+    let validMoves = [];
+
+    for (const move of directions) {
+        const newColumn = column + move[0];
+        const newRow = row + move[1];
+
+        if (newColumn >= 0 && newColumn <= 7 && newRow >= 0 && newRow <= 7) {
+            validMoves.push([newColumn, newRow]);
+        }
+    }
+
+    return validMoves.length;
+}
