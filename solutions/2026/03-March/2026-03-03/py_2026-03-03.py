cd23265ca4aa241e5c981bc5dfcab17321c8d5f1
Quoriee <snowii.torii@gmail.com>
2026-07-15 02:47:24 +0200

    perfect cube count python solution

--- solutions/2026/03-March/2026-03-03/py_2026-03-03.py
+++ solutions/2026/03-March/2026-03-03/py_2026-03-03.py
@@ -1,1 +1,11 @@
 # Python solution for 2026-03-03
+# Perfect Cube Count
+def count_perfect_cubes(a, b):
+    s, e = sorted((a, b))
+    counter = 0
+
+    for i in range(int(-abs(e)**(1/3))-1, int(abs(e)**(1/3))+2):
+        if s <= i**3 <= e:
+            counter += 1
+
+    return counter
