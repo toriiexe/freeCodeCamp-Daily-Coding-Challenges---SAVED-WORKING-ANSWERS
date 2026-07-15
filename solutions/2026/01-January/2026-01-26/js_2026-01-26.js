38453a9e7fa3d4f225e4cda1b44117cbc74ec126
Quoriee <snowii.torii@gmail.com>
2026-07-15 03:19:28 +0200

    fizzbuzz mini js solution

--- solutions/2026/01-January/2026-01-26/js_2026-01-26.js
+++ solutions/2026/01-January/2026-01-26/js_2026-01-26.js
@@ -1,1 +1,8 @@
 // JavaScript solution for 2026-01-26
+// FizzBuzz Mini
+function fizzBuzzMini(n) {
+    if (n % 15 === 0) return 'FizzBuzz';
+    if (n % 3 == 0) return 'Fizz';
+    if (n % 5 == 0) return 'Buzz';
+    return n.toString();
+}
