bb46587cc62888b101149efbe0c52c8906a94717
Quoriee <snowii.torii@gmail.com>
2026-07-15 03:15:41 +0200

    pascal's triangle row js solution

--- solutions/2026/03-March/2026-03-28/js_2026-03-28.js
+++ solutions/2026/03-March/2026-03-28/js_2026-03-28.js
@@ -1,1 +1,18 @@
 // JavaScript solution for 2026-03-28
+// Pascal's Triangle Row
+function pascalRow(n) {
+    let row = [1];
+
+    for (let i = 0; i < n - 1; i++) {
+        let newRow = [1];
+
+        for (let j = 0; j < row.length - 1; j++) {
+            newRow.push(row[j] + row[j + 1]);
+        }
+
+        newRow.push(1);
+        row = newRow;
+    }
+
+    return row;
+}
