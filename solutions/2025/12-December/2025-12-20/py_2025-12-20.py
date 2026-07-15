2c68a8959bbab1c129da44cd6bf9d32f171a5fed
Quoriee <snowii.torii@gmail.com>
2026-07-15 22:53:53 +0200

    purge most frequent python solution

--- solutions/2025/12-December/2025-12-20/py_2025-12-20.py
+++ solutions/2025/12-December/2025-12-20/py_2025-12-20.py
@@ -1,1 +1,36 @@
 # Python solution for 2025-12-20
+# Purge Most Frequent
+def purge_most_frequent(arr):
+    res = []
+    most_frequent = max([arr.count(item) for item in arr])
+
+    for el in arr:
+        if arr.count(el) != most_frequent:
+            res.append(el)
+            
+    return res
+
+"""
+# Purge Most Frequent - ANOTHER VERSION
+def purge_most_frequent(arr):
+    counts = {}
+
+    for el in arr:
+        counts[el] = counts.get(el, 0) + 1
+
+    most = max(counts.values())
+
+    return [el for el in arr if counts[el] != most]
+"""
+
+
+"""
+# Purge Most Frequent - ANOTHER VERSION
+from collections import Counter
+
+def purge_most_frequent(arr):
+    counts = Counter(arr)
+    most = max(counts.values())
+
+    return [el for el in arr if counts[el] != most]
+"""
