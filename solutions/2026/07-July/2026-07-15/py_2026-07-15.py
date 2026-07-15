0fa2ec6b4ebf172de13cc94667014c7d27d235a6
Quoriee <snowii.torii@gmail.com>
2026-07-15 21:58:51 +0200

    array chunks python solution

--- solutions/2026/07-July/2026-07-15/py_2026-07-15.py
+++ solutions/2026/07-July/2026-07-15/py_2026-07-15.py
@@ -1,1 +1,9 @@
 # Python solution for 2026-07-15
+# Array Chunks
+def chunk_array(arr, size):
+    res = []
+    
+    for i in range(0, len(arr), size):
+        res.append(arr[i:i+size])
+
+    return res
