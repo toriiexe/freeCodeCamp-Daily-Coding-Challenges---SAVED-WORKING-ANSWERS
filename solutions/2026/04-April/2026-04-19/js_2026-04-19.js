39616a82ccba98736e3baadcc007f84c1e511d0c
Quoriee <snowii.torii@gmail.com>
2026-07-15 02:24:32 +0200

    unique stair climber js solution

--- solutions/2026/04-April/2026-04-19/js_2026-04-19.js
+++ solutions/2026/04-April/2026-04-19/js_2026-04-19.js
@@ -1,1 +1,16 @@
 // JavaScript solution for 2026-04-19
+// Unique Stair Climber
+function getUniqueClimbs(steps) {
+    const dp = new Array(steps + 1).fill(0);
+    dp[0] = 1;
+
+    for (let i = 1; i <= steps; i++) {
+        dp[i] += dp[i - 1];
+
+        if (i >= 2) {
+            dp[i] += dp[i - 2];
+        }
+    }
+
+    return dp[steps];
+}
