4808a2f085703a3fd4e987d19e28ed32848f05f3
Quoriee <snowii.torii@gmail.com>
2026-07-15 02:08:24 +0200

    word compressor js solution

--- solutions/2026/04-April/2026-04-24/js_2026-04-24.js
+++ solutions/2026/04-April/2026-04-24/js_2026-04-24.js
@@ -1,1 +1,21 @@
 // JavaScript solution for 2026-04-24
+// Word Compressor
+function compress(str) {
+    const firstPos = new Map();
+    const result = [];
+
+    const words = str.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+
+        if (firstPos.has(word)) {
+            result.push(String(firstPos.get(word)));
+        } else {
+            firstPos.set(word, i + 1);
+            result.push(word);
+        }
+    }
+
+    return result.join(' ');
+}
