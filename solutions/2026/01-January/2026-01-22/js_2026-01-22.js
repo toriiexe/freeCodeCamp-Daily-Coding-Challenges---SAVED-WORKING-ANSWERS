e53c771083d1ab0f321600f65576f8490d225c2a
Quoriee <snowii.torii@gmail.com>
2026-07-15 03:30:33 +0200

    class average js solution

--- solutions/2026/01-January/2026-01-22/js_2026-01-22.js
+++ solutions/2026/01-January/2026-01-22/js_2026-01-22.js
@@ -1,1 +1,27 @@
 // JavaScript solution for 2026-01-22
+// Class Average
+function getAverageGrade(scores) {
+    const avgScore = scores.reduce((sum, score) => sum + score, 0) / scores.length;
+
+    const grades = [
+        [97, "A+"],
+        [93, "A"],
+        [90, "A-"],
+        [87, "B+"],
+        [83, "B"],
+        [80, "B-"],
+        [77, "C+"],
+        [73, "C"],
+        [70, "C-"],
+        [67, "D+"],
+        [63, "D"],
+        [60, "D-"],
+        [0, "F"]
+    ];
+
+    for (const [minimum, grade] of grades) {
+        if (avgScore >= minimum) {
+            return grade;
+        }
+    }
+}
