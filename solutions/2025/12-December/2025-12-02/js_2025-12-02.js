4d1304ff179be69b4ab20380b32f4a2c7aaac6b3
Quoriee <snowii.torii@gmail.com>
2026-07-15 22:23:11 +0200

    camel to snake js solution

--- solutions/2025/12-December/2025-12-02/js_2025-12-02.js
+++ solutions/2025/12-December/2025-12-02/js_2025-12-02.js
@@ -1,1 +1,7 @@
 // JavaScript solution for 2025-12-02
+// Camel to Snake
+function toSnake(camel) {
+    return camel.replace(/(?=[A-Z])/g, "_").toLowerCase();
+}
+
+// return camel.replace(/(?<!^)(?=[A-Z])/g, "_").toLowerCase(); // Prevents adding a leading underscore if the input starts with an uppercase letter.
