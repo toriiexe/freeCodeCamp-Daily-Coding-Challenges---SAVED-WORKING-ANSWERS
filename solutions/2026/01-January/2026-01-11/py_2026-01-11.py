09f072710be439c1d2f3cdf9e540d44ea4200821
Quoriee <snowii.torii@gmail.com>
2026-07-15 03:41:12 +0200

    par for the hole python solution

--- solutions/2026/01-January/2026-01-11/py_2026-01-11.py
+++ solutions/2026/01-January/2026-01-11/py_2026-01-11.py
@@ -1,1 +1,15 @@
 # Python solution for 2026-01-11
+# Par for the Hole
+def golf_score(par, strokes):
+    if strokes == 1:
+        return 'Hole in one!'
+    elif strokes == par - 2:
+        return 'Eagle'
+    elif strokes == par - 1:
+        return 'Birdie'
+    elif par == strokes:
+        return 'Par'
+    elif strokes == par + 1:
+        return 'Bogey'
+    elif strokes == par + 2:
+        return 'Double bogey'
