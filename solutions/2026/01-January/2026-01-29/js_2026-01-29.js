cec8a5deb62b272d0edb7b4ddff4a604bf589bdd
Quoriee <snowii.torii@gmail.com>
2026-07-15 04:22:29 +0200

    letters-numbers js solution

--- solutions/2026/01-January/2026-01-29/js_2026-01-29.js
+++ solutions/2026/01-January/2026-01-29/js_2026-01-29.js
@@ -1,1 +1,22 @@
 // JavaScript solution for 2026-01-29
+// Letters-Numbers
+function separateLettersAndNumbers(str) {
+    let prev = '';
+    let res = '';
+
+    for (const c of str) {
+        if ((/\d/.test(c) && /\d/.test(prev)) || (/[a-zA-Z]/.test(c) && /[a-zA-Z]/.test(prev))) {
+            res += c;
+        } 
+        else if ((/\d/.test(c) && /[a-zA-Z]/.test(prev)) || (/[a-zA-Z]/.test(c) && /\d/.test(prev))) {
+            res += '-' + c;
+        }
+        else {
+            res += c;
+        }
+
+        prev = c;
+    }
+
+    return res;
+}
