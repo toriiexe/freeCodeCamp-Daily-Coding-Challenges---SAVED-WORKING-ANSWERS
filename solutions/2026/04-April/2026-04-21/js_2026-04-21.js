bf423855bb6e66c6344f89086a22bb053d319496
Quoriee <snowii.torii@gmail.com>
2026-07-15 05:01:46 +0200

    odd words js solution

--- solutions/2026/04-April/2026-04-21/js_2026-04-21.js
+++ solutions/2026/04-April/2026-04-21/js_2026-04-21.js
@@ -1,1 +1,12 @@
 // JavaScript solution for 2026-04-21
+// Odd Words
+function getOddWords(str) {
+    const res = [];
+
+    for (const word of str.split(' ')){
+        if (word.length % 2 === 1){
+        res.push(word);
+        }
+    }
+    return res.join(' ');
+}
