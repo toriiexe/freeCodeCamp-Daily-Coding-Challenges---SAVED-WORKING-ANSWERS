c4320a865dd06fb6b4bfe82b5b620c4971728c40
Quoriee <snowii.torii@gmail.com>
2026-07-15 22:35:59 +0200

    daylight hours js solution

--- solutions/2025/12-December/2025-12-21/js_2025-12-21.js
+++ solutions/2025/12-December/2025-12-21/js_2025-12-21.js
@@ -1,1 +1,33 @@
 // JavaScript solution for 2025-12-21
+// Daylight Hours
+function daylightHours(latitude) {
+    const daylight = new Map([
+        [-90, 24],
+        [-75, 23],
+        [-60, 21],
+        [-45, 15],
+        [-30, 13],
+        [-15, 12],
+        [0, 12],
+        [15, 11],
+        [30, 10],
+        [45, 9],
+        [60, 6],
+        [75, 2],
+        [90, 0]
+    ]);
+
+    let closestLatitude = null;
+    let smallestDifference = Infinity;
+
+    for (const lat of daylight.keys()) {
+        const difference = Math.abs(lat - latitude);
+
+        if (difference < smallestDifference) {
+            smallestDifference = difference;
+            closestLatitude = lat;
+        }
+    }
+
+    return daylight.get(closestLatitude);
+}
