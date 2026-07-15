4434d37461bc6142d1a33ec02fae7e736dcd8289
Quoriee <snowii.torii@gmail.com>
2026-07-15 03:27:09 +0200

    energy consumption js solution

--- solutions/2026/01-January/2026-01-19/js_2026-01-19.js
+++ solutions/2026/01-January/2026-01-19/js_2026-01-19.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2026-01-19
+// Energy Consumption
+function compareEnergy(caloriesBurned, wattHoursUsed) {
+    const calsToJoules = caloriesBurned * 4184;
+    const wattToJoules = wattHoursUsed * 3600;
+
+    if (calsToJoules > wattToJoules) {
+        return 'Workout';
+    } else if (calsToJoules === wattToJoules) {
+        return 'Equal';
+    } else {
+        return 'Devices';
+    }
+}
