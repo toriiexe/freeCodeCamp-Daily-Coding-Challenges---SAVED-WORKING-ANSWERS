fa840d5b371d24f61b9147f32ec5636da568ca3c
Quoriee <snowii.torii@gmail.com>
2026-07-16 01:52:41 +0200

    vowel balance js solution

--- solutions/2025/08-August/2025-08-11/js_2025-08-11.js
+++ solutions/2025/08-August/2025-08-11/js_2025-08-11.js
@@ -1,1 +1,19 @@
 // JavaScript solution for 2025-08-11
+// Vowel Balance
+function isBalanced(s) {
+    let fHalf = 0;
+    let sHalf = 0;
+    const vowels = 'aeiouAEIOU';
+
+    for (let i = 0; i < s.length / 2; i++) {
+        if (vowels.includes(s[i])) {
+        fHalf++;
+        }
+
+        if (vowels.includes(s[s.length - i - 1])) {
+        sHalf++;
+        }
+    }
+
+    return fHalf === sHalf;
+}
