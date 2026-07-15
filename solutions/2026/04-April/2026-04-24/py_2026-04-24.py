5c02a467c153ac06cbcfb327ba6dee695a901b3f
Quoriee <snowii.torii@gmail.com>
2026-07-15 02:08:08 +0200

    word compressor python solution

--- solutions/2026/04-April/2026-04-24/py_2026-04-24.py
+++ solutions/2026/04-April/2026-04-24/py_2026-04-24.py
@@ -1,1 +1,14 @@
 # Python solution for 2026-04-24
+# Word Compressor
+def compress(s):
+    first_pos = {}
+    result = []
+
+    for i, word in enumerate(s.split(), start=1):
+        if word in first_pos:
+            result.append(str(first_pos[word]))
+        else:
+            first_pos[word] = i
+            result.append(word)
+
+    return ' '.join(result)
