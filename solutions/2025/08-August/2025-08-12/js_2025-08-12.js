fa5c50aae4e71ebbe4c6ffd2e93f17ccd450c553
Quoriee <snowii.torii@gmail.com>
2026-07-16 01:55:10 +0200

    base check js solution

--- solutions/2025/08-August/2025-08-12/js_2025-08-12.js
+++ solutions/2025/08-August/2025-08-12/js_2025-08-12.js
@@ -1,1 +1,13 @@
 // JavaScript solution for 2025-08-12
+// Base Check
+function isValidNumber(n, base) {
+    const valid = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'.slice(0,base);
+
+    for (const c of n.toUpperCase()){
+        if (!valid.includes(c)){
+            return false;
+        }
+    }
+
+    return true;
+}
