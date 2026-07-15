ae06087e4fd3f5b84a912848310386bc2147b61d
Quoriee <snowii.torii@gmail.com>
2026-07-15 04:19:01 +0200

    letters-numbers python solution

--- solutions/2026/01-January/2026-01-29/py_2026-01-29.py
+++ solutions/2026/01-January/2026-01-29/py_2026-01-29.py
@@ -1,1 +1,17 @@
 # Python solution for 2026-01-29
+# Letters-Numbers
+def separate_letters_and_numbers(s):
+    prev = ''
+    res = ''
+
+    for c in s:
+        if prev and ((c.isdigit() and prev.isdigit()) or (c.isalpha() and prev.isalpha())):
+            res += c
+        elif prev and ((c.isdigit() and prev.isalpha()) or (c.isalpha() and prev.isdigit())):
+            res += '-' + c
+        else:
+            res += c
+
+        prev = c
+
+    return res
