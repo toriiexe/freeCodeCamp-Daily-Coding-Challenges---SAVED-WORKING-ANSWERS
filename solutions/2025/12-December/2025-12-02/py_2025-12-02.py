e85ea371d995ee96309e9bf34a1f0cb6d6758fb2
Quoriee <snowii.torii@gmail.com>
2026-07-15 22:22:04 +0200

    camel to snake python solution

--- solutions/2025/12-December/2025-12-02/py_2025-12-02.py
+++ solutions/2025/12-December/2025-12-02/py_2025-12-02.py
@@ -1,1 +1,8 @@
 # Python solution for 2025-12-02
+# Camel to Snake
+import re
+def to_snake(camel):
+    return re.sub(r'(?=[A-Z])', '_', camel).lower()
+
+
+# return re.sub(r'(?<!^)(?=[A-Z])', '_', camel).lower() # Prevents adding a leading underscore if the string starts with an uppercase letter.
