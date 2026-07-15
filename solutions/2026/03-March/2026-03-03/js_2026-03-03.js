ba2ed0479524e8d1b07595dedc7ce9fddb2e5e63
Quoriee <snowii.torii@gmail.com>
2026-07-15 02:49:18 +0200

    perfect cube count js solution

--- solutions/2026/03-March/2026-03-03/js_2026-03-03.js
+++ solutions/2026/03-March/2026-03-03/js_2026-03-03.js
@@ -1,1 +1,16 @@
 // JavaScript solution for 2026-03-03
+// Perfect Cube Count
+function countPerfectCubes(a, b) {
+    let [s, e] = [a, b].sort((x, y) => x - y);
+    let counter = 0;
+
+    let root = Math.abs(e) ** (1 / 3);
+
+    for (let i = -Math.floor(root) - 1; i <= Math.floor(root) + 1; i++) {
+        if (s <= i ** 3 && i ** 3 <= e) {
+            counter++;
+        }
+    }
+
+    return counter;
+}
