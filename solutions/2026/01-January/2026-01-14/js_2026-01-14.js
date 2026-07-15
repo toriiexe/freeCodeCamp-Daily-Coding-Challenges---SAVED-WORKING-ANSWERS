d808a1ab5c328e44796a985017e0e5ca1fa54869
Quoriee <snowii.torii@gmail.com>
2026-07-15 04:38:03 +0200

    markdown link parser js solution

--- solutions/2026/01-January/2026-01-14/js_2026-01-14.js
+++ solutions/2026/01-January/2026-01-14/js_2026-01-14.js
@@ -1,1 +1,8 @@
 // JavaScript solution for 2026-01-14
+// Markdown Link Parser
+function parseLink(markdown) {
+    let [text, href] = markdown.split('](');
+    text = text.slice(1);
+    href = href.slice(0, -1);
+    return `<a href="${href}">${text}</a>`;
+}
