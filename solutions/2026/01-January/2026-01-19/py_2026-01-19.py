9eeaba4a5837cce2139a561379d3e6419d0ca24c
Quoriee <snowii.torii@gmail.com>
2026-07-15 03:26:35 +0200

    energy consumption python solution

--- solutions/2026/01-January/2026-01-19/py_2026-01-19.py
+++ solutions/2026/01-January/2026-01-19/py_2026-01-19.py
@@ -1,1 +1,12 @@
 # Python solution for 2026-01-19
+# Energy Consumption
+def compare_energy(calories_burned, watt_hours_used):
+    cals_to_joules = calories_burned * 4184
+    watt_to_joules = watt_hours_used * 3600
+    
+    if cals_to_joules > watt_to_joules:
+        return 'Workout'
+    elif cals_to_joules == watt_to_joules:
+        return 'Equal'
+    else:
+        return 'Devices'
