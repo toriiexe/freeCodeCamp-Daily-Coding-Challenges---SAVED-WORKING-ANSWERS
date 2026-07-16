5e0369f1159c362d63de29fa4d3e80b505ae6cfc
Quoriee <snowii.torii@gmail.com>
2026-07-16 17:11:05 +0200

    snowflake generator python solution

--- solutions/2025/12-December/2025-12-25/py_2025-12-25.py
+++ solutions/2025/12-December/2025-12-25/py_2025-12-25.py
@@ -1,1 +1,10 @@
 # Python solution for 2025-12-25
+# Snowflake Generator
+def generate_snowflake(crystals):
+    lines = crystals.split('\n')
+    res = []
+    
+    for line in lines:
+        res.append(line + line[::-1])
+        
+    return '\n'.join(res)
