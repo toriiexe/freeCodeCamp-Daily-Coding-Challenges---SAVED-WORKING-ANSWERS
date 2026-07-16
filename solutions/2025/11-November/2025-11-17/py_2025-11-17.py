d3e1c7fe088714ca7a23783f486dbb4b764e646a
Quoriee <snowii.torii@gmail.com>
2026-07-16 21:33:16 +0200

    fingerprint test python solution

--- solutions/2025/11-November/2025-11-17/py_2025-11-17.py
+++ solutions/2025/11-November/2025-11-17/py_2025-11-17.py
@@ -1,1 +1,16 @@
 # Python solution for 2025-11-17
+# Fingerprint Test
+def is_match(fingerprint_a, fingerprint_b):
+    if not isinstance(fingerprint_a, str) or not isinstance(fingerprint_b, str):
+        return False
+
+    if len(fingerprint_a) != len(fingerprint_b):
+        return False
+
+    differences = 0
+
+    for a, b in zip(fingerprint_a, fingerprint_b):
+        if a != b:
+            differences += 1
+
+    return differences / len(fingerprint_a) <= 0.1
