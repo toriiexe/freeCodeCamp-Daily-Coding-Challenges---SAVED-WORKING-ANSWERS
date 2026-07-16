1a45efb384a5e6211cb1b62516f646b9e31c8bbe
Quoriee <snowii.torii@gmail.com>
2026-07-16 16:34:15 +0200

    reverse parenthesis python solution

--- solutions/2025/08-August/2025-08-26/py_2025-08-26.py
+++ solutions/2025/08-August/2025-08-26/py_2025-08-26.py
@@ -1,1 +1,15 @@
 # Python solution for 2025-08-26
+# Reverse Parenthesis
+def decode(s):
+    stack = ['']
+
+    for ch in s:
+        if ch == '(':
+            stack.append('')
+        elif ch == ')':
+            temp = stack.pop()[::-1]
+            stack[-1] += temp
+        else:
+            stack[-1] += ch
+
+    return stack[0]
