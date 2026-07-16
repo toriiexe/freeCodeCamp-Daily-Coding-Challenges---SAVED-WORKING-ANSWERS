18cf3ac7803f4c50f74086b203eec18d19a860ee
Quoriee <snowii.torii@gmail.com>
2026-07-16 02:22:43 +0200

    second best js solution

--- solutions/2025/08-August/2025-08-28/js_2025-08-28.js
+++ solutions/2025/08-August/2025-08-28/js_2025-08-28.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2025-08-28
+// Second Best
+function getLaptopCost(laptops, budget) {
+    const laptopSorted = [...new Set(laptops)].sort((a, b) => b - a);
+
+    if (laptopSorted.length > 1 && budget >= laptopSorted[1]) {
+        return laptopSorted[1];
+    }
+
+    for (const laptop of laptopSorted) {
+        if (budget >= laptop) {
+            return laptop;
+        }
+    }
+
+    return 0;
+}
