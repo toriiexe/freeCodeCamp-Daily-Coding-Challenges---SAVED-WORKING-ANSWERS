bf8c329c41f4391dc676586586d58bc0ef264613
Quoriee <snowii.torii@gmail.com>
2026-07-16 02:11:50 +0200

    mile pace js solution

--- solutions/2025/08-August/2025-08-21/js_2025-08-21.js
+++ solutions/2025/08-August/2025-08-21/js_2025-08-21.js
@@ -1,1 +1,13 @@
 // JavaScript solution for 2025-08-21
+// Mile Pace
+function milePace(miles, duration) {
+    const [m, s] = duration.split(':');
+
+    const t = Number(m) * 60 + Number(s);
+    const av = Math.round(t / miles);
+
+    const mm = Math.floor(av / 60);
+    const ss = av % 60;
+
+    return `${String(mm).padStart(2, '0')}:${String(ss).padStart(2, '0')}`;
+}
