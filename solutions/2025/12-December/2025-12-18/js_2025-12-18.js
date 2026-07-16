1628efced01569c1fb4cf8d334e42bcb87af0983
Quoriee <snowii.torii@gmail.com>
2026-07-16 16:57:29 +0200

    checkerboard js solution

--- solutions/2025/12-December/2025-12-18/js_2025-12-18.js
+++ solutions/2025/12-December/2025-12-18/js_2025-12-18.js
@@ -1,1 +1,23 @@
 // JavaScript solution for 2025-12-18
+// Checkerboard
+function createBoard(dimensions) {
+    const rows = dimensions[0];
+    const cols = dimensions[1];
+
+    const res = [];
+
+    for (let r = 0; r < rows; r++){
+        const temp = [];
+        
+        for (let c = 0; c < cols; c++){
+            if ((r + c) % 2 == 0){
+                temp.push('X');
+            } else{
+                temp.push('O');
+            }
+        }
+        res.push(temp);
+    }
+
+    return res;
+}
