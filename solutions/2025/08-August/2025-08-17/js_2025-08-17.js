c328e98bb59eafbd89dbeea6a3283ba14f68b4de
Quoriee <snowii.torii@gmail.com>
2026-07-16 02:05:27 +0200

    targeted sum js solution

--- solutions/2025/08-August/2025-08-17/js_2025-08-17.js
+++ solutions/2025/08-August/2025-08-17/js_2025-08-17.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2025-08-17
+// Targeted Sum
+function findTarget(arr, target) {
+    const d = new Map();
+
+    for (let i = 0; i < arr.length; i++) {
+        const needed = target - arr[i];
+
+        if (d.has(needed)) {
+            return [d.get(needed), i];
+        }
+
+        d.set(arr[i], i);
+    }
+
+    return "Target not found";
+}
