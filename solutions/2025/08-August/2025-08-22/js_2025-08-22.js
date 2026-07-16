35b7f7790e8b0fad0dfe05aa41a9a42f5970b698
Quoriee <snowii.torii@gmail.com>
2026-07-16 02:13:16 +0200

    message decoder js solution

--- solutions/2025/08-August/2025-08-22/js_2025-08-22.js
+++ solutions/2025/08-August/2025-08-22/js_2025-08-22.js
@@ -1,1 +1,19 @@
 // JavaScript solution for 2025-08-22
+// Message Decoder
+function decode(message, shift) {
+    const alphaU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const alphaL = 'abcdefghijklmnopqrstuvwxyz';
+    let s = '';
+
+    for (const c of message) {
+        if (alphaU.includes(c)) {
+            s += alphaU[(alphaU.indexOf(c) - shift + 26) % 26];
+        } else if (alphaL.includes(c)) {
+            s += alphaL[(alphaL.indexOf(c) - shift + 26) % 26];
+        } else {
+            s += c;
+        }
+    }
+
+    return s;
+}
