59907a57c078538ff2d24fdb5330d2fa8bfcd138
Quoriee <snowii.torii@gmail.com>
2026-07-16 21:42:09 +0200

    message validator js solution

--- solutions/2025/11-November/2025-11-24/js_2025-11-24.js
+++ solutions/2025/11-November/2025-11-24/js_2025-11-24.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2025-11-24
+// Message Validator
+function isValidMessage(message, validator) {
+    const words = message.toLowerCase().split(' ');
+    const validation = validator.toLowerCase();
+
+    if (words.length !== validation.length){
+        return false;
+    }
+
+    for (let i = 0; i < validation.length; i++){
+        if (words[i][0] !== validation[i]){
+            return false;
+        }
+    }
+    return true;
+}
