06894f4179feb1e7663e447c373bab9398c4fbf1
Quoriee <snowii.torii@gmail.com>
2026-07-16 16:54:56 +0200

    checker board pyhon solution

--- solutions/2025/12-December/2025-12-18/py_2025-12-18.py
+++ solutions/2025/12-December/2025-12-18/py_2025-12-18.py
@@ -1,1 +1,16 @@
 # Python solution for 2025-12-18
+# Checkerboard
+def create_board(dimensions):
+    rows, cols = dimensions[0], dimensions[1]
+    res = []
+
+    for r in range(rows):
+        temp = []
+        for c in range(cols):
+            if (r + c) % 2 == 0:
+                temp.append('X')
+            else:
+                temp.append('O')
+        res.append(temp)
+
+    return res
