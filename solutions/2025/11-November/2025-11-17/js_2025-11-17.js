b54c4d960f4a190e05f033a0f1b6281b749d5f84
Quoriee <snowii.torii@gmail.com>
2026-07-16 21:36:42 +0200

    fingerprint test js solution

--- solutions/2025/11-November/2025-11-17/js_2025-11-17.js
+++ solutions/2025/11-November/2025-11-17/js_2025-11-17.js
@@ -1,1 +1,21 @@
 // JavaScript solution for 2025-11-17
+// Fingerprint Test
+function isMatch(fingerprintA, fingerprintB) {
+    if (typeof fingerprintA !== 'string' || typeof fingerprintB !== 'string') {
+        return false;
+    }
+
+    if (fingerprintA.length !== fingerprintB.length) {
+        return false;
+    }
+
+    let differences = 0;
+
+    for (let i = 0; i < fingerprintA.length; i++) {
+        if (fingerprintA[i] !== fingerprintB[i]) {
+            differences++;
+        }
+    }
+
+    return differences / fingerprintA.length <= 0.1;
+}
