e1a8e2e2554daedf31b8a5cffae2c77aa2651893
Quoriee <snowii.torii@gmail.com>
2026-07-16 21:46:19 +0200

    extension extractor js solution

--- solutions/2025/11-November/2025-11-10/js_2025-11-10.js
+++ solutions/2025/11-November/2025-11-10/js_2025-11-10.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2025-11-10
+// Extension Extractor
+function getExtension(filename) {
+    const parts = filename.split('.');
+
+    if (parts.length === 1 || filename[filename.length - 1] === '.') {
+        return "none";
+    }
+
+    return parts[parts.length - 1];
+}
