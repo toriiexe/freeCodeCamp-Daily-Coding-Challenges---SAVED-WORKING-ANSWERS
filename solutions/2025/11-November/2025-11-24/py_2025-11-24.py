9fec414e7a0af4e0e9ce770d3cce0637257b0411
Quoriee <snowii.torii@gmail.com>
2026-07-16 21:39:48 +0200

    message validator python solution

--- solutions/2025/11-November/2025-11-24/py_2025-11-24.py
+++ solutions/2025/11-November/2025-11-24/py_2025-11-24.py
@@ -1,1 +1,14 @@
 # Python solution for 2025-11-24
+# Message Validator
+def is_valid_message(message, validation):
+    words = message.lower().split()
+    validation = validation.lower()
+
+    if len(words) != len(validation):
+        return False
+
+    for i in range(len(validation)):
+        if words[i][0] != validation[i]:
+            return False
+
+    return True
