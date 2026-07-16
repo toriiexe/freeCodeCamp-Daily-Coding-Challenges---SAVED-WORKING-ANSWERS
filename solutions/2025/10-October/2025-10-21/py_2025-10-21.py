b5fae2f2d553a7047e62b8fb5f61fe51131cd6e6
Quoriee <snowii.torii@gmail.com>
2026-07-17 01:45:29 +0200

    thermostat adjuster 2 python solution

--- solutions/2025/10-October/2025-10-21/py_2025-10-21.py
+++ solutions/2025/10-October/2025-10-21/py_2025-10-21.py
@@ -1,1 +1,11 @@
 # Python solution for 2025-10-21
+# Thermostat Adjuster 2
+def adjust_thermostat(current_f, target_c):
+    target_f = (target_c * 1.8) + 32
+
+    if current_f < target_f:
+        return f'Heat: {(target_f - current_f):.1f} degrees Fahrenheit'
+    elif current_f > target_f:
+        return f'Cool: {(current_f - target_f):.1f} degrees Fahrenheit'
+
+    return 'Hold'
