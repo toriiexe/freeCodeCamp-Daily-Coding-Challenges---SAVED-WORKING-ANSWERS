703215861874f50f5b98c0da25f47eea0230e166
Quoriee <snowii.torii@gmail.com>
2026-07-16 02:35:51 +0200

    unorder of operations python solution

--- solutions/2025/08-August/2025-08-27/py_2025-08-27.py
+++ solutions/2025/08-August/2025-08-27/py_2025-08-27.py
@@ -1,1 +1,21 @@
 # Python solution for 2025-08-27
+# Unorder of Operations
+def evaluate(numbers, operators):
+    op_count = len(operators)
+    total = numbers[0]
+
+    for i in range(1, len(numbers)):
+        op = operators[(i - 1) % op_count]
+
+        if op == '+':
+            total += numbers[i]
+        elif op == '-':
+            total -= numbers[i]
+        elif op == '*':
+            total *= numbers[i]
+        elif op == '/':
+            total /= numbers[i]
+        elif op == '%':
+            total %= numbers[i]
+
+    return total
