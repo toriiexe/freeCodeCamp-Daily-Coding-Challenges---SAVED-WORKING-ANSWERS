62524a2e6a069543b63225dabe9318bcb7751d41
Quoriee <snowii.torii@gmail.com>
2026-07-16 02:09:10 +0200

    sum of squares js solution

--- solutions/2025/08-August/2025-08-19/js_2025-08-19.js
+++ solutions/2025/08-August/2025-08-19/js_2025-08-19.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2025-08-19
+// Sum of Squares
+function sumOfSquares(n) {
+    let s = 0;
+
+    for (let i = 1; i <= n; i++){
+        s += i * i;
+    }
+
+    return s;
+}
