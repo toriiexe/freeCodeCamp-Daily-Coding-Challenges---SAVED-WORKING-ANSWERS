73e3f2458f9438a317e5903e24d0ace29dd65505
Quoriee <snowii.torii@gmail.com>
2026-07-16 02:14:22 +0200

    unnatural prime js solution

--- solutions/2025/08-August/2025-08-23/js_2025-08-23.js
+++ solutions/2025/08-August/2025-08-23/js_2025-08-23.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2025-08-23
+// Unnatural Prime
+function isUnnaturalPrime(n) {
+    const num = Math.abs(n);
+
+    if (num <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+        return false;
+        }
+    }
+
+    return true;
+}
