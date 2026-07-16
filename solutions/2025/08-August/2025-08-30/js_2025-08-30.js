531e4edcb07f02219d021f4e82ede97552950252
Quoriee <snowii.torii@gmail.com>
2026-07-16 02:27:52 +0200

    array duplicates js solution

--- solutions/2025/08-August/2025-08-30/js_2025-08-30.js
+++ solutions/2025/08-August/2025-08-30/js_2025-08-30.js
@@ -1,1 +1,18 @@
 // JavaScript solution for 2025-08-30
+// Array Duplicates
+function findDuplicates(arr) {
+    const count = {};
+    const res = [];
+
+    for (const num of arr) {
+        count[num] = (count[num] || 0) + 1;
+    }
+
+    for (const num in count) {
+        if (count[num] > 1) {
+            res.push(Number(num));
+        }
+    }
+
+    return res.sort((a, b) => a - b);
+}
