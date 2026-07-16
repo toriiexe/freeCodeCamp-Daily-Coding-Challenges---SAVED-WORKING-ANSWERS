9df53116c15a9249f5ef6e21ba0a62281f436db5
Quoriee <snowii.torii@gmail.com>
2026-07-16 23:57:54 +0200

    markdown heading converter js solution

--- solutions/2025/11-November/2025-11-19/js_2025-11-19.js
+++ solutions/2025/11-November/2025-11-19/js_2025-11-19.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2025-11-19
+// Markdown Heading Converter
+function convert(heading) {
+    const match = heading.match(/^\s*(#{1,6})\s+(.+)$/);
+
+    if (!match) {
+        return 'Invalid format';
+    }
+
+    const level = match[1].length;
+    const text = match[2];
+
+    return `<h${level}>${text}</h${level}>`;
+}
