e1112063790b57e1e2d11418a681cc28d0c4b4ce
Quoriee <snowii.torii@gmail.com>
2026-07-16 03:01:34 +0200

    character battle js solution

--- solutions/2025/08-August/2025-08-24/js_2025-08-24.js
+++ solutions/2025/08-August/2025-08-24/js_2025-08-24.js
@@ -1,1 +1,39 @@
 // JavaScript solution for 2025-08-24
+// Character Battle
+function battle(myArmy, opposingArmy) {
+    if (myArmy.length > opposingArmy.length) {
+        return 'Opponent retreated';
+    }
+
+    if (myArmy.length < opposingArmy.length) {
+        return 'We retreated';
+    }
+
+    const values = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    let myWins = 0;
+    let oppWins = 0;
+
+    for (let i = 0; i < myArmy.length; i++) {
+        const m = myArmy[i];
+        const o = opposingArmy[i];
+
+        const mVal = values.includes(m) ? values.indexOf(m) + 1 : !isNaN(m) && m !== '' ? Number(m) : 0;
+
+        const oVal = values.includes(o) ? values.indexOf(o) + 1 : !isNaN(o) && o !== '' ? Number(o) : 0;
+
+        if (mVal > oVal) {
+            myWins++;
+        } else if (oVal > mVal) {
+            oppWins++;
+        }
+    }
+
+    if (myWins > oppWins) {
+        return 'We won';
+    } else if (oppWins > myWins) {
+        return 'We lost';
+    }
+
+    return 'It was a tie';
+}
