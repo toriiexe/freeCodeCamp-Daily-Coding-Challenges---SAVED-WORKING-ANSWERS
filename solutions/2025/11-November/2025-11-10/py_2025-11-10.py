1176d3e6ffb626fef8db57dd8b0f5ec44fba6487
Quoriee <snowii.torii@gmail.com>
2026-07-16 21:44:21 +0200

    extension extractor python solution

--- solutions/2025/11-November/2025-11-10/py_2025-11-10.py
+++ solutions/2025/11-November/2025-11-10/py_2025-11-10.py
@@ -1,1 +1,4 @@
 # Python solution for 2025-11-10
+# Extension Extractor
+def get_extension(filename):
+    return 'none' if (filename.count('.') == 0 or filename.endswith('.')) else filename.split('.')[-1]
