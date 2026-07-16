3b42bc48265841b29d02c39a80e93bf21e90710b
Quoriee <snowii.torii@gmail.com>
2026-07-16 23:56:56 +0200

    markdown heading converter python solution

--- solutions/2025/11-November/2025-11-19/py_2025-11-19.py
+++ solutions/2025/11-November/2025-11-19/py_2025-11-19.py
@@ -1,1 +1,13 @@
 # Python solution for 2025-11-19
+# Markdown Heading Converter
+import re
+def convert(heading):
+    m = re.fullmatch(r'\s*(#{1,6})\s+(.+)', heading)
+
+    if not m:
+        return 'Invalid format'
+
+    level = len(m.group(1))
+    text = m.group(2)
+
+    return f'<h{level}>{text}</h{level}>'
