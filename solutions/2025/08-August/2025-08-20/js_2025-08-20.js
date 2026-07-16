28d0289150a708f5f2b05450ecda06e08e1dbfdf
Quoriee <snowii.torii@gmail.com>
2026-07-16 02:10:32 +0200

    3 strikes js solution

--- solutions/2025/08-August/2025-08-20/js_2025-08-20.js
+++ solutions/2025/08-August/2025-08-20/js_2025-08-20.js
@@ -1,1 +1,13 @@
 // JavaScript solution for 2025-08-20
+// 3 Strikes
+function squaresWithThree(n) {
+    let count = 0;
+
+    for (let i = 1; i <= n; i++) {
+        if ((i * i).toString().includes('3')) {
+        count++;
+        }
+    }
+
+    return count;
+}
