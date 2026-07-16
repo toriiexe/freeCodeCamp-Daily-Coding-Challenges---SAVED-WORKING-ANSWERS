cda12d472f30d38c8b6638f426b517aeaa26f6ca
Quoriee <snowii.torii@gmail.com>
2026-07-16 17:10:36 +0200

    snowflake generator js solution

--- solutions/2025/12-December/2025-12-25/js_2025-12-25.js
+++ solutions/2025/12-December/2025-12-25/js_2025-12-25.js
@@ -1,1 +1,12 @@
 // JavaScript solution for 2025-12-25
+// Snowflake Generator
+function generateSnowflake(crystals) {
+    const lines = crystals.split('\n');
+    const res = [];
+
+    for (const line of lines) {
+        res.push(line + line.split('').reverse().join(''));
+    }
+
+    return res.join('\n');
+}
