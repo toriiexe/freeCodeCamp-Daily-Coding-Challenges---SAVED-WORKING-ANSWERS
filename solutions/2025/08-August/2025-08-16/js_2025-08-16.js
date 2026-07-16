7c66da8bcba2875be437886de6cad06ab9fd4ef8
Quoriee <snowii.torii@gmail.com>
2026-07-16 02:02:13 +0200

    anagram checker js solution

--- solutions/2025/08-August/2025-08-16/js_2025-08-16.js
+++ solutions/2025/08-August/2025-08-16/js_2025-08-16.js
@@ -1,1 +1,8 @@
 // JavaScript solution for 2025-08-16
+// Anagram Checker
+function areAnagrams(str1, str2) {
+    const s1 = str1.toLowerCase().split('').sort().join('');
+    const s2 = str2.toLowerCase().split('').sort().join('');
+    
+    return s1 === s2;
+}
