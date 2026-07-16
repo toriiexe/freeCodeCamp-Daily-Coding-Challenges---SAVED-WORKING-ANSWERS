d306ca51459f221a7045a7102555aabed9c281d4
Quoriee <snowii.torii@gmail.com>
2026-07-16 02:36:31 +0200

    unorder of operations js solution

--- solutions/2025/08-August/2025-08-27/js_2025-08-27.js
+++ solutions/2025/08-August/2025-08-27/js_2025-08-27.js
@@ -1,1 +1,24 @@
 // JavaScript solution for 2025-08-27
+// Unorder of Operations
+function evaluate(numbers, operators) {
+    const opCount = operators.length;
+    let total = numbers[0];
+
+    for (let i = 1; i < numbers.length; i++) {
+        const op = operators[(i - 1) % opCount];
+
+        if (op === '+') {
+            total += numbers[i];
+        } else if (op === '-') {
+            total -= numbers[i];
+        } else if (op === '*') {
+            total *= numbers[i];
+        } else if (op === '/') {
+            total /= numbers[i];
+        } else if (op === '%') {
+            total %= numbers[i];
+        }
+    }
+
+    return total;
+}
