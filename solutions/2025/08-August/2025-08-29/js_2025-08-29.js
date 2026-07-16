fca98498a1de6add1be8738fda99b391dd40c102
Quoriee <snowii.torii@gmail.com>
2026-07-16 02:23:44 +0200

    candlelight js solution

--- solutions/2025/08-August/2025-08-29/js_2025-08-29.js
+++ solutions/2025/08-August/2025-08-29/js_2025-08-29.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2025-08-29
+// Candlelight
+function burnCandles(candles, leftoversNeeded) {
+    let burned = 0;
+    let leftovers = 0;
+    let current = candles;
+
+    while (current > 0) {
+        burned += current;
+        leftovers += current;
+
+        current = Math.floor(leftovers / leftoversNeeded);
+        leftovers = leftovers % leftoversNeeded;
+    }
+
+    return burned;
+}
