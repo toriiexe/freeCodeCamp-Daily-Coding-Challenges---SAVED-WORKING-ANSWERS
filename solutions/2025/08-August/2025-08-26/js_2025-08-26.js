4a0d7052339c38c46f2082f8da8143c62f28928d
Quoriee <snowii.torii@gmail.com>
2026-07-16 16:35:15 +0200

    reverse parenthesis js solution

--- solutions/2025/08-August/2025-08-26/js_2025-08-26.js
+++ solutions/2025/08-August/2025-08-26/js_2025-08-26.js
@@ -1,1 +1,18 @@
 // JavaScript solution for 2025-08-26
+// Reverse Parenthesis
+function decode(s) {
+    const stack = [''];
+
+    for (const ch of s) {
+        if (ch === '(') {
+            stack.push('');
+        } else if (ch === ')') {
+            const reversed = stack.pop().split('').reverse().join('');
+            stack[stack.length - 1] += reversed;
+        } else {
+            stack[stack.length - 1] += ch;
+        }
+    }
+
+    return stack[0];
+}
