7fabd4dbc27df1e9f544e63a449f110fccf02a59
Quoriee <snowii.torii@gmail.com>
2026-07-18 00:45:44 +0200

    array shift js solution

--- solutions/2025/11-November/2025-11-13/js_2025-11-13.js
+++ solutions/2025/11-November/2025-11-13/js_2025-11-13.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2025-11-13
+// Array Shift
+function shiftArray(arr, n) {
+    const mod = n % arr.length;
+
+    if (n >= 0) {
+        return arr.slice(mod).concat(arr.slice(0, mod));
+    } else {
+        return arr.slice(mod).concat(arr.slice(0, mod));
+    }
+}
