b1148c2026864e47598727a98190b9882935d301
Quoriee <snowii.torii@gmail.com>
2026-07-17 22:27:42 +0200

    space week day 6: moon phase js solution

--- solutions/2025/10-October/2025-10-09/js_2025-10-09.js
+++ solutions/2025/10-October/2025-10-09/js_2025-10-09.js
@@ -1,1 +1,19 @@
 // JavaScript solution for 2025-10-09
+// Space Week Day 6: Moon Phase
+function moonPhase(dateString) {
+    const reference = new Date('2000-01-06');
+    const target = new Date(dateString);
+
+    const diff = (target - reference) / (1000 * 60 * 60 * 24);
+    const cycleDay = (diff % 28) + 1;
+
+    if (cycleDay <= 7) {
+        return 'New';
+    } else if (cycleDay <= 14) {
+        return 'Waxing';
+    } else if (cycleDay <= 21) {
+        return 'Full';
+    } else {
+        return 'Waning';
+    }
+}
