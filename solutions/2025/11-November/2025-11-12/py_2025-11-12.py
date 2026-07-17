455e4daeee7036625b3c83cb481b77e742cd97d2
Quoriee <snowii.torii@gmail.com>
2026-07-18 00:29:06 +0200

    email signature generator python solution

--- solutions/2025/11-November/2025-11-12/py_2025-11-12.py
+++ solutions/2025/11-November/2025-11-12/py_2025-11-12.py
@@ -1,1 +1,11 @@
 # Python solution for 2025-11-12
+# Email Signature Generator
+def generate_signature(name, title, company):
+    if 'A' <= name[0].upper() <= 'I':
+        prefix = '>>'
+    elif 'J' <= name[0].upper() <= 'R':
+        prefix = '--'
+    elif 'S' <= name[0].upper() <= 'Z':
+        prefix = '::'
+
+    return f'{prefix}{name}, {title} at {company}'
