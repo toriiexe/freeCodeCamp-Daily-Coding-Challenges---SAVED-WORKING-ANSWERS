09b47a8f05ea7f1848813829fe7191357bf325fb
Quoriee <snowii.torii@gmail.com>
2026-07-18 01:09:12 +0200

    recipe scaler python solution

--- solutions/2025/11-November/2025-11-22/py_2025-11-22.py
+++ solutions/2025/11-November/2025-11-22/py_2025-11-22.py
@@ -1,1 +1,13 @@
 # Python solution for 2025-11-22
+# Recipe Scaler
+def scale_recipe(ingredients, scale):
+    res = []
+
+    for ingredient in ingredients:
+        quantity, unit, name = ingredient.split(' ', 2)
+
+        quantity = float(quantity) * scale
+
+        res.append(f'{quantity:g} {unit} {name}')
+
+    return res
