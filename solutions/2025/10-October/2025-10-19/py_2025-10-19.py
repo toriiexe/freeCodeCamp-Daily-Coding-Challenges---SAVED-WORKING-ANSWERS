446ece1cc6f843e4622ab579b37c1f625a7ad440
Quoriee <snowii.torii@gmail.com>
2026-07-17 11:26:36 +0200

    html attribute extractor python solution

--- solutions/2025/10-October/2025-10-19/py_2025-10-19.py
+++ solutions/2025/10-October/2025-10-19/py_2025-10-19.py
@@ -1,1 +1,6 @@
 # Python solution for 2025-10-19
+# HTML Attribute Extractor
+import re
+def extract_attributes(element):
+    matches = re.findall(r'([a-z]+)="([^"]*)"', element)
+    return [f"{name}, {value}" for name, value in matches]
