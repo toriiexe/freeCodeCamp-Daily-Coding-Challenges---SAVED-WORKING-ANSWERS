d7b331d0c4c3ce302302ea38501b8c59731abcac
Quoriee <snowii.torii@gmail.com>
2026-07-18 00:31:04 +0200

    email signature generator js solution

--- solutions/2025/11-November/2025-11-12/js_2025-11-12.js
+++ solutions/2025/11-November/2025-11-12/js_2025-11-12.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2025-11-12
+// Email Signature Generator
+function generateSignature(name, title, company) {
+    let prefix = '';
+
+    const firstLetter = name[0].toUpperCase();
+
+    if ('A' <= firstLetter && firstLetter <= 'I') {
+        prefix = '>>';
+    } else if ('J' <= firstLetter && firstLetter <= 'R') {
+        prefix = '--';
+    } else if ('S' <= firstLetter && firstLetter <= 'Z') {
+        prefix = '::';
+    }
+
+    return `${prefix}${name}, ${title} at ${company}`;
+}
