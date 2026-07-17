7ab8cc0c9829d839e52d4ca3314091e820c76c18
Quoriee <snowii.torii@gmail.com>
2026-07-18 01:11:43 +0200

    recipe scaler js solution

--- solutions/2025/11-November/2025-11-22/js_2025-11-22.js
+++ solutions/2025/11-November/2025-11-22/js_2025-11-22.js
@@ -1,1 +1,15 @@
 // JavaScript solution for 2025-11-22
+// Recipe Scaler
+function scaleRecipe(ingredients, scale) {
+    const res = [];
+
+    for (const ingredient of ingredients) {
+        let [quantity, unit, ...name] = ingredient.split(' ');
+
+        quantity = (Number(quantity) * scale).toString();
+
+        res.push(`${quantity} ${unit} ${name.join(' ')}`);
+    }
+
+    return res;
+}
