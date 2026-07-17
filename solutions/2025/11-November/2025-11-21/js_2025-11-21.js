addc9283ea17bca4603f648f704618b9b22e80f3
Quoriee <snowii.torii@gmail.com>
2026-07-18 01:04:53 +0200

    lcm js solution

--- solutions/2025/11-November/2025-11-21/js_2025-11-21.js
+++ solutions/2025/11-November/2025-11-21/js_2025-11-21.js
@@ -1,1 +1,19 @@
 // JavaScript solution for 2025-11-21
+// LCM
+function gcd(x, y) {
+    const lowest = Math.min(x,y);
+    let hcf = 1;
+
+    for (let i = 1; i <= lowest; i++){
+        if (x % i == 0 && y % i == 0){
+        hcf = i;
+        }
+    }
+    
+    return hcf;
+}
+
+function lcm(a, b) {
+
+  return (a * b)/gcd(a, b);
+}
