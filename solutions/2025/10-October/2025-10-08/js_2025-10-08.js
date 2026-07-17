715c9fd738c264b882fbd06e51fb3792709cb780
Quoriee <snowii.torii@gmail.com>
2026-07-17 22:45:32 +0200

    space week day 5: goldilocks zone js solution

--- solutions/2025/10-October/2025-10-08/js_2025-10-08.js
+++ solutions/2025/10-October/2025-10-08/js_2025-10-08.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2025-10-08
+// Space Week Day 5: Goldilocks Zone
+function goldilocksZone(mass) {
+    const luminosity = mass ** 3.5;
+    const luminositySqrt = Math.sqrt(luminosity);
+
+    const startOfTheZone = 0.95 * luminositySqrt;
+    const endOfTheZone = 1.37 * luminositySqrt;
+
+    return [Number(startOfTheZone.toFixed(2)), Number(endOfTheZone.toFixed(2))];
+}
