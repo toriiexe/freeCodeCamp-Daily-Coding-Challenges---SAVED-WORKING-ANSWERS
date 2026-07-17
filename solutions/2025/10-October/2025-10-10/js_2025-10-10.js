5e29a9f9aeab491c9c87f2c5f1acc37c2aa1eecb
Quoriee <snowii.torii@gmail.com>
2026-07-17 23:17:20 +0200

    space week day 7: launch fuel js solution

--- solutions/2025/10-October/2025-10-10/js_2025-10-10.js
+++ solutions/2025/10-October/2025-10-10/js_2025-10-10.js
@@ -1,1 +1,16 @@
 // JavaScript solution for 2025-10-10
+// Space Week Day 7: Launch Fuel
+function launchFuel(payload) {
+    let weight = payload;
+    let fuel = weight / 5;
+    let extra = fuel;
+
+    while (extra >= 1) {
+        weight += extra;
+        const newFuel = weight / 5;
+        extra = newFuel - fuel;
+        fuel = newFuel;
+    }
+
+    return Number(fuel.toFixed(1));
+}
