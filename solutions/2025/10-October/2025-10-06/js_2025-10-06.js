c9e1b61dc4776680094efbd5ad24a8a62a25262e
Quoriee <snowii.torii@gmail.com>
2026-07-17 22:57:06 +0200

    space week day 3: phone home js solution

--- solutions/2025/10-October/2025-10-06/js_2025-10-06.js
+++ solutions/2025/10-October/2025-10-06/js_2025-10-06.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2025-10-06
+// Space Week Day 3: Phone Home
+function sendMessage(route) {
+    const speed = 300000;
+
+    const distance = route.reduce((sum, d) => sum + d, 0);
+    const travelTime = distance / speed;
+    const delay = (route.length - 1) * 0.5;
+
+    return Number((travelTime + delay).toFixed(4));
+}
