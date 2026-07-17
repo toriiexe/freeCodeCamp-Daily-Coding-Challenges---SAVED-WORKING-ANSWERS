4d8339d72e539f84fba8a3dac4bda1936e1ba908
Quoriee <snowii.torii@gmail.com>
2026-07-18 00:16:10 +0200

    speak wisely, you must python solution

--- solutions/2025/10-October/2025-10-22/py_2025-10-22.py
+++ solutions/2025/10-October/2025-10-22/py_2025-10-22.py
@@ -1,1 +1,12 @@
 # Python solution for 2025-10-22
+# Speak Wisely, You Must
+def wise_speak(sentence):
+    words = sentence[:-1].split()
+
+    targets = ['have', 'must', 'are', 'will', 'can']
+    idx = next(i for i, word in enumerate(words) if word.lower() in targets)
+
+    first_half = [word for word in words[idx+1:]]
+    second_half = [word.lower() for word in words[:idx+1]]
+
+    return ' '.join(first_half).capitalize() + ', ' + ' '.join(second_half) + sentence[-1]
