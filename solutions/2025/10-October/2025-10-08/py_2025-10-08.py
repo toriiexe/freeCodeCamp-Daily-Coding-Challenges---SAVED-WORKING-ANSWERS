7be220adef6491ad79e2e0db70b36b50a198f9ee
Quoriee <snowii.torii@gmail.com>
2026-07-17 22:44:53 +0200

    space week day 5: goldilocks zone python solution

--- solutions/2025/10-October/2025-10-08/py_2025-10-08.py
+++ solutions/2025/10-October/2025-10-08/py_2025-10-08.py
@@ -1,1 +1,10 @@
 # Python solution for 2025-10-08
+# Space Week Day 5: Goldilocks Zone
+def goldilocks_zone(mass):
+    luminosity = mass**3.5
+    luminosity_sqrt = luminosity**0.5
+
+    start_of_the_zone = 0.95 * luminosity_sqrt
+    end_of_the_zone = 1.37 * luminosity_sqrt
+    
+    return [round(start_of_the_zone, 2), round(end_of_the_zone, 2)]
