b5f2a049754047067674091b5b2be01426ca350a
Quoriee <snowii.torii@gmail.com>
2026-07-17 11:27:45 +0200

    html attribute extractor js solution

--- solutions/2025/10-October/2025-10-19/js_2025-10-19.js
+++ solutions/2025/10-October/2025-10-19/js_2025-10-19.js
@@ -1,1 +1,7 @@
 // JavaScript solution for 2025-10-19
+// HTML Attribute Extractor
+function extractAttributes(element) {
+    const matches = [...element.matchAll(/([a-z]+)="([^"]*)"/g)];
+
+    return matches.map(match => `${match[1]}, ${match[2]}`);
+}
