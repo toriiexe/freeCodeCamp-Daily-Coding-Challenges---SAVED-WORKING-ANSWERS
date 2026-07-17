135b846d16d4e3366843369ce8a5e71a253736bf
Quoriee <snowii.torii@gmail.com>
2026-07-18 00:16:24 +0200

    speak wisely, you must js solution

--- solutions/2025/10-October/2025-10-22/js_2025-10-22.js
+++ solutions/2025/10-October/2025-10-22/js_2025-10-22.js
@@ -1,1 +1,13 @@
 // JavaScript solution for 2025-10-22
+// Speak Wisely, You Must
+function wiseSpeak(sentence) {
+    const words = sentence.slice(0, -1).split(" ");
+
+    const targets = ["have", "must", "are", "will", "can"];
+    const idx = words.findIndex(word => targets.includes(word.toLowerCase()));
+
+    const firstHalf = words.slice(idx + 1);
+    const secondHalf = words.slice(0, idx + 1).map(word => word.toLowerCase());
+
+    return firstHalf.join(" ").charAt(0).toUpperCase() + firstHalf.join(" ").slice(1) + ", " + secondHalf.join(" ") + sentence.slice(-1);
+}
