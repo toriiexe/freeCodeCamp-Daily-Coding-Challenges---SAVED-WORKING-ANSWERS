20feccdeeb5a99d8f14de5541c1cf7abbc84ae32
Quoriee <snowii.torii@gmail.com>
2026-07-18 01:01:29 +0200

    is it the weekend? js solution

--- solutions/2025/11-November/2025-11-14/js_2025-11-14.js
+++ solutions/2025/11-November/2025-11-14/js_2025-11-14.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2025-11-14
+// Is It the Weekend?
+function daysUntilWeekend(dateString) {
+    const [year, month, day] = dateString.split('-').map(Number);
+
+    const date = new Date(year, month - 1, day);
+    const weekday = date.getDay();
+
+    if (weekday === 0 || weekday === 6) {
+        return "It's the weekend!";
+    }
+
+    const days = 6 - weekday;
+    const dayStr = days === 1 ? "day" : "days";
+
+    return `${days} ${dayStr} until the weekend.`;
+}
