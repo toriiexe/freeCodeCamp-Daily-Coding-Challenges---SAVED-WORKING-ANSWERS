21c5b47a8be584b4b5d59c8e3a08347e3058665e
Quoriee <snowii.torii@gmail.com>
2026-07-17 22:26:08 +0200

    space week day 6: moon phase python solution

--- solutions/2025/10-October/2025-10-09/py_2025-10-09.py
+++ solutions/2025/10-October/2025-10-09/py_2025-10-09.py
@@ -1,1 +1,21 @@
 # Python solution for 2025-10-09
+# Space Week Day 6: Moon Phase
+from datetime import date
+
+def moon_phase(date_string):
+    reference = date(2000, 1, 6)
+
+    year, month, day = map(int, date_string.split('-'))
+    target = date(year, month, day)
+
+    diff = (target - reference).days
+    cycle_day = (diff % 28) + 1
+
+    if cycle_day <= 7:
+        return 'New'
+    elif cycle_day <= 14:
+        return 'Waxing'
+    elif cycle_day <= 21:
+        return 'Full'
+    else:
+        return 'Waning'
