83577e6892db4b1f6d1b66b7f7678fd7bdd2e0c2
Quoriee <snowii.torii@gmail.com>
2026-07-17 22:53:18 +0200

    space week day 3: phone home python solution

--- solutions/2025/10-October/2025-10-06/py_2025-10-06.py
+++ solutions/2025/10-October/2025-10-06/py_2025-10-06.py
@@ -1,1 +1,25 @@
 # Python solution for 2025-10-06
+# Space Week Day 3: Phone Home
+def send_message(route):
+    speed = 300_000
+
+    distance = sum(route)
+    travel_time = distance / speed
+    delay = (len(route) - 1) * 0.5
+
+    return round(travel_time + delay, 4)
+
+"""
+# Space Week Day 3: Phone Home - ANOTHER VERSION
+def send_message(route):
+    speed = 300_000
+    res = 0
+
+    for i in range(len(route)):
+        res += route[i] / speed
+
+        if i != len(route) - 1:
+            res += 0.5
+
+    return round(res, 4)
+"""
