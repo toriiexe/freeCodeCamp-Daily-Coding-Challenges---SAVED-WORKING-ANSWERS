ee9d68bcbf55af4b8f3528bf4a33212cfd42f217
Quoriee <snowii.torii@gmail.com>
2026-07-19 01:09:57 +0200

    url query parser js solution

--- solutions/2026/04-April/2026-04-29/js_2026-04-29.js
+++ solutions/2026/04-April/2026-04-29/js_2026-04-29.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2026-04-29
+// URL Query Parser
+function parseUrlQuery(url) {
+    if (!url.includes("?")) {
+        return {};
+    }
+
+    const query = url.split("?", 2)[1];
+    const result = {};
+
+    for (const param of query.split("&")) {
+        const [key, value] = param.split("=", 2);
+        result[key] = value;
+    }
+
+    return result;
+}
