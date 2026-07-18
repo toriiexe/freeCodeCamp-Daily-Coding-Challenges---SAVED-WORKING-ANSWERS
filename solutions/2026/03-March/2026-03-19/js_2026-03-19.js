0296cd2562b07edceddb9ac22fab0441768ff697
Quoriee <snowii.torii@gmail.com>
2026-07-18 02:59:00 +0200

    inverted matrix js solution

--- solutions/2026/03-March/2026-03-19/js_2026-03-19.js
+++ solutions/2026/03-March/2026-03-19/js_2026-03-19.js
@@ -1,1 +1,39 @@
 // JavaScript solution for 2026-03-19
+// Inverted Matrix
+function invertMatrix(matrix) {
+    const values = [];
+
+    for (let r = 0; r < matrix.length; r++) {
+        for (let c = 0; c < matrix[r].length; c++) {
+            if (!values.includes(matrix[r][c])) {
+                values.push(matrix[r][c]);
+            }
+
+            if (values.length === 2) {
+                break;
+            }
+        }
+
+        if (values.length === 2) {
+            break;
+        }
+    }
+
+    const res = [];
+
+    for (const row of matrix) {
+        const temp = [];
+
+        for (const col of row) {
+            if (col === values[0]) {
+                temp.push(values[1]);
+            } else {
+                temp.push(values[0]);
+            }
+        }
+
+        res.push(temp);
+    }
+
+    return res;
+}
