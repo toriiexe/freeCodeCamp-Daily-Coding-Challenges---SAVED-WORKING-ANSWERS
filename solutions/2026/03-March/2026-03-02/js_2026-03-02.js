8f0a86263012ef8d13f1f32de242990074d3622d
Quoriee <snowii.torii@gmail.com>
2026-07-18 02:23:51 +0200

    sum the letters js solution

--- solutions/2026/03-March/2026-03-02/js_2026-03-02.js
+++ solutions/2026/03-March/2026-03-02/js_2026-03-02.js
@@ -1,1 +1,5 @@
 // JavaScript solution for 2026-03-02
+// Sum the Letters
+function sumLetters(str) {
+    return [...str.toUpperCase()].reduce((sum, c) => sum + (/[A-Z]/.test(c) ? c.charCodeAt(0) - 'A'.charCodeAt(0) + 1 : 0), 0);
+}
