63129f853dce63427d033d46e4cb8c2cb8a477c6
Quoriee <snowii.torii@gmail.com>
2026-07-19 00:47:16 +0200

    number words js solution

--- solutions/2026/04-April/2026-04-28/js_2026-04-28.js
+++ solutions/2026/04-April/2026-04-28/js_2026-04-28.js
@@ -1,1 +1,19 @@
 // JavaScript solution for 2026-04-28
+// Number Words
+function getNumberWords(n) {
+    const ones = [
+        'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+    ];
+
+    const tens = [
+        '', '', 'twenty', 'thirty', 'forty',
+        'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+    ];
+
+    if (n < 20) return ones[n];
+
+    if (n % 10 === 0) return tens[Math.floor(n / 10)];
+
+    return `${tens[Math.floor(n / 10)]}-${ones[n % 10]}`;
+}
