c581cd32b9864b7b03a49a631f12e8d831db6ece
Quoriee <snowii.torii@gmail.com>
2026-07-19 00:46:56 +0200

    number words python solution

--- solutions/2026/04-April/2026-04-28/py_2026-04-28.py
+++ solutions/2026/04-April/2026-04-28/py_2026-04-28.py
@@ -1,1 +1,20 @@
 # Python solution for 2026-04-28
+# Number Words
+def get_number_words(n):
+    ones = [
+        'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+    ]
+
+    tens = [
+        '', '', 'twenty', 'thirty', 'forty',
+        'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+    ]
+
+    if n < 20:
+        return ones[n]
+
+    if n % 10 == 0:
+        return tens[n // 10]
+
+    return f'{tens[n // 10]}-{ones[n % 10]}'
