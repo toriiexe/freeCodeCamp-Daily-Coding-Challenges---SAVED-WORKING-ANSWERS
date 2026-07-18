88a2c71f1ab359ec738ddec1b2a515573efc9cdd
Quoriee <snowii.torii@gmail.com>
2026-07-18 02:57:38 +0200

    inverted matrix python solution

--- solutions/2026/03-March/2026-03-19/py_2026-03-19.py
+++ solutions/2026/03-March/2026-03-19/py_2026-03-19.py
@@ -1,1 +1,26 @@
 # Python solution for 2026-03-19
+# Inverted Matrix
+def invert_matrix(matrix):
+    values = []
+
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):
+            if matrix[r][c] not in values:
+                values.append(matrix[r][c])
+            if len(values) == 2:
+                break
+        if len(values) == 2:
+            break
+    
+    res = []
+
+    for row in matrix:
+        temp = []
+        for col in row:
+            if col == values[0]:
+                temp.append(values[1])
+            else:
+                temp.append(values[0])
+        res.append(temp)
+
+    return res
