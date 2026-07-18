34d5c90497dab79708ea18d6e4be05da3e403f98
Quoriee <snowii.torii@gmail.com>
2026-07-18 02:11:28 +0200

    2026 winter games day 17: closing day python solution

--- solutions/2026/02-February/2026-02-22/py_2026-02-22.py
+++ solutions/2026/02-February/2026-02-22/py_2026-02-22.py
@@ -1,1 +1,25 @@
 # Python solution for 2026-02-22
+# 2026 Winter Games Day 17: Closing Day
+def count_medals(winners):
+    countries = set()
+
+    for row in winners:
+        for country in row:
+            countries.add(country)
+
+    medals = {country: [0, 0, 0] for country in countries}
+
+    for row in winners:
+        for i, country in enumerate(row):
+            medals[country][i] += 1
+
+    sorted_countries = sorted(countries, key=lambda c: (-medals[c][0], c))
+
+    res = 'Country,Gold,Silver,Bronze,Total'
+
+    for country in sorted_countries:
+        gold, silver, bronze = medals[country]
+        total = gold + silver + bronze
+        res += f'\n{country},{gold},{silver},{bronze},{total}'
+
+    return res
