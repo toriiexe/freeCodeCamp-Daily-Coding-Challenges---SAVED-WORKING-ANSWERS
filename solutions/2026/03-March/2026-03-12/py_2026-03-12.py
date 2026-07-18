515092bd4b4e94771e960cdfd9be9873c6c60e70
Quoriee <snowii.torii@gmail.com>
2026-07-18 02:30:23 +0200

    domino chain validator python solution

--- solutions/2026/03-March/2026-03-12/py_2026-03-12.py
+++ solutions/2026/03-March/2026-03-12/py_2026-03-12.py
@@ -1,1 +1,7 @@
 # Python solution for 2026-03-12
+# Domino Chain Validator
+def is_valid_domino_chain(dominoes):
+    for i in range(len(dominoes)-1):
+        if dominoes[i][1] != dominoes[i+1][0]:
+            return False
+    return True
