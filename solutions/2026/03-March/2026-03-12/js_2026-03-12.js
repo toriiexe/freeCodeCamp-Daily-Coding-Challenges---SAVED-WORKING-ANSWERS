f0e69b5cf0011933cdde87177c8dff358ac7a10e
Quoriee <snowii.torii@gmail.com>
2026-07-18 02:31:18 +0200

    domino chain validator js solution

--- solutions/2026/03-March/2026-03-12/js_2026-03-12.js
+++ solutions/2026/03-March/2026-03-12/js_2026-03-12.js
@@ -1,1 +1,10 @@
 // JavaScript solution for 2026-03-12
+// Domino Chain Validator
+function isValidDominoChain(dominoes) {
+    for (let i = 0; i < dominoes.length - 1; i++){
+        if (dominoes[i][1] !== dominoes[i+1][0]){
+        return false;
+        }
+    }
+    return true;
+}
