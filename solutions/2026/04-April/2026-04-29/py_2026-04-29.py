858903b9dba3999fb284723abf1b25b769340a0d
Quoriee <snowii.torii@gmail.com>
2026-07-19 01:08:26 +0200

    url query parser python solution

--- solutions/2026/04-April/2026-04-29/py_2026-04-29.py
+++ solutions/2026/04-April/2026-04-29/py_2026-04-29.py
@@ -1,1 +1,14 @@
 # Python solution for 2026-04-29
+# URL Query Parser
+def parse_url_query(url):
+    if '?' not in url:
+        return {}
+
+    query = url.split('?', 1)[1]
+    result = {}
+
+    for param in query.split('&'):
+        key, value = param.split('=', 1)
+        result[key] = value
+
+    return result
