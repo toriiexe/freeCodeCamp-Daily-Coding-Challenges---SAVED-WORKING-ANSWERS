f2848cc0d27d82aa607887691b55a1268322c310
Quoriee <snowii.torii@gmail.com>
2026-07-18 02:37:41 +0200

    captured chess piece js solution

--- solutions/2026/03-March/2026-03-15/js_2026-03-15.js
+++ solutions/2026/03-March/2026-03-15/js_2026-03-15.js
@@ -1,1 +1,24 @@
 // JavaScript solution for 2026-03-15
+// Captured Chess Piece
+function getCapturedValue(pieces) {
+    const board = {
+        P: 1,
+        R: 5,
+        N: 3,
+        B: 3,
+        Q: 9,
+        K: 0
+    };
+
+    if (!pieces.includes('K')) {
+        return "Checkmate";
+    }
+
+    let total = 8 * 1 + 2 * 5 + 2 * 3 + 2 * 3 + 1 * 9;
+
+    for (const piece of pieces) {
+        total -= board[piece];
+    }
+
+    return total;
+}
