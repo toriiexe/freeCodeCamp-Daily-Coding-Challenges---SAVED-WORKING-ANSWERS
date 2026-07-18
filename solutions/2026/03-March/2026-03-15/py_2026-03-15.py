6967b878245950ad6d180c2162eb2fb53ccc8c5b
Quoriee <snowii.torii@gmail.com>
2026-07-18 02:37:00 +0200

    captured chess pieces python solution

--- solutions/2026/03-March/2026-03-15/py_2026-03-15.py
+++ solutions/2026/03-March/2026-03-15/py_2026-03-15.py
@@ -1,1 +1,21 @@
 # Python solution for 2026-03-15
+# Captured Chess Pieces
+def get_captured_value(pieces):
+    board = {
+        'P': 1,
+        'R': 5,
+        'N': 3,
+        'B': 3,
+        'Q': 9,
+        'K': 0
+    }
+
+    if not 'K' in pieces:
+        return 'Checkmate'
+
+    total = 8 * 1 + 2 * 5 + 2 * 3 + 2 * 3 + 1 * 9
+
+    for piece in pieces:
+        total -= board[piece]
+        
+    return total
