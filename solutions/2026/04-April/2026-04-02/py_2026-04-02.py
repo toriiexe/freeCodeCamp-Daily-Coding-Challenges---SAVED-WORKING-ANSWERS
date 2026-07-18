158541d36993043277d19e05d70af97c4939dd70
Quoriee <snowii.torii@gmail.com>
2026-07-18 12:11:43 +0200

    capitalized fibonacci python solution

--- solutions/2026/04-April/2026-04-02/py_2026-04-02.py
+++ solutions/2026/04-April/2026-04-02/py_2026-04-02.py
@@ -1,1 +1,19 @@
 # Python solution for 2026-04-02
+# Capitalized Fibonacci
+def capitalize_fibonacci(s):
+    fib = [0, 1]
+    a, b = 0, 1
+
+    while b < len(s):
+        a, b = b, a + b
+        fib.append(b)
+
+    result = ''
+
+    for i, c in enumerate(s):
+        if c.isalpha():
+            result += c.upper() if i in fib else c.lower()
+        else:
+            result += c
+
+    return result
