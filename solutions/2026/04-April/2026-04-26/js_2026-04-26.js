5d81c71cfa690b5ae556bd77a96c6fe02a59ea3b
Quoriee <snowii.torii@gmail.com>
2026-07-19 01:54:27 +0200

    fizzbuzz explosion js solution

--- solutions/2026/04-April/2026-04-26/js_2026-04-26.js
+++ solutions/2026/04-April/2026-04-26/js_2026-04-26.js
@@ -1,1 +1,29 @@
 // JavaScript solution for 2026-04-26
+// FizzBuzz Explosion
+function explodeFizzbuzz(targetZCount) {
+    let res = 'fizzbuzz';
+    let temp = '';
+    let steps = 0;
+
+    while ((res.match(/z/g) || []).length < targetZCount) {
+        for (const [i, c] of [...res].entries()) {
+        const pos = i + 1;
+
+        if (pos % 15 === 0) {
+            temp += 'fizzbuzz';
+        } else if (pos % 3 === 0) {
+            temp += 'fizz';
+        } else if (pos % 5 === 0) {
+            temp += 'buzz';
+        } else {
+            temp += c;
+        }
+        }
+
+        res = temp;
+        temp = '';
+        steps++;
+    }
+
+    return steps;
+}
