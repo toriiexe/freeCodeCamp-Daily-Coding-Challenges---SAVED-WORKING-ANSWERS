90fb42fe78e539485d07a9c35f753a4edca3eacd
Quoriee <snowii.torii@gmail.com>
2026-07-19 03:26:24 +0200

    spellcaster js solution

--- solutions/2026/06-June/2026-06-17/js_2026-06-17.js
+++ solutions/2026/06-June/2026-06-17/js_2026-06-17.js
@@ -1,1 +1,42 @@
 // JavaScript solution for 2026-06-17
+// Spellcaster
+function cast(spells) {
+    const categories = {
+        f: 'Destruction',
+        l: 'Destruction',
+        i: 'Control',
+        w: 'Control',
+        h: 'Restoration',
+        s: 'Restoration'
+    };
+
+    const scores = {
+        f: 3,
+        l: 3,
+        i: 2,
+        w: 2,
+        h: 1,
+        s: 1
+    };
+
+    let total = 0;
+    let multiplier = 1;
+    let lastCategory = '';
+
+    for (let i = 0; i < spells.length; i++) {
+        const spell = spells[i];
+        let score = 0;
+
+        if (i > 0 && categories[spell] !== lastCategory) {
+            multiplier++;
+        } else {
+            multiplier = 1;
+        }
+
+        score += scores[spell] * multiplier;
+        lastCategory = categories[spell];
+        total += score;
+    }
+
+    return total;
+}
