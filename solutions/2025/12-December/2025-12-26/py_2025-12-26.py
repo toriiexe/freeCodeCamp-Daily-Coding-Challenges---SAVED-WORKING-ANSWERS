48b5e3bb309ddde80913307b63dda9d287ae2a7e
Quoriee <snowii.torii@gmail.com>
2026-07-19 03:54:18 +0200

    sum of divisors python solution

--- solutions/2025/12-December/2025-12-26/py_2025-12-26.py
+++ solutions/2025/12-December/2025-12-26/py_2025-12-26.py
@@ -1,1 +1,8 @@
 # Python solution for 2025-12-26
+# Sum of Divisors
+def sum_divisors(n):
+    total = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            total += i
+    return total
