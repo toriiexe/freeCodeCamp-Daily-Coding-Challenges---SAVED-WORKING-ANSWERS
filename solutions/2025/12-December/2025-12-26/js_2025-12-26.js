167127c57904cc4ecddc396750fd7a5a178ba59f
Quoriee <snowii.torii@gmail.com>
2026-07-19 03:55:31 +0200

    sum of divisors js solution

--- solutions/2025/12-December/2025-12-26/js_2025-12-26.js
+++ solutions/2025/12-December/2025-12-26/js_2025-12-26.js
@@ -1,1 +1,11 @@
 // JavaScript solution for 2025-12-26
+// Sum of Divisors
+function sumDivisors(n) {
+    let total = 0;
+    for (let i = 1; i <= n; i++){
+        if (n % i === 0){
+            total += i;
+        }
+    }
+    return total;
+}
