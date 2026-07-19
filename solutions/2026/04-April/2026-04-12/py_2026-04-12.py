7c569d34fd4aa388ebb7e0f481d0711f1cfe14cf
Quoriee <snowii.torii@gmail.com>
2026-07-19 02:46:17 +0200

    spiral matrix python solution

--- solutions/2026/04-April/2026-04-12/py_2026-04-12.py
+++ solutions/2026/04-April/2026-04-12/py_2026-04-12.py
@@ -1,1 +1,9 @@
 # Python solution for 2026-04-12
+# Spiral Matrix
+def spiral_matrix(matrix):
+    snail = []
+    while len(matrix):
+        snail += matrix[0]
+        matrix.pop(0)
+        matrix = list(zip(*matrix))[::-1]
+    return snail
