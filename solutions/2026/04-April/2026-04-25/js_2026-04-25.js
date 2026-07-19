bff516af86100037a520e31a4b172ce0c03da335
Quoriee <snowii.torii@gmail.com>
2026-07-19 02:06:21 +0200

    word decompressor js solution

--- solutions/2026/04-April/2026-04-25/js_2026-04-25.js
+++ solutions/2026/04-April/2026-04-25/js_2026-04-25.js
@@ -1,1 +1,21 @@
 // JavaScript solution for 2026-04-25
+// Word Decompressor
+function decompress(str) {
+    const firstPos = new Map();
+    const result = [];
+
+    const words = str.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+
+        if (/^\d+$/.test(word)) {
+            result.push(firstPos.get(Number(word)));
+        } else {
+            firstPos.set(i + 1, word);
+            result.push(word);
+        }
+    }
+
+    return result.join(' ');
+}
