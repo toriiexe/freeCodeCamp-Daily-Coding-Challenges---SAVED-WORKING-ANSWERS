768bd4f16de13671a4124f5a60f0c93c76dd5f8b
Quoriee <snowii.torii@gmail.com>
2026-07-19 03:25:26 +0200

    spellcaster python solution

--- solutions/2026/06-June/2026-06-17/py_2026-06-17.py
+++ solutions/2026/06-June/2026-06-17/py_2026-06-17.py
@@ -1,1 +1,35 @@
 # Python solution for 2026-06-17
+# Spellcaster
+def cast(spells):
+    categories = {
+        'f': 'Destruction',
+        'l': 'Destruction',
+        'i': 'Control',
+        'w': 'Control',
+        'h': 'Restoration',
+        's': 'Restoration'
+    }
+
+    scores = {
+        'f': 3,
+        'l': 3,
+        'i': 2,
+        'w': 2,
+        'h': 1,
+        's': 1
+    }
+
+    total = 0
+    multiplier = 1
+    last_category = ''
+    for i, spell in enumerate(spells):
+        score = 0
+        if i > 0 and categories[spell] != last_category:
+            multiplier += 1
+        else:
+            multiplier = 1
+        score += scores[spell] * multiplier
+        last_category = categories[spell]
+        total += score
+
+    return total
