eb1ea605ec980d266e80bcaf9f10a0d839dba5e8
Quoriee <snowii.torii@gmail.com>
2026-07-19 02:48:49 +0200

    spiral matrix js solution

--- solutions/2026/04-April/2026-04-12/js_2026-04-12.js
+++ solutions/2026/04-April/2026-04-12/js_2026-04-12.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2026-04-12
+// Spiral Matrix
+function spiralMatrix(matrix) {
+    const snail = [];
+
+    while (matrix.length) {
+        snail.push(...matrix[0]);
+        matrix.shift();
+
+        matrix = matrix[0] ? matrix[0].map((_, i) => matrix.map(row => row[i])).reverse() : [];
+    }
+
+    return snail;
+}
