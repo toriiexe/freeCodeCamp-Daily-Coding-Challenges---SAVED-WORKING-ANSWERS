c5917b72c1f08ae9734f8be629b72600ef42d5fc
Quoriee <snowii.torii@gmail.com>
2026-07-19 03:49:36 +0200

    pairwise python solution

--- solutions/2025/12-December/2025-12-19/py_2025-12-19.py
+++ solutions/2025/12-December/2025-12-19/py_2025-12-19.py
@@ -1,1 +1,18 @@
 # Python solution for 2025-12-19
+# Pairwise
+def pairwise(arr, target):
+    indx = 0
+    used = [False] * len(arr)
+
+    for i in range(len(arr)):
+        if used[i]:
+            continue
+
+        for j in range(i + 1, len(arr)):
+            if not used[j] and arr[i] + arr[j] == target:
+                indx += i + j
+                used[i] = True
+                used[j] = True
+                break
+
+    return indx
