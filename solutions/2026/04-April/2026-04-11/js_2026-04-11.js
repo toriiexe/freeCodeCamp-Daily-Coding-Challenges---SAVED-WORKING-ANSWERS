824ff066b4570c20ab6a519884f1e6cf7844a2c5
Quoriee <snowii.torii@gmail.com>
2026-07-19 03:10:53 +0200

    rook and bishop attack js solution

--- solutions/2026/04-April/2026-04-11/js_2026-04-11.js
+++ solutions/2026/04-April/2026-04-11/js_2026-04-11.js
@@ -1,1 +1,13 @@
 // JavaScript solution for 2026-04-11
+// Rook and Bishop Attack
+function rookBishopAttack(rook, bishop) {
+    if (rook[0] === bishop[0] || rook[1] === bishop[1]) {
+        return 'rook';
+    }
+
+    if (Math.abs(rook.charCodeAt(0) - bishop.charCodeAt(0)) === Math.abs(Number(rook[1]) - Number(bishop[1]))) {
+        return 'bishop';
+    }
+
+    return 'neither';
+}
