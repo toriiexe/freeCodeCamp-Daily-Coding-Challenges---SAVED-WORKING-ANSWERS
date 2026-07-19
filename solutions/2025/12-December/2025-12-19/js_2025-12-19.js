1b71921dbc88bd35398c8196cb52f5ebeaba72ce
Quoriee <snowii.torii@gmail.com>
2026-07-19 03:50:16 +0200

    pairwise js solution

--- solutions/2025/12-December/2025-12-19/js_2025-12-19.js
+++ solutions/2025/12-December/2025-12-19/js_2025-12-19.js
@@ -1,1 +1,21 @@
 // JavaScript solution for 2025-12-19
+// Pairwise
+function pairwise(arr, target) {
+    let indx = 0;
+    const used = new Array(arr.length).fill(false);
+
+    for (let i = 0; i < arr.length; i++) {
+        if (used[i]) continue;
+
+        for (let j = i + 1; j < arr.length; j++) {
+            if (!used[j] && arr[i] + arr[j] === target) {
+                indx += i + j;
+                used[i] = true;
+                used[j] = true;
+                break;
+            }
+        }
+    }
+
+    return indx;
+}
