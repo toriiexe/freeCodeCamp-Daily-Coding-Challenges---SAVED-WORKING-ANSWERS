f63e5ce4cdc5572283b47be724b4086ccba0dfa0
Quoriee <snowii.torii@gmail.com>
2026-07-19 02:03:23 +0200

    word decompressor python solution

--- solutions/2026/04-April/2026-04-25/py_2026-04-25.py
+++ solutions/2026/04-April/2026-04-25/py_2026-04-25.py
@@ -1,1 +1,14 @@
 # Python solution for 2026-04-25
+# Word Decompressor
+def decompress(s):
+    first_pos = {}
+    result = []
+
+    for i, token in enumerate(s.split(), start=1):
+        if token.isdigit():
+            result.append(first_pos[int(token)])
+        else:
+            first_pos[i] = token
+            result.append(token)
+
+    return ' '.join(result)
