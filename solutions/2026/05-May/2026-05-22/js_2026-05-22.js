0c74be21f05e7cfeab2ec008be42720bf0447042
Quoriee <snowii.torii@gmail.com>
2026-07-20 08:36:17 +0200

    meeting time js solution

--- solutions/2026/05-May/2026-05-22/js_2026-05-22.js
+++ solutions/2026/05-May/2026-05-22/js_2026-05-22.js
@@ -1,1 +1,27 @@
 // JavaScript solution for 2026-05-22
+// Meeting Time
+function getMeetingTime(availability) {
+    const res = [];
+
+    for (const person of availability) {
+        const time = [];
+
+        for (const hours of person) {
+            for (let i = hours[0]; i < hours[1]; i++) {
+                time.push(i);
+            }
+        }
+
+        res.push(time);
+    }
+
+    let common = new Set(res[0]);
+
+    for (const sublist of res.slice(1)) {
+        const current = new Set(sublist);
+
+        common = new Set([...common].filter(hour => current.has(hour)));
+    }
+
+    return common.size ? Math.min(...common) : 'None';
+}
