9480845a69e35654ad6305b6d6936f9b0d38994e
Quoriee <snowii.torii@gmail.com>
2026-07-20 07:07:59 +0200

    golden ratio js solution

--- solutions/2026/07-July/2026-07-20/js_2026-07-20.js
+++ solutions/2026/07-July/2026-07-20/js_2026-07-20.js
@@ -1,1 +1,10 @@
 // JavaScript solution for 2026-07-20
+// Golden Ratio
+function isGoldenRatio(a, b) {
+    const goldenRatio = 1.618;
+    const tolerance = 0.01;
+
+    const ratio = Math.max(a, b) / Math.min(a, b);
+
+    return ratio - tolerance <= goldenRatio && goldenRatio <= ratio + tolerance;
+}
