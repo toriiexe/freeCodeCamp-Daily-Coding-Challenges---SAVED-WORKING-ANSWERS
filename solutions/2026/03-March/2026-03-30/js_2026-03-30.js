b599494f834b7a9da4c19cf4e9d8249aa2ea75ba
Quoriee <snowii.torii@gmail.com>
2026-07-20 04:19:14 +0200

    due date js solution

--- solutions/2026/03-March/2026-03-30/js_2026-03-30.js
+++ solutions/2026/03-March/2026-03-30/js_2026-03-30.js
@@ -1,1 +1,19 @@
 // JavaScript solution for 2026-03-30
+// Due Date
+function getDueDate(dateStr) {
+    const [year, month, day] = dateStr.split('-').map(Number);
+
+    let newYear = year;
+    let newMonth = month + 9;
+
+    if (newMonth > 12) {
+        newYear += Math.floor((newMonth - 1) / 12);
+        newMonth = ((newMonth - 1) % 12) + 1;
+    }
+
+    const lastDay = new Date(newYear, newMonth, 0).getDate();
+
+    const newDay = Math.min(day, lastDay);
+
+    return `${newYear}-${String(newMonth).padStart(2, '0')}-${String(newDay).padStart(2, '0')}`;
+}
