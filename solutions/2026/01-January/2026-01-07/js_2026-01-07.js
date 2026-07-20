41f5e5e36eb607186702411d6ba4cb0ee84981e7
Quoriee <snowii.torii@gmail.com>
2026-07-20 05:30:39 +0200

    markdown unordered list parser js solution

--- solutions/2026/01-January/2026-01-07/js_2026-01-07.js
+++ solutions/2026/01-January/2026-01-07/js_2026-01-07.js
@@ -1,1 +1,6 @@
 // JavaScript solution for 2026-01-07
+// Markdown Unordered List Parser
+function parseUnorderedList(markdown) {
+    const items = markdown.replace(/-\s+(.+?)(?:\n|$)/g, '<li>$1</li>');
+    return '<ul>' + items + '</ul>';
+}
