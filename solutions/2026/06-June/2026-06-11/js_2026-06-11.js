d3772e55dc798538a23da19b3940b684c3210ed8
Quoriee <snowii.torii@gmail.com>
2026-07-20 07:03:27 +0200

    idea rankings js solution

--- solutions/2026/06-June/2026-06-11/js_2026-06-11.js
+++ solutions/2026/06-June/2026-06-11/js_2026-06-11.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2026-06-11
+// Idea Rankings
+function analyzeIdeas(ideas) {
+    const res = [];
+
+    for (const idea of ideas) {
+        const expected = ((idea[1] + 4 * idea[2] + idea[3]) / 6) * idea[0].length;
+        res.push([expected, idea[0]]);
+    }
+
+    res.sort((a, b) => a[0] - b[0]);
+
+    return res.map(item => item[1]);
+}
