24dd1f85e409f9dadc9715880fcc396795bffd17
Quoriee <snowii.torii@gmail.com>
2026-07-20 03:47:26 +0200

    digit rotation escape python solution

--- solutions/2026/04-April/2026-04-05/py_2026-04-05.py
+++ solutions/2026/04-April/2026-04-05/py_2026-04-05.py
@@ -1,1 +1,14 @@
 # Python solution for 2026-04-05
+# Digit Rotation Escape
+def get_rotation(n):
+    s = str(n)
+    length = len(s)
+    step = -1
+
+    for i in range(length):
+        step += 1
+        rot = int(s[i:] + s[:i])
+        if rot % length == 0:
+            return step
+        
+    return 'none'
