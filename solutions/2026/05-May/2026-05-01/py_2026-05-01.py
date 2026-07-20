a6548e2a7463c1328c7d5f9a0234274995630554
Quoriee <snowii.torii@gmail.com>
2026-07-20 08:45:09 +0200

    anagram groups python solution

--- solutions/2026/05-May/2026-05-01/py_2026-05-01.py
+++ solutions/2026/05-May/2026-05-01/py_2026-05-01.py
@@ -1,1 +1,18 @@
 # Python solution for 2026-05-01
+# Anagram Groups
+def group_anagrams(words):
+    res = []
+
+    for word in words:
+        found = False
+
+        for group in res:
+            if sorted(word) == sorted(group[0]):
+                group.append(word)
+                found = True
+                break
+
+        if not found:
+            res.append([word])
+
+    return res
