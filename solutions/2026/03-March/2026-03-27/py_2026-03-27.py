5f4e66e0592784b7470fa21aee7aa9d5c27f0dbc
Quoriee <snowii.torii@gmail.com>
2026-07-20 04:06:36 +0200

    truncate the text 2 python solution

--- solutions/2026/03-March/2026-03-27/py_2026-03-27.py
+++ solutions/2026/03-March/2026-03-27/py_2026-03-27.py
@@ -1,1 +1,51 @@
 # Python solution for 2026-03-27
+# Truncate the Text 2
+def truncate_text(s):
+    width1 = 'ilI.'
+    width2 = 'fjrt '
+    width3 = 'abcdeghkmnopqrstuvwxyzJL'
+    width4 = 'ABCDEFGHKMNOPQRSTUVWXYZ'
+
+    total = 0
+    res = ''
+
+    for c in s:
+        if c in width1:
+            total += 1
+        elif c in width2:
+            total += 2
+        elif c in width3:
+            total += 3
+        elif c in width4:
+            total += 4
+
+        if total > 50:
+            if c in width1:
+                total -= 1
+            elif c in width2:
+                total -= 2
+            elif c in width3:
+                total -= 3
+            elif c in width4:
+                total -= 4
+
+            total += 3
+
+            while total > 50:
+                last_c = res[-1]
+                if last_c in width1:
+                    total -= 1
+                elif last_c in width2:
+                    total -= 2
+                elif last_c in width3:
+                    total -= 3
+                elif last_c in width4:
+                    total -= 4
+                res = res[:-1]
+
+            res += '...'
+            break
+
+        res += c
+
+    return res
