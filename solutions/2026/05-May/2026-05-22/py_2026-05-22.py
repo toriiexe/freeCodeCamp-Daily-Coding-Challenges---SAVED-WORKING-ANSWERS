3951f45f785d9713c2a79d38e704fa08374f548d
Quoriee <snowii.torii@gmail.com>
2026-07-20 08:35:02 +0200

    meeting time python solution

--- solutions/2026/05-May/2026-05-22/py_2026-05-22.py
+++ solutions/2026/05-May/2026-05-22/py_2026-05-22.py
@@ -1,1 +1,17 @@
 # Python solution for 2026-05-22
+# Meeting Time
+def get_meeting_time(availability):
+    res = []
+
+    for person in availability:
+        time = []
+        for hours in person:
+            time.extend(range(hours[0], hours[1]))
+        res.append(time)
+
+    common = set(res[0])
+
+    for sublist in res[1:]:
+        common &= set(sublist)
+
+    return min(common) if common else 'None'
