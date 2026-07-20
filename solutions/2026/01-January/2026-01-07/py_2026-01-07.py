a9446b0fa4109088843afc69d37596c549e04311
Quoriee <snowii.torii@gmail.com>
2026-07-20 05:29:45 +0200

    markdown unordered list parser python solution

--- solutions/2026/01-January/2026-01-07/py_2026-01-07.py
+++ solutions/2026/01-January/2026-01-07/py_2026-01-07.py
@@ -1,1 +1,6 @@
 # Python solution for 2026-01-07
+# Markdown Unordered List Parser
+import re
+def parse_unordered_list(markdown):
+    items = re.sub(r'-\s+(.+?)(?:\n|$)', r'<li>\1</li>', markdown)
+    return '<ul>' + items + '</ul>'
