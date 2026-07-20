ae784984bc65f30e9101d880d8fd4a38928ed236
Quoriee <snowii.torii@gmail.com>
2026-07-20 05:37:52 +0200

    left-handed seat at the table python solution

--- solutions/2026/01-January/2026-01-03/py_2026-01-03.py
+++ solutions/2026/01-January/2026-01-03/py_2026-01-03.py
@@ -1,1 +1,22 @@
 # Python solution for 2026-01-03
+# Left-Handed Seat at the Table
+def find_left_handed_seats(table):
+    total = 0
+
+    for r in range(2):
+        if r == 0:
+            for p in range(4):
+                if table[r][p] == 'U':
+                    if p == 3:
+                        total += 1
+                    elif table[r][p+1] != 'R':
+                        total += 1
+        else:   
+            for p in range(4):
+                if table[r][p] == 'U':
+                    if p == 0:
+                        total += 1
+                    elif table[r][p-1] != 'R':
+                        total += 1
+
+    return total
