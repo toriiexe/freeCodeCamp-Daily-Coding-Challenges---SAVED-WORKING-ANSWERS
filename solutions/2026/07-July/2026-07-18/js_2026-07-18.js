9f19b8a523f077050007ed73612db3a457fa3a7f
Quoriee <snowii.torii@gmail.com>
2026-07-20 05:51:35 +0200

    dice odds js solution

--- solutions/2026/07-July/2026-07-18/js_2026-07-18.js
+++ solutions/2026/07-July/2026-07-18/js_2026-07-18.js
@@ -1,1 +1,29 @@
 // JavaScript solution for 2026-07-18
+// Dice Odds
+function getOdds(dice, target) {
+    let dp = Array(6 * dice + 1).fill(0);
+    dp[0] = 1;
+
+    for (let i = 0; i < dice; i++) {
+        let newDp = Array(6 * dice + 1).fill(0);
+
+        for (let sum = 0; sum < dp.length; sum++) {
+            if (dp[sum] > 0) {
+                for (let roll = 1; roll <= 6; roll++) {
+                    if (sum + roll < newDp.length) {
+                        newDp[sum + roll] += dp[sum];
+                    }
+                }
+            }
+        }
+
+        dp = newDp;
+    }
+
+    const favorable = dp[target];
+    const total = 6 ** dice;
+
+    const x = Math.round(total / favorable);
+
+    return `1 in ${x}`;
+}
