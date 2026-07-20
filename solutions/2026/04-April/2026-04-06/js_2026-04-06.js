e5c43f8b5b8671ad4f5d70f2c59bc629ef6a811b
Quoriee <snowii.torii@gmail.com>
2026-07-20 03:52:27 +0200

    what day is it? js solution

--- solutions/2026/04-April/2026-04-06/js_2026-04-06.js
+++ solutions/2026/04-April/2026-04-06/js_2026-04-06.js
@@ -1,1 +1,5 @@
 // JavaScript solution for 2026-04-06
+// What Day Is It?
+function getDayOfWeek(timestamp) {
+    return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][new Date(timestamp).getUTCDay()];
+}
