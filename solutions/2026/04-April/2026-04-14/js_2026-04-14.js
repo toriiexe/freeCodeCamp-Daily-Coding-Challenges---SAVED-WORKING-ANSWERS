567d7db925a33cb4e96d289fb9d34350a9927ce0
Quoriee <snowii.torii@gmail.com>
2026-07-20 03:02:22 +0200

    last letter js solution

--- solutions/2026/04-April/2026-04-14/js_2026-04-14.js
+++ solutions/2026/04-April/2026-04-14/js_2026-04-14.js
@@ -1,1 +1,15 @@
 // JavaScript solution for 2026-04-14
+// Last Letter
+function getLastLetter(str) {
+    let lastLetter = '';
+
+    for (const c of str) {
+        if (/[a-z]/i.test(c)) {
+            if (lastLetter.toLowerCase() < c.toLowerCase()) {
+                lastLetter = c;
+            }
+        }
+    }
+
+    return lastLetter;
+}
