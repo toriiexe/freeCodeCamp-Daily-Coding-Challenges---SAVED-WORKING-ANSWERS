418e17a6ec44945197586743ef4be35e6758e559
Quoriee <snowii.torii@gmail.com>
2026-07-20 03:31:28 +0200

    hidden key js solution

--- solutions/2026/04-April/2026-04-17/js_2026-04-17.js
+++ solutions/2026/04-April/2026-04-17/js_2026-04-17.js
@@ -1,1 +1,20 @@
 // JavaScript solution for 2026-04-17
+// Hidden Key
+function decode(message) {
+    const key = 'VLHCGMDLNH';
+    let res = '';
+    let j = 0;
+
+    for (const ch of message) {
+        if (ch === ' ') {
+            res += ' ';
+            continue;
+        }
+
+        const shift = key.charCodeAt(j % key.length) - 'A'.charCodeAt(0) + 1;
+        res += String.fromCharCode((ch.charCodeAt(0) - 'A'.charCodeAt(0) - shift + 26) % 26 + 'A'.charCodeAt(0));
+        j++;
+    }
+
+    return res;
+}
