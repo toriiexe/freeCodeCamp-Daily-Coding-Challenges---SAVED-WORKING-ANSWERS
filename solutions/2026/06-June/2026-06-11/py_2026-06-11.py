cea3c64839a44abd0d27b453065bc16a2333e381
Quoriee <snowii.torii@gmail.com>
2026-07-20 07:02:34 +0200

    idea rankings python solution

--- solutions/2026/06-June/2026-06-11/py_2026-06-11.py
+++ solutions/2026/06-June/2026-06-11/py_2026-06-11.py
@@ -1,1 +1,12 @@
 # Python solution for 2026-06-11
+# Idea Rankings
+def analyze_ideas(ideas):
+    res = []
+    
+    for idea in ideas:
+        expected = ((idea[1] + 4 * idea[2] + idea[3]) / 6) * len(idea[0])
+        res.append([expected, idea[0]])
+
+    sorted_res = sorted(res, key=lambda i: i[0])
+
+    return [item[1] for item in sorted_res]
