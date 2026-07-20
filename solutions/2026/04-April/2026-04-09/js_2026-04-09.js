44ad00e24a05ba12ed652e02e8050b44c4280a2c
Quoriee <snowii.torii@gmail.com>
2026-07-20 03:23:12 +0200

    next bingo number js solution

--- solutions/2026/04-April/2026-04-09/js_2026-04-09.js
+++ solutions/2026/04-April/2026-04-09/js_2026-04-09.js
@@ -1,1 +1,26 @@
 // JavaScript solution for 2026-04-09
+// Next Bingo Number
+function getNextBingoNumber(n) {
+    const nums = {
+        1: 'B',
+        16: 'I',
+        31: 'N',
+        46: 'G',
+        61: 'O'
+    };
+
+    let num = Number(n.slice(1)) + 1;
+
+    if (num === 76) {
+        num = 1;
+    }
+
+    let letter = '';
+    for (const key in nums) {
+        if (num >= Number(key)) {
+            letter = nums[key];
+        }
+    }
+
+    return `${letter}${num}`;
+}
