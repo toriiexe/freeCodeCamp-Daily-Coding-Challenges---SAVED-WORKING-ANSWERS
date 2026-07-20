e4c4c39e86f4274ef89cb5a8ff870d59ffdec903
Quoriee <snowii.torii@gmail.com>
2026-07-20 03:16:04 +0200

    string math python solution

--- solutions/2026/04-April/2026-04-16/py_2026-04-16.py
+++ solutions/2026/04-April/2026-04-16/py_2026-04-16.py
@@ -1,1 +1,21 @@
 # Python solution for 2026-04-16
+# String Math
+import re
+def do_math(s):
+    nums = list(map(int, re.findall(r'\d+', s)))
+    seps = re.findall(r'\D+', s)
+
+    if s and not s[0].isdigit():
+        seps = seps[1:]
+    if s and not s[-1].isdigit():
+        seps = seps[:-1]
+
+    total = nums[0] if nums else 0
+
+    for i in range(1, len(nums)):
+        if len(seps[i - 1]) % 2 == 0:
+            total += nums[i]
+        else:
+            total -= nums[i]
+
+    return total
