f549250904ff203de3afc0cbcfeeb43ca3a9662e
Quoriee <snowii.torii@gmail.com>
2026-07-20 03:47:44 +0200

    digit rotation escape js solution

--- solutions/2026/04-April/2026-04-05/js_2026-04-05.js
+++ solutions/2026/04-April/2026-04-05/js_2026-04-05.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2026-04-05
+// Digit Rotation Escape
+function getRotation(n) {
+    const s = String(n);
+    const length = s.length;
+    let step = -1;
+
+    for (let i = 0; i < length; i++) {
+        step++;
+        const rot = Number(s.slice(i) + s.slice(0, i));
+        if (rot % length === 0) {
+        return step;
+        }
+    }
+
+    return 'none';
+}
