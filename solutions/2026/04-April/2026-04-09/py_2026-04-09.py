e69124fb45afdee88fd47db2e53f06b3cb1b3e0e
Quoriee <snowii.torii@gmail.com>
2026-07-20 03:22:27 +0200

    next bingo number python solution

--- solutions/2026/04-April/2026-04-09/py_2026-04-09.py
+++ solutions/2026/04-April/2026-04-09/py_2026-04-09.py
@@ -1,1 +1,22 @@
 # Python solution for 2026-04-09
+# Next Bingo Number
+def get_next_bingo_number(n):
+    letters = {
+        1: 'B',
+        16: 'I',
+        31: 'N',
+        46: 'G',
+        61: 'O'
+    }
+
+    num = int(n[1:]) + 1
+
+    if num == 76:
+        num = 1
+
+    letter = ''
+    for start in letters:
+        if num >= start:
+            letter = letters[start]
+
+    return f'{letter}{num}'
