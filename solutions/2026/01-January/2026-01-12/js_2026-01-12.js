55568b1afdd1e359f1c4be4cedcb5f0bf78615ea
Quoriee <snowii.torii@gmail.com>
2026-07-20 05:04:42 +0200

    plant the crop js solution

--- solutions/2026/01-January/2026-01-12/js_2026-01-12.js
+++ solutions/2026/01-January/2026-01-12/js_2026-01-12.js
@@ -1,1 +1,15 @@
 // JavaScript solution for 2026-01-12
+// Plant the Crop
+function getNumberOfPlants(fieldSize, unit, crop) {
+    const crops = {
+        'corn': 1,
+        'wheat': 0.1,
+        'soybeans': 0.5,
+        'tomatoes': 0.25,
+        'lettuce': 0.2
+    }
+
+    const sqrt_m = unit === 'acres' ? fieldSize * 4046.86 : fieldSize * 10000;
+
+    return Math.floor(sqrt_m / crops[crop]);
+}
