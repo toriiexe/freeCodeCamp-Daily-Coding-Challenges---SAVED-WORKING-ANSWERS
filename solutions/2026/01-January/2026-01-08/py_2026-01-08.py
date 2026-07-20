75c49865f59e00a56930ec8003b1b54a927fc3aa
Quoriee <snowii.torii@gmail.com>
2026-07-20 05:08:09 +0200

    sorted array? python solution

--- solutions/2026/01-January/2026-01-08/py_2026-01-08.py
+++ solutions/2026/01-January/2026-01-08/py_2026-01-08.py
@@ -1,1 +1,18 @@
 # Python solution for 2026-01-08
+# Sorted Array?
+def is_sorted(arr):
+    asc = False
+    desc = False
+
+    for i in range(len(arr) - 1):
+        if arr[i] < arr[i + 1]:
+            asc = True
+        elif arr[i] > arr[i + 1]:
+            desc = True
+
+    if asc and desc:
+        return 'Not sorted'
+    elif asc:
+        return 'Ascending'
+    else:
+        return 'Descending'
