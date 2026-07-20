bc438f51a5d16cb035359158cf93e6aa73d73ce2
Quoriee <snowii.torii@gmail.com>
2026-07-20 05:38:40 +0200

    left-handed seat at the table js solution

--- solutions/2026/01-January/2026-01-03/js_2026-01-03.js
+++ solutions/2026/01-January/2026-01-03/js_2026-01-03.js
@@ -1,1 +1,31 @@
 // JavaScript solution for 2026-01-03
+// Left-Handed Seat at the Table
+function findLeftHandedSeats(table) {
+    let total = 0;
+
+    for (let r = 0; r < 2; r++) {
+        if (r === 0) {
+            for (let p = 0; p < 4; p++) {
+                if (table[r][p] === 'U') {
+                    if (p === 3) {
+                        total += 1;
+                    } else if (table[r][p + 1] !== 'R') {
+                        total += 1;
+                    }
+                }
+            }
+        } else {
+            for (let p = 0; p < 4; p++) {
+                if (table[r][p] === 'U') {
+                    if (p === 0) {
+                        total += 1;
+                    } else if (table[r][p - 1] !== 'R') {
+                        total += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    return total;
+}
