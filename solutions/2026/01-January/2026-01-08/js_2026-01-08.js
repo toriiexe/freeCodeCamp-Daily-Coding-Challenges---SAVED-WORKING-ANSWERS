6045fb377a1c81a9c0577113ccbc38fa642c59d1
Quoriee <snowii.torii@gmail.com>
2026-07-20 05:08:46 +0200

    sorted array? js solution

--- solutions/2026/01-January/2026-01-08/js_2026-01-08.js
+++ solutions/2026/01-January/2026-01-08/js_2026-01-08.js
@@ -1,1 +1,22 @@
 // JavaScript solution for 2026-01-08
+// Sorted Array?
+function isSorted(arr) {
+    let asc = false;
+    let desc = false;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < arr[i + 1]) {
+            asc = true;
+        } else if (arr[i] > arr[i + 1]) {
+            desc = true;
+        }
+    }
+
+    if (asc && desc) {
+        return 'Not sorted';
+    } else if (asc) {
+        return 'Ascending';
+    } else {
+        return 'Descending';
+    }
+}
