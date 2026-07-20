19ad2d06372da89b5319e3dabfa3a774756b32ff
Quoriee <snowii.torii@gmail.com>
2026-07-20 03:01:35 +0200

    last letter python solution

--- solutions/2026/04-April/2026-04-14/py_2026-04-14.py
+++ solutions/2026/04-April/2026-04-14/py_2026-04-14.py
@@ -1,1 +1,9 @@
 # Python solution for 2026-04-14
+# Last Letter
+def get_last_letter(s):
+    last_letter = ''
+    for c in s:
+        if c.isalpha():
+            if last_letter.lower() < c.lower():
+                last_letter = c
+    return last_letter
