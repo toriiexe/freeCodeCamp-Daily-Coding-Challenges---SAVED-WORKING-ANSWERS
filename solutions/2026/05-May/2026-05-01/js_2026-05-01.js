10a7b761ef0a69fb5a3807c7a2f5573a3e7e8a34
Quoriee <snowii.torii@gmail.com>
2026-07-20 08:45:44 +0200

    anagram groups js solution

--- solutions/2026/05-May/2026-05-01/js_2026-05-01.js
+++ solutions/2026/05-May/2026-05-01/js_2026-05-01.js
@@ -1,1 +1,17 @@
 // JavaScript solution for 2026-05-01
+// Anagram Groups
+function groupAnagrams(words) {
+    const groups = {};
+
+    for (const word of words) {
+        const key = word.split('').sort().join('');
+
+        if (!groups[key]) {
+            groups[key] = [];
+        }
+
+        groups[key].push(word);
+    }
+
+    return Object.values(groups);
+}
