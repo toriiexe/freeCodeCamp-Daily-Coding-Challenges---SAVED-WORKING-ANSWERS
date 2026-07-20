4be514645a487975d84bd3a0a1c5ce7805b598cf
Quoriee <snowii.torii@gmail.com>
2026-07-20 03:41:17 +0200

    closest time direction js solution

--- solutions/2026/04-April/2026-04-23/js_2026-04-23.js
+++ solutions/2026/04-April/2026-04-23/js_2026-04-23.js
@@ -1,1 +1,20 @@
 // JavaScript solution for 2026-04-23
+// Closest Time Direction
+function getDirection(time1, time2) {
+    const [h1, m1] = time1.split(':').map(Number);
+    const [h2, m2] = time2.split(':').map(Number);
+
+    const t1 = h1 * 60 + m1;
+    const t2 = h2 * 60 + m2;
+
+    const diff1 = (t2 - t1 + 24 * 60) % (24 * 60);
+    const diff2 = (t1 - t2 + 24 * 60) % (24 * 60);
+
+    if (diff1 < diff2) {
+        return 'forward';
+    } else if (diff1 > diff2) {
+        return 'backward';
+    } else {
+        return 'equal';
+    }
+}
