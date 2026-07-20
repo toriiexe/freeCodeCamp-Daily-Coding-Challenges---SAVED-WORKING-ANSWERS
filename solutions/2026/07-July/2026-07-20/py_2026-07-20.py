264012153a5e2a4e09cca77e4265223d7f6a1588
Quoriee <snowii.torii@gmail.com>
2026-07-20 07:07:27 +0200

    golden ratio python solution

--- solutions/2026/07-July/2026-07-20/py_2026-07-20.py
+++ solutions/2026/07-July/2026-07-20/py_2026-07-20.py
@@ -1,1 +1,9 @@
 # Python solution for 2026-07-20
+# Golden Ratio
+def is_golden_ratio(a, b):
+    golden_ratio = 1.618
+    tolerance = 0.01
+
+    ratio = max(a, b) / min(a, b)
+    
+    return ratio - tolerance <= golden_ratio <= ratio + tolerance
