8036cf998b070865f6bfeba2acb972ed8651b93a
Quoriee <snowii.torii@gmail.com>
2026-07-20 03:51:29 +0200

    what day is it? python solution

--- solutions/2026/04-April/2026-04-06/py_2026-04-06.py
+++ solutions/2026/04-April/2026-04-06/py_2026-04-06.py
@@ -1,1 +1,6 @@
 # Python solution for 2026-04-06
+# What Day Is It?
+import datetime
+def get_day_of_week(timestamp):
+    my_datetime = datetime.datetime.fromtimestamp(timestamp / 1000)
+    return my_datetime.strftime('%A')
