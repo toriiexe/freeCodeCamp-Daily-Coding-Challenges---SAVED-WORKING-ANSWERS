6ed15b197f15aafbb26a398646ad91c803787b76
Quoriee <snowii.torii@gmail.com>
2026-07-20 04:28:02 +0200

    cooldown time js solution

--- solutions/2026/03-March/2026-03-25/js_2026-03-25.js
+++ solutions/2026/03-March/2026-03-25/js_2026-03-25.js
@@ -1,1 +1,8 @@
 // JavaScript solution for 2026-03-25
+// Cooldown Time
+function canRetake(finishTime, currentTime) {
+    const finish = new Date(finishTime);
+    const current = new Date(currentTime);
+
+    return current - finish >= 48 * 60 * 60 * 1000;
+}
