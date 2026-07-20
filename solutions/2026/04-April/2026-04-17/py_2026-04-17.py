5994d2b78026e0e923ad57cf05b5caf6cf898892
Quoriee <snowii.torii@gmail.com>
2026-07-20 03:30:42 +0200

    hidden key python solution

--- solutions/2026/04-April/2026-04-17/py_2026-04-17.py
+++ solutions/2026/04-April/2026-04-17/py_2026-04-17.py
@@ -1,1 +1,17 @@
 # Python solution for 2026-04-17
+# Hidden Key
+def decode(message):
+    key = 'VLHCGMDLNH'
+    res = ''
+    j = 0
+
+    for ch in message:
+        if ch == ' ':
+            res += ' '
+            continue
+
+        shift = ord(key[j % len(key)]) - ord('A') + 1
+        res += chr((ord(ch) - ord('A') - shift) % 26 + ord('A'))
+        j += 1
+
+    return res
