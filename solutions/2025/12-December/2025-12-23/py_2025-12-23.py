295c6cbeec3da32313fb6724e64e7d35cd73af71
Quoriee <snowii.torii@gmail.com>
2026-07-21 16:37:57 +0200

    re: fwd: fw: count python solution

--- solutions/2025/12-December/2025-12-23/py_2025-12-23.py
+++ solutions/2025/12-December/2025-12-23/py_2025-12-23.py
@@ -1,1 +1,18 @@
 # Python solution for 2025-12-23
+# Re: Fwd: Fw: Count
+import re
+
+def email_chain_count(subject):
+    return len(re.findall(r'fwd:|fw:|re:', subject, re.I))
+
+
+"""
+# Re: Fwd: Fw: Count - ANOTHER SOLUTION
+def email_chain_count(subject):
+    s = subject.lower()
+
+    fw_counter = s.count('fw:')
+    fwd_counter = s.count('fwd:')
+    re_counter = s.count('re:')
+    return fw_counter + fwd_counter + re_counter
+"""
