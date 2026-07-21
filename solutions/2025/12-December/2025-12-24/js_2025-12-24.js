dc330557c4e64ffe736c9c4760dc5878857a5f61
Quoriee <snowii.torii@gmail.com>
2026-07-21 18:25:30 +0200

    markdown image parser js solution

--- solutions/2025/12-December/2025-12-24/js_2025-12-24.js
+++ solutions/2025/12-December/2025-12-24/js_2025-12-24.js
@@ -1,1 +1,5 @@
 // JavaScript solution for 2025-12-24
+// Markdown Image Parser
+function parseImage(markdown) {
+    return markdown.replace(/!\[([^\]]+)\]\(([^)]+)\)/g, '<img src="$2" alt="$1">');
+}
