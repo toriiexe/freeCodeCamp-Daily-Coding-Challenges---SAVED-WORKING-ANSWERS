7a9b265e06445411fe9630621a0514475981a025
Quoriee <snowii.torii@gmail.com>
2026-07-21 22:50:33 +0200

    game of life js solution

--- solutions/2025/12-December/2025-12-13/js_2025-12-13.js
+++ solutions/2025/12-December/2025-12-13/js_2025-12-13.js
@@ -1,1 +1,50 @@
 // JavaScript solution for 2025-12-13
+// Game of Life
+function gameOfLife(grid) {
+    const neighbours = [
+        [-1, 1], [0, 1], [1, 1],
+        [-1, 0],         [1, 0],
+        [-1, -1], [0, -1], [1, -1]
+    ];
+
+    const res = [];
+
+    for (let r = 0; r < grid.length; r++) {
+        const line = [];
+
+        for (let c = 0; c < grid[r].length; c++) {
+            let neighboursCounter = 0;
+
+            for (const neighbour of neighbours) {
+                const nx = r + neighbour[0];
+                const ny = c + neighbour[1];
+
+                if (0 <= nx && nx < grid.length &&
+                    0 <= ny && ny < grid[0].length) {
+
+                    if (grid[nx][ny] === 1) {
+                        neighboursCounter++;
+                    }
+                }
+            }
+
+            if (grid[r][c] === 0) {
+                if (neighboursCounter === 3) {
+                    line.push(1);
+                } else {
+                    line.push(0);
+                }
+            } else {
+                if (neighboursCounter === 2 || neighboursCounter === 3) {
+                    line.push(1);
+                } else {
+                    line.push(0);
+                }
+            }
+        }
+
+        res.push(line);
+    }
+
+    return res;
+}
