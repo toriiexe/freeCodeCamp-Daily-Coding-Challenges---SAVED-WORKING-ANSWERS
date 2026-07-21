68966a6eb22474bbae8773d22c2457bd265dfbed
Quoriee <snowii.torii@gmail.com>
2026-07-21 15:35:50 +0200

    markdown blockquote parser js solution

--- solutions/2025/12-December/2025-12-17/js_2025-12-17.js
+++ solutions/2025/12-December/2025-12-17/js_2025-12-17.js
@@ -1,1 +1,5 @@
 // JavaScript solution for 2025-12-17
+// Markdown Blockquote Parser
+function parseBlockquote(markdown) {
+    return markdown.replace(/\s*>\s+(.*)/, '<blockquote>$1</blockquote>');
+}
