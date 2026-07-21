558a4a78cd8c036b7801456302c028c43fd6a836
Quoriee <snowii.torii@gmail.com>
2026-07-21 15:19:06 +0200

    date formatter js solution

--- solutions/2025/12-December/2025-12-06/js_2025-12-06.js
+++ solutions/2025/12-December/2025-12-06/js_2025-12-06.js
@@ -1,1 +1,22 @@
 // JavaScript solution for 2025-12-06
+// Date Formatter
+const monthDict = {
+    January: 1,
+    February: 2,
+    March: 3,
+    April: 4,
+    May: 5,
+    June: 6,
+    July: 7,
+    August: 8,
+    September: 9,
+    October: 10,
+    November: 11,
+    December: 12
+};
+
+function formatDate(dateString) {
+    const [month, day, year] = dateString.replace(",", "").split(" ");
+
+    return `${year}-${String(monthDict[month]).padStart(2, "0")}-${day.padStart(2, "0")}`;
+}
