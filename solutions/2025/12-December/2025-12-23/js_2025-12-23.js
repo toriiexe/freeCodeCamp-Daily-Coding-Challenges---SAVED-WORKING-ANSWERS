da9e9519677a7c5609a2ce993ed7915432ee077d
Quoriee <snowii.torii@gmail.com>
2026-07-21 16:39:15 +0200

    re: fwd: fw: count js solution

--- solutions/2025/12-December/2025-12-23/js_2025-12-23.js
+++ solutions/2025/12-December/2025-12-23/js_2025-12-23.js
@@ -1,1 +1,7 @@
 // JavaScript solution for 2025-12-23
+// Re: Fwd: Fw: Count
+function emailChainCount(subject) {
+    const s = subject.toLowerCase();
+
+    return (s.match(/fw:/g) || []).length + (s.match(/fwd:/g) || []).length + (s.match(/re:/g) || []).length;
+}
