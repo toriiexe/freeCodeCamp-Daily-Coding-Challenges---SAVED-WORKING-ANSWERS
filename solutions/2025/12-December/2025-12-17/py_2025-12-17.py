80fbdc6700efc27a69f2d56803ed2eac124dddf0
Quoriee <snowii.torii@gmail.com>
2026-07-21 15:35:01 +0200

    markdown blockquote parser python solution

--- solutions/2025/12-December/2025-12-17/py_2025-12-17.py
+++ solutions/2025/12-December/2025-12-17/py_2025-12-17.py
@@ -1,1 +1,5 @@
 # Python solution for 2025-12-17
+# Markdown Blockquote Parser
+import re
+def parse_blockquote(markdown):
+    return re.sub(r'\s*>\s+(.*)', r'<blockquote>\1</blockquote>', markdown)
