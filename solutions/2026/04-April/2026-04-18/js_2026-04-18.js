9bc44d64e6536d055e7f01685468293525246aac
Quoriee <snowii.torii@gmail.com>
2026-07-22 21:21:15 +0200

    array sum finder js solution

--- solutions/2026/04-April/2026-04-18/js_2026-04-18.js
+++ solutions/2026/04-April/2026-04-18/js_2026-04-18.js
@@ -1,1 +1,25 @@
 // JavaScript solution for 2026-04-18
+// Array Sum Finder
+function findSum(arr, target) {
+	function dfs(indx, els, total) {
+		if (total === target && els.length >= 2) {
+			return els;
+		}
+
+		if (indx >= arr.length) {
+			return null;
+		}
+
+		let res = dfs(indx + 1, [...els, arr[indx]], total + arr[indx]);
+
+		if (res) {
+			return res;
+		}
+
+		res = dfs(indx + 1, els, total);
+
+		return res;
+	}
+
+	return dfs(0, [], 0) || 'Sum not found';
+}
