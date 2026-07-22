7b946be8c5b45a242ff09581901820f15f7be641
Quoriee <snowii.torii@gmail.com>
2026-07-22 07:43:05 +0200

    longest common substring js solution

--- solutions/2026/05-May/2026-05-07/js_2026-05-07.js
+++ solutions/2026/05-May/2026-05-07/js_2026-05-07.js
@@ -1,1 +1,25 @@
 // JavaScript solution for 2026-05-07
+// Longest Common Substring
+function getLongestSubstring(str) {
+	let substring = '';
+
+	for (let i = 0; i < str.length; i++) {
+		for (let j = i; j < str.length; j++) {
+			const sub = str.slice(i, j + 1);
+
+			let counter = 0;
+
+			for (let k = 0; k <= str.length - sub.length; k++) {
+				if (str.slice(k, k + sub.length) === sub) {
+					counter++;
+				}
+			}
+
+			if (counter > 1 && sub.length > substring.length) {
+				substring = sub;
+			}
+		}
+	}
+
+	return substring;
+}
