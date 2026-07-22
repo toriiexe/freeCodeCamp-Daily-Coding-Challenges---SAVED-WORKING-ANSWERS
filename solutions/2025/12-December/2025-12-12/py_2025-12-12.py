eb9f25887d535f6cc58318026c0978ef65716de4
Quoriee <snowii.torii@gmail.com>
2026-07-22 03:10:14 +0200

    inventory update python solution

--- solutions/2025/12-December/2025-12-12/py_2025-12-12.py
+++ solutions/2025/12-December/2025-12-12/py_2025-12-12.py
@@ -1,1 +1,21 @@
 # Python solution for 2025-12-12
+# Inventory Update
+def update_inventory(inventory, shipment):
+    eq = {}
+
+    for inv in inventory:
+        eq[inv[1]] = inv[0]
+
+    for s in shipment:
+        if s[1] not in eq:
+            eq[s[1]] = s[0]
+        else:
+            eq[s[1]] += s[0]
+
+    res = []
+
+    for key, value in eq.items():
+        res.append([value, key])
+
+
+    return res
