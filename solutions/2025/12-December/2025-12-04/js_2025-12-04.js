967a8940ba0972e5270aca02a8c0afb278991d19
Quoriee <snowii.torii@gmail.com>
2026-07-22 03:20:30 +0200

    permutation count js solution

--- solutions/2025/12-December/2025-12-04/js_2025-12-04.js
+++ solutions/2025/12-December/2025-12-04/js_2025-12-04.js
@@ -1,1 +1,29 @@
 // JavaScript solution for 2025-12-04
+// Permutation Count
+function fact(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+
+    return n * fact(n - 1);
+}
+
+function countPermutations(s) {
+    const chars = {};
+
+    for (const c of s) {
+        if (!(c in chars)) {
+            chars[c] = 1;
+        } else {
+            chars[c] += 1;
+        }
+    }
+
+    let repetitionFactor = 1;
+
+    for (const v of Object.values(chars)) {
+        repetitionFactor *= fact(v);
+    }
+
+    return fact(s.length) / repetitionFactor;
+}
