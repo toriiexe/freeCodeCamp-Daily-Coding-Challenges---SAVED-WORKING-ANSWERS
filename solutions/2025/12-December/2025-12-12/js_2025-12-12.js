3ed940b0f8c4816f2d5fdfd904a13fb870e682e1
Quoriee <snowii.torii@gmail.com>
2026-07-22 03:11:00 +0200

    inventory update js solution

--- solutions/2025/12-December/2025-12-12/js_2025-12-12.js
+++ solutions/2025/12-December/2025-12-12/js_2025-12-12.js
@@ -1,1 +1,25 @@
 // JavaScript solution for 2025-12-12
+// Inventory Update
+function updateInventory(inventory, shipment) {
+    const eq = {};
+
+    for (const inv of inventory) {
+        eq[inv[1]] = inv[0];
+    }
+
+    for (const s of shipment) {
+        if (!(s[1] in eq)) {
+            eq[s[1]] = s[0];
+        } else {
+            eq[s[1]] += s[0];
+        }
+    }
+
+    const res = [];
+
+    for (const key in eq) {
+        res.push([eq[key], key]);
+    }
+
+    return res;
+}
