13345c29f52482fa5b91a66377b716ea703e0b90
Quoriee <snowii.torii@gmail.com>
2026-07-22 03:19:44 +0200

    permutation count python solution

--- solutions/2025/12-December/2025-12-04/py_2025-12-04.py
+++ solutions/2025/12-December/2025-12-04/py_2025-12-04.py
@@ -1,1 +1,21 @@
 # Python solution for 2025-12-04
+# Permutation Count
+def fact(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * fact(n - 1)
+
+def count_permutations(s):
+    chars = {}
+
+    for c in s:
+        if c not in chars:
+            chars[c] = 1
+        else:
+            chars[c] += 1
+
+    repetition_factor = 1
+    for v in chars.values():
+        repetition_factor *= fact(v)
+
+    return fact(len(s)) // repetition_factor
