d677350ec7beaf5a3571936ee2e4c8622c8d9474
Quoriee <snowii.torii@gmail.com>
2026-07-22 03:48:12 +0200

    summer solstice js solution

--- solutions/2026/06-June/2026-06-21/js_2026-06-21.js
+++ solutions/2026/06-June/2026-06-21/js_2026-06-21.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2026-06-21
+// Summer Solstice
+function getDaytimeHours(latitude) {
+    const s = '☀️';
+    const n = '🌑';
+
+    let daytimeHours = 12 + (latitude / 90) * 12;
+    daytimeHours = Math.round(daytimeHours / 2) * 2;
+
+    const nighttimeHours = 24 - daytimeHours;
+    const halfOfNighttimeHours = nighttimeHours / 2;
+
+    return n.repeat(halfOfNighttimeHours) + s.repeat(daytimeHours) + n.repeat(halfOfNighttimeHours);
+}
