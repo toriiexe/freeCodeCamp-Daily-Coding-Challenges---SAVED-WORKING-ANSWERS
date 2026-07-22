5952a17380fc2ad068ad319d4bc24df95ed9dd18
Quoriee <snowii.torii@gmail.com>
2026-07-22 07:24:11 +0200

    piggy bank python solution

--- solutions/2026/07-July/2026-07-22/py_2026-07-22.py
+++ solutions/2026/07-July/2026-07-22/py_2026-07-22.py
@@ -1,1 +1,16 @@
 # Python solution for 2026-07-22
+# Piggy Bank
+def piggy_bank(coins):
+    values = {
+        'pennies': 1,
+        'nickels': 5,
+        'dimes': 10,
+        'quarters': 25
+    }
+
+    total = 0
+
+    for coin, amount in coins.items():
+        total += values[coin] * amount
+
+    return f'${total // 100}.{total % 100:02d}'
