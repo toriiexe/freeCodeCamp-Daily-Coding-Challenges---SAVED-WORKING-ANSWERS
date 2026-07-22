13a476fa1cf97b832b5d94c1dfec0c290a752599
Quoriee <snowii.torii@gmail.com>
2026-07-22 04:09:44 +0200

    zoning regulations js solution

--- solutions/2026/06-June/2026-06-13/js_2026-06-13.js
+++ solutions/2026/06-June/2026-06-13/js_2026-06-13.js
@@ -1,1 +1,46 @@
 // JavaScript solution for 2026-06-13
+// Zoning Regulations
+const forbidden = {
+    i: ['R', 'I'],
+    A: ['C'],
+    R: ['i', 'C'],
+    I: ['i'],
+    C: ['R', 'A']
+};
+
+function getZoneViolations(grid) {
+    const dirs = [
+             [0, 1],
+        [-1, 0], [1, 0],
+             [0, -1]
+    ];
+
+    const res = [];
+
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[r].length; c++) {
+            const cell = grid[r][c];
+
+            if (cell === '') continue;
+
+            for (const d of dirs) {
+                const dx = r + d[0];
+                const dy = c + d[1];
+
+                if (
+                    dx >= 0 && dx < grid.length &&
+                    dy >= 0 && dy < grid[dx].length
+                ) {
+                    const neighbour = grid[dx][dy];
+
+                    if (forbidden[cell].includes(neighbour)) {
+                        res.push([r, c]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    return res;
+}
