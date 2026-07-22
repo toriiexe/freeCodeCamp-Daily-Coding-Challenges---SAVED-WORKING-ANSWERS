ad37bd9bd385e327f0bfeb65689277855bf1fb5e
Quoriee <snowii.torii@gmail.com>
2026-07-22 03:47:18 +0200

    summer solstice python solution

--- solutions/2026/06-June/2026-06-21/py_2026-06-21.py
+++ solutions/2026/06-June/2026-06-21/py_2026-06-21.py
@@ -1,1 +1,13 @@
 # Python solution for 2026-06-21
+# Summer Solstice
+def get_daytime_hours(latitude):
+    s = '☀️'
+    n = '🌑'
+
+    daytime_hours = 12 + (latitude / 90) * 12
+    daytime_hours = round(daytime_hours / 2) * 2
+
+    nighttime_hours = 24 - daytime_hours
+    half_of_nighttime_hours = nighttime_hours // 2
+
+    return f'{half_of_nighttime_hours * n}{daytime_hours * s}{half_of_nighttime_hours * n}'
