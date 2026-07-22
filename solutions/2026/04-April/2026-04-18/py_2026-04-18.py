6f77213617b85ecb1e6a9b76486da4d0639d45d5
Quoriee <snowii.torii@gmail.com>
2026-07-22 21:20:22 +0200

    array sum finder python solution

--- solutions/2026/04-April/2026-04-18/py_2026-04-18.py
+++ solutions/2026/04-April/2026-04-18/py_2026-04-18.py
@@ -1,1 +1,20 @@
 # Python solution for 2026-04-18
+# Array Sum Finder
+def find_sum(arr, target):
+    def dfs(indx, els, total):
+        if total == target and len(els) >= 2:
+            return els
+
+        if indx >= len(arr):
+            return None
+
+        res = dfs(indx + 1, els + [arr[indx]], total + arr[indx])
+
+        if res:
+            return res
+
+        res = dfs(indx + 1, els, total)
+
+        return res
+
+    return dfs(0, [], 0) or 'Sum not found'
