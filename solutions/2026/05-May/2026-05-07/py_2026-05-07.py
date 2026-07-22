671bb74b13426c0055806524b2befcfab9cf5a05
Quoriee <snowii.torii@gmail.com>
2026-07-22 07:41:01 +0200

    longest common substring python solution

--- solutions/2026/05-May/2026-05-07/py_2026-05-07.py
+++ solutions/2026/05-May/2026-05-07/py_2026-05-07.py
@@ -1,1 +1,18 @@
 # Python solution for 2026-05-07
+# Longest Common Substring
+def get_longest_substring(s):
+    substring = ''
+
+    for i in range(len(s)):
+        for j in range(i, len(s)):
+            sub = s[i:j+1]
+
+            counter = 0
+            for k in range(len(s) - len(sub) + 1):
+                if s[k:k+len(sub)] == sub:
+                    counter += 1
+
+            if counter > 1 and len(sub) > len(substring):
+                substring = sub
+
+    return substring
