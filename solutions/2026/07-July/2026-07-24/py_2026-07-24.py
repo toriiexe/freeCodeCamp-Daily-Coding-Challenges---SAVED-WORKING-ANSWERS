18ed2e78c43eee9aff0ae65f783896db1f402684
Quoriee <snowii.torii@gmail.com>
2026-07-25 08:26:51 +0200

    loan calculator python solution

--- solutions/2026/07-July/2026-07-24/py_2026-07-24.py
+++ solutions/2026/07-July/2026-07-24/py_2026-07-24.py
@@ -1,1 +1,11 @@
 # Python solution for 2026-07-24
+# Loan Calculator
+def get_loan_schedule(loan_amount, annual_rate, monthly_payment):
+    res = [loan_amount]
+
+    while loan_amount > 0:
+        interest = loan_amount * (annual_rate / 100) / 12
+        loan_amount = max(0, loan_amount + interest - monthly_payment)
+        res.append(round(loan_amount))
+
+    return res
