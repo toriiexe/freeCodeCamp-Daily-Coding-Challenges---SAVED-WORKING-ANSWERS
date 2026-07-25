8e6d8eb52c6c84fc4d110dcc68320be97255457e
Quoriee <snowii.torii@gmail.com>
2026-07-25 08:59:53 +0200

    cell signal python solution

--- solutions/2026/07-July/2026-07-25/py_2026-07-25.py
+++ solutions/2026/07-July/2026-07-25/py_2026-07-25.py
@@ -1,1 +1,23 @@
 # Python solution for 2026-07-25
+# Cell Signal
+from collections import Counter
+def find_signal(grid):
+    res = []
+
+    for r, row in enumerate(grid):
+        for c, dist in enumerate(row):
+            if dist != 0:
+                positions = []
+
+                for dr in range(-dist, dist + 1):
+                    for dc in range(-dist, dist + 1):
+                        if max(abs(dr), abs(dc)) == dist:
+                            nr = r + dr
+                            nc = c + dc
+
+                            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+                                positions.append([nr, nc])
+
+                res.extend(positions)
+
+    return list(Counter(map(tuple, res)).most_common(1)[0][0])
