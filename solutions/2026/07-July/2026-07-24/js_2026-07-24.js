f8cbf82f37e5df001d718044f596fa7badf98a80
Quoriee <snowii.torii@gmail.com>
2026-07-25 08:28:45 +0200

    loan calculator js solution

--- solutions/2026/07-July/2026-07-24/js_2026-07-24.js
+++ solutions/2026/07-July/2026-07-24/js_2026-07-24.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2026-07-24
+// Loan Calculator
+function getLoanSchedule(loanAmount, annualRate, monthlyPayment) {
+    const res = [loanAmount];
+    const monthlyRate = (annualRate / 100) / 12;
+
+    while (loanAmount > 0) {
+        const interest = loanAmount * monthlyRate;
+        loanAmount = Math.max(0, loanAmount + interest - monthlyPayment);
+        res.push(Math.round(loanAmount));
+    }
+
+    return res;
+}
