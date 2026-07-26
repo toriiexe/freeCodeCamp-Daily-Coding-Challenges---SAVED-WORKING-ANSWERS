f9daaa1795d0be6528773f6dab54f10f77673d4e
Quoriee <snowii.torii@gmail.com>
2026-07-26 05:16:20 +0200

    equinox shadows python solution

--- solutions/2026/03-March/2026-03-20/py_2026-03-20.py
+++ solutions/2026/03-March/2026-03-20/py_2026-03-20.py
@@ -1,1 +1,15 @@
 # Python solution for 2026-03-20
+# Equinox Shadows
+def get_shadow(time):
+    h, m = map(int, time.split(':'))
+    t = h + 0.5 if m == 30 else h
+
+    if t < 6 or t >= 18 or t == 12:
+        return 'No shadow'
+
+    shadow = (12 - t) ** 3
+
+    if shadow > 0:
+        return f'{shadow}ft west'
+    else:
+        return f'{abs(shadow)}ft east'
