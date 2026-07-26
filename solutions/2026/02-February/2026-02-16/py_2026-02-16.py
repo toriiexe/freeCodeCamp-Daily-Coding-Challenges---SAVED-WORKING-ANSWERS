dd80627c1ada336042e8bd575a7afb1297692fb2
Quoriee <snowii.torii@gmail.com>
2026-07-26 04:45:50 +0200

    2026 winter games day 11: ice hockey python solution

--- solutions/2026/02-February/2026-02-16/py_2026-02-16.py
+++ solutions/2026/02-February/2026-02-16/py_2026-02-16.py
@@ -1,1 +1,13 @@
 # Python solution for 2026-02-16
+# 2026 Winter Games Day 11: Ice Hockey
+import re
+def get_semifinal_matchups(teams):
+    scores = {}
+
+    for team in teams:
+        t = re.match(r'(.+): (\d+)-(\d+)-(\d+)-(\d+)', team)
+        scores[t.group(1)] = int(t.group(2)) * 3 + int(t.group(3)) * 2 + int(t.group(4))
+
+    s = sorted(scores, key=scores.get, reverse=True)
+
+    return f'The semi-final games will be {s[0]} vs {s[3]} and {s[1]} vs {s[2]}.'
