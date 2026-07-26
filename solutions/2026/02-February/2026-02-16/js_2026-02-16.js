172b3394e203543ff0a329c57cda6b3261f34ef7
Quoriee <snowii.torii@gmail.com>
2026-07-26 04:47:17 +0200

    2026 winter games day 11: ice hockey js solution

--- solutions/2026/02-February/2026-02-16/js_2026-02-16.js
+++ solutions/2026/02-February/2026-02-16/js_2026-02-16.js
@@ -1,1 +1,15 @@
 // JavaScript solution for 2026-02-16
+// 2026 Winter Games Day 11: Ice Hockey
+function getSemifinalMatchups(teams) {
+	const scores = {};
+
+	for (const team of teams) {
+		const [, name, w, otw, otl] = team.match(/(.+): (\d+)-(\d+)-(\d+)-(\d+)/);
+
+		scores[name] = Number(w) * 3 + Number(otw) * 2 + Number(otl);
+	}
+
+	const s = Object.keys(scores).sort((a, b) => scores[b] - scores[a]);
+
+	return `The semi-final games will be ${s[0]} vs ${s[3]} and ${s[1]} vs ${s[2]}.`;
+}
