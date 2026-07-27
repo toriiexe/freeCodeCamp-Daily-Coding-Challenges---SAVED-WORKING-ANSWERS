f40eb9b40c4fd8dd19d3f150a06d1071ae305661
Quoriee <snowii.torii@gmail.com>
2026-07-27 15:09:00 +0200

    pronic number python solution

--- solutions/2026/07-July/2026-07-27/py_2026-07-27.py
+++ solutions/2026/07-July/2026-07-27/py_2026-07-27.py
@@ -1,1 +1,11 @@
 # Python solution for 2026-07-27
+# Pronic Number
+def is_pronic(n):
+    i = 0
+
+    while i * (i+1) <= n:
+        if i * (i+1) == n:
+            return True
+        i += 1
+
+    return False
