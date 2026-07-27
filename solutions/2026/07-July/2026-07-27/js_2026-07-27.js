7e71344828e09272497dff3163bc189bd794f575
Quoriee <snowii.torii@gmail.com>
2026-07-27 15:10:02 +0200

    pronic number js solution

--- solutions/2026/07-July/2026-07-27/js_2026-07-27.js
+++ solutions/2026/07-July/2026-07-27/js_2026-07-27.js
@@ -1,1 +1,14 @@
 // JavaScript solution for 2026-07-27
+// Pronic Number
+function isPronic(n) {
+    let i = 0;
+
+    while (i * (i+1) <= n){
+        if (i * (i+1) === n){
+            return true;
+        }
+        i++;
+    }
+
+    return false;
+}
