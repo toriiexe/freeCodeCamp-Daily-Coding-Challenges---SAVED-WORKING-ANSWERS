59af51e03b8294f046e47fb6a78500e2f9eaa58b
Quoriee <snowii.torii@gmail.com>
2026-07-27 15:04:24 +0200

    letter distance python solution

--- solutions/2026/07-July/2026-07-26/py_2026-07-26.py
+++ solutions/2026/07-July/2026-07-26/py_2026-07-26.py
@@ -1,1 +1,23 @@
 # Python solution for 2026-07-26
+# Letter Distance
+def letter_distance(str1, str2):
+    total_dist = 0
+
+    for s1, s2 in zip(str1, str2):
+        diff = abs(ord(s1) - ord(s2))
+        total_dist += min(diff, 26 - diff)
+
+    return total_dist
+
+"""
+# Letter Distance
+def letter_distance(str1, str2):
+    alp = 'abcdefghijklmnopqrstuvwxyz'
+    total_dist = 0
+
+    for s1, s2 in zip(str1, str2):
+        diff = abs(alp.index(s1) - alp.index(s2))
+        total_dist += min(diff, 26 - diff)
+
+    return total_dist
+"""
