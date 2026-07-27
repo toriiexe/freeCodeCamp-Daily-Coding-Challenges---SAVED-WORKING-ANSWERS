c8c175ec52753bbb4ad31bf0543f64f55f048e52
Quoriee <snowii.torii@gmail.com>
2026-07-28 01:06:45 +0200

    roommates js solution

--- solutions/2026/06-June/2026-06-09/js_2026-06-09.js
+++ solutions/2026/06-June/2026-06-09/js_2026-06-09.js
@@ -1,1 +1,29 @@
 // JavaScript solution for 2026-06-09
+// Roommates
+function getRoommates(people) {
+	const groups = {};
+
+	for (const person of people) {
+		const group = person.group;
+
+		if (!(group in groups)) {
+			groups[group] = [];
+		}
+
+		groups[group].push(person.name);
+	}
+
+	const res = [];
+
+	for (const names of Object.values(groups)) {
+		for (let i = 0; i < names.length; i += 2) {
+			if (i + 1 < names.length) {
+				res.push(`${names[i]} and ${names[i + 1]}`);
+			} else {
+				res.push(names[i]);
+			}
+		}
+	}
+
+	return res;
+}
