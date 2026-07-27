653863e8ba24cede04339e7eeb70e91ae99427a0
Quoriee <snowii.torii@gmail.com>
2026-07-28 01:38:25 +0200

    smallest gap js solution

--- solutions/2026/03-March/2026-03-05/js_2026-03-05.js
+++ solutions/2026/03-March/2026-03-05/js_2026-03-05.js
@@ -1,1 +1,23 @@
 // JavaScript solution for 2026-03-05
+// Smallest Gap
+function smallestGap(str) {
+	let len = Infinity;
+	let res = '';
+
+	for (let i = 0; i < str.length; i++) {
+		for (let j = i + 1; j < str.length; j++) {
+			if (str[i] === str[j]) {
+				let gap = str.slice(i + 1, j);
+
+				if (gap.length < len) {
+					len = gap.length;
+					res = gap;
+				}
+
+				break;
+			}
+		}
+	}
+
+	return res;
+}
