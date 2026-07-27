27b4cb64089d9b43215b16ad7bf957e1c572d88e
Quoriee <snowii.torii@gmail.com>
2026-07-28 01:38:06 +0200

    smallest gap python solution

--- solutions/2026/03-March/2026-03-05/py_2026-03-05.py
+++ solutions/2026/03-March/2026-03-05/py_2026-03-05.py
@@ -1,1 +1,18 @@
 # Python solution for 2026-03-05
+# Smallest Gap
+def smallest_gap(s):
+	length = float('inf')
+	res = ''
+
+	for i in range(len(s)):
+		for j in range(i + 1, len(s)):
+			if s[i] == s[j]:
+				gap = s[i + 1:j]
+
+				if len(gap) < length:
+					length = len(gap)
+					res = gap
+
+				break
+
+	return res
