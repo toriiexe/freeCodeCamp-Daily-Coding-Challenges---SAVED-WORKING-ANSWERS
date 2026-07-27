0ed1edae1bc68ec83b4bf704f5e0a27b65606314
Quoriee <snowii.torii@gmail.com>
2026-07-28 01:06:33 +0200

    roommates python solution

--- solutions/2026/06-June/2026-06-09/py_2026-06-09.py
+++ solutions/2026/06-June/2026-06-09/py_2026-06-09.py
@@ -1,1 +1,23 @@
 # Python solution for 2026-06-09
+# Roommates
+def get_roommates(people):
+    groups = {}
+
+    for person in people:
+        group = person['group']
+
+        if group not in groups:
+            groups[group] = []
+
+        groups[group].append(person['name'])
+
+    res = []
+
+    for names in groups.values():
+        for i in range(0, len(names), 2):
+            if i + 1 < len(names):
+                res.append(f'{names[i]} and {names[i + 1]}')
+            else:
+                res.append(names[i])
+
+    return res
