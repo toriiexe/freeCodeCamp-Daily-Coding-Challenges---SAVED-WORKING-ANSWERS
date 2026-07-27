f6ea2c56afd26a20988324e26727c8501ab8734f
Quoriee <snowii.torii@gmail.com>
2026-07-27 15:06:41 +0200

    letter distance js solution

--- solutions/2026/07-July/2026-07-26/js_2026-07-26.js
+++ solutions/2026/07-July/2026-07-26/js_2026-07-26.js
@@ -1,1 +1,16 @@
 // JavaScript solution for 2026-07-26
+// Letter Distance
+function letterDistance(str1, str2) {
+    let totalDist = 0;
+
+    for (let i = 0; i < Math.max(str1.length, str2.length); i++){
+        let s1 = str1[i];
+        let s2 = str2[i];
+
+        const diff = Math.abs(s1.charCodeAt(0) - s2.charCodeAt(0));
+
+        totalDist += Math.min(diff, 26 - diff);
+    }
+
+    return totalDist;
+}
