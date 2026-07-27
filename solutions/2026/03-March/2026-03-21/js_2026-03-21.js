60325f667e6a82b5feeea989a4dec03b0249a5df
Quoriee <snowii.torii@gmail.com>
2026-07-28 01:30:17 +0200

    qr decoder js solution

--- solutions/2026/03-March/2026-03-21/js_2026-03-21.js
+++ solutions/2026/03-March/2026-03-21/js_2026-03-21.js
@@ -1,1 +1,20 @@
 // JavaScript solution for 2026-03-21
+// QR Decoder
+function rotateRight(matrix) {
+	return matrix[0].split('').map((_, i) => matrix.map(row => row[i]).reverse().join(''));
+}
+
+function decodeQr(qrCode) {
+	let res = '';
+
+	for (let i = 0; i < 4; i++) {
+		if (qrCode[0].slice(0, 2) === '11' && qrCode[0].slice(4, 6) === '11' && qrCode[1].slice(0, 2) === '11' && qrCode[1].slice(4, 6) === '11' && qrCode[4].slice(0, 2) === '11' && qrCode[5].slice(0, 2) === '11') {
+			res = qrCode[0].slice(2, 4) + qrCode[1].slice(2, 4) + qrCode[2] + qrCode[3] + qrCode[4].slice(2) + qrCode[5].slice(2);
+			return res;
+		} else {
+			qrCode = rotateRight(qrCode);
+		}
+	}
+
+	return res;
+}
