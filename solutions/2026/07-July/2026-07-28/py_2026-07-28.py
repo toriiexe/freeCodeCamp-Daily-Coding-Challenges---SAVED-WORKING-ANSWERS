8e019d2be9ded62d5cc4d56cc34b56a8dc459f64
Quoriee <snowii.torii@gmail.com>
2026-07-28 07:38:50 +0200

    contrast rating 1 python solution

--- solutions/2026/07-July/2026-07-28/py_2026-07-28.py
+++ solutions/2026/07-July/2026-07-28/py_2026-07-28.py
@@ -1,1 +1,26 @@
 # Python solution for 2026-07-28
+# Contrast Rating 1
+def get_contrast_rating(ratio, is_large_text):
+    ratio_to_num = float(ratio)
+    if ratio_to_num >= (4.5 if is_large_text else 7.0):
+        return 'AAA'
+    if ratio_to_num >= (3.0 if is_large_text else 4.5):
+        return 'AA'
+    return 'Fail'
+
+"""
+# Contrast Rating 1 - ANOTHER SOLUTION
+def get_contrast_rating(ratio, is_large_text):
+    ratio_to_num = float(ratio)
+    if is_large_text:
+        if ratio_to_num >= 4.5:
+            return 'AAA'
+        elif ratio_to_num >= 3.0:
+            return 'AA'
+    else:
+        if ratio_to_num >= 7.0:
+            return 'AAA'
+        elif ratio_to_num >= 4.5:
+            return 'AA'
+    return 'Fail'
+"""
