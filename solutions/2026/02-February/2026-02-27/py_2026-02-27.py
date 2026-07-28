f4c74775530ac780360e0d97f582c8fd24dc9f35
Quoriee <snowii.torii@gmail.com>
2026-07-28 02:16:04 +0200

    matrix shift python solution

--- solutions/2026/02-February/2026-02-27/py_2026-02-27.py
+++ solutions/2026/02-February/2026-02-27/py_2026-02-27.py
@@ -1,1 +1,21 @@
 # Python solution for 2026-02-27
+# Matrix Shift
+def shift_matrix(matrix, shift):
+	res = [[0 for col in range(len(matrix[row]))] for row in range(len(matrix))]
+
+	rows = len(matrix)
+	cols = len(matrix[0])
+
+	s = shift % (rows * cols)
+
+	for r in range(rows):
+		for c in range(cols):
+			index = r * cols + c
+			new_index = (index + s) % (rows * cols)
+
+			res_r = new_index // cols
+			res_c = new_index % cols
+
+			res[res_r][res_c] = matrix[r][c]
+
+	return res
