1bda23052b8afa47f0f17ac486c065f9c256868d
Quoriee <snowii.torii@gmail.com>
2026-07-28 02:16:19 +0200

    matrix shift js solution

--- solutions/2026/02-February/2026-02-27/js_2026-02-27.js
+++ solutions/2026/02-February/2026-02-27/js_2026-02-27.js
@@ -1,1 +1,28 @@
 // JavaScript solution for 2026-02-27
+// Matrix Shift
+function shiftMatrix(matrix, shift) {
+	let res = matrix.map(row => [...row].fill(0));
+
+	let rows = matrix.length;
+	let cols = matrix[0].length;
+
+	let s = shift % (rows * cols);
+
+	if (s < 0) {
+		s += rows * cols;
+	}
+
+	for (let r = 0; r < rows; r++) {
+		for (let c = 0; c < cols; c++) {
+			let index = r * cols + c;
+			let newIndex = (index + s) % (rows * cols);
+
+			let resR = Math.floor(newIndex / cols);
+			let resC = newIndex % cols;
+
+			res[resR][resC] = matrix[r][c];
+		}
+	}
+
+	return res;
+}
