a4b14cb732bbec64564f56002e19420caee9e9df
Quoriee <snowii.torii@gmail.com>
2026-07-30 09:24:38 +0200

    ai detector python solution

--- solutions/2025/11-November/2025-11-30/py_2025-11-30.py
+++ solutions/2025/11-November/2025-11-30/py_2025-11-30.py
@@ -1,1 +1,20 @@
 # Python solution for 2025-11-30
+# AI Detector
+import re
+def detect_ai(text):
+    if text.count('-') >= 2:
+        return 'AI'
+
+    if len(re.findall(r'\([^)]*\)', text)) >= 2:
+        return 'AI'
+
+    long_words_counter = 0
+    for word in text.split():
+        w = re.sub('[^a-zA-Z]', '', word)
+        if len(w) >= 7:
+            long_words_counter += 1
+
+    if long_words_counter >= 3:
+        return 'AI'
+
+    return 'Human'
