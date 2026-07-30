911a3343d2289f395eebe195bcaa1dd6bbf6f770
Quoriee <snowii.torii@gmail.com>
2026-07-30 02:37:52 +0200

    contrast rating 2 python solution

--- solutions/2026/07-July/2026-07-29/py_2026-07-29.py
+++ solutions/2026/07-July/2026-07-29/py_2026-07-29.py
@@ -1,1 +1,29 @@
 # Python solution for 2026-07-29
+# Contrast Rating 2
+def get_contrast_rating(l1, l2, is_large_text):
+    contrast = (l1 + 0.05) / (l2 + 0.05)
+
+    if contrast >= (4.5 if is_large_text else 7.0):
+        return 'AAA'
+    if contrast >= (3.0 if is_large_text else 4.5):
+        return 'AA'
+    return 'Fail'
+
+"""
+# Contrast Rating 2
+def get_contrast_rating(l1, l2, is_large_text):
+    contrast = (l1 + 0.05) / (l2 + 0.05)
+
+    if is_large_text:
+        if contrast >= 4.5:
+            return 'AAA'
+        elif contrast >= 3.0:
+            return 'AA'
+    else:
+        if contrast >= 7.0:
+            return 'AAA'
+        elif contrast >= 4.5:
+            return 'AA'
+
+    return 'Fail'
+"""
