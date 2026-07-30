9d920e053a70b88781a84498c10efc42b1e79799
Quoriee <snowii.torii@gmail.com>
2026-07-30 02:38:02 +0200

    contrast rating 2 js solution

--- solutions/2026/07-July/2026-07-29/js_2026-07-29.js
+++ solutions/2026/07-July/2026-07-29/js_2026-07-29.js
@@ -1,1 +1,15 @@
 // JavaScript solution for 2026-07-29
+// Contrast Rating 2
+function getContrastRating(l1, l2, isLargeText) {
+	const contrast = (l1 + 0.05) / (l2 + 0.05);
+
+	if (isLargeText) {
+		if (contrast >= 4.5) return 'AAA';
+		if (contrast >= 3.0) return 'AA';
+	} else {
+		if (contrast >= 7.0) return 'AAA';
+		if (contrast >= 4.5) return 'AA';
+	}
+
+	return 'Fail';
+}
