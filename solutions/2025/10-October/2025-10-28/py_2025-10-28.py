3367cec58b763d9f3f53c50a3e1d1a3a7bfdefb3
Quoriee <snowii.torii@gmail.com>
2026-07-30 08:03:54 +0200

    navigator python solution

--- solutions/2025/10-October/2025-10-28/py_2025-10-28.py
+++ solutions/2025/10-October/2025-10-28/py_2025-10-28.py
@@ -1,1 +1,19 @@
 # Python solution for 2025-10-28
+# Navigator
+def navigate(commands):
+    history = ['Home']
+    indx = 0
+
+    for c in commands:
+        if c == 'Back':
+            if indx > 0:
+                indx -= 1
+        elif c == 'Forward':
+            if indx < len(history) - 1:
+                indx += 1
+        else:
+            history = history[:indx + 1]
+            history.append(c[6:])
+            indx += 1
+
+    return history[indx]
