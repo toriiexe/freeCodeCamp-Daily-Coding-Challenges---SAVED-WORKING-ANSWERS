4323b4afb90abe64c0810a0df89dad1f2aa5d8a6
Quoriee <snowii.torii@gmail.com>
2026-07-30 02:49:52 +0200

    ball trajectory js solution

--- solutions/2025/11-November/2025-11-29/js_2025-11-29.js
+++ solutions/2025/11-November/2025-11-29/js_2025-11-29.js
@@ -1,1 +1,35 @@
 // JavaScript solution for 2025-11-29
+// Ball Trajectory
+function getNextLocation(matrix) {
+	let prev = [0, 0];
+	for (let r = 0; r < matrix.length; r++) {
+		if (matrix[r].includes(1)) {
+			prev = [r, matrix[r].indexOf(1)];
+		}
+	}
+
+	let curr = [0, 0];
+	for (let r = 0; r < matrix.length; r++) {
+		if (matrix[r].includes(2)) {
+			curr = [r, matrix[r].indexOf(2)];
+		}
+	}
+
+	let dx = curr[0] - prev[0];
+	let dy = curr[1] - prev[1];
+  
+	let nxtX = curr[0] + dx;
+	let nxtY = curr[1] + dy;
+
+	if (nxtX < 0 || nxtX >= matrix.length) {
+		dx = -dx;
+		nxtX = curr[0] + dx;
+	}
+
+	if (nxtY < 0 || nxtY >= matrix[0].length) {
+		dy = -dy;
+		nxtY = curr[1] + dy;
+	}
+
+	return [nxtX, nxtY];
+}
