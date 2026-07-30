835122a8f3fae55f96e7931ef07092890f547fc1
Quoriee <snowii.torii@gmail.com>
2026-07-30 08:24:24 +0200

    email sorter python solution

--- solutions/2025/10-October/2025-10-29/py_2025-10-29.py
+++ solutions/2025/10-October/2025-10-29/py_2025-10-29.py
@@ -1,1 +1,14 @@
 # Python solution for 2025-10-29
+# Email Sorter
+def sort(emails):
+    ems = emails[:]
+
+    for e in range(len(ems)):
+        ems[e] = ems[e].split('@')
+
+    ems = sorted(ems, key=lambda item: (item[1].casefold(), item[0].casefold()))
+
+    for e in range(len(ems)):
+        ems[e] = '@'.join(ems[e])
+
+    return ems
