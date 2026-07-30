2363ef2ab34d4071c079489c33ef8e6711625528
Quoriee <snowii.torii@gmail.com>
2026-07-30 08:30:51 +0200

    email sorter js solution

--- solutions/2025/10-October/2025-10-29/js_2025-10-29.js
+++ solutions/2025/10-October/2025-10-29/js_2025-10-29.js
@@ -1,1 +1,25 @@
 // JavaScript solution for 2025-10-29
+// Email Sorter
+function sort(emails) {
+	let ems = [...emails];
+
+	for (let e = 0; e < ems.length; e++) {
+		ems[e] = ems[e].split('@');
+	}
+
+    ems.sort((a, b) => {
+        let domain = a[1].toLowerCase().localeCompare(b[1].toLowerCase());
+
+        if (domain !== 0) {
+            return domain;
+        }
+
+        return a[0].toLowerCase().localeCompare(b[0].toLowerCase());
+    });
+
+	for (let e = 0; e < ems.length; e++) {
+		ems[e] = ems[e].join('@');
+	}
+
+	return ems;
+}
