f8517ae61d7f93903dd3fb072ceefb6353c05e3b
Quoriee <snowii.torii@gmail.com>
2026-07-30 08:44:17 +0200

    browser history js solution

--- solutions/2026/04-April/2026-04-03/js_2026-04-03.js
+++ solutions/2026/04-April/2026-04-03/js_2026-04-03.js
@@ -1,1 +1,26 @@
 // JavaScript solution for 2026-04-03
+// Browser History
+function getBrowserHistory(commands) {
+	let history = [];
+	let indx = -1;
+
+	for (let c of commands) {
+		if (c === 'Back') {
+			if (indx > 0) {
+				indx--;
+			}
+		}
+		else if (c === 'Forward') {
+			if (indx < history.length - 1) {
+				indx++;
+			}
+		}
+		else {
+			history = history.slice(0, indx + 1);
+			history.push(c);
+			indx++;
+		}
+	}
+
+	return [history, indx];
+}
