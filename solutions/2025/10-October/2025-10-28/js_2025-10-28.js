95794f7e7f2bbbef7ce543e7ab41d06d3a831d4b
Quoriee <snowii.torii@gmail.com>
2026-07-30 08:04:24 +0200

    navigator js solution

--- solutions/2025/10-October/2025-10-28/js_2025-10-28.js
+++ solutions/2025/10-October/2025-10-28/js_2025-10-28.js
@@ -1,1 +1,26 @@
 // JavaScript solution for 2025-10-28
+// Navigator
+function navigate(commands) {
+	let history = ['Home'];
+	let indx = 0;
+
+	for (let c of commands) {
+		if (c === 'Back') {
+			if (indx > 0) {
+				indx--;
+			}
+		}
+		else if (c === 'Forward') {
+			if (indx < history.length - 1) {
+				indx++;
+			}
+		}
+		else {
+			history = history.slice(0, indx + 1);
+			history.push(c.slice(6));
+			indx++;
+		}
+	}
+
+	return history[indx];
+}
