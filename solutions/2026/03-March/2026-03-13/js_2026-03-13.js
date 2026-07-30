b69ba5c48ed01bb6214b0c1376515df8234cb71e
Quoriee <snowii.torii@gmail.com>
2026-07-31 01:59:43 +0200

    parking fee calculator js solution

--- solutions/2026/03-March/2026-03-13/js_2026-03-13.js
+++ solutions/2026/03-March/2026-03-13/js_2026-03-13.js
@@ -1,1 +1,22 @@
 // JavaScript solution for 2026-03-13
+// Parking Fee Calculator
+function calculateParkingFee(parkTime, pickupTime) {
+	const [startHour, startMinute] = parkTime.split(':').map(Number);
+	const [endHour, endMinute] = pickupTime.split(':').map(Number);
+
+	let start = startHour * 60 + startMinute;
+	let end = endHour * 60 + endMinute;
+
+	let totalCost = 0;
+
+	if (end < start) {
+		totalCost += 10;
+		end += 24 * 60;
+	}
+
+	let totalHours = Math.ceil((end - start) / 60);
+
+	totalCost += totalHours * 3;
+
+	return totalCost >= 5 ? `$${totalCost}` : '$5';
+}
