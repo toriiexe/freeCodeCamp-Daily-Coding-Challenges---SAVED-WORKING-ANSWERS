055fa8d5f218a7f698d6ef2f393bb1504518fd72
Quoriee <snowii.torii@gmail.com>
2026-07-30 03:03:04 +0200

    space week day 4: landing spot js solution

--- solutions/2025/10-October/2025-10-07/js_2025-10-07.js
+++ solutions/2025/10-October/2025-10-07/js_2025-10-07.js
@@ -1,1 +1,36 @@
 // JavaScript solution for 2025-10-07
+// Space Week Day 4: Landing Spot
+function findLandingSpot(matrix) {
+	let dirs = [
+		[0, 1],
+		[-1, 0], [1, 0],
+		[0, -1]
+	];
+
+	let lowestRisk = Infinity;
+	let coords = [];
+
+	for (let r = 0; r < matrix.length; r++) {
+		for (let c = 0; c < matrix[r].length; c++) {
+			if (matrix[r][c] === 0) {
+				let s = 0;
+
+				for (let d of dirs) {
+					let dx = r + d[0];
+					let dy = c + d[1];
+
+					if (!(dx < 0 || dx >= matrix.length || dy < 0 || dy >= matrix[r].length)) {
+						s += matrix[dx][dy];
+					}
+				}
+
+				if (s < lowestRisk) {
+					lowestRisk = s;
+					coords = [r, c];
+				}
+			}
+		}
+	}
+
+	return coords;
+}
