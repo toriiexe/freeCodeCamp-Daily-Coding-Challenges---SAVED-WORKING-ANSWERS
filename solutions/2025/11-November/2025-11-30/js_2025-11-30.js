39a0f95c7fd25884a7d9e4aef6e2530bf488a863
Quoriee <snowii.torii@gmail.com>
2026-07-30 09:27:10 +0200

    ai detector js solution

--- solutions/2025/11-November/2025-11-30/js_2025-11-30.js
+++ solutions/2025/11-November/2025-11-30/js_2025-11-30.js
@@ -1,1 +1,31 @@
 // JavaScript solution for 2025-11-30
+// AI Detector
+function detectAI(text) {
+	let dashes = (text.match(/-/g) || []).length;
+
+	if (dashes >= 2) {
+		return 'AI';
+	}
+
+	let brackets = text.match(/\([^)]*\)/g) || [];
+
+	if (brackets.length >= 2) {
+		return 'AI';
+	}
+
+	let longWordsCounter = 0;
+
+	for (let word of text.split(' ')) {
+		let w = word.replace(/[^a-zA-Z]/g, '');
+
+		if (w.length >= 7) {
+			longWordsCounter++;
+		}
+	}
+
+	if (longWordsCounter >= 3) {
+		return 'AI';
+	}
+
+	return 'Human';
+}
