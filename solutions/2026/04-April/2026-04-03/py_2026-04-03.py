a6e1ad2abbc871bf4b1d2ed21ad8e16448b876f9
Quoriee <snowii.torii@gmail.com>
2026-07-30 08:43:02 +0200

    browser history python solution

--- solutions/2026/04-April/2026-04-03/py_2026-04-03.py
+++ solutions/2026/04-April/2026-04-03/py_2026-04-03.py
@@ -1,1 +1,21 @@
 # Python solution for 2026-04-03
+# Browser History
+def get_browser_history(commands):
+    history = []
+    indx = -1
+
+    for c in commands:
+        if c == 'Back':
+            if indx > 0:
+                indx -= 1
+        elif c == 'Forward':
+            if indx < len(history) - 1:
+                indx += 1
+        else:
+            history = history[:indx + 1]
+            history.append(c)
+            indx += 1
+
+    return [history, indx]
+
+print(get_browser_history(["freecodecamp.org", "freecodecamp.org/learn", "Back"]))
