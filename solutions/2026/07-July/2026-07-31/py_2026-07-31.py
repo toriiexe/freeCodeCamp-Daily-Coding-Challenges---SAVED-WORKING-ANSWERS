a0d0db7c207e49a5e8c7243a94d2278a1a933483
Quoriee <snowii.torii@gmail.com>
2026-07-31 13:11:48 +0200

    morse code python solution

--- solutions/2026/07-July/2026-07-31/py_2026-07-31.py
+++ solutions/2026/07-July/2026-07-31/py_2026-07-31.py
@@ -1,1 +1,72 @@
 # Python solution for 2026-07-31
+# Morse Code
+MORSE_CODE = {
+    '.-': 'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..': 'D',
+    '.': 'E',
+    '..-.': 'F',
+    '--.': 'G',
+    '....': 'H',
+    '..': 'I',
+    '.---': 'J',
+    '-.-': 'K',
+    '.-..': 'L',
+    '--': 'M',
+    '-.': 'N',
+    '---': 'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.': 'R',
+    '...': 'S',
+    '-': 'T',
+    '..-': 'U',
+    '...-': 'V',
+    '.--': 'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z'
+}
+def decode_morse(code):
+    return ' '.join(''.join(MORSE_CODE[letter] for letter in word.split()) for word in code.split('   '))
+
+"""
+# Morse Code
+MORSE_CODE = {
+    '.-': 'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..': 'D',
+    '.': 'E',
+    '..-.': 'F',
+    '--.': 'G',
+    '....': 'H',
+    '..': 'I',
+    '.---': 'J',
+    '-.-': 'K',
+    '.-..': 'L',
+    '--': 'M',
+    '-.': 'N',
+    '---': 'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.': 'R',
+    '...': 'S',
+    '-': 'T',
+    '..-': 'U',
+    '...-': 'V',
+    '.--': 'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z'
+}
+def decode_morse(code):
+    res = ''
+    for word in code.split('   '):
+        temp = ''
+        for letter in word.split():
+            temp += MORSE_CODE[letter]
+        res += temp + ' '
+    return res.strip()
+"""
