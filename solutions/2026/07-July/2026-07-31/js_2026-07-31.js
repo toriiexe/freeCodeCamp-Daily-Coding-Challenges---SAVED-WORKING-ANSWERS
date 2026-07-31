845775346679b2cc40edd15a2e2a323e8c9ea806
Quoriee <snowii.torii@gmail.com>
2026-07-31 13:14:10 +0200

    morse code js solution

--- solutions/2026/07-July/2026-07-31/js_2026-07-31.js
+++ solutions/2026/07-July/2026-07-31/js_2026-07-31.js
@@ -1,1 +1,46 @@
 // JavaScript solution for 2026-07-31
+// Morse Code
+const MORSE_CODE = {
+    '.-': 'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..': 'D',
+    '.': 'E',
+    '..-.': 'F',
+    '--.': 'G',
+    '....': 'H',
+    '..': 'I',
+    '.---': 'J',
+    '-.-': 'K',
+    '.-..': 'L',
+    '--': 'M',
+    '-.': 'N',
+    '---': 'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.': 'R',
+    '...': 'S',
+    '-': 'T',
+    '..-': 'U',
+    '...-': 'V',
+    '.--': 'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z'
+};
+
+function decodeMorse(code) {
+    let res = '';
+
+    for (const word of code.split('   ')){
+        let temp = '';
+
+        for (const letter of word.split(' ')){
+            temp += MORSE_CODE[letter];
+        }
+        
+        res += temp + ' ';
+    }
+
+    return res.trim();
+}
